475c8129aa153178cee53b588fcccd58ee95a7cc
Frank Shen <fshen01@fortinet.com>
2019-01-12 02:33:07 +0800

    Add resource firewall object address

---
+++ src/github.com/terraform-providers/terraform-provider-fortios/fortios/resource_firewall_object_address.go
@@ -0,0 +1,236 @@
+package fortios
+
+import (
+	"fmt"
+	//"strconv"
+
+	"github.com/fortios/fortios-sdk/sdkcore"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceFirewallObjectAddress() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFirewallObjectAddressCreate,
+		Read:   resourceFirewallObjectAddressRead,
+		Update: resourceFirewallObjectAddressUpdate,
+		Delete: resourceFirewallObjectAddressDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"subnet": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"start_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"end_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"fqdn": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"country": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func resourceFirewallObjectAddressCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	//Get Params from d
+	name := d.Get("name").(string)
+	typef := d.Get("type").(string)
+	subnet := d.Get("subnet").(string)
+	startIP := d.Get("start_ip").(string)
+	endIP := d.Get("end_ip").(string)
+	fqdn := d.Get("fqdn").(string)
+	country := d.Get("country").(string)
+	comment := d.Get("comment").(string)
+
+	j1 := &forticlient.JSONFirewallObjectAddressCommon{
+		Name:    name,
+		Type:    typef,
+		Comment: comment,
+	}
+	var j2 *forticlient.JSONFirewallObjectAddressIPRange
+	var j3 *forticlient.JSONFirewallObjectAddressCountry
+	var j4 *forticlient.JSONFirewallObjectAddressFqdn
+	var j5 *forticlient.JSONFirewallObjectAddressIPMask
+
+	switch typef {
+	case "iprange":
+		j2 = &forticlient.JSONFirewallObjectAddressIPRange{
+			StartIP: startIP,
+			EndIP:   endIP,
+		}
+
+	case "geography":
+		j3 = &forticlient.JSONFirewallObjectAddressCountry{
+			Country: country,
+		}
+
+	case "fqdn":
+		j4 = &forticlient.JSONFirewallObjectAddressFqdn{
+			Fqdn: fqdn,
+		}
+
+	case "ipmask":
+		j5 = &forticlient.JSONFirewallObjectAddressIPMask{
+			Subnet: subnet,
+		}
+
+	}
+
+	//Build input data by sdk
+	i := &forticlient.JSONFirewallObjectAddress{
+		JSONFirewallObjectAddressCommon:  j1,
+		JSONFirewallObjectAddressIPRange: j2,
+		JSONFirewallObjectAddressCountry: j3,
+		JSONFirewallObjectAddressFqdn:    j4,
+		JSONFirewallObjectAddressIPMask:  j5,
+	}
+
+	//Call process by sdk
+	o, err := c.CreateFirewallObjectAddress(i)
+	if err != nil {
+		return fmt.Errorf("Error creating Firewall Object Address: %s", err)
+	}
+
+	//Set index for d
+	d.SetId(o.Mkey)
+
+	return nil
+}
+
+func resourceFirewallObjectAddressUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	//Get Params from d
+	name := d.Get("name").(string)
+	typef := d.Get("type").(string)
+	subnet := d.Get("subnet").(string)
+	startIP := d.Get("start_ip").(string)
+	endIP := d.Get("end_ip").(string)
+	fqdn := d.Get("fqdn").(string)
+	country := d.Get("country").(string)
+	comment := d.Get("comment").(string)
+
+	j1 := &forticlient.JSONFirewallObjectAddressCommon{
+		Name:    name,
+		Type:    typef,
+		Comment: comment,
+	}
+	var j2 *forticlient.JSONFirewallObjectAddressIPRange
+	var j3 *forticlient.JSONFirewallObjectAddressCountry
+	var j4 *forticlient.JSONFirewallObjectAddressFqdn
+	var j5 *forticlient.JSONFirewallObjectAddressIPMask
+
+	switch typef {
+	case "iprange":
+		j2 = &forticlient.JSONFirewallObjectAddressIPRange{
+			StartIP: startIP,
+			EndIP:   endIP,
+		}
+
+	case "geography":
+		j3 = &forticlient.JSONFirewallObjectAddressCountry{
+			Country: country,
+		}
+
+	case "fqdn":
+		j4 = &forticlient.JSONFirewallObjectAddressFqdn{
+			Fqdn: fqdn,
+		}
+
+	case "ipmask":
+		j5 = &forticlient.JSONFirewallObjectAddressIPMask{
+			Subnet: subnet,
+		}
+
+	}
+
+	//Build input data by sdk
+	i := &forticlient.JSONFirewallObjectAddress{
+		JSONFirewallObjectAddressCommon:  j1,
+		JSONFirewallObjectAddressIPRange: j2,
+		JSONFirewallObjectAddressCountry: j3,
+		JSONFirewallObjectAddressFqdn:    j4,
+		JSONFirewallObjectAddressIPMask:  j5,
+	}
+
+	//Call process by sdk
+	_, err := c.UpdateFirewallObjectAddress(i, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating Firewall Object Address: %s", err)
+	}
+
+	//Set index for d
+	//d.SetId(o.Mkey)
+
+	return nil
+}
+
+func resourceFirewallObjectAddressDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	//Call process by sdk
+	err := c.DeleteFirewallObjectAddress(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting Firewall Object Address: %s", err)
+	}
+
+	//Set index for d
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFirewallObjectAddressRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	//Call process by sdk
+	o, err := c.ReadFirewallObjectAddress(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading Firewall Object Address: %s", err)
+	}
+
+	//Refresh property
+	d.Set("name", o.Name)
+	d.Set("type", o.Type)
+	d.Set("subnet", o.Subnet)
+	d.Set("start_ip", o.StartIP)
+	d.Set("end_ip", o.EndIP)
+	d.Set("fqdn", o.Fqdn)
+	d.Set("country", o.Country)
+	d.Set("comment", o.Comment)
+
+	return nil
+}
