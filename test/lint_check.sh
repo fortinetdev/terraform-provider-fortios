b254ff1ed0582fbf6f5f1e0966e4fa96c575421e
Frank Shen <fshen01@fortinet.com>
2019-03-03 23:52:55 +0800

    Prepare for directory structure change

--- test/lint_check.sh
+++
@@ -1,5 +0,0 @@
-export GOPATH=$(dirname "$PWD")
-make -C ../src/github.com/terraform-providers/terraform-provider-fortios/  fmt
-golint ../src/github.com/terraform-providers/terraform-provider-fortios/fortios
-make -C ../src/github.com/terraform-providers/terraform-provider-fortios/  build
-
