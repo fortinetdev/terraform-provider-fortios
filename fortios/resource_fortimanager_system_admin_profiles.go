922bbb4d423504d0e11f6fbec8724e7539a173e2
Mark Gao <zgao@fortinet.com>
2019-08-08 19:26:45 -0700

    Initial version for provider fortimanager
    1. Core sdk implement
    2. Trace debug system support
    3. Implement feature "system admin profiles"
       Only "profileid, description and device-manager" options are supported now

---
+++ fortios/resource_fortimanager_system_admin_profiles.go
@@ -0,0 +1,128 @@
+package fortios
+
+import (
+	"fmt"
+	"log"
+
+	fortimngclient "github.com/fgtdev/fortimanager-sdk-go/sdkcore"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceFortimanagerSystemAdminProfiles() *schema.Resource {
+	return &schema.Resource{
+		Create: createFTMSystemAdminProfiles,
+		Read:   readFTMSystemAdminProfiles,
+		Update: updateFTMSystemAdminProfiles,
+		Delete: deleteFTMSystemAdminProfiles,
+
+		Schema: map[string]*schema.Schema{
+			"profile_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "Try to give description here...",
+			},
+			"device_manager": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "none",
+			},
+		},
+	}
+}
+
+func createFTMSystemAdminProfiles(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("createFTMSystemAdminProfiles")()
+
+	//Get Params from d
+	profileId := d.Get("profile_id").(string)
+	description := d.Get("description").(string)
+	deviceManager := d.Get("device_manager").(string)
+
+	//Build input data by sdk
+	i := &fortimngclient.JSONSysAdminProfiles{
+		ProfileId:     profileId,
+		Description:   description,
+		DeviceManager: deviceManager,
+	}
+
+	err := c.CreateUpdateSystemAdminProfiles(i, "add")
+	if err != nil {
+		return fmt.Errorf("Error creating System Admin Profiles: %s", err)
+	}
+
+	d.SetId(profileId)
+
+	return readFTMSystemAdminProfiles(d, m)
+}
+
+func readFTMSystemAdminProfiles(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("readFTMSystemAdminProfiles")()
+
+	profileId := d.Id()
+	o, err := c.ReadSystemAdminProfiles(profileId)
+	if err != nil {
+		return fmt.Errorf("Error reading System Admin Profiles: %s", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("profile_id", o.ProfileId)
+	d.Set("description", o.Description)
+	d.Set("device_manager", o.DeviceManager)
+
+	return nil
+}
+
+func updateFTMSystemAdminProfiles(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("updateFTMSystemAdminProfiles")()
+
+	//Get Params from d
+	profileId := d.Get("profile_id").(string)
+	description := d.Get("description").(string)
+	deviceManager := d.Get("device_manager").(string)
+
+	if d.HasChange("profile_id") {
+		return fmt.Errorf("the profile_id argument is the key and should not be modified here")
+	}
+
+	//Build input data by sdk
+	i := &fortimngclient.JSONSysAdminProfiles{
+		ProfileId:     profileId,
+		Description:   description,
+		DeviceManager: deviceManager,
+	}
+
+	err := c.CreateUpdateSystemAdminProfiles(i, "update")
+	if err != nil {
+		return fmt.Errorf("Error updating System Admin Profiles: %s", err)
+	}
+
+	return readFTMSystemAdminProfiles(d, m)
+}
+
+func deleteFTMSystemAdminProfiles(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("deleteFTMSystemAdminProfiles")()
+
+	profileId := d.Id()
+
+	err := c.DeleteSystemAdminProfiles(profileId)
+	if err != nil {
+		return fmt.Errorf("Error deleting System Admin Profiles: %s", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
