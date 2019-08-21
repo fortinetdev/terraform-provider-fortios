f9b7c4c549104f0493469483f4cd772b3de5e751
Mark Gao <zgao@fortinet.com>
2019-08-21 01:34:46 -0700

    Fortimanager: add feature "firewall object ippool"
    Support Add/Read/Update/Delete firewall object ippool

---
+++ fortios/resource_fortimanager_firewall_object_ippool.go
@@ -0,0 +1,150 @@
+package fortios
+
+import (
+	"fmt"
+	"log"
+
+	fortimngclient "github.com/fgtdev/fortimanager-sdk-go/sdkcore"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceFortimanagerFirewallObjectIppool() *schema.Resource {
+	return &schema.Resource{
+		Create: createFTMFirewallObjectIppool,
+		Read:   readFTMFirewallObjectIppool,
+		Update: updateFTMFirewallObjectIppool,
+		Delete: deleteFTMFirewallObjectIppool,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"startip": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"endip": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"arp_reply": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "disable",
+			},
+			"arp_intf": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"associated_intf": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func createFTMFirewallObjectIppool(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("createFTMFirewallObjectIppool")()
+
+	i := &fortimngclient.JSONFirewallObjectIppool{
+		Name:           d.Get("name").(string),
+		Comment:        d.Get("comment").(string),
+		Type:           d.Get("type").(string),
+		ArpIntf:        d.Get("arp_intf").(string),
+		ArpReply:       d.Get("arp_reply").(string),
+		AssociatedIntf: d.Get("associated_intf").(string),
+		StartIp:        d.Get("startip").(string),
+		EndIp:          d.Get("endip").(string),
+	}
+
+	err := c.CreateUpdateFirewallObjectIppool(i, "add")
+	if err != nil {
+		return fmt.Errorf("Error creating Firewall Object Ippool: %s", err)
+	}
+
+	d.SetId(i.Name)
+
+	return readFTMFirewallObjectIppool(d, m)
+}
+
+func readFTMFirewallObjectIppool(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("readFTMFirewallObjectIppool")()
+
+	name := d.Id()
+	o, err := c.ReadFirewallObjectIppool(name)
+	if err != nil {
+		return fmt.Errorf("Error reading Firewall Object Ippool: %s", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("name", o.Name)
+	d.Set("comment", o.Comment)
+	d.Set("type", o.Type)
+	d.Set("arp-intf", o.ArpIntf)
+	d.Set("arp-reply", o.ArpReply)
+	d.Set("associated-intf", o.AssociatedIntf)
+	d.Set("startip", o.StartIp)
+	d.Set("endip", o.EndIp)
+
+	return nil
+}
+
+func updateFTMFirewallObjectIppool(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("updateFTMFirewallObjectIppool")()
+
+	if d.HasChange("name") {
+		return fmt.Errorf("the name argument is the key and should not be modified here")
+	}
+
+	i := &fortimngclient.JSONFirewallObjectIppool{
+		Name:           d.Get("name").(string),
+		Comment:        d.Get("comment").(string),
+		Type:           d.Get("type").(string),
+		ArpIntf:        d.Get("arp_intf").(string),
+		ArpReply:       d.Get("arp_reply").(string),
+		AssociatedIntf: d.Get("associated_intf").(string),
+		StartIp:        d.Get("startip").(string),
+		EndIp:          d.Get("endip").(string),
+	}
+
+	err := c.CreateUpdateFirewallObjectIppool(i, "update")
+	if err != nil {
+		return fmt.Errorf("Error updating Firewall Object Ippool: %s", err)
+	}
+
+	return readFTMFirewallObjectIppool(d, m)
+}
+
+func deleteFTMFirewallObjectIppool(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).ClientFortimanager
+	defer c.Trace("deleteFTMFirewallObjectIppool")()
+
+	name := d.Id()
+
+	err := c.DeleteFirewallObjectIppool(name)
+	if err != nil {
+		return fmt.Errorf("Error deleting Firewall Object Ippool: %s", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
