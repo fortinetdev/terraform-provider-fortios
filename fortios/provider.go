c2dc4638397124bbaba7d9fc819a336efad569f6
Mark Gao <zgao@fortinet.com>
2019-08-22 02:00:11 -0700

    Fortimanager: add feature "firewall security adom revision"
    Support Add/Read/Update/Delete firewall security adom revision

--- fortios/provider.go
+++ fortios/provider.go
@@ -98,6 +98,7 @@ func Provider() terraform.ResourceProvider {
 			"fortios_fortimanager_devicemanager_install_policypackage": resourceFortimanagerDVMInstallPolicyPackage(),
 			"fortios_fortimanager_firewall_security_policy":            resourceFortimanagerFirewallSecurityPolicy(),
 			"fortios_fortimanager_firewall_security_policypackage":     resourceFortimanagerFirewallSecurityPolicyPackage(),
+			"fortios_fortimanager_firewall_security_adomrevision":      resourceFortimanagerFirewallSecurityAdomRevision(),
 			"fortios_fortimanager_firewall_object_address":             resourceFortimanagerFirewallObjectAddress(),
 			"fortios_fortimanager_firewall_object_service":             resourceFortimanagerFirewallObjectService(),
 			"fortios_fortimanager_firewall_object_ippool":              resourceFortimanagerFirewallObjectIppool(),
