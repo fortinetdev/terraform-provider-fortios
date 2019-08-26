7620e2e750448c47b3692549a52f191e31a93089
Mark Gao <zgao@fortinet.com>
2019-08-26 02:39:19 -0700

    Fortimanager: add feature "system license forticare"
    Support adding registration code to managed forticare

--- fortios/provider.go
+++ fortios/provider.go
@@ -112,6 +112,7 @@ func Provider() terraform.ResourceProvider {
 			"fortios_fortimanager_system_dns_setting":                  resourceFortimanagerSystemDNSSetting(),
 			"fortios_fortimanager_system_ntp_setting":                  resourceFortimanagerSystemNTPSetting(),
 			"fortios_fortimanager_system_license_vm":                   resourceFortimanagerSystemLicenseVM(),
+			"fortios_fortimanager_system_license_forticare":            resourceFortimanagerSystemLicenseFortiCare(),
 		},
 
 		ConfigureFunc: providerConfigure,
