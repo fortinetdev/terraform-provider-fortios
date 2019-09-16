d87fa7fb8c5a83d1c6f1726fea0eb8e056947a40
Mark Gao <zgao@fortinet.com>
2019-09-16 03:00:13 -0700

    Fortimanager: improve feature "system license vm"

--- vendor/github.com/fgtdev/fortimanager-sdk-go/sdkcore/system_license_vm.go
+++ vendor/github.com/fgtdev/fortimanager-sdk-go/sdkcore/system_license_vm.go
@@ -5,20 +5,20 @@ import (
 )
 
 type JSONSystemLicenseVM struct {
-	Target      string `json:"target"`
-	FileContent string `json:"file_content"`
+	Target      string
+	FileContent string
 }
 
 func (c *FortiMngClient) AddSystemLicenseVM(params *JSONSystemLicenseVM) (err error) {
 	defer c.Trace("AddSystemLicenseVM")()
 
 	d := map[string]interface{}{
-		"action": "POST",
+		"action": "post",
 		"payload": map[string]string{
 			"file_content": params.FileContent,
 		},
 		"resource": "/api/v2/monitor/system/vmlicense/upload",
-		"target":   params.Target,
+		"target":   []string{"/adom/root/device/" + params.Target},
 	}
 
 	p := map[string]interface{}{
@@ -26,10 +26,23 @@ func (c *FortiMngClient) AddSystemLicenseVM(params *JSONSystemLicenseVM) (err er
 		"url":  "/sys/proxy/json",
 	}
 
-	_, err = c.Do("exec", p)
-
+	result, err := c.Do("exec", p)
 	if err != nil {
 		err = fmt.Errorf("AddSystemLicenseVM failed: %s", err)
+		return
+	}
+
+	data := (result["result"].([]interface{}))[0].(map[string]interface{})["data"].([]interface{})
+	if data == nil {
+		err = fmt.Errorf("cannot get the results from the response")
+		return
+	}
+
+	status := data[0].(map[string]interface{})["status"].(map[string]interface{})
+	code := uint(status["code"].(float64))
+	msg := status["message"].(string)
+	if code != 0 || msg != "OK" {
+		err = fmt.Errorf("status not right: code is %d, message is %s", code, msg)
 		return
 	}
 
