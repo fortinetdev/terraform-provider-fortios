960c0c0a3d2da992e95a94c6528d033493133fac
Mark Gao <zgao@fortinet.com>
2019-09-16 19:14:25 -0700

    Fortimanager: improve feature "system license forticare"

--- vendor/github.com/fgtdev/fortimanager-sdk-go/sdkcore/system_license_forticare.go
+++ vendor/github.com/fgtdev/fortimanager-sdk-go/sdkcore/system_license_forticare.go
@@ -13,12 +13,12 @@ func (c *FortiMngClient) AddSystemLicenseFortiCare(params *SystemLicenseFortiCar
 	defer c.Trace("AddSystemLicenseFortiCare")()
 
 	d := map[string]interface{}{
-		"action": "POST",
+		"action": "post",
 		"payload": map[string]string{
 			"registration_code": params.RegistrationCode,
 		},
 		"resource": "/api/v2/monitor/registration/forticare/add-license",
-		"target":   params.Target,
+		"target":   []string{"/adom/root/device/" + params.Target},
 	}
 
 	p := map[string]interface{}{
@@ -26,10 +26,23 @@ func (c *FortiMngClient) AddSystemLicenseFortiCare(params *SystemLicenseFortiCar
 		"url":  "/sys/proxy/json",
 	}
 
-	_, err = c.Do("exec", p)
-
+	result, err := c.Do("exec", p)
 	if err != nil {
 		err = fmt.Errorf("AddSystemLicenseFortiCare failed: %s", err)
+		return
+	}
+
+	data := (result["result"].([]interface{}))[0].(map[string]interface{})["data"].([]interface{})
+	if data == nil {
+		err = fmt.Errorf("cannot get the results from the response")
+		return
+	}
+
+	status := data[0].(map[string]interface{})["status"].(map[string]interface{})
+	code := uint(status["code"].(float64))
+	msg := status["message"].(string)
+	if code != 0 || msg != "OK" {
+		err = fmt.Errorf("status not right: code is %d, message is %s", code, msg)
 		return
 	}
 
