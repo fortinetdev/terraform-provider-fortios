f2eb6e8cb0ab4d78fc6be6b168d870ccd9724224
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2020-04-19 15:16:04 +1000

    Added tests for new fields in phase 1 interface

--- fortios/resource_vpn_ipsec_phase1interface_test.go
+++ fortios/resource_vpn_ipsec_phase1interface_test.go
@@ -9,11 +9,7 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-func TestAccFortiOSVPNIPsecPhase1Interface_custom(t *testing.T) {
-	// TODO
-}
-
-func TestAccFortiOSVPNIPsecPhase1Interface_basic(t *testing.T) {
+func TestAccFortiOSVPNIPsecPhase1InterfaceWizardCustom(t *testing.T) {
 	rname := "i1" + acctest.RandString(12)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -21,27 +17,57 @@ func TestAccFortiOSVPNIPsecPhase1Interface_basic(t *testing.T) {
 		CheckDestroy: testAccCheckVPNIPsecPhase1InterfaceDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccFortiOSVPNIPsecPhase1InterfaceConfig1(rname),
+				Config: testAccFortiOSVPNIPsecPhase1InterfaceConfigWizardCustom(rname),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckFortiOSVPNIPsecPhase1InterfaceExists("fortios_vpn_ipsec_phase1interface.test1"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "name", rname),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "type", "static"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "interface", "port3"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "peertype", "any"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "proposal", "aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "comments", "VPN 001Test P1"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "wizard_type", "static-fortigate"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "remote_gw", "5.2.2.2"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "psksecret", "testscecret112233445566778899"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "authmethod", "psk"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "mode_cfg", "disable"),
+					testAccCheckFortiOSVPNIPsecPhase1InterfaceExists("fortios_vpn_ipsec_phase1interface.wizard_custom"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "name", "%s"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "type", "static"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "dh_group", "5"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "interface", "port2"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "peertype", "any"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "proposal", "aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "comments", "VPN 001Test P1"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "wizard_type", "custom"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "remote_gw", "1.2.2.2"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "psksecret", "testscecret112233445566778899"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "authmethod", "psk"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "authmethod_remote", ""),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_custom", "mode_cfg", "disable"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccFortiOSVPNIPsecPhase1Interface_basic2(t *testing.T) {
+func TestAccFortiOSVPNIPsecPhase1InterfaceWizardStatic(t *testing.T) {
+	rname := "i1" + acctest.RandString(12)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckVPNIPsecPhase1InterfaceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFortiOSVPNIPsecPhase1InterfaceConfigWizardStatic(rname),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckFortiOSVPNIPsecPhase1InterfaceExists("fortios_vpn_ipsec_phase1interface.wizard_static"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "name", rname),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "type", "static"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "interface", "port3"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "peertype", "any"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "proposal", "aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "comments", "VPN 001Test P1"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "wizard_type", "static-fortigate"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "remote_gw", "5.2.2.2"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "psksecret", "testscecret112233445566778899"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "authmethod", "psk"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_static", "mode_cfg", "disable"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccFortiOSVPNIPsecPhase1InterfaceWizardDialup(t *testing.T) {
 	rname := "i2" + acctest.RandString(12)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -49,16 +75,17 @@ func TestAccFortiOSVPNIPsecPhase1Interface_basic2(t *testing.T) {
 		CheckDestroy: testAccCheckVPNIPsecPhase1InterfaceDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccFortiOSVPNIPsecPhase1InterfaceConfig2(rname),
+				Config: testAccFortiOSVPNIPsecPhase1InterfaceConfigWizardDialUp(rname),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "name", rname),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "type", "dynamic"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "interface", "port3"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "peertype", "any"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "proposal", "aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "comments", "VPN 001Test P2"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "wizard_type", "dialup-forticlient"),
-					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.test1", "remote_gw", "0.0.0.0"),
+					testAccCheckFortiOSVPNIPsecPhase1InterfaceExists("fortios_vpn_ipsec_phase1interface.wizard_dialup"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "name", rname),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "type", "dynamic"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "interface", "port3"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "peertype", "any"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "proposal", "aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "comments", "VPN 001Test P2"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "wizard_type", "dialup-forticlient"),
+					resource.TestCheckResourceAttr("fortios_vpn_ipsec_phase1interface.wizard_dialup", "remote_gw", "0.0.0.0"),
 				),
 			},
 		},
@@ -116,7 +143,27 @@ func testAccCheckVPNIPsecPhase1InterfaceDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccFortiOSVPNIPsecPhase1InterfaceConfig1(name string) string {
+func testAccFortiOSVPNIPsecPhase1InterfaceConfigWizardCustom(name string) string {
+	return fmt.Sprintf(`
+resource "fortios_vpn_ipsec_phase1interface" "wizard_custom" {
+	name = "%s"
+    type = "static"
+    dh_group = "5"
+    interface = "port2"
+    peertype = "any"
+    proposal = "aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1"
+    comments = "VPN 001Test P1"
+    wizard_type = "custom"
+    remote_gw = "1.2.2.2"
+    psksecret = "testscecret112233445566778899"
+    authmethod = "psk"
+    authmethod_remote = ""
+    mode_cfg = "disable"
+}
+`, name)
+}
+
+func testAccFortiOSVPNIPsecPhase1InterfaceConfigWizardStatic(name string) string {
 	return fmt.Sprintf(`
 resource "fortios_vpn_ipsec_phase1interface" "test1" {
 	name = "%s"
@@ -135,7 +182,7 @@ resource "fortios_vpn_ipsec_phase1interface" "test1" {
 `, name)
 }
 
-func testAccFortiOSVPNIPsecPhase1InterfaceConfig2(name string) string {
+func testAccFortiOSVPNIPsecPhase1InterfaceConfigWizardDialUp(name string) string {
 	return fmt.Sprintf(`
 resource "fortios_vpn_ipsec_phase1interface" "test1" {
 	name = "%s"
