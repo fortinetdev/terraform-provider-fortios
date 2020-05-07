75219c0fb15ec4eaa382467b926efa6e1420c30f
Frank Shen <fshen01@fortinet.com>
2020-05-07 17:59:56 +0800

    Improve IPPool Error Information and verify fortiAPIErrorFormat function

--- vendor/github.com/fgtdev/fortios-sdk-go/sdkcore/firewall_object_ippool.go
+++ vendor/github.com/fgtdev/fortios-sdk-go/sdkcore/firewall_object_ippool.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-
-	"github.com/fgtdev/fortios-sdk-go/util"
 )
 
 // JSONFirewallObjectIPPool contains the parameters for Create and Update API function
@@ -70,40 +68,16 @@ func (c *FortiSDKClient) CreateFirewallObjectIPPool(params *JSONFirewallObjectIP
 
 	req.HTTPResponse.Body.Close()
 
-	if result != nil {
+	err = fortiAPIErrorFormat(result, string(body))
+
+	if err == nil {
 		if result["vdom"] != nil {
 			output.Vdom = result["vdom"].(string)
 		}
+
 		if result["mkey"] != nil {
 			output.Mkey = result["mkey"].(string)
 		}
-		if result["status"] != nil {
-			if result["status"] != "success" {
-				if result["error"] != nil {
-					err = fmt.Errorf("status is %s and error no is %.0f", result["status"], result["error"])
-				} else {
-					err = fmt.Errorf("status is %s and error no is not found", result["status"])
-				}
-
-				if result["http_status"] != nil {
-					err = fmt.Errorf("%s, details: %s", err, util.HttpStatus2Str(int(result["http_status"].(float64))))
-				} else {
-					err = fmt.Errorf("%s, and http_status no is not found", err)
-				}
-
-				return
-			}
-			output.Status = result["status"].(string)
-		} else {
-			err = fmt.Errorf("cannot get status from the response")
-			return
-		}
-		if result["http_status"] != nil {
-			output.HTTPStatus = result["http_status"].(float64)
-		}
-	} else {
-		err = fmt.Errorf("cannot get the right response")
-		return
 	}
 
 	return
@@ -144,40 +118,16 @@ func (c *FortiSDKClient) UpdateFirewallObjectIPPool(params *JSONFirewallObjectIP
 
 	req.HTTPResponse.Body.Close()
 
-	if result != nil {
+	err = fortiAPIErrorFormat(result, string(body))
+
+	if err == nil {
 		if result["vdom"] != nil {
 			output.Vdom = result["vdom"].(string)
 		}
+
 		if result["mkey"] != nil {
 			output.Mkey = result["mkey"].(string)
 		}
-		if result["status"] != nil {
-			if result["status"] != "success" {
-				if result["error"] != nil {
-					err = fmt.Errorf("status is %s and error no is %.0f", result["status"], result["error"])
-				} else {
-					err = fmt.Errorf("status is %s and error no is not found", result["status"])
-				}
-
-				if result["http_status"] != nil {
-					err = fmt.Errorf("%s, details: %s", err, util.HttpStatus2Str(int(result["http_status"].(float64))))
-				} else {
-					err = fmt.Errorf("%s, and http_status no is not found", err)
-				}
-
-				return
-			}
-			output.Status = result["status"].(string)
-		} else {
-			err = fmt.Errorf("cannot get status from the response")
-			return
-		}
-		if result["http_status"] != nil {
-			output.HTTPStatus = result["http_status"].(float64)
-		}
-	} else {
-		err = fmt.Errorf("cannot get the right response")
-		return
 	}
 
 	return
@@ -210,32 +160,7 @@ func (c *FortiSDKClient) DeleteFirewallObjectIPPool(mkey string) (err error) {
 
 	req.HTTPResponse.Body.Close()
 
-	if result != nil {
-		if result["status"] == nil {
-			err = fmt.Errorf("cannot get status from the response")
-			return
-		}
-
-		if result["status"] != "success" {
-			if result["error"] != nil {
-				err = fmt.Errorf("status is %s and error no is %.0f", result["status"], result["error"])
-			} else {
-				err = fmt.Errorf("status is %s and error no is not found", result["status"])
-			}
-
-			if result["http_status"] != nil {
-				err = fmt.Errorf("%s, details: %s", err, util.HttpStatus2Str(int(result["http_status"].(float64))))
-			} else {
-				err = fmt.Errorf("%s, and http_status no is not found", err)
-			}
-
-			return
-		}
-
-	} else {
-		err = fmt.Errorf("cannot get the right response")
-		return
-	}
+	err = fortiAPIErrorFormat(result, string(body))
 
 	return
 }
@@ -271,42 +196,18 @@ func (c *FortiSDKClient) ReadFirewallObjectIPPool(mkey string) (output *JSONFire
 
 	req.HTTPResponse.Body.Close()
 
-	if result != nil {
-		if result["http_status"] == nil {
-			err = fmt.Errorf("cannot get http_status from the response")
-			return
-		}
+	if fortiAPIHttpStatus404Checking(result) == true {
+		output = nil
+		return
+	}
+	
+	err = fortiAPIErrorFormat(result, string(body))
 
-		if result["http_status"] == 404.0 {
-			output = nil
-			return
-		}
-
-		if result["status"] == nil {
-			err = fmt.Errorf("cannot get status from the response")
-			return
-		}
-
-		if result["status"] != "success" {
-			if result["error"] != nil {
-				err = fmt.Errorf("status is %s and error no is %.0f", result["status"], result["error"])
-			} else {
-				err = fmt.Errorf("status is %s and error no is not found", result["status"])
-			}
-
-			if result["http_status"] != nil {
-				err = fmt.Errorf("%s, details: %s", err, util.HttpStatus2Str(int(result["http_status"].(float64))))
-			} else {
-				err = fmt.Errorf("%s, and http_status no is not found", err)
-			}
-
-			return
-		}
-
+	if err == nil {
 		mapTmp := (result["results"].([]interface{}))[0].(map[string]interface{})
 
 		if mapTmp == nil {
-			err = fmt.Errorf("cannot get the results from the response")
+			err = fmt.Errorf("The set of return values is empty")
 			return
 		}
 
@@ -329,8 +230,6 @@ func (c *FortiSDKClient) ReadFirewallObjectIPPool(mkey string) (output *JSONFire
 			output.Comments = mapTmp["comments"].(string)
 		}
 
-	} else {
-		err = fmt.Errorf("cannot get the right response")
 		return
 	}
 
