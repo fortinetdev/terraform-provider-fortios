abd81cb012c57a79a7aec310bace205034e3db97
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:13:23 +0800

    Add logsyslogd2_filter

---
+++ fortios/resource_logsyslogd2_filter.go
@@ -0,0 +1,520 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Filters for remote system server.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceLogSyslogd2Filter() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLogSyslogd2FilterUpdate,
+		Read:   resourceLogSyslogd2FilterRead,
+		Update: resourceLogSyslogd2FilterUpdate,
+		Delete: resourceLogSyslogd2FilterDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"severity": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"forward_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"local_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"multicast_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"sniffer_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"anomaly": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"netscan_discovery": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"netscan_vulnerability": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"voip": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"gtp": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dns": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ssh": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"filter": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 511),
+				Optional: true,
+				Computed: true,
+			},
+			"filter_type": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+
+func resourceLogSyslogd2FilterUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectLogSyslogd2Filter(d)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogd2Filter resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateLogSyslogd2Filter(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogd2Filter resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("LogSyslogd2Filter")
+	}
+
+	return resourceLogSyslogd2FilterRead(d, m)
+}
+
+func resourceLogSyslogd2FilterDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteLogSyslogd2Filter(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting LogSyslogd2Filter resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceLogSyslogd2FilterRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadLogSyslogd2Filter(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogd2Filter resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectLogSyslogd2Filter(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogd2Filter resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenLogSyslogd2FilterSeverity(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterForwardTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterLocalTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterMulticastTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterSnifferTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterAnomaly(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterNetscanDiscovery(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterNetscanVulnerability(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterVoip(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterGtp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterDns(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterSsh(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterFilter(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd2FilterFilterType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectLogSyslogd2Filter(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("severity", flattenLogSyslogd2FilterSeverity(o["severity"], d, "severity")); err != nil {
+		if !fortiAPIPatch(o["severity"]) {
+			return fmt.Errorf("Error reading severity: %v", err)
+		}
+	}
+
+	if err = d.Set("forward_traffic", flattenLogSyslogd2FilterForwardTraffic(o["forward-traffic"], d, "forward_traffic")); err != nil {
+		if !fortiAPIPatch(o["forward-traffic"]) {
+			return fmt.Errorf("Error reading forward_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("local_traffic", flattenLogSyslogd2FilterLocalTraffic(o["local-traffic"], d, "local_traffic")); err != nil {
+		if !fortiAPIPatch(o["local-traffic"]) {
+			return fmt.Errorf("Error reading local_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("multicast_traffic", flattenLogSyslogd2FilterMulticastTraffic(o["multicast-traffic"], d, "multicast_traffic")); err != nil {
+		if !fortiAPIPatch(o["multicast-traffic"]) {
+			return fmt.Errorf("Error reading multicast_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("sniffer_traffic", flattenLogSyslogd2FilterSnifferTraffic(o["sniffer-traffic"], d, "sniffer_traffic")); err != nil {
+		if !fortiAPIPatch(o["sniffer-traffic"]) {
+			return fmt.Errorf("Error reading sniffer_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("anomaly", flattenLogSyslogd2FilterAnomaly(o["anomaly"], d, "anomaly")); err != nil {
+		if !fortiAPIPatch(o["anomaly"]) {
+			return fmt.Errorf("Error reading anomaly: %v", err)
+		}
+	}
+
+	if err = d.Set("netscan_discovery", flattenLogSyslogd2FilterNetscanDiscovery(o["netscan-discovery"], d, "netscan_discovery")); err != nil {
+		if !fortiAPIPatch(o["netscan-discovery"]) {
+			return fmt.Errorf("Error reading netscan_discovery: %v", err)
+		}
+	}
+
+	if err = d.Set("netscan_vulnerability", flattenLogSyslogd2FilterNetscanVulnerability(o["netscan-vulnerability"], d, "netscan_vulnerability")); err != nil {
+		if !fortiAPIPatch(o["netscan-vulnerability"]) {
+			return fmt.Errorf("Error reading netscan_vulnerability: %v", err)
+		}
+	}
+
+	if err = d.Set("voip", flattenLogSyslogd2FilterVoip(o["voip"], d, "voip")); err != nil {
+		if !fortiAPIPatch(o["voip"]) {
+			return fmt.Errorf("Error reading voip: %v", err)
+		}
+	}
+
+	if err = d.Set("gtp", flattenLogSyslogd2FilterGtp(o["gtp"], d, "gtp")); err != nil {
+		if !fortiAPIPatch(o["gtp"]) {
+			return fmt.Errorf("Error reading gtp: %v", err)
+		}
+	}
+
+	if err = d.Set("dns", flattenLogSyslogd2FilterDns(o["dns"], d, "dns")); err != nil {
+		if !fortiAPIPatch(o["dns"]) {
+			return fmt.Errorf("Error reading dns: %v", err)
+		}
+	}
+
+	if err = d.Set("ssh", flattenLogSyslogd2FilterSsh(o["ssh"], d, "ssh")); err != nil {
+		if !fortiAPIPatch(o["ssh"]) {
+			return fmt.Errorf("Error reading ssh: %v", err)
+		}
+	}
+
+	if err = d.Set("filter", flattenLogSyslogd2FilterFilter(o["filter"], d, "filter")); err != nil {
+		if !fortiAPIPatch(o["filter"]) {
+			return fmt.Errorf("Error reading filter: %v", err)
+		}
+	}
+
+	if err = d.Set("filter_type", flattenLogSyslogd2FilterFilterType(o["filter-type"], d, "filter_type")); err != nil {
+		if !fortiAPIPatch(o["filter-type"]) {
+			return fmt.Errorf("Error reading filter_type: %v", err)
+		}
+	}
+
+
+	return nil
+}
+
+func flattenLogSyslogd2FilterFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandLogSyslogd2FilterSeverity(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterForwardTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterLocalTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterMulticastTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterSnifferTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterAnomaly(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterNetscanDiscovery(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterNetscanVulnerability(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterVoip(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterGtp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterDns(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterSsh(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterFilter(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd2FilterFilterType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectLogSyslogd2Filter(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("severity"); ok {
+		t, err := expandLogSyslogd2FilterSeverity(d, v, "severity")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["severity"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("forward_traffic"); ok {
+		t, err := expandLogSyslogd2FilterForwardTraffic(d, v, "forward_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["forward-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("local_traffic"); ok {
+		t, err := expandLogSyslogd2FilterLocalTraffic(d, v, "local_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["local-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("multicast_traffic"); ok {
+		t, err := expandLogSyslogd2FilterMulticastTraffic(d, v, "multicast_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["multicast-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("sniffer_traffic"); ok {
+		t, err := expandLogSyslogd2FilterSnifferTraffic(d, v, "sniffer_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["sniffer-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("anomaly"); ok {
+		t, err := expandLogSyslogd2FilterAnomaly(d, v, "anomaly")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["anomaly"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("netscan_discovery"); ok {
+		t, err := expandLogSyslogd2FilterNetscanDiscovery(d, v, "netscan_discovery")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["netscan-discovery"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("netscan_vulnerability"); ok {
+		t, err := expandLogSyslogd2FilterNetscanVulnerability(d, v, "netscan_vulnerability")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["netscan-vulnerability"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("voip"); ok {
+		t, err := expandLogSyslogd2FilterVoip(d, v, "voip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["voip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("gtp"); ok {
+		t, err := expandLogSyslogd2FilterGtp(d, v, "gtp")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["gtp"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dns"); ok {
+		t, err := expandLogSyslogd2FilterDns(d, v, "dns")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dns"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ssh"); ok {
+		t, err := expandLogSyslogd2FilterSsh(d, v, "ssh")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ssh"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("filter"); ok {
+		t, err := expandLogSyslogd2FilterFilter(d, v, "filter")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["filter"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("filter_type"); ok {
+		t, err := expandLogSyslogd2FilterFilterType(d, v, "filter_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["filter-type"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
