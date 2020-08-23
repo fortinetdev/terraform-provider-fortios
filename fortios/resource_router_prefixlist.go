8c1428be7784d3e256c7ae7a4e222eca2dbb8427
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:10:45 +0800

    Add router_prefixlist

---
+++ fortios/resource_router_prefixlist.go
@@ -0,0 +1,431 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure IPv4 prefix lists.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceRouterPrefixList() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceRouterPrefixListCreate,
+		Read:   resourceRouterPrefixListRead,
+		Update: resourceRouterPrefixListUpdate,
+		Delete: resourceRouterPrefixListDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Required: true,
+			},
+			"comments": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 127),
+				Optional: true,
+				Computed: true,
+			},
+			"rule": &schema.Schema{
+				Type: schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type: schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 4294967295),
+							Optional: true,
+							Computed: true,
+						},
+						"action": &schema.Schema{
+							Type: schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"prefix": &schema.Schema{
+							Type: schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"ge": &schema.Schema{
+							Type: schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 32),
+							Optional: true,
+							Computed: true,
+						},
+						"le": &schema.Schema{
+							Type: schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 32),
+							Optional: true,
+							Computed: true,
+						},
+						"flags": &schema.Schema{
+							Type: schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 4294967295),
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceRouterPrefixListCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectRouterPrefixList(d)
+	if err != nil {
+		return fmt.Errorf("Error creating RouterPrefixList resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateRouterPrefixList(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating RouterPrefixList resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("RouterPrefixList")
+	}
+
+	return resourceRouterPrefixListRead(d, m)
+}
+
+func resourceRouterPrefixListUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectRouterPrefixList(d)
+	if err != nil {
+		return fmt.Errorf("Error updating RouterPrefixList resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateRouterPrefixList(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating RouterPrefixList resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("RouterPrefixList")
+	}
+
+	return resourceRouterPrefixListRead(d, m)
+}
+
+func resourceRouterPrefixListDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteRouterPrefixList(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting RouterPrefixList resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceRouterPrefixListRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadRouterPrefixList(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading RouterPrefixList resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectRouterPrefixList(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading RouterPrefixList resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenRouterPrefixListName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterPrefixListComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterPrefixListRule(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = flattenRouterPrefixListRuleId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := i["action"]; ok {
+			tmp["action"] = flattenRouterPrefixListRuleAction(i["action"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "prefix"
+		if _, ok := i["prefix"]; ok {
+			tmp["prefix"] = flattenRouterPrefixListRulePrefix(i["prefix"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ge"
+		if _, ok := i["ge"]; ok {
+			tmp["ge"] = flattenRouterPrefixListRuleGe(i["ge"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "le"
+		if _, ok := i["le"]; ok {
+			tmp["le"] = flattenRouterPrefixListRuleLe(i["le"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "flags"
+		if _, ok := i["flags"]; ok {
+			tmp["flags"] = flattenRouterPrefixListRuleFlags(i["flags"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenRouterPrefixListRuleId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterPrefixListRuleAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterPrefixListRulePrefix(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterPrefixListRuleGe(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterPrefixListRuleLe(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterPrefixListRuleFlags(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectRouterPrefixList(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("name", flattenRouterPrefixListName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("comments", flattenRouterPrefixListComments(o["comments"], d, "comments")); err != nil {
+		if !fortiAPIPatch(o["comments"]) {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+    if isImportTable() {
+        if err = d.Set("rule", flattenRouterPrefixListRule(o["rule"], d, "rule")); err != nil {
+            if !fortiAPIPatch(o["rule"]) {
+                return fmt.Errorf("Error reading rule: %v", err)
+            }
+        }
+    } else {
+        if _, ok := d.GetOk("rule"); ok {
+            if err = d.Set("rule", flattenRouterPrefixListRule(o["rule"], d, "rule")); err != nil {
+                if !fortiAPIPatch(o["rule"]) {
+                    return fmt.Errorf("Error reading rule: %v", err)
+                }
+            }
+        }
+    }
+
+
+	return nil
+}
+
+func flattenRouterPrefixListFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandRouterPrefixListName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterPrefixListComments(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterPrefixListRule(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := ""  // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandRouterPrefixListRuleId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["action"], _ = expandRouterPrefixListRuleAction(d, i["action"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "prefix"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["prefix"], _ = expandRouterPrefixListRulePrefix(d, i["prefix"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ge"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["ge"], _ = expandRouterPrefixListRuleGe(d, i["ge"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "le"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["le"], _ = expandRouterPrefixListRuleLe(d, i["le"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "flags"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["flags"], _ = expandRouterPrefixListRuleFlags(d, i["flags"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandRouterPrefixListRuleId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterPrefixListRuleAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterPrefixListRulePrefix(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterPrefixListRuleGe(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterPrefixListRuleLe(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterPrefixListRuleFlags(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectRouterPrefixList(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandRouterPrefixListName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comments"); ok {
+		t, err := expandRouterPrefixListComments(d, v, "comments")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comments"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("rule"); ok {
+		t, err := expandRouterPrefixListRule(d, v, "rule")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["rule"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
---
+++ website/docs/r/fortios_router_prefixlist.html.markdown
@@ -0,0 +1,50 @@
+---
+subcategory: "FortiGate Router"
+layout: "fortios"
+page_title: "FortiOS: fortios_router_prefixlist"
+description: |-
+  Configure IPv4 prefix lists.
+---
+
+# fortios_router_prefixlist
+Configure IPv4 prefix lists.
+
+## Example Usage
+
+```hcl
+resource "fortios_router_prefixlist" "trname" {
+  name = "pr1"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) Name.
+* `comments` - Comment.
+* `rule` - IPv4 prefix list rule.
+
+The `rule` block supports:
+
+* `id` - Rule ID.
+* `action` - Permit or deny this IP address and netmask prefix.
+* `prefix` - IPv4 prefix to define regular filter criteria, such as "any" or subnets.
+* `ge` - Minimum prefix length to be matched (0 - 32).
+* `le` - Maximum prefix length to be matched (0 - 32).
+* `flags` - Flags.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+Router PrefixList can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_router_prefixlist.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
