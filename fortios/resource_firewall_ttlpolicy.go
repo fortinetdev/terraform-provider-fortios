e35887edba1e4a00a784739bfd441854bafebfbf
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:18:29 +0800

    Add firewall_ttlpolicy

---
+++ fortios/resource_firewall_ttlpolicy.go
@@ -0,0 +1,529 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure TTL policies.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceFirewallTtlPolicy() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFirewallTtlPolicyCreate,
+		Read:   resourceFirewallTtlPolicyRead,
+		Update: resourceFirewallTtlPolicyUpdate,
+		Delete: resourceFirewallTtlPolicyDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 4294967295),
+				Required: true,
+			},
+			"status": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"action": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"srcintf": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Required: true,
+			},
+			"srcaddr": &schema.Schema{
+				Type: schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type: schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 64),
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"service": &schema.Schema{
+				Type: schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type: schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 64),
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"schedule": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Required: true,
+			},
+			"ttl": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceFirewallTtlPolicyCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallTtlPolicy(d)
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallTtlPolicy resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateFirewallTtlPolicy(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallTtlPolicy resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallTtlPolicy")
+	}
+
+	return resourceFirewallTtlPolicyRead(d, m)
+}
+
+func resourceFirewallTtlPolicyUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallTtlPolicy(d)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallTtlPolicy resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateFirewallTtlPolicy(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallTtlPolicy resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallTtlPolicy")
+	}
+
+	return resourceFirewallTtlPolicyRead(d, m)
+}
+
+func resourceFirewallTtlPolicyDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteFirewallTtlPolicy(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting FirewallTtlPolicy resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFirewallTtlPolicyRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadFirewallTtlPolicy(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallTtlPolicy resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectFirewallTtlPolicy(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallTtlPolicy resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenFirewallTtlPolicyId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallTtlPolicyStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallTtlPolicyAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallTtlPolicySrcintf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallTtlPolicySrcaddr(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenFirewallTtlPolicySrcaddrName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenFirewallTtlPolicySrcaddrName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallTtlPolicyService(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenFirewallTtlPolicyServiceName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenFirewallTtlPolicyServiceName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallTtlPolicySchedule(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallTtlPolicyTtl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectFirewallTtlPolicy(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("fosid", flattenFirewallTtlPolicyId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenFirewallTtlPolicyStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("action", flattenFirewallTtlPolicyAction(o["action"], d, "action")); err != nil {
+		if !fortiAPIPatch(o["action"]) {
+			return fmt.Errorf("Error reading action: %v", err)
+		}
+	}
+
+	if err = d.Set("srcintf", flattenFirewallTtlPolicySrcintf(o["srcintf"], d, "srcintf")); err != nil {
+		if !fortiAPIPatch(o["srcintf"]) {
+			return fmt.Errorf("Error reading srcintf: %v", err)
+		}
+	}
+
+    if isImportTable() {
+        if err = d.Set("srcaddr", flattenFirewallTtlPolicySrcaddr(o["srcaddr"], d, "srcaddr")); err != nil {
+            if !fortiAPIPatch(o["srcaddr"]) {
+                return fmt.Errorf("Error reading srcaddr: %v", err)
+            }
+        }
+    } else {
+        if _, ok := d.GetOk("srcaddr"); ok {
+            if err = d.Set("srcaddr", flattenFirewallTtlPolicySrcaddr(o["srcaddr"], d, "srcaddr")); err != nil {
+                if !fortiAPIPatch(o["srcaddr"]) {
+                    return fmt.Errorf("Error reading srcaddr: %v", err)
+                }
+            }
+        }
+    }
+
+    if isImportTable() {
+        if err = d.Set("service", flattenFirewallTtlPolicyService(o["service"], d, "service")); err != nil {
+            if !fortiAPIPatch(o["service"]) {
+                return fmt.Errorf("Error reading service: %v", err)
+            }
+        }
+    } else {
+        if _, ok := d.GetOk("service"); ok {
+            if err = d.Set("service", flattenFirewallTtlPolicyService(o["service"], d, "service")); err != nil {
+                if !fortiAPIPatch(o["service"]) {
+                    return fmt.Errorf("Error reading service: %v", err)
+                }
+            }
+        }
+    }
+
+	if err = d.Set("schedule", flattenFirewallTtlPolicySchedule(o["schedule"], d, "schedule")); err != nil {
+		if !fortiAPIPatch(o["schedule"]) {
+			return fmt.Errorf("Error reading schedule: %v", err)
+		}
+	}
+
+	if err = d.Set("ttl", flattenFirewallTtlPolicyTtl(o["ttl"], d, "ttl")); err != nil {
+		if !fortiAPIPatch(o["ttl"]) {
+			return fmt.Errorf("Error reading ttl: %v", err)
+		}
+	}
+
+
+	return nil
+}
+
+func flattenFirewallTtlPolicyFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandFirewallTtlPolicyId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallTtlPolicyStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallTtlPolicyAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallTtlPolicySrcintf(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallTtlPolicySrcaddr(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := ""  // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandFirewallTtlPolicySrcaddrName(d, i["name"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandFirewallTtlPolicySrcaddrName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallTtlPolicyService(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := ""  // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandFirewallTtlPolicyServiceName(d, i["name"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandFirewallTtlPolicyServiceName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallTtlPolicySchedule(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallTtlPolicyTtl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectFirewallTtlPolicy(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandFirewallTtlPolicyId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandFirewallTtlPolicyStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("action"); ok {
+		t, err := expandFirewallTtlPolicyAction(d, v, "action")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["action"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("srcintf"); ok {
+		t, err := expandFirewallTtlPolicySrcintf(d, v, "srcintf")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["srcintf"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("srcaddr"); ok {
+		t, err := expandFirewallTtlPolicySrcaddr(d, v, "srcaddr")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["srcaddr"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("service"); ok {
+		t, err := expandFirewallTtlPolicyService(d, v, "service")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["service"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("schedule"); ok {
+		t, err := expandFirewallTtlPolicySchedule(d, v, "schedule")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["schedule"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ttl"); ok {
+		t, err := expandFirewallTtlPolicyTtl(d, v, "ttl")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ttl"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
---
+++ website/docs/r/fortios_firewall_ttlpolicy.html.markdown
@@ -0,0 +1,67 @@
+---
+subcategory: "FortiGate Firewall"
+layout: "fortios"
+page_title: "FortiOS: fortios_firewall_ttlpolicy"
+description: |-
+  Configure TTL policies.
+---
+
+# fortios_firewall_ttlpolicy
+Configure TTL policies.
+
+## Example Usage
+
+```hcl
+resource "fortios_firewall_ttlpolicy" "trname" {
+  action   = "accept"
+  fosid    = 1
+  schedule = "always"
+  srcintf  = "port3"
+  status   = "enable"
+  ttl      = "23"
+
+  service {
+    name = "ALL"
+  }
+
+  srcaddr {
+    name = "all"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `fosid` - (Required) ID.
+* `status` - Enable/disable this TTL policy.
+* `action` - Action to be performed on traffic matching this policy (default = deny).
+* `srcintf` - (Required) Source interface name from available interfaces.
+* `srcaddr` - (Required) Source address object(s) from available options. Separate multiple names with a space.
+* `service` - (Required) Service object(s) from available options. Separate multiple names with a space.
+* `schedule` - (Required) Schedule object from available options.
+* `ttl` - (Required) Value/range to match against the packet's Time to Live value (format: ttl[ - ttl_high], 1 - 255).
+
+The `srcaddr` block supports:
+
+* `name` - Address name.
+
+The `service` block supports:
+
+* `name` - Service name.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{fosid}}.
+
+## Import
+
+Firewall TtlPolicy can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_firewall_ttlpolicy.labelname {{fosid}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
