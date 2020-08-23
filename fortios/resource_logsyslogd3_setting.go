daad172f9fde32f8b56eeb85dd5652ad3b2a0924
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:12:58 +0800

    Add logsyslogd3_setting

---
+++ fortios/resource_logsyslogd3_setting.go
@@ -0,0 +1,594 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Global settings for remote syslog server.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceLogSyslogd3Setting() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLogSyslogd3SettingUpdate,
+		Read:   resourceLogSyslogd3SettingRead,
+		Update: resourceLogSyslogd3SettingUpdate,
+		Delete: resourceLogSyslogd3SettingDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"status": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"server": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional: true,
+				Computed: true,
+			},
+			"mode": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"port": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 65535),
+				Optional: true,
+				Computed: true,
+			},
+			"facility": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional: true,
+				Computed: true,
+			},
+			"format": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"enc_algorithm": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ssl_min_proto_version": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"certificate": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Optional: true,
+				Computed: true,
+			},
+			"custom_field_name": &schema.Schema{
+				Type: schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type: schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 255),
+							Optional: true,
+							Computed: true,
+						},
+						"name": &schema.Schema{
+							Type: schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 35),
+							Optional: true,
+							Computed: true,
+						},
+						"custom": &schema.Schema{
+							Type: schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 35),
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"syslog_type": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 4294967295),
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+
+func resourceLogSyslogd3SettingUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectLogSyslogd3Setting(d)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogd3Setting resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateLogSyslogd3Setting(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogd3Setting resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("LogSyslogd3Setting")
+	}
+
+	return resourceLogSyslogd3SettingRead(d, m)
+}
+
+func resourceLogSyslogd3SettingDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteLogSyslogd3Setting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting LogSyslogd3Setting resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceLogSyslogd3SettingRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadLogSyslogd3Setting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogd3Setting resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectLogSyslogd3Setting(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogd3Setting resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenLogSyslogd3SettingStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingMode(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingFacility(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingFormat(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingEncAlgorithm(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingSslMinProtoVersion(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingCertificate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingCustomFieldName(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = flattenLogSyslogd3SettingCustomFieldNameId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenLogSyslogd3SettingCustomFieldNameName(i["name"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "custom"
+		if _, ok := i["custom"]; ok {
+			tmp["custom"] = flattenLogSyslogd3SettingCustomFieldNameCustom(i["custom"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenLogSyslogd3SettingCustomFieldNameId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingCustomFieldNameName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingCustomFieldNameCustom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogd3SettingSyslogType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectLogSyslogd3Setting(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("status", flattenLogSyslogd3SettingStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("server", flattenLogSyslogd3SettingServer(o["server"], d, "server")); err != nil {
+		if !fortiAPIPatch(o["server"]) {
+			return fmt.Errorf("Error reading server: %v", err)
+		}
+	}
+
+	if err = d.Set("mode", flattenLogSyslogd3SettingMode(o["mode"], d, "mode")); err != nil {
+		if !fortiAPIPatch(o["mode"]) {
+			return fmt.Errorf("Error reading mode: %v", err)
+		}
+	}
+
+	if err = d.Set("port", flattenLogSyslogd3SettingPort(o["port"], d, "port")); err != nil {
+		if !fortiAPIPatch(o["port"]) {
+			return fmt.Errorf("Error reading port: %v", err)
+		}
+	}
+
+	if err = d.Set("facility", flattenLogSyslogd3SettingFacility(o["facility"], d, "facility")); err != nil {
+		if !fortiAPIPatch(o["facility"]) {
+			return fmt.Errorf("Error reading facility: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", flattenLogSyslogd3SettingSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("format", flattenLogSyslogd3SettingFormat(o["format"], d, "format")); err != nil {
+		if !fortiAPIPatch(o["format"]) {
+			return fmt.Errorf("Error reading format: %v", err)
+		}
+	}
+
+	if err = d.Set("enc_algorithm", flattenLogSyslogd3SettingEncAlgorithm(o["enc-algorithm"], d, "enc_algorithm")); err != nil {
+		if !fortiAPIPatch(o["enc-algorithm"]) {
+			return fmt.Errorf("Error reading enc_algorithm: %v", err)
+		}
+	}
+
+	if err = d.Set("ssl_min_proto_version", flattenLogSyslogd3SettingSslMinProtoVersion(o["ssl-min-proto-version"], d, "ssl_min_proto_version")); err != nil {
+		if !fortiAPIPatch(o["ssl-min-proto-version"]) {
+			return fmt.Errorf("Error reading ssl_min_proto_version: %v", err)
+		}
+	}
+
+	if err = d.Set("certificate", flattenLogSyslogd3SettingCertificate(o["certificate"], d, "certificate")); err != nil {
+		if !fortiAPIPatch(o["certificate"]) {
+			return fmt.Errorf("Error reading certificate: %v", err)
+		}
+	}
+
+    if isImportTable() {
+        if err = d.Set("custom_field_name", flattenLogSyslogd3SettingCustomFieldName(o["custom-field-name"], d, "custom_field_name")); err != nil {
+            if !fortiAPIPatch(o["custom-field-name"]) {
+                return fmt.Errorf("Error reading custom_field_name: %v", err)
+            }
+        }
+    } else {
+        if _, ok := d.GetOk("custom_field_name"); ok {
+            if err = d.Set("custom_field_name", flattenLogSyslogd3SettingCustomFieldName(o["custom-field-name"], d, "custom_field_name")); err != nil {
+                if !fortiAPIPatch(o["custom-field-name"]) {
+                    return fmt.Errorf("Error reading custom_field_name: %v", err)
+                }
+            }
+        }
+    }
+
+	if err = d.Set("syslog_type", flattenLogSyslogd3SettingSyslogType(o["syslog-type"], d, "syslog_type")); err != nil {
+		if !fortiAPIPatch(o["syslog-type"]) {
+			return fmt.Errorf("Error reading syslog_type: %v", err)
+		}
+	}
+
+
+	return nil
+}
+
+func flattenLogSyslogd3SettingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandLogSyslogd3SettingStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingMode(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingFacility(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingSourceIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingFormat(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingEncAlgorithm(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingSslMinProtoVersion(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingCertificate(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingCustomFieldName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := ""  // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandLogSyslogd3SettingCustomFieldNameId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandLogSyslogd3SettingCustomFieldNameName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "custom"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["custom"], _ = expandLogSyslogd3SettingCustomFieldNameCustom(d, i["custom"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandLogSyslogd3SettingCustomFieldNameId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingCustomFieldNameName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingCustomFieldNameCustom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogd3SettingSyslogType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectLogSyslogd3Setting(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandLogSyslogd3SettingStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("server"); ok {
+		t, err := expandLogSyslogd3SettingServer(d, v, "server")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["server"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("mode"); ok {
+		t, err := expandLogSyslogd3SettingMode(d, v, "mode")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["mode"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("port"); ok {
+		t, err := expandLogSyslogd3SettingPort(d, v, "port")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("facility"); ok {
+		t, err := expandLogSyslogd3SettingFacility(d, v, "facility")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["facility"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("source_ip"); ok {
+		t, err := expandLogSyslogd3SettingSourceIp(d, v, "source_ip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["source-ip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("format"); ok {
+		t, err := expandLogSyslogd3SettingFormat(d, v, "format")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["format"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("enc_algorithm"); ok {
+		t, err := expandLogSyslogd3SettingEncAlgorithm(d, v, "enc_algorithm")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["enc-algorithm"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ssl_min_proto_version"); ok {
+		t, err := expandLogSyslogd3SettingSslMinProtoVersion(d, v, "ssl_min_proto_version")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ssl-min-proto-version"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("certificate"); ok {
+		t, err := expandLogSyslogd3SettingCertificate(d, v, "certificate")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["certificate"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("custom_field_name"); ok {
+		t, err := expandLogSyslogd3SettingCustomFieldName(d, v, "custom_field_name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["custom-field-name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("syslog_type"); ok {
+		t, err := expandLogSyslogd3SettingSyslogType(d, v, "syslog_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["syslog-type"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
---
+++ fortios/resource_logsyslogd3_setting_test.go
@@ -0,0 +1,109 @@
+
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+package fortios
+import (
+    "fmt"
+	"log"
+    "testing"
+    "github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+    "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+    "github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccFortiOSLogSyslogd3Setting_basic(t *testing.T) {
+    rname := acctest.RandString(8)
+    log.Printf("TestAccFortiOSLogSyslogd3Setting_basic %s", rname)
+
+    resource.Test(t, resource.TestCase{
+        PreCheck:     func() { testAccPreCheck(t) },
+        Providers:    testAccProviders,
+        Steps: []resource.TestStep{
+            {
+                Config: testAccFortiOSLogSyslogd3SettingConfig(rname),
+                Check: resource.ComposeTestCheckFunc(
+                    testAccCheckFortiOSLogSyslogd3SettingExists("fortios_logsyslogd3_setting.trname"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "enc_algorithm", "disable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "facility", "local7"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "format", "default"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "mode", "udp"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "port", "514"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "ssl_min_proto_version", "default"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "status", "disable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd3_setting.trname", "syslog_type", "3"),
+                ),
+            },
+        },
+    })
+}
+
+func testAccCheckFortiOSLogSyslogd3SettingExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found LogSyslogd3Setting: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No LogSyslogd3Setting is set")
+		}
+
+		c := testAccProvider.Meta().(*FortiClient).Client
+
+		i := rs.Primary.ID
+		o, err := c.ReadLogSyslogd3Setting(i)
+
+		if err != nil {
+			return fmt.Errorf("Error reading LogSyslogd3Setting: %s", err)
+		}
+
+		if o == nil {
+			return fmt.Errorf("Error creating LogSyslogd3Setting: %s", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckLogSyslogd3SettingDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(*FortiClient).Client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "fortios_logsyslogd3_setting" {
+			continue
+		}
+
+		i := rs.Primary.ID
+		o, err := c.ReadLogSyslogd3Setting(i)
+
+		if err == nil {
+			if o != nil {
+				return fmt.Errorf("Error LogSyslogd3Setting %s still exists", rs.Primary.ID)
+			}
+		}
+
+		return nil
+	}
+
+	return nil
+}
+
+func testAccFortiOSLogSyslogd3SettingConfig(name string) string {
+	return fmt.Sprintf(`
+resource "fortios_logsyslogd3_setting" "trname" {
+  enc_algorithm         = "disable"
+  facility              = "local7"
+  format                = "default"
+  mode                  = "udp"
+  port                  = 514
+  ssl_min_proto_version = "default"
+  status                = "disable"
+  syslog_type           = 3
+}
+
+`)
+}
