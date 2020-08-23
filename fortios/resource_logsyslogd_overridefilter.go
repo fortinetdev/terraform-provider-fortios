5581c1381a29232d63a55e53e2983b081416aac0
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:13:34 +0800

    Add logsyslogd_overridefilter

---
+++ fortios/resource_logsyslogd_overridefilter.go
@@ -0,0 +1,520 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Override filters for remote system server.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceLogSyslogdOverrideFilter() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLogSyslogdOverrideFilterUpdate,
+		Read:   resourceLogSyslogdOverrideFilterRead,
+		Update: resourceLogSyslogdOverrideFilterUpdate,
+		Delete: resourceLogSyslogdOverrideFilterDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"severity": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"forward_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"local_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"multicast_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"sniffer_traffic": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"anomaly": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"netscan_discovery": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"netscan_vulnerability": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"voip": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"gtp": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dns": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ssh": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"filter": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 511),
+				Optional: true,
+				Computed: true,
+			},
+			"filter_type": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+
+func resourceLogSyslogdOverrideFilterUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectLogSyslogdOverrideFilter(d)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogdOverrideFilter resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateLogSyslogdOverrideFilter(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogdOverrideFilter resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("LogSyslogdOverrideFilter")
+	}
+
+	return resourceLogSyslogdOverrideFilterRead(d, m)
+}
+
+func resourceLogSyslogdOverrideFilterDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteLogSyslogdOverrideFilter(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting LogSyslogdOverrideFilter resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceLogSyslogdOverrideFilterRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadLogSyslogdOverrideFilter(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogdOverrideFilter resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectLogSyslogdOverrideFilter(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogdOverrideFilter resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenLogSyslogdOverrideFilterSeverity(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterForwardTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterLocalTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterMulticastTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterSnifferTraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterAnomaly(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterNetscanDiscovery(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterNetscanVulnerability(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterVoip(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterGtp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterDns(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterSsh(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterFilter(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideFilterFilterType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectLogSyslogdOverrideFilter(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("severity", flattenLogSyslogdOverrideFilterSeverity(o["severity"], d, "severity")); err != nil {
+		if !fortiAPIPatch(o["severity"]) {
+			return fmt.Errorf("Error reading severity: %v", err)
+		}
+	}
+
+	if err = d.Set("forward_traffic", flattenLogSyslogdOverrideFilterForwardTraffic(o["forward-traffic"], d, "forward_traffic")); err != nil {
+		if !fortiAPIPatch(o["forward-traffic"]) {
+			return fmt.Errorf("Error reading forward_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("local_traffic", flattenLogSyslogdOverrideFilterLocalTraffic(o["local-traffic"], d, "local_traffic")); err != nil {
+		if !fortiAPIPatch(o["local-traffic"]) {
+			return fmt.Errorf("Error reading local_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("multicast_traffic", flattenLogSyslogdOverrideFilterMulticastTraffic(o["multicast-traffic"], d, "multicast_traffic")); err != nil {
+		if !fortiAPIPatch(o["multicast-traffic"]) {
+			return fmt.Errorf("Error reading multicast_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("sniffer_traffic", flattenLogSyslogdOverrideFilterSnifferTraffic(o["sniffer-traffic"], d, "sniffer_traffic")); err != nil {
+		if !fortiAPIPatch(o["sniffer-traffic"]) {
+			return fmt.Errorf("Error reading sniffer_traffic: %v", err)
+		}
+	}
+
+	if err = d.Set("anomaly", flattenLogSyslogdOverrideFilterAnomaly(o["anomaly"], d, "anomaly")); err != nil {
+		if !fortiAPIPatch(o["anomaly"]) {
+			return fmt.Errorf("Error reading anomaly: %v", err)
+		}
+	}
+
+	if err = d.Set("netscan_discovery", flattenLogSyslogdOverrideFilterNetscanDiscovery(o["netscan-discovery"], d, "netscan_discovery")); err != nil {
+		if !fortiAPIPatch(o["netscan-discovery"]) {
+			return fmt.Errorf("Error reading netscan_discovery: %v", err)
+		}
+	}
+
+	if err = d.Set("netscan_vulnerability", flattenLogSyslogdOverrideFilterNetscanVulnerability(o["netscan-vulnerability"], d, "netscan_vulnerability")); err != nil {
+		if !fortiAPIPatch(o["netscan-vulnerability"]) {
+			return fmt.Errorf("Error reading netscan_vulnerability: %v", err)
+		}
+	}
+
+	if err = d.Set("voip", flattenLogSyslogdOverrideFilterVoip(o["voip"], d, "voip")); err != nil {
+		if !fortiAPIPatch(o["voip"]) {
+			return fmt.Errorf("Error reading voip: %v", err)
+		}
+	}
+
+	if err = d.Set("gtp", flattenLogSyslogdOverrideFilterGtp(o["gtp"], d, "gtp")); err != nil {
+		if !fortiAPIPatch(o["gtp"]) {
+			return fmt.Errorf("Error reading gtp: %v", err)
+		}
+	}
+
+	if err = d.Set("dns", flattenLogSyslogdOverrideFilterDns(o["dns"], d, "dns")); err != nil {
+		if !fortiAPIPatch(o["dns"]) {
+			return fmt.Errorf("Error reading dns: %v", err)
+		}
+	}
+
+	if err = d.Set("ssh", flattenLogSyslogdOverrideFilterSsh(o["ssh"], d, "ssh")); err != nil {
+		if !fortiAPIPatch(o["ssh"]) {
+			return fmt.Errorf("Error reading ssh: %v", err)
+		}
+	}
+
+	if err = d.Set("filter", flattenLogSyslogdOverrideFilterFilter(o["filter"], d, "filter")); err != nil {
+		if !fortiAPIPatch(o["filter"]) {
+			return fmt.Errorf("Error reading filter: %v", err)
+		}
+	}
+
+	if err = d.Set("filter_type", flattenLogSyslogdOverrideFilterFilterType(o["filter-type"], d, "filter_type")); err != nil {
+		if !fortiAPIPatch(o["filter-type"]) {
+			return fmt.Errorf("Error reading filter_type: %v", err)
+		}
+	}
+
+
+	return nil
+}
+
+func flattenLogSyslogdOverrideFilterFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandLogSyslogdOverrideFilterSeverity(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterForwardTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterLocalTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterMulticastTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterSnifferTraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterAnomaly(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterNetscanDiscovery(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterNetscanVulnerability(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterVoip(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterGtp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterDns(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterSsh(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterFilter(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideFilterFilterType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectLogSyslogdOverrideFilter(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("severity"); ok {
+		t, err := expandLogSyslogdOverrideFilterSeverity(d, v, "severity")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["severity"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("forward_traffic"); ok {
+		t, err := expandLogSyslogdOverrideFilterForwardTraffic(d, v, "forward_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["forward-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("local_traffic"); ok {
+		t, err := expandLogSyslogdOverrideFilterLocalTraffic(d, v, "local_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["local-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("multicast_traffic"); ok {
+		t, err := expandLogSyslogdOverrideFilterMulticastTraffic(d, v, "multicast_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["multicast-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("sniffer_traffic"); ok {
+		t, err := expandLogSyslogdOverrideFilterSnifferTraffic(d, v, "sniffer_traffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["sniffer-traffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("anomaly"); ok {
+		t, err := expandLogSyslogdOverrideFilterAnomaly(d, v, "anomaly")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["anomaly"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("netscan_discovery"); ok {
+		t, err := expandLogSyslogdOverrideFilterNetscanDiscovery(d, v, "netscan_discovery")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["netscan-discovery"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("netscan_vulnerability"); ok {
+		t, err := expandLogSyslogdOverrideFilterNetscanVulnerability(d, v, "netscan_vulnerability")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["netscan-vulnerability"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("voip"); ok {
+		t, err := expandLogSyslogdOverrideFilterVoip(d, v, "voip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["voip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("gtp"); ok {
+		t, err := expandLogSyslogdOverrideFilterGtp(d, v, "gtp")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["gtp"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dns"); ok {
+		t, err := expandLogSyslogdOverrideFilterDns(d, v, "dns")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dns"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ssh"); ok {
+		t, err := expandLogSyslogdOverrideFilterSsh(d, v, "ssh")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ssh"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("filter"); ok {
+		t, err := expandLogSyslogdOverrideFilterFilter(d, v, "filter")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["filter"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("filter_type"); ok {
+		t, err := expandLogSyslogdOverrideFilterFilterType(d, v, "filter_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["filter-type"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
---
+++ fortios/resource_logsyslogd_overridefilter_test.go
@@ -0,0 +1,117 @@
+
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+package fortios
+import (
+    "fmt"
+	"log"
+    "testing"
+    "github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+    "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+    "github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccFortiOSLogSyslogdOverrideFilter_basic(t *testing.T) {
+    rname := acctest.RandString(8)
+    log.Printf("TestAccFortiOSLogSyslogdOverrideFilter_basic %s", rname)
+
+    resource.Test(t, resource.TestCase{
+        PreCheck:     func() { testAccPreCheck(t) },
+        Providers:    testAccProviders,
+        Steps: []resource.TestStep{
+            {
+                Config: testAccFortiOSLogSyslogdOverrideFilterConfig(rname),
+                Check: resource.ComposeTestCheckFunc(
+                    testAccCheckFortiOSLogSyslogdOverrideFilterExists("fortios_logsyslogd_overridefilter.trname"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "anomaly", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "dns", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "filter_type", "include"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "forward_traffic", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "gtp", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "local_traffic", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "multicast_traffic", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "severity", "information"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "sniffer_traffic", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "ssh", "enable"),
+                    resource.TestCheckResourceAttr("fortios_logsyslogd_overridefilter.trname", "voip", "enable"),
+                ),
+            },
+        },
+    })
+}
+
+func testAccCheckFortiOSLogSyslogdOverrideFilterExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found LogSyslogdOverrideFilter: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No LogSyslogdOverrideFilter is set")
+		}
+
+		c := testAccProvider.Meta().(*FortiClient).Client
+
+		i := rs.Primary.ID
+		o, err := c.ReadLogSyslogdOverrideFilter(i)
+
+		if err != nil {
+			return fmt.Errorf("Error reading LogSyslogdOverrideFilter: %s", err)
+		}
+
+		if o == nil {
+			return fmt.Errorf("Error creating LogSyslogdOverrideFilter: %s", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckLogSyslogdOverrideFilterDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(*FortiClient).Client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "fortios_logsyslogd_overridefilter" {
+			continue
+		}
+
+		i := rs.Primary.ID
+		o, err := c.ReadLogSyslogdOverrideFilter(i)
+
+		if err == nil {
+			if o != nil {
+				return fmt.Errorf("Error LogSyslogdOverrideFilter %s still exists", rs.Primary.ID)
+			}
+		}
+
+		return nil
+	}
+
+	return nil
+}
+
+func testAccFortiOSLogSyslogdOverrideFilterConfig(name string) string {
+	return fmt.Sprintf(`
+resource "fortios_logsyslogd_overridefilter" "trname" {
+  anomaly           = "enable"
+  dns               = "enable"
+  filter_type       = "include"
+  forward_traffic   = "enable"
+  gtp               = "enable"
+  local_traffic     = "enable"
+  multicast_traffic = "enable"
+  severity          = "information"
+  sniffer_traffic   = "enable"
+  ssh               = "enable"
+  voip              = "enable"
+}
+
+
+
+`)
+}
