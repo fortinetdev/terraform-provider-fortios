7b0e90c03c44d14b67414359e3fec1333754147c
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:15:11 +0800

    Add logfortianalyzer_setting

---
+++ fortios/resource_logfortianalyzer_setting.go
@@ -0,0 +1,668 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Global FortiAnalyzer settings.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceLogFortianalyzerSetting() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLogFortianalyzerSettingUpdate,
+		Read:   resourceLogFortianalyzerSettingRead,
+		Update: resourceLogFortianalyzerSettingUpdate,
+		Delete: resourceLogFortianalyzerSettingDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"status": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ips_archive": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"server": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional: true,
+				Computed: true,
+			},
+			"hmac_algorithm": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"enc_algorithm": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ssl_min_proto_version": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"conn_timeout": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(1, 3600),
+				Optional: true,
+				Computed: true,
+			},
+			"monitor_keepalive_period": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(1, 120),
+				Optional: true,
+				Computed: true,
+			},
+			"monitor_failure_retry_period": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(1, 86400),
+				Optional: true,
+				Computed: true,
+			},
+			"mgmt_name": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Optional: true,
+				Computed: true,
+			},
+			"faz_type": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 4294967295),
+				Optional: true,
+				Computed: true,
+			},
+			"certificate": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Optional: true,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional: true,
+				Computed: true,
+			},
+			"__change_ip": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 255),
+				Optional: true,
+				Computed: true,
+			},
+			"upload_option": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"upload_interval": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"upload_day": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"upload_time": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"reliable": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+
+func resourceLogFortianalyzerSettingUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectLogFortianalyzerSetting(d)
+	if err != nil {
+		return fmt.Errorf("Error updating LogFortianalyzerSetting resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateLogFortianalyzerSetting(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating LogFortianalyzerSetting resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("LogFortianalyzerSetting")
+	}
+
+	return resourceLogFortianalyzerSettingRead(d, m)
+}
+
+func resourceLogFortianalyzerSettingDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteLogFortianalyzerSetting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting LogFortianalyzerSetting resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceLogFortianalyzerSettingRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadLogFortianalyzerSetting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading LogFortianalyzerSetting resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectLogFortianalyzerSetting(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading LogFortianalyzerSetting resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenLogFortianalyzerSettingStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingIpsArchive(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingHmacAlgorithm(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingEncAlgorithm(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingSslMinProtoVersion(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingConnTimeout(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingMonitorKeepalivePeriod(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingMonitorFailureRetryPeriod(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingMgmtName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingFazType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingCertificate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSetting__Change_Ip(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingUploadOption(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingUploadInterval(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingUploadDay(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingUploadTime(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogFortianalyzerSettingReliable(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectLogFortianalyzerSetting(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("status", flattenLogFortianalyzerSettingStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("ips_archive", flattenLogFortianalyzerSettingIpsArchive(o["ips-archive"], d, "ips_archive")); err != nil {
+		if !fortiAPIPatch(o["ips-archive"]) {
+			return fmt.Errorf("Error reading ips_archive: %v", err)
+		}
+	}
+
+	if err = d.Set("server", flattenLogFortianalyzerSettingServer(o["server"], d, "server")); err != nil {
+		if !fortiAPIPatch(o["server"]) {
+			return fmt.Errorf("Error reading server: %v", err)
+		}
+	}
+
+	if err = d.Set("hmac_algorithm", flattenLogFortianalyzerSettingHmacAlgorithm(o["hmac-algorithm"], d, "hmac_algorithm")); err != nil {
+		if !fortiAPIPatch(o["hmac-algorithm"]) {
+			return fmt.Errorf("Error reading hmac_algorithm: %v", err)
+		}
+	}
+
+	if err = d.Set("enc_algorithm", flattenLogFortianalyzerSettingEncAlgorithm(o["enc-algorithm"], d, "enc_algorithm")); err != nil {
+		if !fortiAPIPatch(o["enc-algorithm"]) {
+			return fmt.Errorf("Error reading enc_algorithm: %v", err)
+		}
+	}
+
+	if err = d.Set("ssl_min_proto_version", flattenLogFortianalyzerSettingSslMinProtoVersion(o["ssl-min-proto-version"], d, "ssl_min_proto_version")); err != nil {
+		if !fortiAPIPatch(o["ssl-min-proto-version"]) {
+			return fmt.Errorf("Error reading ssl_min_proto_version: %v", err)
+		}
+	}
+
+	if err = d.Set("conn_timeout", flattenLogFortianalyzerSettingConnTimeout(o["conn-timeout"], d, "conn_timeout")); err != nil {
+		if !fortiAPIPatch(o["conn-timeout"]) {
+			return fmt.Errorf("Error reading conn_timeout: %v", err)
+		}
+	}
+
+	if err = d.Set("monitor_keepalive_period", flattenLogFortianalyzerSettingMonitorKeepalivePeriod(o["monitor-keepalive-period"], d, "monitor_keepalive_period")); err != nil {
+		if !fortiAPIPatch(o["monitor-keepalive-period"]) {
+			return fmt.Errorf("Error reading monitor_keepalive_period: %v", err)
+		}
+	}
+
+	if err = d.Set("monitor_failure_retry_period", flattenLogFortianalyzerSettingMonitorFailureRetryPeriod(o["monitor-failure-retry-period"], d, "monitor_failure_retry_period")); err != nil {
+		if !fortiAPIPatch(o["monitor-failure-retry-period"]) {
+			return fmt.Errorf("Error reading monitor_failure_retry_period: %v", err)
+		}
+	}
+
+	if err = d.Set("mgmt_name", flattenLogFortianalyzerSettingMgmtName(o["mgmt-name"], d, "mgmt_name")); err != nil {
+		if !fortiAPIPatch(o["mgmt-name"]) {
+			return fmt.Errorf("Error reading mgmt_name: %v", err)
+		}
+	}
+
+	if err = d.Set("faz_type", flattenLogFortianalyzerSettingFazType(o["faz-type"], d, "faz_type")); err != nil {
+		if !fortiAPIPatch(o["faz-type"]) {
+			return fmt.Errorf("Error reading faz_type: %v", err)
+		}
+	}
+
+	if err = d.Set("certificate", flattenLogFortianalyzerSettingCertificate(o["certificate"], d, "certificate")); err != nil {
+		if !fortiAPIPatch(o["certificate"]) {
+			return fmt.Errorf("Error reading certificate: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", flattenLogFortianalyzerSettingSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("__change_ip", flattenLogFortianalyzerSetting__Change_Ip(o["__change_ip"], d, "__change_ip")); err != nil {
+		if !fortiAPIPatch(o["__change_ip"]) {
+			return fmt.Errorf("Error reading __change_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("upload_option", flattenLogFortianalyzerSettingUploadOption(o["upload-option"], d, "upload_option")); err != nil {
+		if !fortiAPIPatch(o["upload-option"]) {
+			return fmt.Errorf("Error reading upload_option: %v", err)
+		}
+	}
+
+	if err = d.Set("upload_interval", flattenLogFortianalyzerSettingUploadInterval(o["upload-interval"], d, "upload_interval")); err != nil {
+		if !fortiAPIPatch(o["upload-interval"]) {
+			return fmt.Errorf("Error reading upload_interval: %v", err)
+		}
+	}
+
+	if err = d.Set("upload_day", flattenLogFortianalyzerSettingUploadDay(o["upload-day"], d, "upload_day")); err != nil {
+		if !fortiAPIPatch(o["upload-day"]) {
+			return fmt.Errorf("Error reading upload_day: %v", err)
+		}
+	}
+
+	if err = d.Set("upload_time", flattenLogFortianalyzerSettingUploadTime(o["upload-time"], d, "upload_time")); err != nil {
+		if !fortiAPIPatch(o["upload-time"]) {
+			return fmt.Errorf("Error reading upload_time: %v", err)
+		}
+	}
+
+	if err = d.Set("reliable", flattenLogFortianalyzerSettingReliable(o["reliable"], d, "reliable")); err != nil {
+		if !fortiAPIPatch(o["reliable"]) {
+			return fmt.Errorf("Error reading reliable: %v", err)
+		}
+	}
+
+
+	return nil
+}
+
+func flattenLogFortianalyzerSettingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandLogFortianalyzerSettingStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingIpsArchive(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingHmacAlgorithm(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingEncAlgorithm(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingSslMinProtoVersion(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingConnTimeout(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingMonitorKeepalivePeriod(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingMonitorFailureRetryPeriod(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingMgmtName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingFazType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingCertificate(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingSourceIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSetting__Change_Ip(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingUploadOption(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingUploadInterval(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingUploadDay(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingUploadTime(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogFortianalyzerSettingReliable(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectLogFortianalyzerSetting(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandLogFortianalyzerSettingStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ips_archive"); ok {
+		t, err := expandLogFortianalyzerSettingIpsArchive(d, v, "ips_archive")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ips-archive"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("server"); ok {
+		t, err := expandLogFortianalyzerSettingServer(d, v, "server")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["server"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("hmac_algorithm"); ok {
+		t, err := expandLogFortianalyzerSettingHmacAlgorithm(d, v, "hmac_algorithm")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["hmac-algorithm"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("enc_algorithm"); ok {
+		t, err := expandLogFortianalyzerSettingEncAlgorithm(d, v, "enc_algorithm")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["enc-algorithm"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ssl_min_proto_version"); ok {
+		t, err := expandLogFortianalyzerSettingSslMinProtoVersion(d, v, "ssl_min_proto_version")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ssl-min-proto-version"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("conn_timeout"); ok {
+		t, err := expandLogFortianalyzerSettingConnTimeout(d, v, "conn_timeout")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["conn-timeout"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("monitor_keepalive_period"); ok {
+		t, err := expandLogFortianalyzerSettingMonitorKeepalivePeriod(d, v, "monitor_keepalive_period")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["monitor-keepalive-period"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("monitor_failure_retry_period"); ok {
+		t, err := expandLogFortianalyzerSettingMonitorFailureRetryPeriod(d, v, "monitor_failure_retry_period")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["monitor-failure-retry-period"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("mgmt_name"); ok {
+		t, err := expandLogFortianalyzerSettingMgmtName(d, v, "mgmt_name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["mgmt-name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("faz_type"); ok {
+		t, err := expandLogFortianalyzerSettingFazType(d, v, "faz_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["faz-type"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("certificate"); ok {
+		t, err := expandLogFortianalyzerSettingCertificate(d, v, "certificate")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["certificate"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("source_ip"); ok {
+		t, err := expandLogFortianalyzerSettingSourceIp(d, v, "source_ip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["source-ip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("__change_ip"); ok {
+		t, err := expandLogFortianalyzerSetting__Change_Ip(d, v, "__change_ip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["__change_ip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("upload_option"); ok {
+		t, err := expandLogFortianalyzerSettingUploadOption(d, v, "upload_option")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["upload-option"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("upload_interval"); ok {
+		t, err := expandLogFortianalyzerSettingUploadInterval(d, v, "upload_interval")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["upload-interval"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("upload_day"); ok {
+		t, err := expandLogFortianalyzerSettingUploadDay(d, v, "upload_day")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["upload-day"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("upload_time"); ok {
+		t, err := expandLogFortianalyzerSettingUploadTime(d, v, "upload_time")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["upload-time"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("reliable"); ok {
+		t, err := expandLogFortianalyzerSettingReliable(d, v, "reliable")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["reliable"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
