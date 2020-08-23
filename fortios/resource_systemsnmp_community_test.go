5c75016a37d0e7823b1d0d422ea31350b3927e19
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:01:44 +0800

    Add systemsnmp_community

---
+++ fortios/resource_systemsnmp_community_test.go
@@ -0,0 +1,121 @@
+
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+package fortios
+import (
+    "fmt"
+	"log"
+    "testing"
+    "github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+    "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+    "github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccFortiOSSystemSnmpCommunity_basic(t *testing.T) {
+    rname := acctest.RandString(8)
+    log.Printf("TestAccFortiOSSystemSnmpCommunity_basic %s", rname)
+
+    resource.Test(t, resource.TestCase{
+        PreCheck:     func() { testAccPreCheck(t) },
+        Providers:    testAccProviders,
+        Steps: []resource.TestStep{
+            {
+                Config: testAccFortiOSSystemSnmpCommunityConfig(rname),
+                Check: resource.ComposeTestCheckFunc(
+                    testAccCheckFortiOSSystemSnmpCommunityExists("fortios_systemsnmp_community.trname"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "events", "cpu-high mem-low log-full intf-ip vpn-tun-up vpn-tun-down ha-switch ha-hb-failure ips-signature ips-anomaly av-virus av-oversize av-pattern av-fragmented fm-if-change bgp-established bgp-backward-transition ha-member-up ha-member-down ent-conf-change av-conserve av-bypass av-oversize-passed av-oversize-blocked ips-pkg-update ips-fail-open faz-disconnect wc-ap-up wc-ap-down fswctl-session-up fswctl-session-down load-balance-real-server-down per-cpu-high"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "fosid", "1"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "name", rname),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "query_v1_port", "161"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "query_v1_status", "enable"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "query_v2c_port", "161"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "query_v2c_status", "enable"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "status", "enable"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "trap_v1_lport", "162"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "trap_v1_rport", "162"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "trap_v1_status", "enable"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "trap_v2c_lport", "162"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "trap_v2c_rport", "162"),
+                    resource.TestCheckResourceAttr("fortios_systemsnmp_community.trname", "trap_v2c_status", "enable"),
+                ),
+            },
+        },
+    })
+}
+
+func testAccCheckFortiOSSystemSnmpCommunityExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found SystemSnmpCommunity: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No SystemSnmpCommunity is set")
+		}
+
+		c := testAccProvider.Meta().(*FortiClient).Client
+
+		i := rs.Primary.ID
+		o, err := c.ReadSystemSnmpCommunity(i)
+
+		if err != nil {
+			return fmt.Errorf("Error reading SystemSnmpCommunity: %s", err)
+		}
+
+		if o == nil {
+			return fmt.Errorf("Error creating SystemSnmpCommunity: %s", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckSystemSnmpCommunityDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(*FortiClient).Client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "fortios_systemsnmp_community" {
+			continue
+		}
+
+		i := rs.Primary.ID
+		o, err := c.ReadSystemSnmpCommunity(i)
+
+		if err == nil {
+			if o != nil {
+				return fmt.Errorf("Error SystemSnmpCommunity %s still exists", rs.Primary.ID)
+			}
+		}
+
+		return nil
+	}
+
+	return nil
+}
+
+func testAccFortiOSSystemSnmpCommunityConfig(name string) string {
+	return fmt.Sprintf(`
+resource "fortios_systemsnmp_community" "trname" {
+  events           = "cpu-high mem-low log-full intf-ip vpn-tun-up vpn-tun-down ha-switch ha-hb-failure ips-signature ips-anomaly av-virus av-oversize av-pattern av-fragmented fm-if-change bgp-established bgp-backward-transition ha-member-up ha-member-down ent-conf-change av-conserve av-bypass av-oversize-passed av-oversize-blocked ips-pkg-update ips-fail-open faz-disconnect wc-ap-up wc-ap-down fswctl-session-up fswctl-session-down load-balance-real-server-down per-cpu-high"
+  fosid            = 1
+  name             = "%[1]s"
+  query_v1_port    = 161
+  query_v1_status  = "enable"
+  query_v2c_port   = 161
+  query_v2c_status = "enable"
+  status           = "enable"
+  trap_v1_lport    = 162
+  trap_v1_rport    = 162
+  trap_v1_status   = "enable"
+  trap_v2c_lport   = 162
+  trap_v2c_rport   = 162
+  trap_v2c_status  = "enable"
+}
+
+`, name)
+}
---
+++ website/docs/r/fortios_systemsnmp_community.html.markdown
@@ -0,0 +1,83 @@
+---
+subcategory: "FortiGate System"
+layout: "fortios"
+page_title: "FortiOS: fortios_systemsnmp_community"
+description: |-
+  SNMP community configuration.
+---
+
+# fortios_systemsnmp_community
+SNMP community configuration.
+
+## Example Usage
+
+```hcl
+resource "fortios_systemsnmp_community" "trname" {
+  events           = "cpu-high mem-low log-full intf-ip vpn-tun-up vpn-tun-down ha-switch ha-hb-failure ips-signature ips-anomaly av-virus av-oversize av-pattern av-fragmented fm-if-change bgp-established bgp-backward-transition ha-member-up ha-member-down ent-conf-change av-conserve av-bypass av-oversize-passed av-oversize-blocked ips-pkg-update ips-fail-open faz-disconnect wc-ap-up wc-ap-down fswctl-session-up fswctl-session-down load-balance-real-server-down per-cpu-high"
+  fosid            = 1
+  name             = "fdsie"
+  query_v1_port    = 161
+  query_v1_status  = "enable"
+  query_v2c_port   = 161
+  query_v2c_status = "enable"
+  status           = "enable"
+  trap_v1_lport    = 162
+  trap_v1_rport    = 162
+  trap_v1_status   = "enable"
+  trap_v2c_lport   = 162
+  trap_v2c_rport   = 162
+  trap_v2c_status  = "enable"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `fosid` - (Required) Community ID.
+* `name` - (Required) Community name.
+* `status` - Enable/disable this SNMP community.
+* `hosts` - Configure IPv4 SNMP managers (hosts).
+* `hosts6` - Configure IPv6 SNMP managers.
+* `query_v1_status` - Enable/disable SNMP v1 queries.
+* `query_v1_port` - SNMP v1 query port (default = 161).
+* `query_v2c_status` - Enable/disable SNMP v2c queries.
+* `query_v2c_port` - SNMP v2c query port (default = 161).
+* `trap_v1_status` - Enable/disable SNMP v1 traps.
+* `trap_v1_lport` - SNMP v1 trap local port (default = 162).
+* `trap_v1_rport` - SNMP v1 trap remote port (default = 162).
+* `trap_v2c_status` - Enable/disable SNMP v2c traps.
+* `trap_v2c_lport` - SNMP v2c trap local port (default = 162).
+* `trap_v2c_rport` - SNMP v2c trap remote port (default = 162).
+* `events` - SNMP trap events.
+
+The `hosts` block supports:
+
+* `id` - Host entry ID.
+* `source_ip` - Source IPv4 address for SNMP traps.
+* `ip` - IPv4 address of the SNMP manager (host).
+* `ha_direct` - Enable/disable direct management of HA cluster members.
+* `host_type` - Control whether the SNMP manager sends SNMP queries, receives SNMP traps, or both.
+
+The `hosts6` block supports:
+
+* `id` - Host6 entry ID.
+* `source_ipv6` - Source IPv6 address for SNMP traps.
+* `ipv6` - SNMP manager IPv6 address prefix.
+* `ha_direct` - Enable/disable direct management of HA cluster members.
+* `host_type` - Control whether the SNMP manager sends SNMP queries, receives SNMP traps, or both.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{fosid}}.
+
+## Import
+
+SystemSnmp Community can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_systemsnmp_community.labelname {{fosid}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
