55a05f28fee0ab55dab32068c19c2311c7d17d31
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:02:04 +0800

    Add systemautoupdate_schedule

---
+++ fortios/resource_systemautoupdate_schedule.go
@@ -0,0 +1,235 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure update schedule.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemAutoupdateSchedule() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemAutoupdateScheduleUpdate,
+		Read:   resourceSystemAutoupdateScheduleRead,
+		Update: resourceSystemAutoupdateScheduleUpdate,
+		Delete: resourceSystemAutoupdateScheduleDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"status": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},
+			"frequency": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},
+			"time": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},
+			"day": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+
+func resourceSystemAutoupdateScheduleUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemAutoupdateSchedule(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemAutoupdateSchedule resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSystemAutoupdateSchedule(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemAutoupdateSchedule resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemAutoupdateSchedule")
+	}
+
+	return resourceSystemAutoupdateScheduleRead(d, m)
+}
+
+func resourceSystemAutoupdateScheduleDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSystemAutoupdateSchedule(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemAutoupdateSchedule resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemAutoupdateScheduleRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSystemAutoupdateSchedule(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemAutoupdateSchedule resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemAutoupdateSchedule(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemAutoupdateSchedule resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenSystemAutoupdateScheduleStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemAutoupdateScheduleFrequency(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemAutoupdateScheduleTime(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemAutoupdateScheduleDay(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectSystemAutoupdateSchedule(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("status", flattenSystemAutoupdateScheduleStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("frequency", flattenSystemAutoupdateScheduleFrequency(o["frequency"], d, "frequency")); err != nil {
+		if !fortiAPIPatch(o["frequency"]) {
+			return fmt.Errorf("Error reading frequency: %v", err)
+		}
+	}
+
+	if err = d.Set("time", flattenSystemAutoupdateScheduleTime(o["time"], d, "time")); err != nil {
+		if !fortiAPIPatch(o["time"]) {
+			return fmt.Errorf("Error reading time: %v", err)
+		}
+	}
+
+	if err = d.Set("day", flattenSystemAutoupdateScheduleDay(o["day"], d, "day")); err != nil {
+		if !fortiAPIPatch(o["day"]) {
+			return fmt.Errorf("Error reading day: %v", err)
+		}
+	}
+
+
+	return nil
+}
+
+func flattenSystemAutoupdateScheduleFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandSystemAutoupdateScheduleStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemAutoupdateScheduleFrequency(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemAutoupdateScheduleTime(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemAutoupdateScheduleDay(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectSystemAutoupdateSchedule(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandSystemAutoupdateScheduleStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("frequency"); ok {
+		t, err := expandSystemAutoupdateScheduleFrequency(d, v, "frequency")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["frequency"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("time"); ok {
+		t, err := expandSystemAutoupdateScheduleTime(d, v, "time")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["time"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("day"); ok {
+		t, err := expandSystemAutoupdateScheduleDay(d, v, "day")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["day"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
---
+++ website/docs/r/fortios_systemautoupdate_schedule.html.markdown
@@ -0,0 +1,45 @@
+---
+subcategory: "FortiGate System"
+layout: "fortios"
+page_title: "FortiOS: fortios_systemautoupdate_schedule"
+description: |-
+  Configure update schedule.
+---
+
+# fortios_systemautoupdate_schedule
+Configure update schedule.
+
+## Example Usage
+
+```hcl
+resource "fortios_systemautoupdate_schedule" "trname" {
+  day       = "Monday"
+  frequency = "every"
+  status    = "enable"
+  time      = "02:60"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `status` - (Required) Enable/disable scheduled updates.
+* `frequency` - (Required) Update frequency.
+* `time` - (Required) Update time.
+* `day` - (Required) Update day.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource.
+
+## Import
+
+SystemAutoupdate Schedule can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_systemautoupdate_schedule.labelname SystemAutoupdateSchedule
+$ unset "FORTIOS_IMPORT_TABLE"
+```
