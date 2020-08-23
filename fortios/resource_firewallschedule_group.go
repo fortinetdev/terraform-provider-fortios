4a296fa0a0a3c31b81a9e5db729898ebb0f9c64a
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:17:33 +0800

    Add firewallschedule_group

---
+++ fortios/resource_firewallschedule_group.go
@@ -0,0 +1,313 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Schedule group configuration.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceFirewallScheduleGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFirewallScheduleGroupCreate,
+		Read:   resourceFirewallScheduleGroupRead,
+		Update: resourceFirewallScheduleGroupUpdate,
+		Delete: resourceFirewallScheduleGroupDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 31),
+				Required: true,
+			},
+			"member": &schema.Schema{
+				Type: schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type: schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 64),
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"color": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 32),
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceFirewallScheduleGroupCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallScheduleGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallScheduleGroup resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateFirewallScheduleGroup(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallScheduleGroup resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("FirewallScheduleGroup")
+	}
+
+	return resourceFirewallScheduleGroupRead(d, m)
+}
+
+func resourceFirewallScheduleGroupUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallScheduleGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallScheduleGroup resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateFirewallScheduleGroup(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallScheduleGroup resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("FirewallScheduleGroup")
+	}
+
+	return resourceFirewallScheduleGroupRead(d, m)
+}
+
+func resourceFirewallScheduleGroupDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteFirewallScheduleGroup(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting FirewallScheduleGroup resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFirewallScheduleGroupRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadFirewallScheduleGroup(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallScheduleGroup resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectFirewallScheduleGroup(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallScheduleGroup resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenFirewallScheduleGroupName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallScheduleGroupMember(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenFirewallScheduleGroupMemberName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenFirewallScheduleGroupMemberName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallScheduleGroupColor(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectFirewallScheduleGroup(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("name", flattenFirewallScheduleGroupName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+    if isImportTable() {
+        if err = d.Set("member", flattenFirewallScheduleGroupMember(o["member"], d, "member")); err != nil {
+            if !fortiAPIPatch(o["member"]) {
+                return fmt.Errorf("Error reading member: %v", err)
+            }
+        }
+    } else {
+        if _, ok := d.GetOk("member"); ok {
+            if err = d.Set("member", flattenFirewallScheduleGroupMember(o["member"], d, "member")); err != nil {
+                if !fortiAPIPatch(o["member"]) {
+                    return fmt.Errorf("Error reading member: %v", err)
+                }
+            }
+        }
+    }
+
+	if err = d.Set("color", flattenFirewallScheduleGroupColor(o["color"], d, "color")); err != nil {
+		if !fortiAPIPatch(o["color"]) {
+			return fmt.Errorf("Error reading color: %v", err)
+		}
+	}
+
+
+	return nil
+}
+
+func flattenFirewallScheduleGroupFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandFirewallScheduleGroupName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallScheduleGroupMember(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := ""  // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandFirewallScheduleGroupMemberName(d, i["name"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandFirewallScheduleGroupMemberName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallScheduleGroupColor(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectFirewallScheduleGroup(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandFirewallScheduleGroupName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("member"); ok {
+		t, err := expandFirewallScheduleGroupMember(d, v, "member")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["member"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("color"); ok {
+		t, err := expandFirewallScheduleGroupColor(d, v, "color")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["color"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
---
+++ website/docs/r/fortios_firewallschedule_group.html.markdown
@@ -0,0 +1,58 @@
+---
+subcategory: "FortiGate Firewall"
+layout: "fortios"
+page_title: "FortiOS: fortios_firewallschedule_group"
+description: |-
+  Schedule group configuration.
+---
+
+# fortios_firewallschedule_group
+Schedule group configuration.
+
+## Example Usage
+
+```hcl
+resource "fortios_firewallschedule_recurring" "trname1" {
+  color = 0
+  day   = "sunday"
+  end   = "00:00"
+  name  = "recurring2"
+  start = "00:00"
+}
+
+resource "fortios_firewallschedule_group" "trname" {
+  color = 0
+  name  = "groupg1"
+
+  member {
+    name = fortios_firewallschedule_recurring.trname1.name
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) Schedule group name.
+* `member` - (Required) Schedules added to the schedule group.
+* `color` - Color of icon on the GUI.
+
+The `member` block supports:
+
+* `name` - Schedule name.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+FirewallSchedule Group can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_firewallschedule_group.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
