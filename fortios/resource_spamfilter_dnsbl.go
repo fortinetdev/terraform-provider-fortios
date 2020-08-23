e3be7fc589a373b53a6de2498db17a5c3bdf8ac3
Frank Shen <fshen01@fortinet.com>
2020-08-24 02:09:54 +0800

    Add spamfilter_dnsbl

---
+++ fortios/resource_spamfilter_dnsbl.go
@@ -0,0 +1,410 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure AntiSpam DNSBL/ORBL.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSpamfilterDnsbl() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSpamfilterDnsblCreate,
+		Read:   resourceSpamfilterDnsblRead,
+		Update: resourceSpamfilterDnsblUpdate,
+		Delete: resourceSpamfilterDnsblDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type: schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 4294967295),
+				Required: true,
+			},
+			"name": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Required: true,
+			},
+			"comment": &schema.Schema{
+				Type: schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 255),
+				Optional: true,
+			},
+			"entries": &schema.Schema{
+				Type: schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"status": &schema.Schema{
+							Type: schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"id": &schema.Schema{
+							Type: schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 4294967295),
+							Optional: true,
+							Computed: true,
+						},
+						"server": &schema.Schema{
+							Type: schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 127),
+							Optional: true,
+							Computed: true,
+						},
+						"action": &schema.Schema{
+							Type: schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceSpamfilterDnsblCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSpamfilterDnsbl(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SpamfilterDnsbl resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSpamfilterDnsbl(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SpamfilterDnsbl resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("SpamfilterDnsbl")
+	}
+
+	return resourceSpamfilterDnsblRead(d, m)
+}
+
+func resourceSpamfilterDnsblUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSpamfilterDnsbl(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SpamfilterDnsbl resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSpamfilterDnsbl(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SpamfilterDnsbl resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("SpamfilterDnsbl")
+	}
+
+	return resourceSpamfilterDnsblRead(d, m)
+}
+
+func resourceSpamfilterDnsblDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSpamfilterDnsbl(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SpamfilterDnsbl resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSpamfilterDnsblRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSpamfilterDnsbl(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SpamfilterDnsbl resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSpamfilterDnsbl(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SpamfilterDnsbl resource from API: %v", err)
+	}
+	return nil
+}
+
+
+func flattenSpamfilterDnsblId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSpamfilterDnsblName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSpamfilterDnsblComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSpamfilterDnsblEntries(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := i["status"]; ok {
+			tmp["status"] = flattenSpamfilterDnsblEntriesStatus(i["status"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = flattenSpamfilterDnsblEntriesId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "server"
+		if _, ok := i["server"]; ok {
+			tmp["server"] = flattenSpamfilterDnsblEntriesServer(i["server"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := i["action"]; ok {
+			tmp["action"] = flattenSpamfilterDnsblEntriesAction(i["action"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenSpamfilterDnsblEntriesStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSpamfilterDnsblEntriesId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSpamfilterDnsblEntriesServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSpamfilterDnsblEntriesAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+
+func refreshObjectSpamfilterDnsbl(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+
+	if err = d.Set("fosid", flattenSpamfilterDnsblId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenSpamfilterDnsblName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", flattenSpamfilterDnsblComment(o["comment"], d, "comment")); err != nil {
+		if !fortiAPIPatch(o["comment"]) {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+    if isImportTable() {
+        if err = d.Set("entries", flattenSpamfilterDnsblEntries(o["entries"], d, "entries")); err != nil {
+            if !fortiAPIPatch(o["entries"]) {
+                return fmt.Errorf("Error reading entries: %v", err)
+            }
+        }
+    } else {
+        if _, ok := d.GetOk("entries"); ok {
+            if err = d.Set("entries", flattenSpamfilterDnsblEntries(o["entries"], d, "entries")); err != nil {
+                if !fortiAPIPatch(o["entries"]) {
+                    return fmt.Errorf("Error reading entries: %v", err)
+                }
+            }
+        }
+    }
+
+
+	return nil
+}
+
+func flattenSpamfilterDnsblFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+
+func expandSpamfilterDnsblId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSpamfilterDnsblName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSpamfilterDnsblComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSpamfilterDnsblEntries(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := ""  // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["status"], _ = expandSpamfilterDnsblEntriesStatus(d, i["status"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandSpamfilterDnsblEntriesId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "server"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["server"], _ = expandSpamfilterDnsblEntriesServer(d, i["server"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["action"], _ = expandSpamfilterDnsblEntriesAction(d, i["action"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandSpamfilterDnsblEntriesStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSpamfilterDnsblEntriesId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSpamfilterDnsblEntriesServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSpamfilterDnsblEntriesAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+
+func getObjectSpamfilterDnsbl(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandSpamfilterDnsblId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandSpamfilterDnsblName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandSpamfilterDnsblComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("entries"); ok {
+		t, err := expandSpamfilterDnsblEntries(d, v, "entries")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["entries"] = t
+		}
+	}
+
+
+	return &obj, nil
+}
+
---
+++ website/docs/r/fortios_spamfilter_dnsbl.html.markdown
@@ -0,0 +1,57 @@
+---
+subcategory: "FortiGate Spamfilter"
+layout: "fortios"
+page_title: "FortiOS: fortios_spamfilter_dnsbl"
+description: |-
+  Configure AntiSpam DNSBL/ORBL.
+---
+
+# fortios_spamfilter_dnsbl
+Configure AntiSpam DNSBL/ORBL.
+
+## Example Usage
+
+```hcl
+resource "fortios_spamfilter_dnsbl" "trname" {
+  comment = "test"
+  fosid   = 1
+  name    = "s1"
+
+  entries {
+    action = "reject"
+    server = "1.1.1.1"
+    status = "enable"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `fosid` - (Required) ID.
+* `name` - (Required) Name of table.
+* `comment` - Optional comments.
+* `entries` - Spam filter DNSBL and ORBL server.
+
+The `entries` block supports:
+
+* `status` - Enable/disable status.
+* `id` - DNSBL/ORBL entry ID.
+* `server` - DNSBL or ORBL server name.
+* `action` - Reject connection or mark as spam email.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{fosid}}.
+
+## Import
+
+Spamfilter Dnsbl can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_spamfilter_dnsbl.labelname {{fosid}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
