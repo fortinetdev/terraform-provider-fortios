75bce8c760ef16ae7fe3fb72500976f69ce8f568
Frank Shen <fshen01@fortinet.com>
2020-09-01 17:35:37 +0800

    Add firewallssh_localca

---
+++ fortios/resource_firewallssh_localca.go
@@ -0,0 +1,280 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: SSH proxy local CA.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceFirewallSshLocalCa() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFirewallSshLocalCaCreate,
+		Read:   resourceFirewallSshLocalCaRead,
+		Update: resourceFirewallSshLocalCaUpdate,
+		Delete: resourceFirewallSshLocalCaDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Optional:     true,
+				Computed:     true,
+			},
+			"password": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 128),
+				Optional:     true,
+			},
+			"private_key": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"public_key": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"source": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceFirewallSshLocalCaCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallSshLocalCa(d)
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallSshLocalCa resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateFirewallSshLocalCa(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallSshLocalCa resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("FirewallSshLocalCa")
+	}
+
+	return resourceFirewallSshLocalCaRead(d, m)
+}
+
+func resourceFirewallSshLocalCaUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallSshLocalCa(d)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallSshLocalCa resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateFirewallSshLocalCa(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallSshLocalCa resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("FirewallSshLocalCa")
+	}
+
+	return resourceFirewallSshLocalCaRead(d, m)
+}
+
+func resourceFirewallSshLocalCaDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteFirewallSshLocalCa(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting FirewallSshLocalCa resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFirewallSshLocalCaRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadFirewallSshLocalCa(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallSshLocalCa resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectFirewallSshLocalCa(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallSshLocalCa resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenFirewallSshLocalCaName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallSshLocalCaPassword(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallSshLocalCaPrivateKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallSshLocalCaPublicKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenFirewallSshLocalCaSource(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectFirewallSshLocalCa(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenFirewallSshLocalCaName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("password", flattenFirewallSshLocalCaPassword(o["password"], d, "password")); err != nil {
+		if !fortiAPIPatch(o["password"]) {
+			return fmt.Errorf("Error reading password: %v", err)
+		}
+	}
+
+	if err = d.Set("private_key", flattenFirewallSshLocalCaPrivateKey(o["private-key"], d, "private_key")); err != nil {
+		if !fortiAPIPatch(o["private-key"]) {
+			return fmt.Errorf("Error reading private_key: %v", err)
+		}
+	}
+
+	if err = d.Set("public_key", flattenFirewallSshLocalCaPublicKey(o["public-key"], d, "public_key")); err != nil {
+		if !fortiAPIPatch(o["public-key"]) {
+			return fmt.Errorf("Error reading public_key: %v", err)
+		}
+	}
+
+	if err = d.Set("source", flattenFirewallSshLocalCaSource(o["source"], d, "source")); err != nil {
+		if !fortiAPIPatch(o["source"]) {
+			return fmt.Errorf("Error reading source: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenFirewallSshLocalCaFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandFirewallSshLocalCaName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallSshLocalCaPassword(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallSshLocalCaPrivateKey(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallSshLocalCaPublicKey(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallSshLocalCaSource(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectFirewallSshLocalCa(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandFirewallSshLocalCaName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("password"); ok {
+		t, err := expandFirewallSshLocalCaPassword(d, v, "password")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["password"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("private_key"); ok {
+		t, err := expandFirewallSshLocalCaPrivateKey(d, v, "private_key")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["private-key"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("public_key"); ok {
+		t, err := expandFirewallSshLocalCaPublicKey(d, v, "public_key")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["public-key"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("source"); ok {
+		t, err := expandFirewallSshLocalCaSource(d, v, "source")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["source"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_firewallssh_localca.html.markdown
@@ -0,0 +1,35 @@
+---
+subcategory: "FortiGate Firewall"
+layout: "fortios"
+page_title: "FortiOS: fortios_firewallssh_localca"
+description: |-
+  SSH proxy local CA.
+---
+
+# fortios_firewallssh_localca
+SSH proxy local CA.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - SSH proxy local CA name.
+* `password` - Password for SSH private key.
+* `private_key` - (Required) SSH proxy private key, encrypted with a password.
+* `public_key` - (Required) SSH proxy public key.
+* `source` - SSH proxy local CA source type.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+FirewallSsh LocalCa can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_firewallssh_localca.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
