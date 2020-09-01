ce14a7c7f65b0f964fa8e2d303fd4cbb655b3eb6
Frank Shen <fshen01@fortinet.com>
2020-09-01 17:27:43 +0800

    Add user_devicegroup

---
+++ fortios/resource_user_devicegroup.go
@@ -0,0 +1,526 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure device groups.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceUserDeviceGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceUserDeviceGroupCreate,
+		Read:   resourceUserDeviceGroupRead,
+		Update: resourceUserDeviceGroupUpdate,
+		Delete: resourceUserDeviceGroupDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Required:     true,
+			},
+			"member": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 35),
+							Optional:     true,
+							Computed:     true,
+						},
+					},
+				},
+			},
+			"tagging": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 63),
+							Optional:     true,
+							Computed:     true,
+						},
+						"category": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 63),
+							Optional:     true,
+							Computed:     true,
+						},
+						"tags": &schema.Schema{
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": &schema.Schema{
+										Type:         schema.TypeString,
+										ValidateFunc: validation.StringLenBetween(0, 64),
+										Optional:     true,
+										Computed:     true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"comment": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 255),
+				Optional:     true,
+			},
+		},
+	}
+}
+
+func resourceUserDeviceGroupCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectUserDeviceGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error creating UserDeviceGroup resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateUserDeviceGroup(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating UserDeviceGroup resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("UserDeviceGroup")
+	}
+
+	return resourceUserDeviceGroupRead(d, m)
+}
+
+func resourceUserDeviceGroupUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectUserDeviceGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error updating UserDeviceGroup resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateUserDeviceGroup(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating UserDeviceGroup resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("UserDeviceGroup")
+	}
+
+	return resourceUserDeviceGroupRead(d, m)
+}
+
+func resourceUserDeviceGroupDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteUserDeviceGroup(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting UserDeviceGroup resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceUserDeviceGroupRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadUserDeviceGroup(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading UserDeviceGroup resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectUserDeviceGroup(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading UserDeviceGroup resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenUserDeviceGroupName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenUserDeviceGroupMember(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenUserDeviceGroupMemberName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenUserDeviceGroupMemberName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenUserDeviceGroupTagging(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenUserDeviceGroupTaggingName(i["name"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "category"
+		if _, ok := i["category"]; ok {
+			tmp["category"] = flattenUserDeviceGroupTaggingCategory(i["category"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "tags"
+		if _, ok := i["tags"]; ok {
+			tmp["tags"] = flattenUserDeviceGroupTaggingTags(i["tags"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenUserDeviceGroupTaggingName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenUserDeviceGroupTaggingCategory(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenUserDeviceGroupTaggingTags(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenUserDeviceGroupTaggingTagsName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenUserDeviceGroupTaggingTagsName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenUserDeviceGroupComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectUserDeviceGroup(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenUserDeviceGroupName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("member", flattenUserDeviceGroupMember(o["member"], d, "member")); err != nil {
+			if !fortiAPIPatch(o["member"]) {
+				return fmt.Errorf("Error reading member: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("member"); ok {
+			if err = d.Set("member", flattenUserDeviceGroupMember(o["member"], d, "member")); err != nil {
+				if !fortiAPIPatch(o["member"]) {
+					return fmt.Errorf("Error reading member: %v", err)
+				}
+			}
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("tagging", flattenUserDeviceGroupTagging(o["tagging"], d, "tagging")); err != nil {
+			if !fortiAPIPatch(o["tagging"]) {
+				return fmt.Errorf("Error reading tagging: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("tagging"); ok {
+			if err = d.Set("tagging", flattenUserDeviceGroupTagging(o["tagging"], d, "tagging")); err != nil {
+				if !fortiAPIPatch(o["tagging"]) {
+					return fmt.Errorf("Error reading tagging: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("comment", flattenUserDeviceGroupComment(o["comment"], d, "comment")); err != nil {
+		if !fortiAPIPatch(o["comment"]) {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenUserDeviceGroupFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandUserDeviceGroupName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandUserDeviceGroupMember(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandUserDeviceGroupMemberName(d, i["name"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandUserDeviceGroupMemberName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandUserDeviceGroupTagging(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandUserDeviceGroupTaggingName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "category"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["category"], _ = expandUserDeviceGroupTaggingCategory(d, i["category"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "tags"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["tags"], _ = expandUserDeviceGroupTaggingTags(d, i["tags"], pre_append)
+		} else {
+			tmp["tags"] = make([]string, 0)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandUserDeviceGroupTaggingName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandUserDeviceGroupTaggingCategory(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandUserDeviceGroupTaggingTags(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandUserDeviceGroupTaggingTagsName(d, i["name"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandUserDeviceGroupTaggingTagsName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandUserDeviceGroupComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectUserDeviceGroup(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandUserDeviceGroupName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("member"); ok {
+		t, err := expandUserDeviceGroupMember(d, v, "member")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["member"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("tagging"); ok {
+		t, err := expandUserDeviceGroupTagging(d, v, "tagging")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["tagging"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandUserDeviceGroupComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_user_devicegroup.html.markdown
@@ -0,0 +1,67 @@
+---
+subcategory: "FortiGate User"
+layout: "fortios"
+page_title: "FortiOS: fortios_user_devicegroup"
+description: |-
+  Configure device groups.
+---
+
+# fortios_user_devicegroup
+Configure device groups.
+
+## Example Usage
+
+```hcl
+resource "fortios_user_device" "trnames12" {
+  alias    = "user_devices2"
+  category = "amazon-device"
+  mac      = "08:00:20:0a:1c:1d"
+  type     = "unknown"
+}
+
+resource "fortios_user_devicegroup" "trname" {
+  name = "user_devicegroupd1"
+
+  member {
+    name = fortios_user_device.trnames12.alias
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) Device group name.
+* `member` - Device group member.
+* `tagging` - Config object tagging.
+* `comment` - Comment.
+
+The `member` block supports:
+
+* `name` - Device name.
+
+The `tagging` block supports:
+
+* `name` - Tagging entry name.
+* `category` - Tag category.
+* `tags` - Tags.
+
+The `tags` block supports:
+
+* `name` - Tag name.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+User DeviceGroup can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_user_devicegroup.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
