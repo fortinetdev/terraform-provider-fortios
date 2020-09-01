f20c30554f2442d0c62fc91764f70670ae9c8a6a
Frank Shen <fshen01@fortinet.com>
2020-09-01 17:29:29 +0800

    Add system_switchinterface

---
+++ fortios/resource_system_switchinterface.go
@@ -0,0 +1,551 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure software switch interfaces by grouping physical and WiFi interfaces.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemSwitchInterface() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemSwitchInterfaceCreate,
+		Read:   resourceSystemSwitchInterfaceRead,
+		Update: resourceSystemSwitchInterfaceUpdate,
+		Delete: resourceSystemSwitchInterfaceDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 15),
+				Required:     true,
+			},
+			"vdom": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 31),
+				Optional:     true,
+				Computed:     true,
+			},
+			"span_dest_port": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 15),
+				Optional:     true,
+				Computed:     true,
+			},
+			"span_source_port": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"interface_name": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 64),
+							Optional:     true,
+							Computed:     true,
+						},
+					},
+				},
+			},
+			"member": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"interface_name": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 64),
+							Optional:     true,
+							Computed:     true,
+						},
+					},
+				},
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"intra_switch_policy": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"span": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"span_direction": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemSwitchInterfaceCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemSwitchInterface(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemSwitchInterface resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSystemSwitchInterface(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemSwitchInterface resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemSwitchInterface")
+	}
+
+	return resourceSystemSwitchInterfaceRead(d, m)
+}
+
+func resourceSystemSwitchInterfaceUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemSwitchInterface(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemSwitchInterface resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSystemSwitchInterface(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemSwitchInterface resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemSwitchInterface")
+	}
+
+	return resourceSystemSwitchInterfaceRead(d, m)
+}
+
+func resourceSystemSwitchInterfaceDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSystemSwitchInterface(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemSwitchInterface resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemSwitchInterfaceRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSystemSwitchInterface(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemSwitchInterface resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemSwitchInterface(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemSwitchInterface resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemSwitchInterfaceName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceVdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceSpanDestPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceSpanSourcePort(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "interface_name"
+		if _, ok := i["interface-name"]; ok {
+			tmp["interface_name"] = flattenSystemSwitchInterfaceSpanSourcePortInterfaceName(i["interface-name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenSystemSwitchInterfaceSpanSourcePortInterfaceName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceMember(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "interface_name"
+		if _, ok := i["interface-name"]; ok {
+			tmp["interface_name"] = flattenSystemSwitchInterfaceMemberInterfaceName(i["interface-name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenSystemSwitchInterfaceMemberInterfaceName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceIntraSwitchPolicy(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceSpan(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemSwitchInterfaceSpanDirection(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSystemSwitchInterface(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenSystemSwitchInterfaceName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("vdom", flattenSystemSwitchInterfaceVdom(o["vdom"], d, "vdom")); err != nil {
+		if !fortiAPIPatch(o["vdom"]) {
+			return fmt.Errorf("Error reading vdom: %v", err)
+		}
+	}
+
+	if err = d.Set("span_dest_port", flattenSystemSwitchInterfaceSpanDestPort(o["span-dest-port"], d, "span_dest_port")); err != nil {
+		if !fortiAPIPatch(o["span-dest-port"]) {
+			return fmt.Errorf("Error reading span_dest_port: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("span_source_port", flattenSystemSwitchInterfaceSpanSourcePort(o["span-source-port"], d, "span_source_port")); err != nil {
+			if !fortiAPIPatch(o["span-source-port"]) {
+				return fmt.Errorf("Error reading span_source_port: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("span_source_port"); ok {
+			if err = d.Set("span_source_port", flattenSystemSwitchInterfaceSpanSourcePort(o["span-source-port"], d, "span_source_port")); err != nil {
+				if !fortiAPIPatch(o["span-source-port"]) {
+					return fmt.Errorf("Error reading span_source_port: %v", err)
+				}
+			}
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("member", flattenSystemSwitchInterfaceMember(o["member"], d, "member")); err != nil {
+			if !fortiAPIPatch(o["member"]) {
+				return fmt.Errorf("Error reading member: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("member"); ok {
+			if err = d.Set("member", flattenSystemSwitchInterfaceMember(o["member"], d, "member")); err != nil {
+				if !fortiAPIPatch(o["member"]) {
+					return fmt.Errorf("Error reading member: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("type", flattenSystemSwitchInterfaceType(o["type"], d, "type")); err != nil {
+		if !fortiAPIPatch(o["type"]) {
+			return fmt.Errorf("Error reading type: %v", err)
+		}
+	}
+
+	if err = d.Set("intra_switch_policy", flattenSystemSwitchInterfaceIntraSwitchPolicy(o["intra-switch-policy"], d, "intra_switch_policy")); err != nil {
+		if !fortiAPIPatch(o["intra-switch-policy"]) {
+			return fmt.Errorf("Error reading intra_switch_policy: %v", err)
+		}
+	}
+
+	if err = d.Set("span", flattenSystemSwitchInterfaceSpan(o["span"], d, "span")); err != nil {
+		if !fortiAPIPatch(o["span"]) {
+			return fmt.Errorf("Error reading span: %v", err)
+		}
+	}
+
+	if err = d.Set("span_direction", flattenSystemSwitchInterfaceSpanDirection(o["span-direction"], d, "span_direction")); err != nil {
+		if !fortiAPIPatch(o["span-direction"]) {
+			return fmt.Errorf("Error reading span_direction: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemSwitchInterfaceFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemSwitchInterfaceName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceVdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceSpanDestPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceSpanSourcePort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "interface_name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["interface-name"], _ = expandSystemSwitchInterfaceSpanSourcePortInterfaceName(d, i["interface_name"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandSystemSwitchInterfaceSpanSourcePortInterfaceName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceMember(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "interface_name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["interface-name"], _ = expandSystemSwitchInterfaceMemberInterfaceName(d, i["interface_name"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandSystemSwitchInterfaceMemberInterfaceName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceIntraSwitchPolicy(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceSpan(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemSwitchInterfaceSpanDirection(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemSwitchInterface(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandSystemSwitchInterfaceName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("vdom"); ok {
+		t, err := expandSystemSwitchInterfaceVdom(d, v, "vdom")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["vdom"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("span_dest_port"); ok {
+		t, err := expandSystemSwitchInterfaceSpanDestPort(d, v, "span_dest_port")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["span-dest-port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("span_source_port"); ok {
+		t, err := expandSystemSwitchInterfaceSpanSourcePort(d, v, "span_source_port")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["span-source-port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("member"); ok {
+		t, err := expandSystemSwitchInterfaceMember(d, v, "member")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["member"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("type"); ok {
+		t, err := expandSystemSwitchInterfaceType(d, v, "type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["type"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("intra_switch_policy"); ok {
+		t, err := expandSystemSwitchInterfaceIntraSwitchPolicy(d, v, "intra_switch_policy")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["intra-switch-policy"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("span"); ok {
+		t, err := expandSystemSwitchInterfaceSpan(d, v, "span")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["span"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("span_direction"); ok {
+		t, err := expandSystemSwitchInterfaceSpanDirection(d, v, "span_direction")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["span-direction"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_system_switchinterface.html.markdown
@@ -0,0 +1,47 @@
+---
+subcategory: "FortiGate System"
+layout: "fortios"
+page_title: "FortiOS: fortios_system_switchinterface"
+description: |-
+  Configure software switch interfaces by grouping physical and WiFi interfaces.
+---
+
+# fortios_system_switchinterface
+Configure software switch interfaces by grouping physical and WiFi interfaces.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) Interface name (name cannot be in use by any other interfaces, VLANs, or inter-VDOM links).
+* `vdom` - VDOM that the software switch belongs to.
+* `span_dest_port` - SPAN destination port name. All traffic on the SPAN source ports is echoed to the SPAN destination port.
+* `span_source_port` - Physical interface name. Port spanning echoes all traffic on the SPAN source ports to the SPAN destination port.
+* `member` - Names of the interfaces that belong to the virtual switch.
+* `type` - Type of switch based on functionality: switch for normal functionality, or hub to duplicate packets to all port members.
+* `intra_switch_policy` - Allow any traffic between switch interfaces or require firewall policies to allow traffic between switch interfaces.
+* `span` - Enable/disable port spanning. Port spanning echoes traffic received by the software switch to the span destination port.
+* `span_direction` - The direction in which the SPAN port operates, either: rx, tx, or both.
+
+The `span_source_port` block supports:
+
+* `interface_name` - Physical interface name.
+
+The `member` block supports:
+
+* `interface_name` - Physical interface name.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+System SwitchInterface can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_system_switchinterface.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
