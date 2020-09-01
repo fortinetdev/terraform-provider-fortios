9a1860b52dbfb6b7e71fd7496fe078a4e5893577
Frank Shen <fshen01@fortinet.com>
2020-09-01 17:34:53 +0800

    Add router_aspathlist

---
+++ fortios/resource_router_aspathlist.go
@@ -0,0 +1,321 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure Autonomous System (AS) path lists.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceRouterAspathList() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceRouterAspathListCreate,
+		Read:   resourceRouterAspathListRead,
+		Update: resourceRouterAspathListUpdate,
+		Delete: resourceRouterAspathListDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Required:     true,
+			},
+			"rule": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"action": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"regexp": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 63),
+							Optional:     true,
+							Computed:     true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceRouterAspathListCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectRouterAspathList(d)
+	if err != nil {
+		return fmt.Errorf("Error creating RouterAspathList resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateRouterAspathList(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating RouterAspathList resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("RouterAspathList")
+	}
+
+	return resourceRouterAspathListRead(d, m)
+}
+
+func resourceRouterAspathListUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectRouterAspathList(d)
+	if err != nil {
+		return fmt.Errorf("Error updating RouterAspathList resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateRouterAspathList(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating RouterAspathList resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("RouterAspathList")
+	}
+
+	return resourceRouterAspathListRead(d, m)
+}
+
+func resourceRouterAspathListDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteRouterAspathList(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting RouterAspathList resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceRouterAspathListRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadRouterAspathList(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading RouterAspathList resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectRouterAspathList(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading RouterAspathList resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenRouterAspathListName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterAspathListRule(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = flattenRouterAspathListRuleId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := i["action"]; ok {
+			tmp["action"] = flattenRouterAspathListRuleAction(i["action"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "regexp"
+		if _, ok := i["regexp"]; ok {
+			tmp["regexp"] = flattenRouterAspathListRuleRegexp(i["regexp"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenRouterAspathListRuleId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterAspathListRuleAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenRouterAspathListRuleRegexp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectRouterAspathList(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenRouterAspathListName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("rule", flattenRouterAspathListRule(o["rule"], d, "rule")); err != nil {
+			if !fortiAPIPatch(o["rule"]) {
+				return fmt.Errorf("Error reading rule: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("rule"); ok {
+			if err = d.Set("rule", flattenRouterAspathListRule(o["rule"], d, "rule")); err != nil {
+				if !fortiAPIPatch(o["rule"]) {
+					return fmt.Errorf("Error reading rule: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenRouterAspathListFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandRouterAspathListName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterAspathListRule(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandRouterAspathListRuleId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["action"], _ = expandRouterAspathListRuleAction(d, i["action"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "regexp"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["regexp"], _ = expandRouterAspathListRuleRegexp(d, i["regexp"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandRouterAspathListRuleId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterAspathListRuleAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandRouterAspathListRuleRegexp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectRouterAspathList(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandRouterAspathListName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("rule"); ok {
+		t, err := expandRouterAspathListRule(d, v, "rule")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["rule"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_router_aspathlist.html.markdown
@@ -0,0 +1,51 @@
+---
+subcategory: "FortiGate Router"
+layout: "fortios"
+page_title: "FortiOS: fortios_router_aspathlist"
+description: |-
+  Configure Autonomous System (AS) path lists.
+---
+
+# fortios_router_aspathlist
+Configure Autonomous System (AS) path lists.
+
+## Example Usage
+
+```hcl
+resource "fortios_router_aspathlist" "trname" {
+    name = "aspath1"
+
+    rule {
+        action = "deny"
+        regexp = "/d+/n"
+    }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) AS path list name.
+* `rule` - AS path list rule.
+
+The `rule` block supports:
+
+* `id` - ID.
+* `action` - Permit or deny route-based operations, based on the route's AS_PATH attribute.
+* `regexp` - Regular-expression to match the Border Gateway Protocol (BGP) AS paths.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+Router AspathList can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_router_aspathlist.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
