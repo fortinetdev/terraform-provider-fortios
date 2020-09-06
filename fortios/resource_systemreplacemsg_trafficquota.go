6087334d8ba6e0518d44498588a2aa3aa0908bbe
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:49:59 +0800

    Add systemreplacemsg_trafficquota

---
+++ fortios/resource_systemreplacemsg_trafficquota.go
@@ -0,0 +1,253 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Replacement messages.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemReplacemsgTrafficQuota() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemReplacemsgTrafficQuotaCreate,
+		Read:   resourceSystemReplacemsgTrafficQuotaRead,
+		Update: resourceSystemReplacemsgTrafficQuotaUpdate,
+		Delete: resourceSystemReplacemsgTrafficQuotaDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"msg_type": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 28),
+				Required:     true,
+			},
+			"buffer": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 32768),
+				Optional:     true,
+			},
+			"header": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"format": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemReplacemsgTrafficQuotaCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemReplacemsgTrafficQuota(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemReplacemsgTrafficQuota resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSystemReplacemsgTrafficQuota(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemReplacemsgTrafficQuota resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemReplacemsgTrafficQuota")
+	}
+
+	return resourceSystemReplacemsgTrafficQuotaRead(d, m)
+}
+
+func resourceSystemReplacemsgTrafficQuotaUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemReplacemsgTrafficQuota(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemReplacemsgTrafficQuota resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSystemReplacemsgTrafficQuota(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemReplacemsgTrafficQuota resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemReplacemsgTrafficQuota")
+	}
+
+	return resourceSystemReplacemsgTrafficQuotaRead(d, m)
+}
+
+func resourceSystemReplacemsgTrafficQuotaDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSystemReplacemsgTrafficQuota(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemReplacemsgTrafficQuota resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemReplacemsgTrafficQuotaRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSystemReplacemsgTrafficQuota(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemReplacemsgTrafficQuota resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemReplacemsgTrafficQuota(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemReplacemsgTrafficQuota resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemReplacemsgTrafficQuotaMsgType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgTrafficQuotaBuffer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgTrafficQuotaHeader(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgTrafficQuotaFormat(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSystemReplacemsgTrafficQuota(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("msg_type", flattenSystemReplacemsgTrafficQuotaMsgType(o["msg-type"], d, "msg_type")); err != nil {
+		if !fortiAPIPatch(o["msg-type"]) {
+			return fmt.Errorf("Error reading msg_type: %v", err)
+		}
+	}
+
+	if err = d.Set("buffer", flattenSystemReplacemsgTrafficQuotaBuffer(o["buffer"], d, "buffer")); err != nil {
+		if !fortiAPIPatch(o["buffer"]) {
+			return fmt.Errorf("Error reading buffer: %v", err)
+		}
+	}
+
+	if err = d.Set("header", flattenSystemReplacemsgTrafficQuotaHeader(o["header"], d, "header")); err != nil {
+		if !fortiAPIPatch(o["header"]) {
+			return fmt.Errorf("Error reading header: %v", err)
+		}
+	}
+
+	if err = d.Set("format", flattenSystemReplacemsgTrafficQuotaFormat(o["format"], d, "format")); err != nil {
+		if !fortiAPIPatch(o["format"]) {
+			return fmt.Errorf("Error reading format: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemReplacemsgTrafficQuotaFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemReplacemsgTrafficQuotaMsgType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgTrafficQuotaBuffer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgTrafficQuotaHeader(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgTrafficQuotaFormat(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemReplacemsgTrafficQuota(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("msg_type"); ok {
+		t, err := expandSystemReplacemsgTrafficQuotaMsgType(d, v, "msg_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["msg-type"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("buffer"); ok {
+		t, err := expandSystemReplacemsgTrafficQuotaBuffer(d, v, "buffer")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["buffer"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("header"); ok {
+		t, err := expandSystemReplacemsgTrafficQuotaHeader(d, v, "header")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["header"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("format"); ok {
+		t, err := expandSystemReplacemsgTrafficQuotaFormat(d, v, "format")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["format"] = t
+		}
+	}
+
+	return &obj, nil
+}
