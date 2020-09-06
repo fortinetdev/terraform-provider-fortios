b3020def8bd95a1add472ef9213291f5abaf2d67
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:48:37 +0800

    Add system_vdomradiusserver

---
+++ fortios/resource_system_vdomradiusserver.go
@@ -0,0 +1,226 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure a RADIUS server to use as a RADIUS Single Sign On (RSSO) server for this VDOM.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemVdomRadiusServer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemVdomRadiusServerCreate,
+		Read:   resourceSystemVdomRadiusServerRead,
+		Update: resourceSystemVdomRadiusServerUpdate,
+		Delete: resourceSystemVdomRadiusServerDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 31),
+				Optional:     true,
+				Computed:     true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"radius_server_vdom": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 31),
+				Required:     true,
+			},
+		},
+	}
+}
+
+func resourceSystemVdomRadiusServerCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemVdomRadiusServer(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemVdomRadiusServer resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSystemVdomRadiusServer(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemVdomRadiusServer resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemVdomRadiusServer")
+	}
+
+	return resourceSystemVdomRadiusServerRead(d, m)
+}
+
+func resourceSystemVdomRadiusServerUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemVdomRadiusServer(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemVdomRadiusServer resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSystemVdomRadiusServer(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemVdomRadiusServer resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemVdomRadiusServer")
+	}
+
+	return resourceSystemVdomRadiusServerRead(d, m)
+}
+
+func resourceSystemVdomRadiusServerDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSystemVdomRadiusServer(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemVdomRadiusServer resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemVdomRadiusServerRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSystemVdomRadiusServer(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemVdomRadiusServer resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemVdomRadiusServer(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemVdomRadiusServer resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemVdomRadiusServerName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemVdomRadiusServerStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemVdomRadiusServerRadiusServerVdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSystemVdomRadiusServer(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenSystemVdomRadiusServerName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenSystemVdomRadiusServerStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("radius_server_vdom", flattenSystemVdomRadiusServerRadiusServerVdom(o["radius-server-vdom"], d, "radius_server_vdom")); err != nil {
+		if !fortiAPIPatch(o["radius-server-vdom"]) {
+			return fmt.Errorf("Error reading radius_server_vdom: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemVdomRadiusServerFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemVdomRadiusServerName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemVdomRadiusServerStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemVdomRadiusServerRadiusServerVdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemVdomRadiusServer(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandSystemVdomRadiusServerName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandSystemVdomRadiusServerStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("radius_server_vdom"); ok {
+		t, err := expandSystemVdomRadiusServerRadiusServerVdom(d, v, "radius_server_vdom")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["radius-server-vdom"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_system_vdomradiusserver.html.markdown
@@ -0,0 +1,33 @@
+---
+subcategory: "FortiGate System"
+layout: "fortios"
+page_title: "FortiOS: fortios_system_vdomradiusserver"
+description: |-
+  Configure a RADIUS server to use as a RADIUS Single Sign On (RSSO) server for this VDOM.
+---
+
+# fortios_system_vdomradiusserver
+Configure a RADIUS server to use as a RADIUS Single Sign On (RSSO) server for this VDOM.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - Name of the VDOM that you are adding the RADIUS server to.
+* `status` - Enable/disable the RSSO RADIUS server for this VDOM.
+* `radius_server_vdom` - (Required) Use this option to select another VDOM containing a VDOM RSSO RADIUS server to use for the current VDOM.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+System VdomRadiusServer can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_system_vdomradiusserver.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
