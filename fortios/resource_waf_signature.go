e0557f12c96971bc0cc617a1bdcf07b979124502
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:51:11 +0800

    Add waf_signature

---
+++ fortios/resource_waf_signature.go
@@ -0,0 +1,198 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Hidden table for datasource.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceWafSignature() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceWafSignatureCreate,
+		Read:   resourceWafSignatureRead,
+		Update: resourceWafSignatureUpdate,
+		Delete: resourceWafSignatureDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"desc": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 511),
+				Optional:     true,
+				Computed:     true,
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceWafSignatureCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWafSignature(d)
+	if err != nil {
+		return fmt.Errorf("Error creating WafSignature resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateWafSignature(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating WafSignature resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("WafSignature")
+	}
+
+	return resourceWafSignatureRead(d, m)
+}
+
+func resourceWafSignatureUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWafSignature(d)
+	if err != nil {
+		return fmt.Errorf("Error updating WafSignature resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateWafSignature(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating WafSignature resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("WafSignature")
+	}
+
+	return resourceWafSignatureRead(d, m)
+}
+
+func resourceWafSignatureDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteWafSignature(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting WafSignature resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceWafSignatureRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadWafSignature(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading WafSignature resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectWafSignature(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading WafSignature resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenWafSignatureDesc(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWafSignatureId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectWafSignature(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("desc", flattenWafSignatureDesc(o["desc"], d, "desc")); err != nil {
+		if !fortiAPIPatch(o["desc"]) {
+			return fmt.Errorf("Error reading desc: %v", err)
+		}
+	}
+
+	if err = d.Set("fosid", flattenWafSignatureId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenWafSignatureFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandWafSignatureDesc(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWafSignatureId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectWafSignature(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("desc"); ok {
+		t, err := expandWafSignatureDesc(d, v, "desc")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["desc"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandWafSignatureId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_waf_signature.html.markdown
@@ -0,0 +1,32 @@
+---
+subcategory: "FortiGate WAF"
+layout: "fortios"
+page_title: "FortiOS: fortios_waf_signature"
+description: |-
+  Hidden table for datasource.
+---
+
+# fortios_waf_signature
+Hidden table for datasource.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `desc` - Signature description.
+* `fosid` - Signature ID.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{fosid}}.
+
+## Import
+
+Waf Signature can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_waf_signature.labelname {{fosid}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
