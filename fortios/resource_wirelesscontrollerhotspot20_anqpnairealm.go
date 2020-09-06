416c3ab026c9b5b91187fe6da219843b15825304
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:52:43 +0800

    Add wirelesscontrollerhotspot20_anqpnairealm

---
+++ fortios/resource_wirelesscontrollerhotspot20_anqpnairealm.go
@@ -0,0 +1,578 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure network access identifier (NAI) realm.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceWirelessControllerHotspot20AnqpNaiRealm() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceWirelessControllerHotspot20AnqpNaiRealmCreate,
+		Read:   resourceWirelessControllerHotspot20AnqpNaiRealmRead,
+		Update: resourceWirelessControllerHotspot20AnqpNaiRealmUpdate,
+		Delete: resourceWirelessControllerHotspot20AnqpNaiRealmDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Optional:     true,
+				Computed:     true,
+			},
+			"nai_list": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 35),
+							Optional:     true,
+							Computed:     true,
+						},
+						"encoding": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"nai_realm": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 255),
+							Optional:     true,
+							Computed:     true,
+						},
+						"eap_method": &schema.Schema{
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"index": &schema.Schema{
+										Type:         schema.TypeInt,
+										ValidateFunc: validation.IntBetween(1, 5),
+										Optional:     true,
+										Computed:     true,
+									},
+									"method": &schema.Schema{
+										Type:     schema.TypeString,
+										Optional: true,
+										Computed: true,
+									},
+									"auth_param": &schema.Schema{
+										Type:     schema.TypeList,
+										Optional: true,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"index": &schema.Schema{
+													Type:         schema.TypeInt,
+													ValidateFunc: validation.IntBetween(1, 4),
+													Optional:     true,
+													Computed:     true,
+												},
+												"id": &schema.Schema{
+													Type:     schema.TypeString,
+													Optional: true,
+													Computed: true,
+												},
+												"val": &schema.Schema{
+													Type:     schema.TypeString,
+													Optional: true,
+													Computed: true,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceWirelessControllerHotspot20AnqpNaiRealmCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWirelessControllerHotspot20AnqpNaiRealm(d)
+	if err != nil {
+		return fmt.Errorf("Error creating WirelessControllerHotspot20AnqpNaiRealm resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateWirelessControllerHotspot20AnqpNaiRealm(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating WirelessControllerHotspot20AnqpNaiRealm resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("WirelessControllerHotspot20AnqpNaiRealm")
+	}
+
+	return resourceWirelessControllerHotspot20AnqpNaiRealmRead(d, m)
+}
+
+func resourceWirelessControllerHotspot20AnqpNaiRealmUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWirelessControllerHotspot20AnqpNaiRealm(d)
+	if err != nil {
+		return fmt.Errorf("Error updating WirelessControllerHotspot20AnqpNaiRealm resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateWirelessControllerHotspot20AnqpNaiRealm(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating WirelessControllerHotspot20AnqpNaiRealm resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("WirelessControllerHotspot20AnqpNaiRealm")
+	}
+
+	return resourceWirelessControllerHotspot20AnqpNaiRealmRead(d, m)
+}
+
+func resourceWirelessControllerHotspot20AnqpNaiRealmDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteWirelessControllerHotspot20AnqpNaiRealm(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting WirelessControllerHotspot20AnqpNaiRealm resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceWirelessControllerHotspot20AnqpNaiRealmRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadWirelessControllerHotspot20AnqpNaiRealm(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading WirelessControllerHotspot20AnqpNaiRealm resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectWirelessControllerHotspot20AnqpNaiRealm(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading WirelessControllerHotspot20AnqpNaiRealm resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiList(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListName(i["name"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "encoding"
+		if _, ok := i["encoding"]; ok {
+			tmp["encoding"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEncoding(i["encoding"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "nai_realm"
+		if _, ok := i["nai-realm"]; ok {
+			tmp["nai_realm"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListNaiRealm(i["nai-realm"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "eap_method"
+		if _, ok := i["eap-method"]; ok {
+			tmp["eap_method"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethod(i["eap-method"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEncoding(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListNaiRealm(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethod(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := i["index"]; ok {
+			tmp["index"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodIndex(i["index"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "method"
+		if _, ok := i["method"]; ok {
+			tmp["method"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodMethod(i["method"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "auth_param"
+		if _, ok := i["auth-param"]; ok {
+			tmp["auth_param"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParam(i["auth-param"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodIndex(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodMethod(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParam(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := i["index"]; ok {
+			tmp["index"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamIndex(i["index"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "val"
+		if _, ok := i["val"]; ok {
+			tmp["val"] = flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamVal(i["val"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamIndex(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamVal(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectWirelessControllerHotspot20AnqpNaiRealm(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenWirelessControllerHotspot20AnqpNaiRealmName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("nai_list", flattenWirelessControllerHotspot20AnqpNaiRealmNaiList(o["nai-list"], d, "nai_list")); err != nil {
+			if !fortiAPIPatch(o["nai-list"]) {
+				return fmt.Errorf("Error reading nai_list: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("nai_list"); ok {
+			if err = d.Set("nai_list", flattenWirelessControllerHotspot20AnqpNaiRealmNaiList(o["nai-list"], d, "nai_list")); err != nil {
+				if !fortiAPIPatch(o["nai-list"]) {
+					return fmt.Errorf("Error reading nai_list: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenWirelessControllerHotspot20AnqpNaiRealmFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiList(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "encoding"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["encoding"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEncoding(d, i["encoding"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "nai_realm"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["nai-realm"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListNaiRealm(d, i["nai_realm"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "eap_method"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["eap-method"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethod(d, i["eap_method"], pre_append)
+		} else {
+			tmp["eap-method"] = make([]string, 0)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEncoding(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListNaiRealm(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethod(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["index"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodIndex(d, i["index"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "method"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["method"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodMethod(d, i["method"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "auth_param"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["auth-param"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParam(d, i["auth_param"], pre_append)
+		} else {
+			tmp["auth-param"] = make([]string, 0)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodIndex(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodMethod(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParam(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["index"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamIndex(d, i["index"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "val"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["val"], _ = expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamVal(d, i["val"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamIndex(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerHotspot20AnqpNaiRealmNaiListEapMethodAuthParamVal(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectWirelessControllerHotspot20AnqpNaiRealm(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandWirelessControllerHotspot20AnqpNaiRealmName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("nai_list"); ok {
+		t, err := expandWirelessControllerHotspot20AnqpNaiRealmNaiList(d, v, "nai_list")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["nai-list"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_wirelesscontrollerhotspot20_anqpnairealm.html.markdown
@@ -0,0 +1,51 @@
+---
+subcategory: "FortiGate Wireless-Controller"
+layout: "fortios"
+page_title: "FortiOS: fortios_wirelesscontrollerhotspot20_anqpnairealm"
+description: |-
+  Configure network access identifier (NAI) realm.
+---
+
+# fortios_wirelesscontrollerhotspot20_anqpnairealm
+Configure network access identifier (NAI) realm.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - NAI realm list name.
+* `nai_list` - NAI list.
+
+The `nai_list` block supports:
+
+* `name` - NAI realm name.
+* `encoding` - Enable/disable format in accordance with IETF RFC 4282.
+* `nai_realm` - Configure NAI realms (delimited by a semi-colon character).
+* `eap_method` - EAP Methods.
+
+The `eap_method` block supports:
+
+* `index` - EAP method index.
+* `method` - EAP method type.
+* `auth_param` - EAP auth param.
+
+The `auth_param` block supports:
+
+* `index` - Param index.
+* `id` - ID of authentication parameter.
+* `val` - Value of authentication parameter.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+WirelessControllerHotspot20 AnqpNaiRealm can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_wirelesscontrollerhotspot20_anqpnairealm.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
