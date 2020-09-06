bd332ad4f6a3982d5d2d40de1d6571c4ea888a14
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:46:10 +0800

    Add logsyslogd_overridesetting

---
+++ fortios/resource_logsyslogd_overridesetting.go
@@ -0,0 +1,611 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Override settings for remote syslog server.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceLogSyslogdOverrideSetting() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLogSyslogdOverrideSettingUpdate,
+		Read:   resourceLogSyslogdOverrideSettingRead,
+		Update: resourceLogSyslogdOverrideSettingUpdate,
+		Delete: resourceLogSyslogdOverrideSettingDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"override": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"server": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional:     true,
+				Computed:     true,
+			},
+			"mode": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"port": &schema.Schema{
+				Type:         schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 65535),
+				Optional:     true,
+				Computed:     true,
+			},
+			"facility": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional:     true,
+				Computed:     true,
+			},
+			"format": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"enc_algorithm": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ssl_min_proto_version": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"certificate": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Optional:     true,
+				Computed:     true,
+			},
+			"custom_field_name": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:         schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 255),
+							Optional:     true,
+							Computed:     true,
+						},
+						"name": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 35),
+							Optional:     true,
+							Computed:     true,
+						},
+						"custom": &schema.Schema{
+							Type:         schema.TypeString,
+							ValidateFunc: validation.StringLenBetween(0, 35),
+							Optional:     true,
+							Computed:     true,
+						},
+					},
+				},
+			},
+			"syslog_type": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceLogSyslogdOverrideSettingUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectLogSyslogdOverrideSetting(d)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogdOverrideSetting resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateLogSyslogdOverrideSetting(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating LogSyslogdOverrideSetting resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("LogSyslogdOverrideSetting")
+	}
+
+	return resourceLogSyslogdOverrideSettingRead(d, m)
+}
+
+func resourceLogSyslogdOverrideSettingDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteLogSyslogdOverrideSetting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting LogSyslogdOverrideSetting resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceLogSyslogdOverrideSettingRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadLogSyslogdOverrideSetting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogdOverrideSetting resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectLogSyslogdOverrideSetting(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading LogSyslogdOverrideSetting resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenLogSyslogdOverrideSettingOverride(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingMode(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingFacility(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingFormat(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingEncAlgorithm(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingSslMinProtoVersion(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingCertificate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingCustomFieldName(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = flattenLogSyslogdOverrideSettingCustomFieldNameId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = flattenLogSyslogdOverrideSettingCustomFieldNameName(i["name"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "custom"
+		if _, ok := i["custom"]; ok {
+			tmp["custom"] = flattenLogSyslogdOverrideSettingCustomFieldNameCustom(i["custom"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenLogSyslogdOverrideSettingCustomFieldNameId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingCustomFieldNameName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingCustomFieldNameCustom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenLogSyslogdOverrideSettingSyslogType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectLogSyslogdOverrideSetting(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("override", flattenLogSyslogdOverrideSettingOverride(o["override"], d, "override")); err != nil {
+		if !fortiAPIPatch(o["override"]) {
+			return fmt.Errorf("Error reading override: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenLogSyslogdOverrideSettingStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("server", flattenLogSyslogdOverrideSettingServer(o["server"], d, "server")); err != nil {
+		if !fortiAPIPatch(o["server"]) {
+			return fmt.Errorf("Error reading server: %v", err)
+		}
+	}
+
+	if err = d.Set("mode", flattenLogSyslogdOverrideSettingMode(o["mode"], d, "mode")); err != nil {
+		if !fortiAPIPatch(o["mode"]) {
+			return fmt.Errorf("Error reading mode: %v", err)
+		}
+	}
+
+	if err = d.Set("port", flattenLogSyslogdOverrideSettingPort(o["port"], d, "port")); err != nil {
+		if !fortiAPIPatch(o["port"]) {
+			return fmt.Errorf("Error reading port: %v", err)
+		}
+	}
+
+	if err = d.Set("facility", flattenLogSyslogdOverrideSettingFacility(o["facility"], d, "facility")); err != nil {
+		if !fortiAPIPatch(o["facility"]) {
+			return fmt.Errorf("Error reading facility: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", flattenLogSyslogdOverrideSettingSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("format", flattenLogSyslogdOverrideSettingFormat(o["format"], d, "format")); err != nil {
+		if !fortiAPIPatch(o["format"]) {
+			return fmt.Errorf("Error reading format: %v", err)
+		}
+	}
+
+	if err = d.Set("enc_algorithm", flattenLogSyslogdOverrideSettingEncAlgorithm(o["enc-algorithm"], d, "enc_algorithm")); err != nil {
+		if !fortiAPIPatch(o["enc-algorithm"]) {
+			return fmt.Errorf("Error reading enc_algorithm: %v", err)
+		}
+	}
+
+	if err = d.Set("ssl_min_proto_version", flattenLogSyslogdOverrideSettingSslMinProtoVersion(o["ssl-min-proto-version"], d, "ssl_min_proto_version")); err != nil {
+		if !fortiAPIPatch(o["ssl-min-proto-version"]) {
+			return fmt.Errorf("Error reading ssl_min_proto_version: %v", err)
+		}
+	}
+
+	if err = d.Set("certificate", flattenLogSyslogdOverrideSettingCertificate(o["certificate"], d, "certificate")); err != nil {
+		if !fortiAPIPatch(o["certificate"]) {
+			return fmt.Errorf("Error reading certificate: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("custom_field_name", flattenLogSyslogdOverrideSettingCustomFieldName(o["custom-field-name"], d, "custom_field_name")); err != nil {
+			if !fortiAPIPatch(o["custom-field-name"]) {
+				return fmt.Errorf("Error reading custom_field_name: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("custom_field_name"); ok {
+			if err = d.Set("custom_field_name", flattenLogSyslogdOverrideSettingCustomFieldName(o["custom-field-name"], d, "custom_field_name")); err != nil {
+				if !fortiAPIPatch(o["custom-field-name"]) {
+					return fmt.Errorf("Error reading custom_field_name: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("syslog_type", flattenLogSyslogdOverrideSettingSyslogType(o["syslog-type"], d, "syslog_type")); err != nil {
+		if !fortiAPIPatch(o["syslog-type"]) {
+			return fmt.Errorf("Error reading syslog_type: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenLogSyslogdOverrideSettingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandLogSyslogdOverrideSettingOverride(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingMode(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingFacility(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingSourceIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingFormat(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingEncAlgorithm(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingSslMinProtoVersion(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingCertificate(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingCustomFieldName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandLogSyslogdOverrideSettingCustomFieldNameId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandLogSyslogdOverrideSettingCustomFieldNameName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "custom"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["custom"], _ = expandLogSyslogdOverrideSettingCustomFieldNameCustom(d, i["custom"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandLogSyslogdOverrideSettingCustomFieldNameId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingCustomFieldNameName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingCustomFieldNameCustom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogSyslogdOverrideSettingSyslogType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectLogSyslogdOverrideSetting(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("override"); ok {
+		t, err := expandLogSyslogdOverrideSettingOverride(d, v, "override")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["override"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandLogSyslogdOverrideSettingStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("server"); ok {
+		t, err := expandLogSyslogdOverrideSettingServer(d, v, "server")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["server"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("mode"); ok {
+		t, err := expandLogSyslogdOverrideSettingMode(d, v, "mode")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["mode"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("port"); ok {
+		t, err := expandLogSyslogdOverrideSettingPort(d, v, "port")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("facility"); ok {
+		t, err := expandLogSyslogdOverrideSettingFacility(d, v, "facility")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["facility"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("source_ip"); ok {
+		t, err := expandLogSyslogdOverrideSettingSourceIp(d, v, "source_ip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["source-ip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("format"); ok {
+		t, err := expandLogSyslogdOverrideSettingFormat(d, v, "format")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["format"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("enc_algorithm"); ok {
+		t, err := expandLogSyslogdOverrideSettingEncAlgorithm(d, v, "enc_algorithm")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["enc-algorithm"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ssl_min_proto_version"); ok {
+		t, err := expandLogSyslogdOverrideSettingSslMinProtoVersion(d, v, "ssl_min_proto_version")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ssl-min-proto-version"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("certificate"); ok {
+		t, err := expandLogSyslogdOverrideSettingCertificate(d, v, "certificate")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["certificate"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("custom_field_name"); ok {
+		t, err := expandLogSyslogdOverrideSettingCustomFieldName(d, v, "custom_field_name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["custom-field-name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("syslog_type"); ok {
+		t, err := expandLogSyslogdOverrideSettingSyslogType(d, v, "syslog_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["syslog-type"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_logsyslogd_overridesetting.html.markdown
@@ -0,0 +1,49 @@
+---
+subcategory: "FortiGate Log"
+layout: "fortios"
+page_title: "FortiOS: fortios_logsyslogd_overridesetting"
+description: |-
+  Override settings for remote syslog server.
+---
+
+# fortios_logsyslogd_overridesetting
+Override settings for remote syslog server.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `override` - Enable/disable override syslog settings.
+* `status` - Enable/disable remote syslog logging.
+* `server` - Address of remote syslog server.
+* `mode` - Remote syslog logging over UDP/Reliable TCP.
+* `port` - Server listen port.
+* `facility` - Remote syslog facility.
+* `source_ip` - Source IP address of syslog.
+* `format` - Log format.
+* `enc_algorithm` - Enable/disable reliable syslogging with TLS encryption.
+* `ssl_min_proto_version` - Minimum supported protocol version for SSL/TLS connections (default is to follow system global setting).
+* `certificate` - Certificate used to communicate with Syslog server.
+* `custom_field_name` - Custom field name for CEF format logging.
+* `syslog_type` - Hidden setting index of Syslog.
+
+The `custom_field_name` block supports:
+
+* `id` - Entry ID.
+* `name` - Field name.
+* `custom` - Field custom name.
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource.
+
+## Import
+
+LogSyslogd OverrideSetting can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_logsyslogd_overridesetting.labelname LogSyslogdOverrideSetting
+$ unset "FORTIOS_IMPORT_TABLE"
+```
