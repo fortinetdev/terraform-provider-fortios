7dc9061e5af293c0e8933bac896b880ed9f9c2d3
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:49:23 +0800

    Add systemreplacemsg_http

---
+++ fortios/resource_systemreplacemsg_http.go
@@ -0,0 +1,253 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Replacement messages.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemReplacemsgHttp() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemReplacemsgHttpCreate,
+		Read:   resourceSystemReplacemsgHttpRead,
+		Update: resourceSystemReplacemsgHttpUpdate,
+		Delete: resourceSystemReplacemsgHttpDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"msg_type": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 28),
+				Required:     true,
+			},
+			"buffer": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 32768),
+				Optional:     true,
+			},
+			"header": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"format": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemReplacemsgHttpCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemReplacemsgHttp(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemReplacemsgHttp resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSystemReplacemsgHttp(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemReplacemsgHttp resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemReplacemsgHttp")
+	}
+
+	return resourceSystemReplacemsgHttpRead(d, m)
+}
+
+func resourceSystemReplacemsgHttpUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemReplacemsgHttp(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemReplacemsgHttp resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSystemReplacemsgHttp(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemReplacemsgHttp resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemReplacemsgHttp")
+	}
+
+	return resourceSystemReplacemsgHttpRead(d, m)
+}
+
+func resourceSystemReplacemsgHttpDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSystemReplacemsgHttp(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemReplacemsgHttp resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemReplacemsgHttpRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSystemReplacemsgHttp(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemReplacemsgHttp resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemReplacemsgHttp(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemReplacemsgHttp resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemReplacemsgHttpMsgType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgHttpBuffer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgHttpHeader(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgHttpFormat(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSystemReplacemsgHttp(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("msg_type", flattenSystemReplacemsgHttpMsgType(o["msg-type"], d, "msg_type")); err != nil {
+		if !fortiAPIPatch(o["msg-type"]) {
+			return fmt.Errorf("Error reading msg_type: %v", err)
+		}
+	}
+
+	if err = d.Set("buffer", flattenSystemReplacemsgHttpBuffer(o["buffer"], d, "buffer")); err != nil {
+		if !fortiAPIPatch(o["buffer"]) {
+			return fmt.Errorf("Error reading buffer: %v", err)
+		}
+	}
+
+	if err = d.Set("header", flattenSystemReplacemsgHttpHeader(o["header"], d, "header")); err != nil {
+		if !fortiAPIPatch(o["header"]) {
+			return fmt.Errorf("Error reading header: %v", err)
+		}
+	}
+
+	if err = d.Set("format", flattenSystemReplacemsgHttpFormat(o["format"], d, "format")); err != nil {
+		if !fortiAPIPatch(o["format"]) {
+			return fmt.Errorf("Error reading format: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemReplacemsgHttpFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemReplacemsgHttpMsgType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgHttpBuffer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgHttpHeader(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgHttpFormat(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemReplacemsgHttp(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("msg_type"); ok {
+		t, err := expandSystemReplacemsgHttpMsgType(d, v, "msg_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["msg-type"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("buffer"); ok {
+		t, err := expandSystemReplacemsgHttpBuffer(d, v, "buffer")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["buffer"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("header"); ok {
+		t, err := expandSystemReplacemsgHttpHeader(d, v, "header")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["header"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("format"); ok {
+		t, err := expandSystemReplacemsgHttpFormat(d, v, "format")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["format"] = t
+		}
+	}
+
+	return &obj, nil
+}
