4a4d4aeba4fb40df696318f8780cf6b948873360
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:51:16 +0800

    Add waf_subclass

---
+++ fortios/resource_waf_subclass.go
@@ -0,0 +1,198 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Hidden table for datasource.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceWafSubClass() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceWafSubClassCreate,
+		Read:   resourceWafSubClassRead,
+		Update: resourceWafSubClassUpdate,
+		Delete: resourceWafSubClassDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 127),
+				Optional:     true,
+				Computed:     true,
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceWafSubClassCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWafSubClass(d)
+	if err != nil {
+		return fmt.Errorf("Error creating WafSubClass resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateWafSubClass(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating WafSubClass resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("WafSubClass")
+	}
+
+	return resourceWafSubClassRead(d, m)
+}
+
+func resourceWafSubClassUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWafSubClass(d)
+	if err != nil {
+		return fmt.Errorf("Error updating WafSubClass resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateWafSubClass(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating WafSubClass resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("WafSubClass")
+	}
+
+	return resourceWafSubClassRead(d, m)
+}
+
+func resourceWafSubClassDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteWafSubClass(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting WafSubClass resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceWafSubClassRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadWafSubClass(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading WafSubClass resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectWafSubClass(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading WafSubClass resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenWafSubClassName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenWafSubClassId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectWafSubClass(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenWafSubClassName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("fosid", flattenWafSubClassId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenWafSubClassFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandWafSubClassName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWafSubClassId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectWafSubClass(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandWafSubClassName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandWafSubClassId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	return &obj, nil
+}
