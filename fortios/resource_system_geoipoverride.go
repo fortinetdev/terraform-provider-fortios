bec3e621e12b3013b486dda36750773502fcac82
Frank Shen <fshen01@fortinet.com>
2020-09-07 02:48:02 +0800

    Add system_geoipoverride

---
+++ fortios/resource_system_geoipoverride.go
@@ -0,0 +1,379 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Configure geographical location mapping for IP address(es) to override mappings from FortiGuard.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemGeoipOverride() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemGeoipOverrideCreate,
+		Read:   resourceSystemGeoipOverrideRead,
+		Update: resourceSystemGeoipOverrideUpdate,
+		Delete: resourceSystemGeoipOverrideDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Required:     true,
+			},
+			"description": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 127),
+				Optional:     true,
+				Computed:     true,
+			},
+			"country_id": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 2),
+				Optional:     true,
+				Computed:     true,
+			},
+			"ip_range": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:         schema.TypeInt,
+							ValidateFunc: validation.IntBetween(0, 65535),
+							Optional:     true,
+							Computed:     true,
+						},
+						"start_ip": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"end_ip": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceSystemGeoipOverrideCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemGeoipOverride(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemGeoipOverride resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSystemGeoipOverride(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemGeoipOverride resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemGeoipOverride")
+	}
+
+	return resourceSystemGeoipOverrideRead(d, m)
+}
+
+func resourceSystemGeoipOverrideUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemGeoipOverride(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemGeoipOverride resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSystemGeoipOverride(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemGeoipOverride resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemGeoipOverride")
+	}
+
+	return resourceSystemGeoipOverrideRead(d, m)
+}
+
+func resourceSystemGeoipOverrideDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSystemGeoipOverride(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemGeoipOverride resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemGeoipOverrideRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSystemGeoipOverride(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemGeoipOverride resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemGeoipOverride(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemGeoipOverride resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemGeoipOverrideName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGeoipOverrideDescription(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGeoipOverrideCountryId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGeoipOverrideIpRange(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = flattenSystemGeoipOverrideIpRangeId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "start_ip"
+		if _, ok := i["start-ip"]; ok {
+			tmp["start_ip"] = flattenSystemGeoipOverrideIpRangeStartIp(i["start-ip"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "end_ip"
+		if _, ok := i["end-ip"]; ok {
+			tmp["end_ip"] = flattenSystemGeoipOverrideIpRangeEndIp(i["end-ip"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenSystemGeoipOverrideIpRangeId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGeoipOverrideIpRangeStartIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGeoipOverrideIpRangeEndIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSystemGeoipOverride(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenSystemGeoipOverrideName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("description", flattenSystemGeoipOverrideDescription(o["description"], d, "description")); err != nil {
+		if !fortiAPIPatch(o["description"]) {
+			return fmt.Errorf("Error reading description: %v", err)
+		}
+	}
+
+	if err = d.Set("country_id", flattenSystemGeoipOverrideCountryId(o["country-id"], d, "country_id")); err != nil {
+		if !fortiAPIPatch(o["country-id"]) {
+			return fmt.Errorf("Error reading country_id: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("ip_range", flattenSystemGeoipOverrideIpRange(o["ip-range"], d, "ip_range")); err != nil {
+			if !fortiAPIPatch(o["ip-range"]) {
+				return fmt.Errorf("Error reading ip_range: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("ip_range"); ok {
+			if err = d.Set("ip_range", flattenSystemGeoipOverrideIpRange(o["ip-range"], d, "ip_range")); err != nil {
+				if !fortiAPIPatch(o["ip-range"]) {
+					return fmt.Errorf("Error reading ip_range: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemGeoipOverrideFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemGeoipOverrideName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGeoipOverrideDescription(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGeoipOverrideCountryId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGeoipOverrideIpRange(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandSystemGeoipOverrideIpRangeId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "start_ip"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["start-ip"], _ = expandSystemGeoipOverrideIpRangeStartIp(d, i["start_ip"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "end_ip"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["end-ip"], _ = expandSystemGeoipOverrideIpRangeEndIp(d, i["end_ip"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandSystemGeoipOverrideIpRangeId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGeoipOverrideIpRangeStartIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGeoipOverrideIpRangeEndIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemGeoipOverride(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandSystemGeoipOverrideName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		t, err := expandSystemGeoipOverrideDescription(d, v, "description")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["description"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("country_id"); ok {
+		t, err := expandSystemGeoipOverrideCountryId(d, v, "country_id")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["country-id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ip_range"); ok {
+		t, err := expandSystemGeoipOverrideIpRange(d, v, "ip_range")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ip-range"] = t
+		}
+	}
+
+	return &obj, nil
+}
---
+++ website/docs/r/fortios_system_geoipoverride.html.markdown
@@ -0,0 +1,49 @@
+---
+subcategory: "FortiGate System"
+layout: "fortios"
+page_title: "FortiOS: fortios_system_geoipoverride"
+description: |-
+  Configure geographical location mapping for IP address(es) to override mappings from FortiGuard.
+---
+
+# fortios_system_geoipoverride
+Configure geographical location mapping for IP address(es) to override mappings from FortiGuard.
+
+## Example Usage
+
+```hcl
+resource "fortios_system_geoipoverride" "trname" {
+  description = "TEST for country"
+  name        = "1"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) Location name.
+* `description` - Description.
+* `country_id` - Two character Country ID code.
+* `ip_range` - Table of IP ranges assigned to country.
+
+The `ip_range` block supports:
+
+* `id` - ID number for individual entry in the IP-Range table.
+* `start_ip` - Starting IP address, inclusive, of the address range (format: xxx.xxx.xxx.xxx).
+* `end_ip` - Final IP address, inclusive, of the address range (format: xxx.xxx.xxx.xxx).
+
+
+## Attribute Reference
+
+In addition to all the above arguments, the following attributes are exported:
+* `id` - an identifier for the resource with format {{name}}.
+
+## Import
+
+System GeoipOverride can be imported using any of these accepted formats:
+```
+$ export "FORTIOS_IMPORT_TABLE"="true"
+$ terraform import fortios_system_geoipoverride.labelname {{name}}
+$ unset "FORTIOS_IMPORT_TABLE"
+```
