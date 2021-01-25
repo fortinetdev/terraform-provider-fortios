eb5d52779fd89cbb36f7e9820e15952d43bf40e0
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:23:02 -0500

    Support data source fortios_firewall_address

---
+++ fortios/data_source_firewall_address.go
@@ -0,0 +1,611 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure IPv4 addresses.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceFirewallAddress() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceFirewallAddressRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"uuid": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"subnet": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"start_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"end_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"fqdn": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"country": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"wildcard_fqdn": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cache_ttl": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"wildcard": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"sdn": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tenant": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"organization": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"epg_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"subnet_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"sdn_tag": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"policy_group": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"visibility": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"associated_interface": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"color": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"filter": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"obj_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"list": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ip": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"tagging": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"category": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"tags": &schema.Schema{
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": &schema.Schema{
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"allow_routing": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceFirewallAddressRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("name")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing FirewallAddress: type error")
+	}
+
+	o, err := c.ReadFirewallAddress(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallAddress: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectFirewallAddress(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallAddress from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenFirewallAddressName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressUuid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressSubnet(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v1, ok := d.GetOkExists(pre); ok && v != nil {
+		if s, ok := v1.(string); ok {
+			v = validateConvIPMask2CIDR(s, v.(string))
+			return v
+		}
+	}
+
+	return v
+}
+
+func dataSourceFlattenFirewallAddressType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressStartIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressEndIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressFqdn(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressCountry(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressWildcardFqdn(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressCacheTtl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressWildcard(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v1, ok := d.GetOkExists(pre); ok && v != nil {
+		if s, ok := v1.(string); ok {
+			v = validateConvIPMask2CIDR(s, v.(string))
+			return v
+		}
+	}
+
+	return v
+}
+
+func dataSourceFlattenFirewallAddressSdn(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressTenant(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressOrganization(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressEpgName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressSubnetName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressSdnTag(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressPolicyGroup(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressVisibility(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressAssociatedInterface(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressColor(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressFilter(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressObjId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressList(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip"
+		if _, ok := i["ip"]; ok {
+			tmp["ip"] = dataSourceFlattenFirewallAddressListIp(i["ip"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenFirewallAddressListIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressTagging(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenFirewallAddressTaggingName(i["name"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "category"
+		if _, ok := i["category"]; ok {
+			tmp["category"] = dataSourceFlattenFirewallAddressTaggingCategory(i["category"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "tags"
+		if _, ok := i["tags"]; ok {
+			tmp["tags"] = dataSourceFlattenFirewallAddressTaggingTags(i["tags"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenFirewallAddressTaggingName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressTaggingCategory(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressTaggingTags(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenFirewallAddressTaggingTagsName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenFirewallAddressTaggingTagsName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallAddressAllowRouting(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectFirewallAddress(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", dataSourceFlattenFirewallAddressName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("uuid", dataSourceFlattenFirewallAddressUuid(o["uuid"], d, "uuid")); err != nil {
+		if !fortiAPIPatch(o["uuid"]) {
+			return fmt.Errorf("Error reading uuid: %v", err)
+		}
+	}
+
+	if err = d.Set("subnet", dataSourceFlattenFirewallAddressSubnet(o["subnet"], d, "subnet")); err != nil {
+		if !fortiAPIPatch(o["subnet"]) {
+			return fmt.Errorf("Error reading subnet: %v", err)
+		}
+	}
+
+	if err = d.Set("type", dataSourceFlattenFirewallAddressType(o["type"], d, "type")); err != nil {
+		if !fortiAPIPatch(o["type"]) {
+			return fmt.Errorf("Error reading type: %v", err)
+		}
+	}
+
+	if err = d.Set("start_ip", dataSourceFlattenFirewallAddressStartIp(o["start-ip"], d, "start_ip")); err != nil {
+		if !fortiAPIPatch(o["start-ip"]) {
+			return fmt.Errorf("Error reading start_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("end_ip", dataSourceFlattenFirewallAddressEndIp(o["end-ip"], d, "end_ip")); err != nil {
+		if !fortiAPIPatch(o["end-ip"]) {
+			return fmt.Errorf("Error reading end_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("fqdn", dataSourceFlattenFirewallAddressFqdn(o["fqdn"], d, "fqdn")); err != nil {
+		if !fortiAPIPatch(o["fqdn"]) {
+			return fmt.Errorf("Error reading fqdn: %v", err)
+		}
+	}
+
+	if err = d.Set("country", dataSourceFlattenFirewallAddressCountry(o["country"], d, "country")); err != nil {
+		if !fortiAPIPatch(o["country"]) {
+			return fmt.Errorf("Error reading country: %v", err)
+		}
+	}
+
+	if err = d.Set("wildcard_fqdn", dataSourceFlattenFirewallAddressWildcardFqdn(o["wildcard-fqdn"], d, "wildcard_fqdn")); err != nil {
+		if !fortiAPIPatch(o["wildcard-fqdn"]) {
+			return fmt.Errorf("Error reading wildcard_fqdn: %v", err)
+		}
+	}
+
+	if err = d.Set("cache_ttl", dataSourceFlattenFirewallAddressCacheTtl(o["cache-ttl"], d, "cache_ttl")); err != nil {
+		if !fortiAPIPatch(o["cache-ttl"]) {
+			return fmt.Errorf("Error reading cache_ttl: %v", err)
+		}
+	}
+
+	if err = d.Set("wildcard", dataSourceFlattenFirewallAddressWildcard(o["wildcard"], d, "wildcard")); err != nil {
+		if !fortiAPIPatch(o["wildcard"]) {
+			return fmt.Errorf("Error reading wildcard: %v", err)
+		}
+	}
+
+	if err = d.Set("sdn", dataSourceFlattenFirewallAddressSdn(o["sdn"], d, "sdn")); err != nil {
+		if !fortiAPIPatch(o["sdn"]) {
+			return fmt.Errorf("Error reading sdn: %v", err)
+		}
+	}
+
+	if err = d.Set("tenant", dataSourceFlattenFirewallAddressTenant(o["tenant"], d, "tenant")); err != nil {
+		if !fortiAPIPatch(o["tenant"]) {
+			return fmt.Errorf("Error reading tenant: %v", err)
+		}
+	}
+
+	if err = d.Set("organization", dataSourceFlattenFirewallAddressOrganization(o["organization"], d, "organization")); err != nil {
+		if !fortiAPIPatch(o["organization"]) {
+			return fmt.Errorf("Error reading organization: %v", err)
+		}
+	}
+
+	if err = d.Set("epg_name", dataSourceFlattenFirewallAddressEpgName(o["epg-name"], d, "epg_name")); err != nil {
+		if !fortiAPIPatch(o["epg-name"]) {
+			return fmt.Errorf("Error reading epg_name: %v", err)
+		}
+	}
+
+	if err = d.Set("subnet_name", dataSourceFlattenFirewallAddressSubnetName(o["subnet-name"], d, "subnet_name")); err != nil {
+		if !fortiAPIPatch(o["subnet-name"]) {
+			return fmt.Errorf("Error reading subnet_name: %v", err)
+		}
+	}
+
+	if err = d.Set("sdn_tag", dataSourceFlattenFirewallAddressSdnTag(o["sdn-tag"], d, "sdn_tag")); err != nil {
+		if !fortiAPIPatch(o["sdn-tag"]) {
+			return fmt.Errorf("Error reading sdn_tag: %v", err)
+		}
+	}
+
+	if err = d.Set("policy_group", dataSourceFlattenFirewallAddressPolicyGroup(o["policy-group"], d, "policy_group")); err != nil {
+		if !fortiAPIPatch(o["policy-group"]) {
+			return fmt.Errorf("Error reading policy_group: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", dataSourceFlattenFirewallAddressComment(o["comment"], d, "comment")); err != nil {
+		if !fortiAPIPatch(o["comment"]) {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("visibility", dataSourceFlattenFirewallAddressVisibility(o["visibility"], d, "visibility")); err != nil {
+		if !fortiAPIPatch(o["visibility"]) {
+			return fmt.Errorf("Error reading visibility: %v", err)
+		}
+	}
+
+	if err = d.Set("associated_interface", dataSourceFlattenFirewallAddressAssociatedInterface(o["associated-interface"], d, "associated_interface")); err != nil {
+		if !fortiAPIPatch(o["associated-interface"]) {
+			return fmt.Errorf("Error reading associated_interface: %v", err)
+		}
+	}
+
+	if err = d.Set("color", dataSourceFlattenFirewallAddressColor(o["color"], d, "color")); err != nil {
+		if !fortiAPIPatch(o["color"]) {
+			return fmt.Errorf("Error reading color: %v", err)
+		}
+	}
+
+	if err = d.Set("filter", dataSourceFlattenFirewallAddressFilter(o["filter"], d, "filter")); err != nil {
+		if !fortiAPIPatch(o["filter"]) {
+			return fmt.Errorf("Error reading filter: %v", err)
+		}
+	}
+
+	if err = d.Set("obj_id", dataSourceFlattenFirewallAddressObjId(o["obj-id"], d, "obj_id")); err != nil {
+		if !fortiAPIPatch(o["obj-id"]) {
+			return fmt.Errorf("Error reading obj_id: %v", err)
+		}
+	}
+
+	if err = d.Set("list", dataSourceFlattenFirewallAddressList(o["list"], d, "list")); err != nil {
+		if !fortiAPIPatch(o["list"]) {
+			return fmt.Errorf("Error reading list: %v", err)
+		}
+	}
+
+	if err = d.Set("tagging", dataSourceFlattenFirewallAddressTagging(o["tagging"], d, "tagging")); err != nil {
+		if !fortiAPIPatch(o["tagging"]) {
+			return fmt.Errorf("Error reading tagging: %v", err)
+		}
+	}
+
+	if err = d.Set("allow_routing", dataSourceFlattenFirewallAddressAllowRouting(o["allow-routing"], d, "allow_routing")); err != nil {
+		if !fortiAPIPatch(o["allow-routing"]) {
+			return fmt.Errorf("Error reading allow_routing: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenFirewallAddressFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
