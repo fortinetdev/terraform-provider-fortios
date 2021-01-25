c64abadbd7670fc97a0bfd42552d1bf62124a2dc
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:24:18 -0500

    Support data source fortios_router_authpath

---
+++ fortios/data_source_router_authpath.go
@@ -0,0 +1,115 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure authentication based routing.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceRouterAuthPath() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRouterAuthPathRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"device": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"gateway": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceRouterAuthPathRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("name")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing RouterAuthPath: type error")
+	}
+
+	o, err := c.ReadRouterAuthPath(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterAuthPath: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectRouterAuthPath(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterAuthPath from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenRouterAuthPathName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterAuthPathDevice(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterAuthPathGateway(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectRouterAuthPath(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", dataSourceFlattenRouterAuthPathName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("device", dataSourceFlattenRouterAuthPathDevice(o["device"], d, "device")); err != nil {
+		if !fortiAPIPatch(o["device"]) {
+			return fmt.Errorf("Error reading device: %v", err)
+		}
+	}
+
+	if err = d.Set("gateway", dataSourceFlattenRouterAuthPathGateway(o["gateway"], d, "gateway")); err != nil {
+		if !fortiAPIPatch(o["gateway"]) {
+			return fmt.Errorf("Error reading gateway: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenRouterAuthPathFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
