c85722c51c0b327f1a940ac81a1a0f43ca4d0cc8
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:24:32 -0500

    Support data source fortios_router_staticlist

---
+++ fortios/data_source_router_staticlist.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+package fortios
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceRouterStaticList() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRouterStaticListRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"seq_numlist": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeInt},
+			},
+		},
+	}
+}
+
+func dataSourceRouterStaticListRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	filter := d.Get("filter").(string)
+	if filter != "" {
+		filter = escapeFilter(filter)
+	}
+
+	o, err := c.GenericGroupRead("/api/v2/cmdb/router/static", filter)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterStatic: %v", err)
+	}
+
+	var tmps []int
+	if o != nil {
+		if len(o) == 0 || o[0] == nil {
+			return nil
+		}
+
+		for _, r := range o {
+			i := r.(map[string]interface{})
+
+			if _, ok := i["seq-num"]; ok {
+				tmps = append(tmps, fortiIntValue(i["seq-num"]))
+			}
+		}
+	}
+	d.Set("seq_numlist", tmps)
+
+	d.SetId("DataSourceRouterStaticList" + filter)
+
+	return nil
+}
