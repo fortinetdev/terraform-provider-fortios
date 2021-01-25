eb4ccb369aad8dd92798d79f1a1c9ad47225908f
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:23:53 -0500

    Support data source fortios_firewall_addrgrplist

---
+++ fortios/data_source_firewall_addrgrplist.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+package fortios
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceFirewallAddrgrpList() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceFirewallAddrgrpListRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"namelist": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+		},
+	}
+}
+
+func dataSourceFirewallAddrgrpListRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	filter := d.Get("filter").(string)
+	if filter != "" {
+		filter = escapeFilter(filter)
+	}
+
+	o, err := c.GenericGroupRead("/api/v2/cmdb/firewall/addrgrp", filter)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallAddrgrp: %v", err)
+	}
+
+	var tmps []string
+	if o != nil {
+		if len(o) == 0 || o[0] == nil {
+			return nil
+		}
+
+		for _, r := range o {
+			i := r.(map[string]interface{})
+
+			if _, ok := i["name"]; ok {
+				tmps = append(tmps, fortiStringValue(i["name"]))
+			}
+		}
+	}
+	d.Set("namelist", tmps)
+
+	d.SetId("DataSourceFirewallAddrgrpList" + filter)
+
+	return nil
+}
