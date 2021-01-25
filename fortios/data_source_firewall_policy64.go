3806c3525a8ee6255ff3b28cf9fe1f3c9b4ab65a
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:23:54 -0500

    Support data source fortios_firewall_policy64

---
+++ fortios/data_source_firewall_policy64.go
@@ -0,0 +1,527 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure IPv6 to IPv4 policies.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceFirewallPolicy64() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceFirewallPolicy64Read,
+		Schema: map[string]*schema.Schema{
+			"policyid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"uuid": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"srcintf": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"dstintf": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"srcaddr": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dstaddr": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"action": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"schedule": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"service": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"logtraffic": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"permit_any_host": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"traffic_shaper": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"traffic_shaper_reverse": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"per_ip_shaper": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"fixedport": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ippool": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"poolname": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"tcp_mss_sender": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"tcp_mss_receiver": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceFirewallPolicy64Read(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("policyid")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing FirewallPolicy64: type error")
+	}
+
+	o, err := c.ReadFirewallPolicy64(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallPolicy64: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectFirewallPolicy64(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallPolicy64 from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenFirewallPolicy64Policyid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Uuid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Srcintf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Dstintf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Srcaddr(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenFirewallPolicy64SrcaddrName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenFirewallPolicy64SrcaddrName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Dstaddr(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenFirewallPolicy64DstaddrName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenFirewallPolicy64DstaddrName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Action(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Status(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Schedule(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Service(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenFirewallPolicy64ServiceName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenFirewallPolicy64ServiceName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Logtraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64PermitAnyHost(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64TrafficShaper(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64TrafficShaperReverse(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64PerIpShaper(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Fixedport(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Ippool(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Poolname(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenFirewallPolicy64PoolnameName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenFirewallPolicy64PoolnameName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64TcpMssSender(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64TcpMssReceiver(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallPolicy64Comments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectFirewallPolicy64(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("policyid", dataSourceFlattenFirewallPolicy64Policyid(o["policyid"], d, "policyid")); err != nil {
+		if !fortiAPIPatch(o["policyid"]) {
+			return fmt.Errorf("Error reading policyid: %v", err)
+		}
+	}
+
+	if err = d.Set("uuid", dataSourceFlattenFirewallPolicy64Uuid(o["uuid"], d, "uuid")); err != nil {
+		if !fortiAPIPatch(o["uuid"]) {
+			return fmt.Errorf("Error reading uuid: %v", err)
+		}
+	}
+
+	if err = d.Set("srcintf", dataSourceFlattenFirewallPolicy64Srcintf(o["srcintf"], d, "srcintf")); err != nil {
+		if !fortiAPIPatch(o["srcintf"]) {
+			return fmt.Errorf("Error reading srcintf: %v", err)
+		}
+	}
+
+	if err = d.Set("dstintf", dataSourceFlattenFirewallPolicy64Dstintf(o["dstintf"], d, "dstintf")); err != nil {
+		if !fortiAPIPatch(o["dstintf"]) {
+			return fmt.Errorf("Error reading dstintf: %v", err)
+		}
+	}
+
+	if err = d.Set("srcaddr", dataSourceFlattenFirewallPolicy64Srcaddr(o["srcaddr"], d, "srcaddr")); err != nil {
+		if !fortiAPIPatch(o["srcaddr"]) {
+			return fmt.Errorf("Error reading srcaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("dstaddr", dataSourceFlattenFirewallPolicy64Dstaddr(o["dstaddr"], d, "dstaddr")); err != nil {
+		if !fortiAPIPatch(o["dstaddr"]) {
+			return fmt.Errorf("Error reading dstaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("action", dataSourceFlattenFirewallPolicy64Action(o["action"], d, "action")); err != nil {
+		if !fortiAPIPatch(o["action"]) {
+			return fmt.Errorf("Error reading action: %v", err)
+		}
+	}
+
+	if err = d.Set("status", dataSourceFlattenFirewallPolicy64Status(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("schedule", dataSourceFlattenFirewallPolicy64Schedule(o["schedule"], d, "schedule")); err != nil {
+		if !fortiAPIPatch(o["schedule"]) {
+			return fmt.Errorf("Error reading schedule: %v", err)
+		}
+	}
+
+	if err = d.Set("service", dataSourceFlattenFirewallPolicy64Service(o["service"], d, "service")); err != nil {
+		if !fortiAPIPatch(o["service"]) {
+			return fmt.Errorf("Error reading service: %v", err)
+		}
+	}
+
+	if err = d.Set("logtraffic", dataSourceFlattenFirewallPolicy64Logtraffic(o["logtraffic"], d, "logtraffic")); err != nil {
+		if !fortiAPIPatch(o["logtraffic"]) {
+			return fmt.Errorf("Error reading logtraffic: %v", err)
+		}
+	}
+
+	if err = d.Set("permit_any_host", dataSourceFlattenFirewallPolicy64PermitAnyHost(o["permit-any-host"], d, "permit_any_host")); err != nil {
+		if !fortiAPIPatch(o["permit-any-host"]) {
+			return fmt.Errorf("Error reading permit_any_host: %v", err)
+		}
+	}
+
+	if err = d.Set("traffic_shaper", dataSourceFlattenFirewallPolicy64TrafficShaper(o["traffic-shaper"], d, "traffic_shaper")); err != nil {
+		if !fortiAPIPatch(o["traffic-shaper"]) {
+			return fmt.Errorf("Error reading traffic_shaper: %v", err)
+		}
+	}
+
+	if err = d.Set("traffic_shaper_reverse", dataSourceFlattenFirewallPolicy64TrafficShaperReverse(o["traffic-shaper-reverse"], d, "traffic_shaper_reverse")); err != nil {
+		if !fortiAPIPatch(o["traffic-shaper-reverse"]) {
+			return fmt.Errorf("Error reading traffic_shaper_reverse: %v", err)
+		}
+	}
+
+	if err = d.Set("per_ip_shaper", dataSourceFlattenFirewallPolicy64PerIpShaper(o["per-ip-shaper"], d, "per_ip_shaper")); err != nil {
+		if !fortiAPIPatch(o["per-ip-shaper"]) {
+			return fmt.Errorf("Error reading per_ip_shaper: %v", err)
+		}
+	}
+
+	if err = d.Set("fixedport", dataSourceFlattenFirewallPolicy64Fixedport(o["fixedport"], d, "fixedport")); err != nil {
+		if !fortiAPIPatch(o["fixedport"]) {
+			return fmt.Errorf("Error reading fixedport: %v", err)
+		}
+	}
+
+	if err = d.Set("ippool", dataSourceFlattenFirewallPolicy64Ippool(o["ippool"], d, "ippool")); err != nil {
+		if !fortiAPIPatch(o["ippool"]) {
+			return fmt.Errorf("Error reading ippool: %v", err)
+		}
+	}
+
+	if err = d.Set("poolname", dataSourceFlattenFirewallPolicy64Poolname(o["poolname"], d, "poolname")); err != nil {
+		if !fortiAPIPatch(o["poolname"]) {
+			return fmt.Errorf("Error reading poolname: %v", err)
+		}
+	}
+
+	if err = d.Set("tcp_mss_sender", dataSourceFlattenFirewallPolicy64TcpMssSender(o["tcp-mss-sender"], d, "tcp_mss_sender")); err != nil {
+		if !fortiAPIPatch(o["tcp-mss-sender"]) {
+			return fmt.Errorf("Error reading tcp_mss_sender: %v", err)
+		}
+	}
+
+	if err = d.Set("tcp_mss_receiver", dataSourceFlattenFirewallPolicy64TcpMssReceiver(o["tcp-mss-receiver"], d, "tcp_mss_receiver")); err != nil {
+		if !fortiAPIPatch(o["tcp-mss-receiver"]) {
+			return fmt.Errorf("Error reading tcp_mss_receiver: %v", err)
+		}
+	}
+
+	if err = d.Set("comments", dataSourceFlattenFirewallPolicy64Comments(o["comments"], d, "comments")); err != nil {
+		if !fortiAPIPatch(o["comments"]) {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenFirewallPolicy64FortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
