7888017f56236be89e527ddae9fd2a866ebf50a4
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:24:23 -0500

    Support data source fortios_router_multicastflow

---
+++ fortios/data_source_router_multicastflow.go
@@ -0,0 +1,181 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure multicast-flow.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceRouterMulticastFlow() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRouterMulticastFlowRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"flows": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"group_addr": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"source_addr": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceRouterMulticastFlowRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("name")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing RouterMulticastFlow: type error")
+	}
+
+	o, err := c.ReadRouterMulticastFlow(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterMulticastFlow: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectRouterMulticastFlow(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterMulticastFlow from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenRouterMulticastFlowName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterMulticastFlowComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterMulticastFlowFlows(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = dataSourceFlattenRouterMulticastFlowFlowsId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "group_addr"
+		if _, ok := i["group-addr"]; ok {
+			tmp["group_addr"] = dataSourceFlattenRouterMulticastFlowFlowsGroupAddr(i["group-addr"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "source_addr"
+		if _, ok := i["source-addr"]; ok {
+			tmp["source_addr"] = dataSourceFlattenRouterMulticastFlowFlowsSourceAddr(i["source-addr"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenRouterMulticastFlowFlowsId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterMulticastFlowFlowsGroupAddr(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterMulticastFlowFlowsSourceAddr(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectRouterMulticastFlow(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", dataSourceFlattenRouterMulticastFlowName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("comments", dataSourceFlattenRouterMulticastFlowComments(o["comments"], d, "comments")); err != nil {
+		if !fortiAPIPatch(o["comments"]) {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+	if err = d.Set("flows", dataSourceFlattenRouterMulticastFlowFlows(o["flows"], d, "flows")); err != nil {
+		if !fortiAPIPatch(o["flows"]) {
+			return fmt.Errorf("Error reading flows: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenRouterMulticastFlowFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
