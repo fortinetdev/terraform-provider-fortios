2e86104610dda7481b2c43794ba5f0836d13f3ce
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:24:25 -0500

    Support data source fortios_router_policy6

---
+++ fortios/data_source_router_policy6.go
@@ -0,0 +1,255 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure IPv6 routing policies.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceRouterPolicy6() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRouterPolicy6Read,
+		Schema: map[string]*schema.Schema{
+			"seq_num": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"input_device": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"src": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"dst": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"protocol": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"start_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"end_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"gateway": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"output_device": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tos": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tos_mask": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceRouterPolicy6Read(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("seq_num")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing RouterPolicy6: type error")
+	}
+
+	o, err := c.ReadRouterPolicy6(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterPolicy6: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectRouterPolicy6(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterPolicy6 from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenRouterPolicy6SeqNum(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6InputDevice(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6Src(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6Dst(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6Protocol(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6StartPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6EndPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6Gateway(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6OutputDevice(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6Tos(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6TosMask(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6Status(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicy6Comments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectRouterPolicy6(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("seq_num", dataSourceFlattenRouterPolicy6SeqNum(o["seq-num"], d, "seq_num")); err != nil {
+		if !fortiAPIPatch(o["seq-num"]) {
+			return fmt.Errorf("Error reading seq_num: %v", err)
+		}
+	}
+
+	if err = d.Set("input_device", dataSourceFlattenRouterPolicy6InputDevice(o["input-device"], d, "input_device")); err != nil {
+		if !fortiAPIPatch(o["input-device"]) {
+			return fmt.Errorf("Error reading input_device: %v", err)
+		}
+	}
+
+	if err = d.Set("src", dataSourceFlattenRouterPolicy6Src(o["src"], d, "src")); err != nil {
+		if !fortiAPIPatch(o["src"]) {
+			return fmt.Errorf("Error reading src: %v", err)
+		}
+	}
+
+	if err = d.Set("dst", dataSourceFlattenRouterPolicy6Dst(o["dst"], d, "dst")); err != nil {
+		if !fortiAPIPatch(o["dst"]) {
+			return fmt.Errorf("Error reading dst: %v", err)
+		}
+	}
+
+	if err = d.Set("protocol", dataSourceFlattenRouterPolicy6Protocol(o["protocol"], d, "protocol")); err != nil {
+		if !fortiAPIPatch(o["protocol"]) {
+			return fmt.Errorf("Error reading protocol: %v", err)
+		}
+	}
+
+	if err = d.Set("start_port", dataSourceFlattenRouterPolicy6StartPort(o["start-port"], d, "start_port")); err != nil {
+		if !fortiAPIPatch(o["start-port"]) {
+			return fmt.Errorf("Error reading start_port: %v", err)
+		}
+	}
+
+	if err = d.Set("end_port", dataSourceFlattenRouterPolicy6EndPort(o["end-port"], d, "end_port")); err != nil {
+		if !fortiAPIPatch(o["end-port"]) {
+			return fmt.Errorf("Error reading end_port: %v", err)
+		}
+	}
+
+	if err = d.Set("gateway", dataSourceFlattenRouterPolicy6Gateway(o["gateway"], d, "gateway")); err != nil {
+		if !fortiAPIPatch(o["gateway"]) {
+			return fmt.Errorf("Error reading gateway: %v", err)
+		}
+	}
+
+	if err = d.Set("output_device", dataSourceFlattenRouterPolicy6OutputDevice(o["output-device"], d, "output_device")); err != nil {
+		if !fortiAPIPatch(o["output-device"]) {
+			return fmt.Errorf("Error reading output_device: %v", err)
+		}
+	}
+
+	if err = d.Set("tos", dataSourceFlattenRouterPolicy6Tos(o["tos"], d, "tos")); err != nil {
+		if !fortiAPIPatch(o["tos"]) {
+			return fmt.Errorf("Error reading tos: %v", err)
+		}
+	}
+
+	if err = d.Set("tos_mask", dataSourceFlattenRouterPolicy6TosMask(o["tos-mask"], d, "tos_mask")); err != nil {
+		if !fortiAPIPatch(o["tos-mask"]) {
+			return fmt.Errorf("Error reading tos_mask: %v", err)
+		}
+	}
+
+	if err = d.Set("status", dataSourceFlattenRouterPolicy6Status(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("comments", dataSourceFlattenRouterPolicy6Comments(o["comments"], d, "comments")); err != nil {
+		if !fortiAPIPatch(o["comments"]) {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenRouterPolicy6FortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
