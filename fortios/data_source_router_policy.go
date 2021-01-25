65d997b682eb465899a25229cc5e13510d86a78e
Frank Shen <fshen01@fortinet.com>
2021-01-25 11:24:25 -0500

    Support data source fortios_router_policy

---
+++ fortios/data_source_router_policy.go
@@ -0,0 +1,553 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure IPv4 routing policies.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceRouterPolicy() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRouterPolicyRead,
+		Schema: map[string]*schema.Schema{
+			"seq_num": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"input_device": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"src": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"subnet": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"srcaddr": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"src_negate": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"dst": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"subnet": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dstaddr": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dst_negate": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"action": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"protocol": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"start_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"end_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"start_source_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"end_source_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"gateway": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"output_device": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tos": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tos_mask": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceRouterPolicyRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("seq_num")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing RouterPolicy: type error")
+	}
+
+	o, err := c.ReadRouterPolicy(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterPolicy: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectRouterPolicy(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing RouterPolicy from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenRouterPolicySeqNum(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyInputDevice(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenRouterPolicyInputDeviceName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenRouterPolicyInputDeviceName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicySrc(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "subnet"
+		if _, ok := i["subnet"]; ok {
+			tmp["subnet"] = dataSourceFlattenRouterPolicySrcSubnet(i["subnet"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenRouterPolicySrcSubnet(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicySrcaddr(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenRouterPolicySrcaddrName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenRouterPolicySrcaddrName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicySrcNegate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyDst(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "subnet"
+		if _, ok := i["subnet"]; ok {
+			tmp["subnet"] = dataSourceFlattenRouterPolicyDstSubnet(i["subnet"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenRouterPolicyDstSubnet(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyDstaddr(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenRouterPolicyDstaddrName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenRouterPolicyDstaddrName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyDstNegate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyProtocol(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyStartPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyEndPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyStartSourcePort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyEndSourcePort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyGateway(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyOutputDevice(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyTos(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyTosMask(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenRouterPolicyComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectRouterPolicy(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("seq_num", dataSourceFlattenRouterPolicySeqNum(o["seq-num"], d, "seq_num")); err != nil {
+		if !fortiAPIPatch(o["seq-num"]) {
+			return fmt.Errorf("Error reading seq_num: %v", err)
+		}
+	}
+
+	if err = d.Set("input_device", dataSourceFlattenRouterPolicyInputDevice(o["input-device"], d, "input_device")); err != nil {
+		if !fortiAPIPatch(o["input-device"]) {
+			return fmt.Errorf("Error reading input_device: %v", err)
+		}
+	}
+
+	if err = d.Set("src", dataSourceFlattenRouterPolicySrc(o["src"], d, "src")); err != nil {
+		if !fortiAPIPatch(o["src"]) {
+			return fmt.Errorf("Error reading src: %v", err)
+		}
+	}
+
+	if err = d.Set("srcaddr", dataSourceFlattenRouterPolicySrcaddr(o["srcaddr"], d, "srcaddr")); err != nil {
+		if !fortiAPIPatch(o["srcaddr"]) {
+			return fmt.Errorf("Error reading srcaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("src_negate", dataSourceFlattenRouterPolicySrcNegate(o["src-negate"], d, "src_negate")); err != nil {
+		if !fortiAPIPatch(o["src-negate"]) {
+			return fmt.Errorf("Error reading src_negate: %v", err)
+		}
+	}
+
+	if err = d.Set("dst", dataSourceFlattenRouterPolicyDst(o["dst"], d, "dst")); err != nil {
+		if !fortiAPIPatch(o["dst"]) {
+			return fmt.Errorf("Error reading dst: %v", err)
+		}
+	}
+
+	if err = d.Set("dstaddr", dataSourceFlattenRouterPolicyDstaddr(o["dstaddr"], d, "dstaddr")); err != nil {
+		if !fortiAPIPatch(o["dstaddr"]) {
+			return fmt.Errorf("Error reading dstaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("dst_negate", dataSourceFlattenRouterPolicyDstNegate(o["dst-negate"], d, "dst_negate")); err != nil {
+		if !fortiAPIPatch(o["dst-negate"]) {
+			return fmt.Errorf("Error reading dst_negate: %v", err)
+		}
+	}
+
+	if err = d.Set("action", dataSourceFlattenRouterPolicyAction(o["action"], d, "action")); err != nil {
+		if !fortiAPIPatch(o["action"]) {
+			return fmt.Errorf("Error reading action: %v", err)
+		}
+	}
+
+	if err = d.Set("protocol", dataSourceFlattenRouterPolicyProtocol(o["protocol"], d, "protocol")); err != nil {
+		if !fortiAPIPatch(o["protocol"]) {
+			return fmt.Errorf("Error reading protocol: %v", err)
+		}
+	}
+
+	if err = d.Set("start_port", dataSourceFlattenRouterPolicyStartPort(o["start-port"], d, "start_port")); err != nil {
+		if !fortiAPIPatch(o["start-port"]) {
+			return fmt.Errorf("Error reading start_port: %v", err)
+		}
+	}
+
+	if err = d.Set("end_port", dataSourceFlattenRouterPolicyEndPort(o["end-port"], d, "end_port")); err != nil {
+		if !fortiAPIPatch(o["end-port"]) {
+			return fmt.Errorf("Error reading end_port: %v", err)
+		}
+	}
+
+	if err = d.Set("start_source_port", dataSourceFlattenRouterPolicyStartSourcePort(o["start-source-port"], d, "start_source_port")); err != nil {
+		if !fortiAPIPatch(o["start-source-port"]) {
+			return fmt.Errorf("Error reading start_source_port: %v", err)
+		}
+	}
+
+	if err = d.Set("end_source_port", dataSourceFlattenRouterPolicyEndSourcePort(o["end-source-port"], d, "end_source_port")); err != nil {
+		if !fortiAPIPatch(o["end-source-port"]) {
+			return fmt.Errorf("Error reading end_source_port: %v", err)
+		}
+	}
+
+	if err = d.Set("gateway", dataSourceFlattenRouterPolicyGateway(o["gateway"], d, "gateway")); err != nil {
+		if !fortiAPIPatch(o["gateway"]) {
+			return fmt.Errorf("Error reading gateway: %v", err)
+		}
+	}
+
+	if err = d.Set("output_device", dataSourceFlattenRouterPolicyOutputDevice(o["output-device"], d, "output_device")); err != nil {
+		if !fortiAPIPatch(o["output-device"]) {
+			return fmt.Errorf("Error reading output_device: %v", err)
+		}
+	}
+
+	if err = d.Set("tos", dataSourceFlattenRouterPolicyTos(o["tos"], d, "tos")); err != nil {
+		if !fortiAPIPatch(o["tos"]) {
+			return fmt.Errorf("Error reading tos: %v", err)
+		}
+	}
+
+	if err = d.Set("tos_mask", dataSourceFlattenRouterPolicyTosMask(o["tos-mask"], d, "tos_mask")); err != nil {
+		if !fortiAPIPatch(o["tos-mask"]) {
+			return fmt.Errorf("Error reading tos_mask: %v", err)
+		}
+	}
+
+	if err = d.Set("status", dataSourceFlattenRouterPolicyStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("comments", dataSourceFlattenRouterPolicyComments(o["comments"], d, "comments")); err != nil {
+		if !fortiAPIPatch(o["comments"]) {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenRouterPolicyFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
