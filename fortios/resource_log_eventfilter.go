3993ecddc9f5900698baccdd20da57466771c0f2
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:43 -0500

    Update resource resource_log_eventfilter to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_log_eventfilter.go
+++ fortios/resource_log_eventfilter.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -84,6 +85,31 @@ func resourceLogEventfilter() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
+			"fortiextender": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"connector": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"sdwan": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"cifs": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"switch_controller": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -93,7 +119,7 @@ func resourceLogEventfilterUpdate(d *schema.ResourceData, m interface{}) error {
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectLogEventfilter(d)
+	obj, err := getObjectLogEventfilter(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating LogEventfilter resource while getting object: %v", err)
 	}
@@ -146,123 +172,173 @@ func resourceLogEventfilterRead(d *schema.ResourceData, m interface{}) error {
 		return nil
 	}
 
-	err = refreshObjectLogEventfilter(d, o)
+	err = refreshObjectLogEventfilter(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading LogEventfilter resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenLogEventfilterEvent(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterEvent(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterSystem(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterSystem(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterVpn(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterVpn(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterUser(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterUser(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterRouter(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterRouter(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterWirelessActivity(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterWirelessActivity(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterWanOpt(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterWanOpt(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterEndpoint(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterEndpoint(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterHa(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterHa(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterComplianceCheck(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterComplianceCheck(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogEventfilterSecurityRating(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogEventfilterSecurityRating(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectLogEventfilter(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenLogEventfilterFortiextender(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenLogEventfilterConnector(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenLogEventfilterSdwan(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenLogEventfilterCifs(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenLogEventfilterSwitchController(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectLogEventfilter(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("event", flattenLogEventfilterEvent(o["event"], d, "event")); err != nil {
+	if err = d.Set("event", flattenLogEventfilterEvent(o["event"], d, "event", sv)); err != nil {
 		if !fortiAPIPatch(o["event"]) {
 			return fmt.Errorf("Error reading event: %v", err)
 		}
 	}
 
-	if err = d.Set("system", flattenLogEventfilterSystem(o["system"], d, "system")); err != nil {
+	if err = d.Set("system", flattenLogEventfilterSystem(o["system"], d, "system", sv)); err != nil {
 		if !fortiAPIPatch(o["system"]) {
 			return fmt.Errorf("Error reading system: %v", err)
 		}
 	}
 
-	if err = d.Set("vpn", flattenLogEventfilterVpn(o["vpn"], d, "vpn")); err != nil {
+	if err = d.Set("vpn", flattenLogEventfilterVpn(o["vpn"], d, "vpn", sv)); err != nil {
 		if !fortiAPIPatch(o["vpn"]) {
 			return fmt.Errorf("Error reading vpn: %v", err)
 		}
 	}
 
-	if err = d.Set("user", flattenLogEventfilterUser(o["user"], d, "user")); err != nil {
+	if err = d.Set("user", flattenLogEventfilterUser(o["user"], d, "user", sv)); err != nil {
 		if !fortiAPIPatch(o["user"]) {
 			return fmt.Errorf("Error reading user: %v", err)
 		}
 	}
 
-	if err = d.Set("router", flattenLogEventfilterRouter(o["router"], d, "router")); err != nil {
+	if err = d.Set("router", flattenLogEventfilterRouter(o["router"], d, "router", sv)); err != nil {
 		if !fortiAPIPatch(o["router"]) {
 			return fmt.Errorf("Error reading router: %v", err)
 		}
 	}
 
-	if err = d.Set("wireless_activity", flattenLogEventfilterWirelessActivity(o["wireless-activity"], d, "wireless_activity")); err != nil {
+	if err = d.Set("wireless_activity", flattenLogEventfilterWirelessActivity(o["wireless-activity"], d, "wireless_activity", sv)); err != nil {
 		if !fortiAPIPatch(o["wireless-activity"]) {
 			return fmt.Errorf("Error reading wireless_activity: %v", err)
 		}
 	}
 
-	if err = d.Set("wan_opt", flattenLogEventfilterWanOpt(o["wan-opt"], d, "wan_opt")); err != nil {
+	if err = d.Set("wan_opt", flattenLogEventfilterWanOpt(o["wan-opt"], d, "wan_opt", sv)); err != nil {
 		if !fortiAPIPatch(o["wan-opt"]) {
 			return fmt.Errorf("Error reading wan_opt: %v", err)
 		}
 	}
 
-	if err = d.Set("endpoint", flattenLogEventfilterEndpoint(o["endpoint"], d, "endpoint")); err != nil {
+	if err = d.Set("endpoint", flattenLogEventfilterEndpoint(o["endpoint"], d, "endpoint", sv)); err != nil {
 		if !fortiAPIPatch(o["endpoint"]) {
 			return fmt.Errorf("Error reading endpoint: %v", err)
 		}
 	}
 
-	if err = d.Set("ha", flattenLogEventfilterHa(o["ha"], d, "ha")); err != nil {
+	if err = d.Set("ha", flattenLogEventfilterHa(o["ha"], d, "ha", sv)); err != nil {
 		if !fortiAPIPatch(o["ha"]) {
 			return fmt.Errorf("Error reading ha: %v", err)
 		}
 	}
 
-	if err = d.Set("compliance_check", flattenLogEventfilterComplianceCheck(o["compliance-check"], d, "compliance_check")); err != nil {
+	if err = d.Set("compliance_check", flattenLogEventfilterComplianceCheck(o["compliance-check"], d, "compliance_check", sv)); err != nil {
 		if !fortiAPIPatch(o["compliance-check"]) {
 			return fmt.Errorf("Error reading compliance_check: %v", err)
 		}
 	}
 
-	if err = d.Set("security_rating", flattenLogEventfilterSecurityRating(o["security-rating"], d, "security_rating")); err != nil {
+	if err = d.Set("security_rating", flattenLogEventfilterSecurityRating(o["security-rating"], d, "security_rating", sv)); err != nil {
 		if !fortiAPIPatch(o["security-rating"]) {
 			return fmt.Errorf("Error reading security_rating: %v", err)
+		}
+	}
+
+	if err = d.Set("fortiextender", flattenLogEventfilterFortiextender(o["fortiextender"], d, "fortiextender", sv)); err != nil {
+		if !fortiAPIPatch(o["fortiextender"]) {
+			return fmt.Errorf("Error reading fortiextender: %v", err)
+		}
+	}
+
+	if err = d.Set("connector", flattenLogEventfilterConnector(o["connector"], d, "connector", sv)); err != nil {
+		if !fortiAPIPatch(o["connector"]) {
+			return fmt.Errorf("Error reading connector: %v", err)
+		}
+	}
+
+	if err = d.Set("sdwan", flattenLogEventfilterSdwan(o["sdwan"], d, "sdwan", sv)); err != nil {
+		if !fortiAPIPatch(o["sdwan"]) {
+			return fmt.Errorf("Error reading sdwan: %v", err)
+		}
+	}
+
+	if err = d.Set("cifs", flattenLogEventfilterCifs(o["cifs"], d, "cifs", sv)); err != nil {
+		if !fortiAPIPatch(o["cifs"]) {
+			return fmt.Errorf("Error reading cifs: %v", err)
+		}
+	}
+
+	if err = d.Set("switch_controller", flattenLogEventfilterSwitchController(o["switch-controller"], d, "switch_controller", sv)); err != nil {
+		if !fortiAPIPatch(o["switch-controller"]) {
+			return fmt.Errorf("Error reading switch_controller: %v", err)
 		}
 	}
 
@@ -272,58 +348,79 @@ func refreshObjectLogEventfilter(d *schema.ResourceData, o map[string]interface{
 func flattenLogEventfilterFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandLogEventfilterEvent(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterEvent(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterSystem(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterSystem(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterVpn(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterVpn(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterUser(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterUser(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterRouter(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterRouter(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterWirelessActivity(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterWirelessActivity(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterWanOpt(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterWanOpt(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterEndpoint(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterEndpoint(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterHa(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterHa(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterComplianceCheck(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterComplianceCheck(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogEventfilterSecurityRating(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogEventfilterSecurityRating(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandLogEventfilterFortiextender(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogEventfilterConnector(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogEventfilterSdwan(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogEventfilterCifs(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandLogEventfilterSwitchController(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectLogEventfilter(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("event"); ok {
-		t, err := expandLogEventfilterEvent(d, v, "event")
+
+		t, err := expandLogEventfilterEvent(d, v, "event", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -332,7 +429,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("system"); ok {
-		t, err := expandLogEventfilterSystem(d, v, "system")
+
+		t, err := expandLogEventfilterSystem(d, v, "system", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -341,7 +439,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("vpn"); ok {
-		t, err := expandLogEventfilterVpn(d, v, "vpn")
+
+		t, err := expandLogEventfilterVpn(d, v, "vpn", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -350,7 +449,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("user"); ok {
-		t, err := expandLogEventfilterUser(d, v, "user")
+
+		t, err := expandLogEventfilterUser(d, v, "user", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -359,7 +459,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("router"); ok {
-		t, err := expandLogEventfilterRouter(d, v, "router")
+
+		t, err := expandLogEventfilterRouter(d, v, "router", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -368,7 +469,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("wireless_activity"); ok {
-		t, err := expandLogEventfilterWirelessActivity(d, v, "wireless_activity")
+
+		t, err := expandLogEventfilterWirelessActivity(d, v, "wireless_activity", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -377,7 +479,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("wan_opt"); ok {
-		t, err := expandLogEventfilterWanOpt(d, v, "wan_opt")
+
+		t, err := expandLogEventfilterWanOpt(d, v, "wan_opt", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -386,7 +489,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("endpoint"); ok {
-		t, err := expandLogEventfilterEndpoint(d, v, "endpoint")
+
+		t, err := expandLogEventfilterEndpoint(d, v, "endpoint", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -395,7 +499,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("ha"); ok {
-		t, err := expandLogEventfilterHa(d, v, "ha")
+
+		t, err := expandLogEventfilterHa(d, v, "ha", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -404,7 +509,8 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("compliance_check"); ok {
-		t, err := expandLogEventfilterComplianceCheck(d, v, "compliance_check")
+
+		t, err := expandLogEventfilterComplianceCheck(d, v, "compliance_check", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -413,11 +519,62 @@ func getObjectLogEventfilter(d *schema.ResourceData) (*map[string]interface{}, e
 	}
 
 	if v, ok := d.GetOk("security_rating"); ok {
-		t, err := expandLogEventfilterSecurityRating(d, v, "security_rating")
+
+		t, err := expandLogEventfilterSecurityRating(d, v, "security_rating", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
 			obj["security-rating"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fortiextender"); ok {
+
+		t, err := expandLogEventfilterFortiextender(d, v, "fortiextender", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["fortiextender"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("connector"); ok {
+
+		t, err := expandLogEventfilterConnector(d, v, "connector", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["connector"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("sdwan"); ok {
+
+		t, err := expandLogEventfilterSdwan(d, v, "sdwan", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["sdwan"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("cifs"); ok {
+
+		t, err := expandLogEventfilterCifs(d, v, "cifs", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["cifs"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("switch_controller"); ok {
+
+		t, err := expandLogEventfilterSwitchController(d, v, "switch_controller", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["switch-controller"] = t
 		}
 	}
 
