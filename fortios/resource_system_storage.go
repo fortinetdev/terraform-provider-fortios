18bbd8224fd9ba00464274244a2ba898c1e102a5
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:25:54 -0500

    Update resource resource_system_storage to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_system_storage.go
+++ fortios/resource_system_storage.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -87,7 +88,7 @@ func resourceSystemStorageCreate(d *schema.ResourceData, m interface{}) error {
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemStorage(d)
+	obj, err := getObjectSystemStorage(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating SystemStorage resource while getting object: %v", err)
 	}
@@ -112,7 +113,7 @@ func resourceSystemStorageUpdate(d *schema.ResourceData, m interface{}) error {
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemStorage(d)
+	obj, err := getObjectSystemStorage(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemStorage resource while getting object: %v", err)
 	}
@@ -165,101 +166,101 @@ func resourceSystemStorageRead(d *schema.ResourceData, m interface{}) error {
 		return nil
 	}
 
-	err = refreshObjectSystemStorage(d, o)
+	err = refreshObjectSystemStorage(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemStorage resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemStorageName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStorageStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStorageMediaStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageMediaStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStorageOrder(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageOrder(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStoragePartition(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStoragePartition(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStorageDevice(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageDevice(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStorageSize(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageSize(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStorageUsage(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageUsage(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemStorageWanoptMode(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemStorageWanoptMode(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemStorage(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectSystemStorage(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("name", flattenSystemStorageName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenSystemStorageName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenSystemStorageStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenSystemStorageStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("media_status", flattenSystemStorageMediaStatus(o["media-status"], d, "media_status")); err != nil {
+	if err = d.Set("media_status", flattenSystemStorageMediaStatus(o["media-status"], d, "media_status", sv)); err != nil {
 		if !fortiAPIPatch(o["media-status"]) {
 			return fmt.Errorf("Error reading media_status: %v", err)
 		}
 	}
 
-	if err = d.Set("order", flattenSystemStorageOrder(o["order"], d, "order")); err != nil {
+	if err = d.Set("order", flattenSystemStorageOrder(o["order"], d, "order", sv)); err != nil {
 		if !fortiAPIPatch(o["order"]) {
 			return fmt.Errorf("Error reading order: %v", err)
 		}
 	}
 
-	if err = d.Set("partition", flattenSystemStoragePartition(o["partition"], d, "partition")); err != nil {
+	if err = d.Set("partition", flattenSystemStoragePartition(o["partition"], d, "partition", sv)); err != nil {
 		if !fortiAPIPatch(o["partition"]) {
 			return fmt.Errorf("Error reading partition: %v", err)
 		}
 	}
 
-	if err = d.Set("device", flattenSystemStorageDevice(o["device"], d, "device")); err != nil {
+	if err = d.Set("device", flattenSystemStorageDevice(o["device"], d, "device", sv)); err != nil {
 		if !fortiAPIPatch(o["device"]) {
 			return fmt.Errorf("Error reading device: %v", err)
 		}
 	}
 
-	if err = d.Set("size", flattenSystemStorageSize(o["size"], d, "size")); err != nil {
+	if err = d.Set("size", flattenSystemStorageSize(o["size"], d, "size", sv)); err != nil {
 		if !fortiAPIPatch(o["size"]) {
 			return fmt.Errorf("Error reading size: %v", err)
 		}
 	}
 
-	if err = d.Set("usage", flattenSystemStorageUsage(o["usage"], d, "usage")); err != nil {
+	if err = d.Set("usage", flattenSystemStorageUsage(o["usage"], d, "usage", sv)); err != nil {
 		if !fortiAPIPatch(o["usage"]) {
 			return fmt.Errorf("Error reading usage: %v", err)
 		}
 	}
 
-	if err = d.Set("wanopt_mode", flattenSystemStorageWanoptMode(o["wanopt-mode"], d, "wanopt_mode")); err != nil {
+	if err = d.Set("wanopt_mode", flattenSystemStorageWanoptMode(o["wanopt-mode"], d, "wanopt_mode", sv)); err != nil {
 		if !fortiAPIPatch(o["wanopt-mode"]) {
 			return fmt.Errorf("Error reading wanopt_mode: %v", err)
 		}
@@ -271,50 +272,51 @@ func refreshObjectSystemStorage(d *schema.ResourceData, o map[string]interface{}
 func flattenSystemStorageFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemStorageName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStorageStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStorageMediaStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageMediaStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStorageOrder(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageOrder(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStoragePartition(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStoragePartition(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStorageDevice(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageDevice(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStorageSize(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageSize(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStorageUsage(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageUsage(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemStorageWanoptMode(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemStorageWanoptMode(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectSystemStorage(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandSystemStorageName(d, v, "name")
+
+		t, err := expandSystemStorageName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -323,7 +325,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandSystemStorageStatus(d, v, "status")
+
+		t, err := expandSystemStorageStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -332,7 +335,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOk("media_status"); ok {
-		t, err := expandSystemStorageMediaStatus(d, v, "media_status")
+
+		t, err := expandSystemStorageMediaStatus(d, v, "media_status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -341,7 +345,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOkExists("order"); ok {
-		t, err := expandSystemStorageOrder(d, v, "order")
+
+		t, err := expandSystemStorageOrder(d, v, "order", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -350,7 +355,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOk("partition"); ok {
-		t, err := expandSystemStoragePartition(d, v, "partition")
+
+		t, err := expandSystemStoragePartition(d, v, "partition", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -359,7 +365,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOk("device"); ok {
-		t, err := expandSystemStorageDevice(d, v, "device")
+
+		t, err := expandSystemStorageDevice(d, v, "device", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -368,7 +375,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOkExists("size"); ok {
-		t, err := expandSystemStorageSize(d, v, "size")
+
+		t, err := expandSystemStorageSize(d, v, "size", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -377,7 +385,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOk("usage"); ok {
-		t, err := expandSystemStorageUsage(d, v, "usage")
+
+		t, err := expandSystemStorageUsage(d, v, "usage", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -386,7 +395,8 @@ func getObjectSystemStorage(d *schema.ResourceData) (*map[string]interface{}, er
 	}
 
 	if v, ok := d.GetOk("wanopt_mode"); ok {
-		t, err := expandSystemStorageWanoptMode(d, v, "wanopt_mode")
+
+		t, err := expandSystemStorageWanoptMode(d, v, "wanopt_mode", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
