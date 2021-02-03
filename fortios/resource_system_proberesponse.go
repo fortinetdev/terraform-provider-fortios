b94807a290b268ebf3447ef91c70cfbc5f06ccfa
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:25:47 -0500

    Update resource resource_system_proberesponse to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_system_proberesponse.go
+++ fortios/resource_system_proberesponse.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -77,7 +78,7 @@ func resourceSystemProbeResponseUpdate(d *schema.ResourceData, m interface{}) er
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemProbeResponse(d)
+	obj, err := getObjectSystemProbeResponse(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemProbeResponse resource while getting object: %v", err)
 	}
@@ -130,75 +131,75 @@ func resourceSystemProbeResponseRead(d *schema.ResourceData, m interface{}) erro
 		return nil
 	}
 
-	err = refreshObjectSystemProbeResponse(d, o)
+	err = refreshObjectSystemProbeResponse(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemProbeResponse resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemProbeResponsePort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemProbeResponsePort(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemProbeResponseHttpProbeValue(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemProbeResponseHttpProbeValue(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemProbeResponseTtlMode(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemProbeResponseTtlMode(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemProbeResponseMode(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemProbeResponseMode(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemProbeResponseSecurityMode(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemProbeResponseSecurityMode(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemProbeResponsePassword(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemProbeResponsePassword(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemProbeResponseTimeout(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemProbeResponseTimeout(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemProbeResponse(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectSystemProbeResponse(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("port", flattenSystemProbeResponsePort(o["port"], d, "port")); err != nil {
+	if err = d.Set("port", flattenSystemProbeResponsePort(o["port"], d, "port", sv)); err != nil {
 		if !fortiAPIPatch(o["port"]) {
 			return fmt.Errorf("Error reading port: %v", err)
 		}
 	}
 
-	if err = d.Set("http_probe_value", flattenSystemProbeResponseHttpProbeValue(o["http-probe-value"], d, "http_probe_value")); err != nil {
+	if err = d.Set("http_probe_value", flattenSystemProbeResponseHttpProbeValue(o["http-probe-value"], d, "http_probe_value", sv)); err != nil {
 		if !fortiAPIPatch(o["http-probe-value"]) {
 			return fmt.Errorf("Error reading http_probe_value: %v", err)
 		}
 	}
 
-	if err = d.Set("ttl_mode", flattenSystemProbeResponseTtlMode(o["ttl-mode"], d, "ttl_mode")); err != nil {
+	if err = d.Set("ttl_mode", flattenSystemProbeResponseTtlMode(o["ttl-mode"], d, "ttl_mode", sv)); err != nil {
 		if !fortiAPIPatch(o["ttl-mode"]) {
 			return fmt.Errorf("Error reading ttl_mode: %v", err)
 		}
 	}
 
-	if err = d.Set("mode", flattenSystemProbeResponseMode(o["mode"], d, "mode")); err != nil {
+	if err = d.Set("mode", flattenSystemProbeResponseMode(o["mode"], d, "mode", sv)); err != nil {
 		if !fortiAPIPatch(o["mode"]) {
 			return fmt.Errorf("Error reading mode: %v", err)
 		}
 	}
 
-	if err = d.Set("security_mode", flattenSystemProbeResponseSecurityMode(o["security-mode"], d, "security_mode")); err != nil {
+	if err = d.Set("security_mode", flattenSystemProbeResponseSecurityMode(o["security-mode"], d, "security_mode", sv)); err != nil {
 		if !fortiAPIPatch(o["security-mode"]) {
 			return fmt.Errorf("Error reading security_mode: %v", err)
 		}
 	}
 
-	if err = d.Set("timeout", flattenSystemProbeResponseTimeout(o["timeout"], d, "timeout")); err != nil {
+	if err = d.Set("timeout", flattenSystemProbeResponseTimeout(o["timeout"], d, "timeout", sv)); err != nil {
 		if !fortiAPIPatch(o["timeout"]) {
 			return fmt.Errorf("Error reading timeout: %v", err)
 		}
@@ -210,42 +211,43 @@ func refreshObjectSystemProbeResponse(d *schema.ResourceData, o map[string]inter
 func flattenSystemProbeResponseFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemProbeResponsePort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemProbeResponsePort(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemProbeResponseHttpProbeValue(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemProbeResponseHttpProbeValue(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemProbeResponseTtlMode(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemProbeResponseTtlMode(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemProbeResponseMode(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemProbeResponseMode(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemProbeResponseSecurityMode(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemProbeResponseSecurityMode(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemProbeResponsePassword(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemProbeResponsePassword(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemProbeResponseTimeout(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemProbeResponseTimeout(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemProbeResponse(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectSystemProbeResponse(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("port"); ok {
-		t, err := expandSystemProbeResponsePort(d, v, "port")
+
+		t, err := expandSystemProbeResponsePort(d, v, "port", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -254,7 +256,8 @@ func getObjectSystemProbeResponse(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("http_probe_value"); ok {
-		t, err := expandSystemProbeResponseHttpProbeValue(d, v, "http_probe_value")
+
+		t, err := expandSystemProbeResponseHttpProbeValue(d, v, "http_probe_value", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -263,7 +266,8 @@ func getObjectSystemProbeResponse(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("ttl_mode"); ok {
-		t, err := expandSystemProbeResponseTtlMode(d, v, "ttl_mode")
+
+		t, err := expandSystemProbeResponseTtlMode(d, v, "ttl_mode", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -272,7 +276,8 @@ func getObjectSystemProbeResponse(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("mode"); ok {
-		t, err := expandSystemProbeResponseMode(d, v, "mode")
+
+		t, err := expandSystemProbeResponseMode(d, v, "mode", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -281,7 +286,8 @@ func getObjectSystemProbeResponse(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("security_mode"); ok {
-		t, err := expandSystemProbeResponseSecurityMode(d, v, "security_mode")
+
+		t, err := expandSystemProbeResponseSecurityMode(d, v, "security_mode", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -290,7 +296,8 @@ func getObjectSystemProbeResponse(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("password"); ok {
-		t, err := expandSystemProbeResponsePassword(d, v, "password")
+
+		t, err := expandSystemProbeResponsePassword(d, v, "password", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -299,7 +306,8 @@ func getObjectSystemProbeResponse(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("timeout"); ok {
-		t, err := expandSystemProbeResponseTimeout(d, v, "timeout")
+
+		t, err := expandSystemProbeResponseTimeout(d, v, "timeout", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
