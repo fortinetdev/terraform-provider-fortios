21e5cfb928683af917bd7914a9b4aee0199920c8
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:30 -0500

    Add new resource resource_wirelesscontroller_address

---
+++ fortios/resource_wirelesscontroller_address.go
@@ -0,0 +1,231 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure the client with its MAC address.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceWirelessControllerAddress() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceWirelessControllerAddressCreate,
+		Read:   resourceWirelessControllerAddressRead,
+		Update: resourceWirelessControllerAddressUpdate,
+		Delete: resourceWirelessControllerAddressDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				ForceNew:     true,
+				Optional:     true,
+				Computed:     true,
+			},
+			"mac": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"policy": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceWirelessControllerAddressCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWirelessControllerAddress(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating WirelessControllerAddress resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateWirelessControllerAddress(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating WirelessControllerAddress resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("WirelessControllerAddress")
+	}
+
+	return resourceWirelessControllerAddressRead(d, m)
+}
+
+func resourceWirelessControllerAddressUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectWirelessControllerAddress(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating WirelessControllerAddress resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateWirelessControllerAddress(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating WirelessControllerAddress resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("WirelessControllerAddress")
+	}
+
+	return resourceWirelessControllerAddressRead(d, m)
+}
+
+func resourceWirelessControllerAddressDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteWirelessControllerAddress(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting WirelessControllerAddress resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceWirelessControllerAddressRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadWirelessControllerAddress(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading WirelessControllerAddress resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectWirelessControllerAddress(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading WirelessControllerAddress resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenWirelessControllerAddressId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerAddressMac(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenWirelessControllerAddressPolicy(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectWirelessControllerAddress(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("fosid", flattenWirelessControllerAddressId(o["id"], d, "fosid", sv)); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("mac", flattenWirelessControllerAddressMac(o["mac"], d, "mac", sv)); err != nil {
+		if !fortiAPIPatch(o["mac"]) {
+			return fmt.Errorf("Error reading mac: %v", err)
+		}
+	}
+
+	if err = d.Set("policy", flattenWirelessControllerAddressPolicy(o["policy"], d, "policy", sv)); err != nil {
+		if !fortiAPIPatch(o["policy"]) {
+			return fmt.Errorf("Error reading policy: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenWirelessControllerAddressFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandWirelessControllerAddressId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerAddressMac(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandWirelessControllerAddressPolicy(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectWirelessControllerAddress(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("fosid"); ok {
+
+		t, err := expandWirelessControllerAddressId(d, v, "fosid", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("mac"); ok {
+
+		t, err := expandWirelessControllerAddressMac(d, v, "mac", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["mac"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("policy"); ok {
+
+		t, err := expandWirelessControllerAddressPolicy(d, v, "policy", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["policy"] = t
+		}
+	}
+
+	return &obj, nil
+}
