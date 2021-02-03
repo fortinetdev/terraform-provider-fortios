9b19975223108279b39ac80e420e77f83700a794
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:26:42 -0500

    Update resource resource_webfilter_ftgdlocalrating to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_webfilter_ftgdlocalrating.go
+++ fortios/resource_webfilter_ftgdlocalrating.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -41,6 +42,11 @@ func resourceWebfilterFtgdLocalRating() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
+			"comment": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 255),
+				Optional:     true,
+			},
 			"rating": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
@@ -53,7 +59,7 @@ func resourceWebfilterFtgdLocalRatingCreate(d *schema.ResourceData, m interface{
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectWebfilterFtgdLocalRating(d)
+	obj, err := getObjectWebfilterFtgdLocalRating(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating WebfilterFtgdLocalRating resource while getting object: %v", err)
 	}
@@ -78,7 +84,7 @@ func resourceWebfilterFtgdLocalRatingUpdate(d *schema.ResourceData, m interface{
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectWebfilterFtgdLocalRating(d)
+	obj, err := getObjectWebfilterFtgdLocalRating(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating WebfilterFtgdLocalRating resource while getting object: %v", err)
 	}
@@ -131,41 +137,51 @@ func resourceWebfilterFtgdLocalRatingRead(d *schema.ResourceData, m interface{})
 		return nil
 	}
 
-	err = refreshObjectWebfilterFtgdLocalRating(d, o)
+	err = refreshObjectWebfilterFtgdLocalRating(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading WebfilterFtgdLocalRating resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenWebfilterFtgdLocalRatingUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterFtgdLocalRatingUrl(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterFtgdLocalRatingStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterFtgdLocalRatingStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterFtgdLocalRatingRating(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterFtgdLocalRatingComment(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectWebfilterFtgdLocalRating(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenWebfilterFtgdLocalRatingRating(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectWebfilterFtgdLocalRating(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("url", flattenWebfilterFtgdLocalRatingUrl(o["url"], d, "url")); err != nil {
+	if err = d.Set("url", flattenWebfilterFtgdLocalRatingUrl(o["url"], d, "url", sv)); err != nil {
 		if !fortiAPIPatch(o["url"]) {
 			return fmt.Errorf("Error reading url: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenWebfilterFtgdLocalRatingStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenWebfilterFtgdLocalRatingStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("rating", flattenWebfilterFtgdLocalRatingRating(o["rating"], d, "rating")); err != nil {
+	if err = d.Set("comment", flattenWebfilterFtgdLocalRatingComment(o["comment"], d, "comment", sv)); err != nil {
+		if !fortiAPIPatch(o["comment"]) {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("rating", flattenWebfilterFtgdLocalRatingRating(o["rating"], d, "rating", sv)); err != nil {
 		if !fortiAPIPatch(o["rating"]) {
 			return fmt.Errorf("Error reading rating: %v", err)
 		}
@@ -177,26 +193,31 @@ func refreshObjectWebfilterFtgdLocalRating(d *schema.ResourceData, o map[string]
 func flattenWebfilterFtgdLocalRatingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandWebfilterFtgdLocalRatingUrl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterFtgdLocalRatingUrl(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterFtgdLocalRatingStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterFtgdLocalRatingStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterFtgdLocalRatingRating(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterFtgdLocalRatingComment(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectWebfilterFtgdLocalRating(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandWebfilterFtgdLocalRatingRating(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectWebfilterFtgdLocalRating(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("url"); ok {
-		t, err := expandWebfilterFtgdLocalRatingUrl(d, v, "url")
+
+		t, err := expandWebfilterFtgdLocalRatingUrl(d, v, "url", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -205,7 +226,8 @@ func getObjectWebfilterFtgdLocalRating(d *schema.ResourceData) (*map[string]inte
 	}
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandWebfilterFtgdLocalRatingStatus(d, v, "status")
+
+		t, err := expandWebfilterFtgdLocalRatingStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -213,8 +235,19 @@ func getObjectWebfilterFtgdLocalRating(d *schema.ResourceData) (*map[string]inte
 		}
 	}
 
+	if v, ok := d.GetOk("comment"); ok {
+
+		t, err := expandWebfilterFtgdLocalRatingComment(d, v, "comment", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
 	if v, ok := d.GetOk("rating"); ok {
-		t, err := expandWebfilterFtgdLocalRatingRating(d, v, "rating")
+
+		t, err := expandWebfilterFtgdLocalRatingRating(d, v, "rating", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
