a17569b89b1877547c0783ba04d165bbebd8f6db
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:08 -0500

    Add new resource resource_firewall_internetservicelist

---
+++ fortios/resource_firewall_internetservicelist.go
@@ -0,0 +1,201 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Internet Service list.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceFirewallInternetServiceList() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFirewallInternetServiceListCreate,
+		Read:   resourceFirewallInternetServiceListRead,
+		Update: resourceFirewallInternetServiceListUpdate,
+		Delete: resourceFirewallInternetServiceListDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional:     true,
+				Computed:     true,
+			},
+		},
+	}
+}
+
+func resourceFirewallInternetServiceListCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallInternetServiceList(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallInternetServiceList resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateFirewallInternetServiceList(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallInternetServiceList resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallInternetServiceList")
+	}
+
+	return resourceFirewallInternetServiceListRead(d, m)
+}
+
+func resourceFirewallInternetServiceListUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallInternetServiceList(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallInternetServiceList resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateFirewallInternetServiceList(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallInternetServiceList resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallInternetServiceList")
+	}
+
+	return resourceFirewallInternetServiceListRead(d, m)
+}
+
+func resourceFirewallInternetServiceListDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteFirewallInternetServiceList(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting FirewallInternetServiceList resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFirewallInternetServiceListRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadFirewallInternetServiceList(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallInternetServiceList resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectFirewallInternetServiceList(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallInternetServiceList resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenFirewallInternetServiceListId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenFirewallInternetServiceListName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectFirewallInternetServiceList(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("fosid", flattenFirewallInternetServiceListId(o["id"], d, "fosid", sv)); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenFirewallInternetServiceListName(o["name"], d, "name", sv)); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenFirewallInternetServiceListFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandFirewallInternetServiceListId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallInternetServiceListName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectFirewallInternetServiceList(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOkExists("fosid"); ok {
+
+		t, err := expandFirewallInternetServiceListId(d, v, "fosid", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+
+		t, err := expandFirewallInternetServiceListName(d, v, "name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	return &obj, nil
+}
