b8ffd0ca39d768782ad8356008fcfbe2afed9909
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:25:32 -0500

    Update resource resource_system_externalresource to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_system_externalresource.go
+++ fortios/resource_system_externalresource.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -34,6 +35,11 @@ func resourceSystemExternalResource() *schema.Resource {
 				ValidateFunc: validation.StringLenBetween(0, 35),
 				ForceNew:     true,
 				Required:     true,
+			},
+			"uuid": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
 			},
 			"status": &schema.Schema{
 				Type:     schema.TypeString,
@@ -73,10 +79,32 @@ func resourceSystemExternalResource() *schema.Resource {
 				ValidateFunc: validation.StringLenBetween(0, 511),
 				Required:     true,
 			},
+			"user_agent": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 127),
+				Optional:     true,
+				Computed:     true,
+			},
 			"refresh_rate": &schema.Schema{
 				Type:         schema.TypeInt,
 				ValidateFunc: validation.IntBetween(1, 43200),
 				Required:     true,
+			},
+			"source_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"interface_select_method": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"interface": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 15),
+				Optional:     true,
+				Computed:     true,
 			},
 		},
 	}
@@ -86,7 +114,7 @@ func resourceSystemExternalResourceCreate(d *schema.ResourceData, m interface{})
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemExternalResource(d)
+	obj, err := getObjectSystemExternalResource(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating SystemExternalResource resource while getting object: %v", err)
 	}
@@ -111,7 +139,7 @@ func resourceSystemExternalResourceUpdate(d *schema.ResourceData, m interface{})
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemExternalResource(d)
+	obj, err := getObjectSystemExternalResource(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemExternalResource resource while getting object: %v", err)
 	}
@@ -164,97 +192,147 @@ func resourceSystemExternalResourceRead(d *schema.ResourceData, m interface{}) e
 		return nil
 	}
 
-	err = refreshObjectSystemExternalResource(d, o)
+	err = refreshObjectSystemExternalResource(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemExternalResource resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemExternalResourceName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourceStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceUuid(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourceType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourceCategory(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceType(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourceUsername(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceCategory(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourcePassword(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceUsername(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourceComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourcePassword(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourceResource(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceComments(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemExternalResourceRefreshRate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemExternalResourceResource(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemExternalResource(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenSystemExternalResourceUserAgent(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemExternalResourceRefreshRate(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemExternalResourceSourceIp(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemExternalResourceInterfaceSelectMethod(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemExternalResourceInterface(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectSystemExternalResource(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("name", flattenSystemExternalResourceName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenSystemExternalResourceName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenSystemExternalResourceStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("uuid", flattenSystemExternalResourceUuid(o["uuid"], d, "uuid", sv)); err != nil {
+		if !fortiAPIPatch(o["uuid"]) {
+			return fmt.Errorf("Error reading uuid: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenSystemExternalResourceStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("type", flattenSystemExternalResourceType(o["type"], d, "type")); err != nil {
+	if err = d.Set("type", flattenSystemExternalResourceType(o["type"], d, "type", sv)); err != nil {
 		if !fortiAPIPatch(o["type"]) {
 			return fmt.Errorf("Error reading type: %v", err)
 		}
 	}
 
-	if err = d.Set("category", flattenSystemExternalResourceCategory(o["category"], d, "category")); err != nil {
+	if err = d.Set("category", flattenSystemExternalResourceCategory(o["category"], d, "category", sv)); err != nil {
 		if !fortiAPIPatch(o["category"]) {
 			return fmt.Errorf("Error reading category: %v", err)
 		}
 	}
 
-	if err = d.Set("username", flattenSystemExternalResourceUsername(o["username"], d, "username")); err != nil {
+	if err = d.Set("username", flattenSystemExternalResourceUsername(o["username"], d, "username", sv)); err != nil {
 		if !fortiAPIPatch(o["username"]) {
 			return fmt.Errorf("Error reading username: %v", err)
 		}
 	}
 
-	if err = d.Set("comments", flattenSystemExternalResourceComments(o["comments"], d, "comments")); err != nil {
+	if err = d.Set("comments", flattenSystemExternalResourceComments(o["comments"], d, "comments", sv)); err != nil {
 		if !fortiAPIPatch(o["comments"]) {
 			return fmt.Errorf("Error reading comments: %v", err)
 		}
 	}
 
-	if err = d.Set("resource", flattenSystemExternalResourceResource(o["resource"], d, "resource")); err != nil {
+	if err = d.Set("resource", flattenSystemExternalResourceResource(o["resource"], d, "resource", sv)); err != nil {
 		if !fortiAPIPatch(o["resource"]) {
 			return fmt.Errorf("Error reading resource: %v", err)
 		}
 	}
 
-	if err = d.Set("refresh_rate", flattenSystemExternalResourceRefreshRate(o["refresh-rate"], d, "refresh_rate")); err != nil {
+	if err = d.Set("user_agent", flattenSystemExternalResourceUserAgent(o["user-agent"], d, "user_agent", sv)); err != nil {
+		if !fortiAPIPatch(o["user-agent"]) {
+			return fmt.Errorf("Error reading user_agent: %v", err)
+		}
+	}
+
+	if err = d.Set("refresh_rate", flattenSystemExternalResourceRefreshRate(o["refresh-rate"], d, "refresh_rate", sv)); err != nil {
 		if !fortiAPIPatch(o["refresh-rate"]) {
 			return fmt.Errorf("Error reading refresh_rate: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", flattenSystemExternalResourceSourceIp(o["source-ip"], d, "source_ip", sv)); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("interface_select_method", flattenSystemExternalResourceInterfaceSelectMethod(o["interface-select-method"], d, "interface_select_method", sv)); err != nil {
+		if !fortiAPIPatch(o["interface-select-method"]) {
+			return fmt.Errorf("Error reading interface_select_method: %v", err)
+		}
+	}
+
+	if err = d.Set("interface", flattenSystemExternalResourceInterface(o["interface"], d, "interface", sv)); err != nil {
+		if !fortiAPIPatch(o["interface"]) {
+			return fmt.Errorf("Error reading interface: %v", err)
 		}
 	}
 
@@ -264,50 +342,71 @@ func refreshObjectSystemExternalResource(d *schema.ResourceData, o map[string]in
 func flattenSystemExternalResourceFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemExternalResourceName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourceStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceUuid(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourceType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourceCategory(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceType(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourceUsername(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceCategory(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourcePassword(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceUsername(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourceComments(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourcePassword(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourceResource(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceComments(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemExternalResourceRefreshRate(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemExternalResourceResource(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandSystemExternalResourceUserAgent(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemExternalResourceRefreshRate(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemExternalResourceSourceIp(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemExternalResourceInterfaceSelectMethod(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemExternalResourceInterface(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemExternalResource(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandSystemExternalResourceName(d, v, "name")
+
+		t, err := expandSystemExternalResourceName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -315,8 +414,19 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 		}
 	}
 
+	if v, ok := d.GetOk("uuid"); ok {
+
+		t, err := expandSystemExternalResourceUuid(d, v, "uuid", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["uuid"] = t
+		}
+	}
+
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandSystemExternalResourceStatus(d, v, "status")
+
+		t, err := expandSystemExternalResourceStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -325,7 +435,8 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("type"); ok {
-		t, err := expandSystemExternalResourceType(d, v, "type")
+
+		t, err := expandSystemExternalResourceType(d, v, "type", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -334,7 +445,8 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("category"); ok {
-		t, err := expandSystemExternalResourceCategory(d, v, "category")
+
+		t, err := expandSystemExternalResourceCategory(d, v, "category", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -343,7 +455,8 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("username"); ok {
-		t, err := expandSystemExternalResourceUsername(d, v, "username")
+
+		t, err := expandSystemExternalResourceUsername(d, v, "username", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -352,7 +465,8 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("password"); ok {
-		t, err := expandSystemExternalResourcePassword(d, v, "password")
+
+		t, err := expandSystemExternalResourcePassword(d, v, "password", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -361,7 +475,8 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("comments"); ok {
-		t, err := expandSystemExternalResourceComments(d, v, "comments")
+
+		t, err := expandSystemExternalResourceComments(d, v, "comments", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -370,7 +485,8 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("resource"); ok {
-		t, err := expandSystemExternalResourceResource(d, v, "resource")
+
+		t, err := expandSystemExternalResourceResource(d, v, "resource", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -378,12 +494,53 @@ func getObjectSystemExternalResource(d *schema.ResourceData) (*map[string]interf
 		}
 	}
 
+	if v, ok := d.GetOk("user_agent"); ok {
+
+		t, err := expandSystemExternalResourceUserAgent(d, v, "user_agent", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["user-agent"] = t
+		}
+	}
+
 	if v, ok := d.GetOk("refresh_rate"); ok {
-		t, err := expandSystemExternalResourceRefreshRate(d, v, "refresh_rate")
+
+		t, err := expandSystemExternalResourceRefreshRate(d, v, "refresh_rate", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
 			obj["refresh-rate"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("source_ip"); ok {
+
+		t, err := expandSystemExternalResourceSourceIp(d, v, "source_ip", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["source-ip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("interface_select_method"); ok {
+
+		t, err := expandSystemExternalResourceInterfaceSelectMethod(d, v, "interface_select_method", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["interface-select-method"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("interface"); ok {
+
+		t, err := expandSystemExternalResourceInterface(d, v, "interface", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["interface"] = t
 		}
 	}
 
