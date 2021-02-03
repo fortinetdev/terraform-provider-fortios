80327ef1f990c7364ca69b164f45d6c5888d52dc
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:38:48 -0500

    Add new data source data_source_firewall_internetservicegrouplist

---
+++ fortios/data_source_firewall_internetservicegrouplist.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+package fortios
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceFirewallInternetServiceGroupList() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceFirewallInternetServiceGroupListRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"namelist": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+		},
+	}
+}
+
+func dataSourceFirewallInternetServiceGroupListRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	filter := d.Get("filter").(string)
+	if filter != "" {
+		filter = escapeFilter(filter)
+	}
+
+	o, err := c.GenericGroupRead("/api/v2/cmdb/firewall/internet-service-group", filter)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallInternetServiceGroup: %v", err)
+	}
+
+	var tmps []string
+	if o != nil {
+		if len(o) == 0 || o[0] == nil {
+			return nil
+		}
+
+		for _, r := range o {
+			i := r.(map[string]interface{})
+
+			if _, ok := i["name"]; ok {
+				tmps = append(tmps, fortiStringValue(i["name"]))
+			}
+		}
+	}
+	d.Set("namelist", tmps)
+
+	d.SetId("DataSourceFirewallInternetServiceGroupList" + filter)
+
+	return nil
+}
