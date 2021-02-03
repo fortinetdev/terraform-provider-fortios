19272635050909a62a7d2c59e13d82f31cdd5320
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:41 -0500

    Add new data source data_source_system_sflow

---
+++ fortios/data_source_system_sflow.go
@@ -0,0 +1,106 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure sFlow.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemSflow() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemSflowRead,
+		Schema: map[string]*schema.Schema{
+			"collector_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"collector_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemSflowRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := "SystemSflow"
+
+	o, err := c.ReadSystemSflow(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemSflow: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemSflow(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemSflow from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemSflowCollectorIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemSflowCollectorPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemSflowSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemSflow(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("collector_ip", dataSourceFlattenSystemSflowCollectorIp(o["collector-ip"], d, "collector_ip")); err != nil {
+		if !fortiAPIPatch(o["collector-ip"]) {
+			return fmt.Errorf("Error reading collector_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("collector_port", dataSourceFlattenSystemSflowCollectorPort(o["collector-port"], d, "collector_port")); err != nil {
+		if !fortiAPIPatch(o["collector-port"]) {
+			return fmt.Errorf("Error reading collector_port: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", dataSourceFlattenSystemSflowSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemSflowFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
