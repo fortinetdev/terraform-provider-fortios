66a9b8ee2a64400eeba10d12aaae56e257a86ea0
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:16 -0500

    Add new data source data_source_system_dns

---
+++ fortios/data_source_system_dns.go
@@ -0,0 +1,368 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure DNS.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemDns() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemDnsRead,
+		Schema: map[string]*schema.Schema{
+			"primary": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"secondary": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"dns_over_tls": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ssl_certificate": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"server_hostname": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"hostname": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"domain": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"domain": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"ip6_primary": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ip6_secondary": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"timeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"retry": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"dns_cache_limit": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"dns_cache_ttl": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"cache_notfound_responses": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"interface_select_method": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"interface": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemDnsRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := "SystemDns"
+
+	o, err := c.ReadSystemDns(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemDns: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemDns(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemDns from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemDnsPrimary(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsSecondary(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsDnsOverTls(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsSslCertificate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsServerHostname(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "hostname"
+		if _, ok := i["hostname"]; ok {
+			tmp["hostname"] = dataSourceFlattenSystemDnsServerHostnameHostname(i["hostname"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenSystemDnsServerHostnameHostname(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsDomain(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "domain"
+		if _, ok := i["domain"]; ok {
+			tmp["domain"] = dataSourceFlattenSystemDnsDomainDomain(i["domain"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenSystemDnsDomainDomain(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsIp6Primary(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsIp6Secondary(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsTimeout(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsRetry(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsDnsCacheLimit(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsDnsCacheTtl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsCacheNotfoundResponses(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsInterfaceSelectMethod(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDnsInterface(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemDns(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("primary", dataSourceFlattenSystemDnsPrimary(o["primary"], d, "primary")); err != nil {
+		if !fortiAPIPatch(o["primary"]) {
+			return fmt.Errorf("Error reading primary: %v", err)
+		}
+	}
+
+	if err = d.Set("secondary", dataSourceFlattenSystemDnsSecondary(o["secondary"], d, "secondary")); err != nil {
+		if !fortiAPIPatch(o["secondary"]) {
+			return fmt.Errorf("Error reading secondary: %v", err)
+		}
+	}
+
+	if err = d.Set("dns_over_tls", dataSourceFlattenSystemDnsDnsOverTls(o["dns-over-tls"], d, "dns_over_tls")); err != nil {
+		if !fortiAPIPatch(o["dns-over-tls"]) {
+			return fmt.Errorf("Error reading dns_over_tls: %v", err)
+		}
+	}
+
+	if err = d.Set("ssl_certificate", dataSourceFlattenSystemDnsSslCertificate(o["ssl-certificate"], d, "ssl_certificate")); err != nil {
+		if !fortiAPIPatch(o["ssl-certificate"]) {
+			return fmt.Errorf("Error reading ssl_certificate: %v", err)
+		}
+	}
+
+	if err = d.Set("server_hostname", dataSourceFlattenSystemDnsServerHostname(o["server-hostname"], d, "server_hostname")); err != nil {
+		if !fortiAPIPatch(o["server-hostname"]) {
+			return fmt.Errorf("Error reading server_hostname: %v", err)
+		}
+	}
+
+	if err = d.Set("domain", dataSourceFlattenSystemDnsDomain(o["domain"], d, "domain")); err != nil {
+		if !fortiAPIPatch(o["domain"]) {
+			return fmt.Errorf("Error reading domain: %v", err)
+		}
+	}
+
+	if err = d.Set("ip6_primary", dataSourceFlattenSystemDnsIp6Primary(o["ip6-primary"], d, "ip6_primary")); err != nil {
+		if !fortiAPIPatch(o["ip6-primary"]) {
+			return fmt.Errorf("Error reading ip6_primary: %v", err)
+		}
+	}
+
+	if err = d.Set("ip6_secondary", dataSourceFlattenSystemDnsIp6Secondary(o["ip6-secondary"], d, "ip6_secondary")); err != nil {
+		if !fortiAPIPatch(o["ip6-secondary"]) {
+			return fmt.Errorf("Error reading ip6_secondary: %v", err)
+		}
+	}
+
+	if err = d.Set("timeout", dataSourceFlattenSystemDnsTimeout(o["timeout"], d, "timeout")); err != nil {
+		if !fortiAPIPatch(o["timeout"]) {
+			return fmt.Errorf("Error reading timeout: %v", err)
+		}
+	}
+
+	if err = d.Set("retry", dataSourceFlattenSystemDnsRetry(o["retry"], d, "retry")); err != nil {
+		if !fortiAPIPatch(o["retry"]) {
+			return fmt.Errorf("Error reading retry: %v", err)
+		}
+	}
+
+	if err = d.Set("dns_cache_limit", dataSourceFlattenSystemDnsDnsCacheLimit(o["dns-cache-limit"], d, "dns_cache_limit")); err != nil {
+		if !fortiAPIPatch(o["dns-cache-limit"]) {
+			return fmt.Errorf("Error reading dns_cache_limit: %v", err)
+		}
+	}
+
+	if err = d.Set("dns_cache_ttl", dataSourceFlattenSystemDnsDnsCacheTtl(o["dns-cache-ttl"], d, "dns_cache_ttl")); err != nil {
+		if !fortiAPIPatch(o["dns-cache-ttl"]) {
+			return fmt.Errorf("Error reading dns_cache_ttl: %v", err)
+		}
+	}
+
+	if err = d.Set("cache_notfound_responses", dataSourceFlattenSystemDnsCacheNotfoundResponses(o["cache-notfound-responses"], d, "cache_notfound_responses")); err != nil {
+		if !fortiAPIPatch(o["cache-notfound-responses"]) {
+			return fmt.Errorf("Error reading cache_notfound_responses: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", dataSourceFlattenSystemDnsSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("interface_select_method", dataSourceFlattenSystemDnsInterfaceSelectMethod(o["interface-select-method"], d, "interface_select_method")); err != nil {
+		if !fortiAPIPatch(o["interface-select-method"]) {
+			return fmt.Errorf("Error reading interface_select_method: %v", err)
+		}
+	}
+
+	if err = d.Set("interface", dataSourceFlattenSystemDnsInterface(o["interface"], d, "interface")); err != nil {
+		if !fortiAPIPatch(o["interface"]) {
+			return fmt.Errorf("Error reading interface: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemDnsFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
