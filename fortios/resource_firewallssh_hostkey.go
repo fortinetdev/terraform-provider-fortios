9b9bf8a8881d37aa96672ed4e5846d024520eb2b
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:34 -0500

    Update resource resource_firewallssh_hostkey to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_firewallssh_hostkey.go
+++ fortios/resource_firewallssh_hostkey.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -80,7 +81,7 @@ func resourceFirewallSshHostKeyCreate(d *schema.ResourceData, m interface{}) err
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectFirewallSshHostKey(d)
+	obj, err := getObjectFirewallSshHostKey(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating FirewallSshHostKey resource while getting object: %v", err)
 	}
@@ -105,7 +106,7 @@ func resourceFirewallSshHostKeyUpdate(d *schema.ResourceData, m interface{}) err
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectFirewallSshHostKey(d)
+	obj, err := getObjectFirewallSshHostKey(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating FirewallSshHostKey resource while getting object: %v", err)
 	}
@@ -158,85 +159,85 @@ func resourceFirewallSshHostKeyRead(d *schema.ResourceData, m interface{}) error
 		return nil
 	}
 
-	err = refreshObjectFirewallSshHostKey(d, o)
+	err = refreshObjectFirewallSshHostKey(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading FirewallSshHostKey resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenFirewallSshHostKeyName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallSshHostKeyStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallSshHostKeyType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyType(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallSshHostKeyNid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyNid(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallSshHostKeyIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyIp(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallSshHostKeyPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyPort(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallSshHostKeyHostname(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyHostname(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallSshHostKeyPublicKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallSshHostKeyPublicKey(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectFirewallSshHostKey(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectFirewallSshHostKey(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("name", flattenFirewallSshHostKeyName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenFirewallSshHostKeyName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenFirewallSshHostKeyStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenFirewallSshHostKeyStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("type", flattenFirewallSshHostKeyType(o["type"], d, "type")); err != nil {
+	if err = d.Set("type", flattenFirewallSshHostKeyType(o["type"], d, "type", sv)); err != nil {
 		if !fortiAPIPatch(o["type"]) {
 			return fmt.Errorf("Error reading type: %v", err)
 		}
 	}
 
-	if err = d.Set("nid", flattenFirewallSshHostKeyNid(o["nid"], d, "nid")); err != nil {
+	if err = d.Set("nid", flattenFirewallSshHostKeyNid(o["nid"], d, "nid", sv)); err != nil {
 		if !fortiAPIPatch(o["nid"]) {
 			return fmt.Errorf("Error reading nid: %v", err)
 		}
 	}
 
-	if err = d.Set("ip", flattenFirewallSshHostKeyIp(o["ip"], d, "ip")); err != nil {
+	if err = d.Set("ip", flattenFirewallSshHostKeyIp(o["ip"], d, "ip", sv)); err != nil {
 		if !fortiAPIPatch(o["ip"]) {
 			return fmt.Errorf("Error reading ip: %v", err)
 		}
 	}
 
-	if err = d.Set("port", flattenFirewallSshHostKeyPort(o["port"], d, "port")); err != nil {
+	if err = d.Set("port", flattenFirewallSshHostKeyPort(o["port"], d, "port", sv)); err != nil {
 		if !fortiAPIPatch(o["port"]) {
 			return fmt.Errorf("Error reading port: %v", err)
 		}
 	}
 
-	if err = d.Set("hostname", flattenFirewallSshHostKeyHostname(o["hostname"], d, "hostname")); err != nil {
+	if err = d.Set("hostname", flattenFirewallSshHostKeyHostname(o["hostname"], d, "hostname", sv)); err != nil {
 		if !fortiAPIPatch(o["hostname"]) {
 			return fmt.Errorf("Error reading hostname: %v", err)
 		}
@@ -248,46 +249,47 @@ func refreshObjectFirewallSshHostKey(d *schema.ResourceData, o map[string]interf
 func flattenFirewallSshHostKeyFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandFirewallSshHostKeyName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallSshHostKeyStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallSshHostKeyType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyType(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallSshHostKeyNid(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyNid(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallSshHostKeyIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyIp(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallSshHostKeyPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyPort(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallSshHostKeyHostname(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyHostname(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallSshHostKeyPublicKey(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallSshHostKeyPublicKey(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectFirewallSshHostKey(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandFirewallSshHostKeyName(d, v, "name")
+
+		t, err := expandFirewallSshHostKeyName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -296,7 +298,8 @@ func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandFirewallSshHostKeyStatus(d, v, "status")
+
+		t, err := expandFirewallSshHostKeyStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -305,7 +308,8 @@ func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("type"); ok {
-		t, err := expandFirewallSshHostKeyType(d, v, "type")
+
+		t, err := expandFirewallSshHostKeyType(d, v, "type", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -314,7 +318,8 @@ func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("nid"); ok {
-		t, err := expandFirewallSshHostKeyNid(d, v, "nid")
+
+		t, err := expandFirewallSshHostKeyNid(d, v, "nid", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -323,7 +328,8 @@ func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("ip"); ok {
-		t, err := expandFirewallSshHostKeyIp(d, v, "ip")
+
+		t, err := expandFirewallSshHostKeyIp(d, v, "ip", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -332,7 +338,8 @@ func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOkExists("port"); ok {
-		t, err := expandFirewallSshHostKeyPort(d, v, "port")
+
+		t, err := expandFirewallSshHostKeyPort(d, v, "port", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -341,7 +348,8 @@ func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("hostname"); ok {
-		t, err := expandFirewallSshHostKeyHostname(d, v, "hostname")
+
+		t, err := expandFirewallSshHostKeyHostname(d, v, "hostname", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -350,7 +358,8 @@ func getObjectFirewallSshHostKey(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("public_key"); ok {
-		t, err := expandFirewallSshHostKeyPublicKey(d, v, "public_key")
+
+		t, err := expandFirewallSshHostKeyPublicKey(d, v, "public_key", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
