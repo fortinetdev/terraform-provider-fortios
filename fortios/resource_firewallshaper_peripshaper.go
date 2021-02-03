90710da33796d4310a0162698531d61ef0512af2
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:34 -0500

    Update resource resource_firewallshaper_peripshaper to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_firewallshaper_peripshaper.go
+++ fortios/resource_firewallshaper_peripshaper.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -52,6 +53,18 @@ func resourceFirewallShaperPerIpShaper() *schema.Resource {
 				Optional:     true,
 				Computed:     true,
 			},
+			"max_concurrent_tcp_session": &schema.Schema{
+				Type:         schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 2097000),
+				Optional:     true,
+				Computed:     true,
+			},
+			"max_concurrent_udp_session": &schema.Schema{
+				Type:         schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 2097000),
+				Optional:     true,
+				Computed:     true,
+			},
 			"diffserv_forward": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
@@ -80,7 +93,7 @@ func resourceFirewallShaperPerIpShaperCreate(d *schema.ResourceData, m interface
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectFirewallShaperPerIpShaper(d)
+	obj, err := getObjectFirewallShaperPerIpShaper(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating FirewallShaperPerIpShaper resource while getting object: %v", err)
 	}
@@ -105,7 +118,7 @@ func resourceFirewallShaperPerIpShaperUpdate(d *schema.ResourceData, m interface
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectFirewallShaperPerIpShaper(d)
+	obj, err := getObjectFirewallShaperPerIpShaper(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating FirewallShaperPerIpShaper resource while getting object: %v", err)
 	}
@@ -158,91 +171,111 @@ func resourceFirewallShaperPerIpShaperRead(d *schema.ResourceData, m interface{}
 		return nil
 	}
 
-	err = refreshObjectFirewallShaperPerIpShaper(d, o)
+	err = refreshObjectFirewallShaperPerIpShaper(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading FirewallShaperPerIpShaper resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenFirewallShaperPerIpShaperName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallShaperPerIpShaperMaxBandwidth(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperMaxBandwidth(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallShaperPerIpShaperBandwidthUnit(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperBandwidthUnit(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallShaperPerIpShaperMaxConcurrentSession(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperMaxConcurrentSession(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallShaperPerIpShaperDiffservForward(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperMaxConcurrentTcpSession(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallShaperPerIpShaperDiffservReverse(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperMaxConcurrentUdpSession(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallShaperPerIpShaperDiffservcodeForward(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperDiffservForward(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallShaperPerIpShaperDiffservcodeRev(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallShaperPerIpShaperDiffservReverse(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectFirewallShaperPerIpShaper(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenFirewallShaperPerIpShaperDiffservcodeForward(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenFirewallShaperPerIpShaperDiffservcodeRev(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectFirewallShaperPerIpShaper(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("name", flattenFirewallShaperPerIpShaperName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenFirewallShaperPerIpShaperName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("max_bandwidth", flattenFirewallShaperPerIpShaperMaxBandwidth(o["max-bandwidth"], d, "max_bandwidth")); err != nil {
+	if err = d.Set("max_bandwidth", flattenFirewallShaperPerIpShaperMaxBandwidth(o["max-bandwidth"], d, "max_bandwidth", sv)); err != nil {
 		if !fortiAPIPatch(o["max-bandwidth"]) {
 			return fmt.Errorf("Error reading max_bandwidth: %v", err)
 		}
 	}
 
-	if err = d.Set("bandwidth_unit", flattenFirewallShaperPerIpShaperBandwidthUnit(o["bandwidth-unit"], d, "bandwidth_unit")); err != nil {
+	if err = d.Set("bandwidth_unit", flattenFirewallShaperPerIpShaperBandwidthUnit(o["bandwidth-unit"], d, "bandwidth_unit", sv)); err != nil {
 		if !fortiAPIPatch(o["bandwidth-unit"]) {
 			return fmt.Errorf("Error reading bandwidth_unit: %v", err)
 		}
 	}
 
-	if err = d.Set("max_concurrent_session", flattenFirewallShaperPerIpShaperMaxConcurrentSession(o["max-concurrent-session"], d, "max_concurrent_session")); err != nil {
+	if err = d.Set("max_concurrent_session", flattenFirewallShaperPerIpShaperMaxConcurrentSession(o["max-concurrent-session"], d, "max_concurrent_session", sv)); err != nil {
 		if !fortiAPIPatch(o["max-concurrent-session"]) {
 			return fmt.Errorf("Error reading max_concurrent_session: %v", err)
 		}
 	}
 
-	if err = d.Set("diffserv_forward", flattenFirewallShaperPerIpShaperDiffservForward(o["diffserv-forward"], d, "diffserv_forward")); err != nil {
+	if err = d.Set("max_concurrent_tcp_session", flattenFirewallShaperPerIpShaperMaxConcurrentTcpSession(o["max-concurrent-tcp-session"], d, "max_concurrent_tcp_session", sv)); err != nil {
+		if !fortiAPIPatch(o["max-concurrent-tcp-session"]) {
+			return fmt.Errorf("Error reading max_concurrent_tcp_session: %v", err)
+		}
+	}
+
+	if err = d.Set("max_concurrent_udp_session", flattenFirewallShaperPerIpShaperMaxConcurrentUdpSession(o["max-concurrent-udp-session"], d, "max_concurrent_udp_session", sv)); err != nil {
+		if !fortiAPIPatch(o["max-concurrent-udp-session"]) {
+			return fmt.Errorf("Error reading max_concurrent_udp_session: %v", err)
+		}
+	}
+
+	if err = d.Set("diffserv_forward", flattenFirewallShaperPerIpShaperDiffservForward(o["diffserv-forward"], d, "diffserv_forward", sv)); err != nil {
 		if !fortiAPIPatch(o["diffserv-forward"]) {
 			return fmt.Errorf("Error reading diffserv_forward: %v", err)
 		}
 	}
 
-	if err = d.Set("diffserv_reverse", flattenFirewallShaperPerIpShaperDiffservReverse(o["diffserv-reverse"], d, "diffserv_reverse")); err != nil {
+	if err = d.Set("diffserv_reverse", flattenFirewallShaperPerIpShaperDiffservReverse(o["diffserv-reverse"], d, "diffserv_reverse", sv)); err != nil {
 		if !fortiAPIPatch(o["diffserv-reverse"]) {
 			return fmt.Errorf("Error reading diffserv_reverse: %v", err)
 		}
 	}
 
-	if err = d.Set("diffservcode_forward", flattenFirewallShaperPerIpShaperDiffservcodeForward(o["diffservcode-forward"], d, "diffservcode_forward")); err != nil {
+	if err = d.Set("diffservcode_forward", flattenFirewallShaperPerIpShaperDiffservcodeForward(o["diffservcode-forward"], d, "diffservcode_forward", sv)); err != nil {
 		if !fortiAPIPatch(o["diffservcode-forward"]) {
 			return fmt.Errorf("Error reading diffservcode_forward: %v", err)
 		}
 	}
 
-	if err = d.Set("diffservcode_rev", flattenFirewallShaperPerIpShaperDiffservcodeRev(o["diffservcode-rev"], d, "diffservcode_rev")); err != nil {
+	if err = d.Set("diffservcode_rev", flattenFirewallShaperPerIpShaperDiffservcodeRev(o["diffservcode-rev"], d, "diffservcode_rev", sv)); err != nil {
 		if !fortiAPIPatch(o["diffservcode-rev"]) {
 			return fmt.Errorf("Error reading diffservcode_rev: %v", err)
 		}
@@ -254,46 +287,55 @@ func refreshObjectFirewallShaperPerIpShaper(d *schema.ResourceData, o map[string
 func flattenFirewallShaperPerIpShaperFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandFirewallShaperPerIpShaperName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallShaperPerIpShaperMaxBandwidth(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperMaxBandwidth(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallShaperPerIpShaperBandwidthUnit(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperBandwidthUnit(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallShaperPerIpShaperMaxConcurrentSession(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperMaxConcurrentSession(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallShaperPerIpShaperDiffservForward(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperMaxConcurrentTcpSession(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallShaperPerIpShaperDiffservReverse(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperMaxConcurrentUdpSession(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallShaperPerIpShaperDiffservcodeForward(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperDiffservForward(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallShaperPerIpShaperDiffservcodeRev(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallShaperPerIpShaperDiffservReverse(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandFirewallShaperPerIpShaperDiffservcodeForward(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallShaperPerIpShaperDiffservcodeRev(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandFirewallShaperPerIpShaperName(d, v, "name")
+
+		t, err := expandFirewallShaperPerIpShaperName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -302,7 +344,8 @@ func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOkExists("max_bandwidth"); ok {
-		t, err := expandFirewallShaperPerIpShaperMaxBandwidth(d, v, "max_bandwidth")
+
+		t, err := expandFirewallShaperPerIpShaperMaxBandwidth(d, v, "max_bandwidth", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -311,7 +354,8 @@ func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("bandwidth_unit"); ok {
-		t, err := expandFirewallShaperPerIpShaperBandwidthUnit(d, v, "bandwidth_unit")
+
+		t, err := expandFirewallShaperPerIpShaperBandwidthUnit(d, v, "bandwidth_unit", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -320,7 +364,8 @@ func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOkExists("max_concurrent_session"); ok {
-		t, err := expandFirewallShaperPerIpShaperMaxConcurrentSession(d, v, "max_concurrent_session")
+
+		t, err := expandFirewallShaperPerIpShaperMaxConcurrentSession(d, v, "max_concurrent_session", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -328,8 +373,29 @@ func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]int
 		}
 	}
 
+	if v, ok := d.GetOkExists("max_concurrent_tcp_session"); ok {
+
+		t, err := expandFirewallShaperPerIpShaperMaxConcurrentTcpSession(d, v, "max_concurrent_tcp_session", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["max-concurrent-tcp-session"] = t
+		}
+	}
+
+	if v, ok := d.GetOkExists("max_concurrent_udp_session"); ok {
+
+		t, err := expandFirewallShaperPerIpShaperMaxConcurrentUdpSession(d, v, "max_concurrent_udp_session", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["max-concurrent-udp-session"] = t
+		}
+	}
+
 	if v, ok := d.GetOk("diffserv_forward"); ok {
-		t, err := expandFirewallShaperPerIpShaperDiffservForward(d, v, "diffserv_forward")
+
+		t, err := expandFirewallShaperPerIpShaperDiffservForward(d, v, "diffserv_forward", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -338,7 +404,8 @@ func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("diffserv_reverse"); ok {
-		t, err := expandFirewallShaperPerIpShaperDiffservReverse(d, v, "diffserv_reverse")
+
+		t, err := expandFirewallShaperPerIpShaperDiffservReverse(d, v, "diffserv_reverse", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -347,7 +414,8 @@ func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("diffservcode_forward"); ok {
-		t, err := expandFirewallShaperPerIpShaperDiffservcodeForward(d, v, "diffservcode_forward")
+
+		t, err := expandFirewallShaperPerIpShaperDiffservcodeForward(d, v, "diffservcode_forward", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -356,7 +424,8 @@ func getObjectFirewallShaperPerIpShaper(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("diffservcode_rev"); ok {
-		t, err := expandFirewallShaperPerIpShaperDiffservcodeRev(d, v, "diffservcode_rev")
+
+		t, err := expandFirewallShaperPerIpShaperDiffservcodeRev(d, v, "diffservcode_rev", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
