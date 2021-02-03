9ffed3ee3c9f71a08853cf15cd6317c6ac5d2852
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:26:44 -0500

    Update resource resource_webfilter_override to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_webfilter_override.go
+++ fortios/resource_webfilter_override.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -94,7 +95,7 @@ func resourceWebfilterOverrideCreate(d *schema.ResourceData, m interface{}) erro
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectWebfilterOverride(d)
+	obj, err := getObjectWebfilterOverride(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating WebfilterOverride resource while getting object: %v", err)
 	}
@@ -119,7 +120,7 @@ func resourceWebfilterOverrideUpdate(d *schema.ResourceData, m interface{}) erro
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectWebfilterOverride(d)
+	obj, err := getObjectWebfilterOverride(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating WebfilterOverride resource while getting object: %v", err)
 	}
@@ -172,121 +173,121 @@ func resourceWebfilterOverrideRead(d *schema.ResourceData, m interface{}) error 
 		return nil
 	}
 
-	err = refreshObjectWebfilterOverride(d, o)
+	err = refreshObjectWebfilterOverride(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading WebfilterOverride resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenWebfilterOverrideId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideScope(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideScope(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideIp(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideUser(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideUser(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideUserGroup(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideUserGroup(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideOldProfile(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideOldProfile(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideNewProfile(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideNewProfile(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideIp6(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideIp6(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideExpires(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideExpires(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWebfilterOverrideInitiator(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWebfilterOverrideInitiator(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectWebfilterOverride(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectWebfilterOverride(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("fosid", flattenWebfilterOverrideId(o["id"], d, "fosid")); err != nil {
+	if err = d.Set("fosid", flattenWebfilterOverrideId(o["id"], d, "fosid", sv)); err != nil {
 		if !fortiAPIPatch(o["id"]) {
 			return fmt.Errorf("Error reading fosid: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenWebfilterOverrideStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenWebfilterOverrideStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("scope", flattenWebfilterOverrideScope(o["scope"], d, "scope")); err != nil {
+	if err = d.Set("scope", flattenWebfilterOverrideScope(o["scope"], d, "scope", sv)); err != nil {
 		if !fortiAPIPatch(o["scope"]) {
 			return fmt.Errorf("Error reading scope: %v", err)
 		}
 	}
 
-	if err = d.Set("ip", flattenWebfilterOverrideIp(o["ip"], d, "ip")); err != nil {
+	if err = d.Set("ip", flattenWebfilterOverrideIp(o["ip"], d, "ip", sv)); err != nil {
 		if !fortiAPIPatch(o["ip"]) {
 			return fmt.Errorf("Error reading ip: %v", err)
 		}
 	}
 
-	if err = d.Set("user", flattenWebfilterOverrideUser(o["user"], d, "user")); err != nil {
+	if err = d.Set("user", flattenWebfilterOverrideUser(o["user"], d, "user", sv)); err != nil {
 		if !fortiAPIPatch(o["user"]) {
 			return fmt.Errorf("Error reading user: %v", err)
 		}
 	}
 
-	if err = d.Set("user_group", flattenWebfilterOverrideUserGroup(o["user-group"], d, "user_group")); err != nil {
+	if err = d.Set("user_group", flattenWebfilterOverrideUserGroup(o["user-group"], d, "user_group", sv)); err != nil {
 		if !fortiAPIPatch(o["user-group"]) {
 			return fmt.Errorf("Error reading user_group: %v", err)
 		}
 	}
 
-	if err = d.Set("old_profile", flattenWebfilterOverrideOldProfile(o["old-profile"], d, "old_profile")); err != nil {
+	if err = d.Set("old_profile", flattenWebfilterOverrideOldProfile(o["old-profile"], d, "old_profile", sv)); err != nil {
 		if !fortiAPIPatch(o["old-profile"]) {
 			return fmt.Errorf("Error reading old_profile: %v", err)
 		}
 	}
 
-	if err = d.Set("new_profile", flattenWebfilterOverrideNewProfile(o["new-profile"], d, "new_profile")); err != nil {
+	if err = d.Set("new_profile", flattenWebfilterOverrideNewProfile(o["new-profile"], d, "new_profile", sv)); err != nil {
 		if !fortiAPIPatch(o["new-profile"]) {
 			return fmt.Errorf("Error reading new_profile: %v", err)
 		}
 	}
 
-	if err = d.Set("ip6", flattenWebfilterOverrideIp6(o["ip6"], d, "ip6")); err != nil {
+	if err = d.Set("ip6", flattenWebfilterOverrideIp6(o["ip6"], d, "ip6", sv)); err != nil {
 		if !fortiAPIPatch(o["ip6"]) {
 			return fmt.Errorf("Error reading ip6: %v", err)
 		}
 	}
 
-	if err = d.Set("expires", flattenWebfilterOverrideExpires(o["expires"], d, "expires")); err != nil {
+	if err = d.Set("expires", flattenWebfilterOverrideExpires(o["expires"], d, "expires", sv)); err != nil {
 		if !fortiAPIPatch(o["expires"]) {
 			return fmt.Errorf("Error reading expires: %v", err)
 		}
 	}
 
-	if err = d.Set("initiator", flattenWebfilterOverrideInitiator(o["initiator"], d, "initiator")); err != nil {
+	if err = d.Set("initiator", flattenWebfilterOverrideInitiator(o["initiator"], d, "initiator", sv)); err != nil {
 		if !fortiAPIPatch(o["initiator"]) {
 			return fmt.Errorf("Error reading initiator: %v", err)
 		}
@@ -298,58 +299,59 @@ func refreshObjectWebfilterOverride(d *schema.ResourceData, o map[string]interfa
 func flattenWebfilterOverrideFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandWebfilterOverrideId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideScope(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideScope(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideIp(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideUser(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideUser(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideUserGroup(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideUserGroup(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideOldProfile(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideOldProfile(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideNewProfile(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideNewProfile(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideIp6(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideIp6(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideExpires(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideExpires(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWebfilterOverrideInitiator(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWebfilterOverrideInitiator(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectWebfilterOverride(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOkExists("fosid"); ok {
-		t, err := expandWebfilterOverrideId(d, v, "fosid")
+
+		t, err := expandWebfilterOverrideId(d, v, "fosid", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -358,7 +360,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandWebfilterOverrideStatus(d, v, "status")
+
+		t, err := expandWebfilterOverrideStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -367,7 +370,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("scope"); ok {
-		t, err := expandWebfilterOverrideScope(d, v, "scope")
+
+		t, err := expandWebfilterOverrideScope(d, v, "scope", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -376,7 +380,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("ip"); ok {
-		t, err := expandWebfilterOverrideIp(d, v, "ip")
+
+		t, err := expandWebfilterOverrideIp(d, v, "ip", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -385,7 +390,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("user"); ok {
-		t, err := expandWebfilterOverrideUser(d, v, "user")
+
+		t, err := expandWebfilterOverrideUser(d, v, "user", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -394,7 +400,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("user_group"); ok {
-		t, err := expandWebfilterOverrideUserGroup(d, v, "user_group")
+
+		t, err := expandWebfilterOverrideUserGroup(d, v, "user_group", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -403,7 +410,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("old_profile"); ok {
-		t, err := expandWebfilterOverrideOldProfile(d, v, "old_profile")
+
+		t, err := expandWebfilterOverrideOldProfile(d, v, "old_profile", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -412,7 +420,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("new_profile"); ok {
-		t, err := expandWebfilterOverrideNewProfile(d, v, "new_profile")
+
+		t, err := expandWebfilterOverrideNewProfile(d, v, "new_profile", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -421,7 +430,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("ip6"); ok {
-		t, err := expandWebfilterOverrideIp6(d, v, "ip6")
+
+		t, err := expandWebfilterOverrideIp6(d, v, "ip6", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -430,7 +440,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("expires"); ok {
-		t, err := expandWebfilterOverrideExpires(d, v, "expires")
+
+		t, err := expandWebfilterOverrideExpires(d, v, "expires", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -439,7 +450,8 @@ func getObjectWebfilterOverride(d *schema.ResourceData) (*map[string]interface{}
 	}
 
 	if v, ok := d.GetOk("initiator"); ok {
-		t, err := expandWebfilterOverrideInitiator(d, v, "initiator")
+
+		t, err := expandWebfilterOverrideInitiator(d, v, "initiator", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
