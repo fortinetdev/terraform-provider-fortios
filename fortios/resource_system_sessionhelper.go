a269445507527bfa26892fd350a559e7f8538d54
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:25:50 -0500

    Update resource resource_system_sessionhelper to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_system_sessionhelper.go
+++ fortios/resource_system_sessionhelper.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -57,7 +58,7 @@ func resourceSystemSessionHelperCreate(d *schema.ResourceData, m interface{}) er
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemSessionHelper(d)
+	obj, err := getObjectSystemSessionHelper(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating SystemSessionHelper resource while getting object: %v", err)
 	}
@@ -82,7 +83,7 @@ func resourceSystemSessionHelperUpdate(d *schema.ResourceData, m interface{}) er
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemSessionHelper(d)
+	obj, err := getObjectSystemSessionHelper(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemSessionHelper resource while getting object: %v", err)
 	}
@@ -135,51 +136,51 @@ func resourceSystemSessionHelperRead(d *schema.ResourceData, m interface{}) erro
 		return nil
 	}
 
-	err = refreshObjectSystemSessionHelper(d, o)
+	err = refreshObjectSystemSessionHelper(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemSessionHelper resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemSessionHelperId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemSessionHelperId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemSessionHelperName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemSessionHelperName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemSessionHelperProtocol(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemSessionHelperProtocol(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemSessionHelperPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemSessionHelperPort(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemSessionHelper(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectSystemSessionHelper(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("fosid", flattenSystemSessionHelperId(o["id"], d, "fosid")); err != nil {
+	if err = d.Set("fosid", flattenSystemSessionHelperId(o["id"], d, "fosid", sv)); err != nil {
 		if !fortiAPIPatch(o["id"]) {
 			return fmt.Errorf("Error reading fosid: %v", err)
 		}
 	}
 
-	if err = d.Set("name", flattenSystemSessionHelperName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenSystemSessionHelperName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("protocol", flattenSystemSessionHelperProtocol(o["protocol"], d, "protocol")); err != nil {
+	if err = d.Set("protocol", flattenSystemSessionHelperProtocol(o["protocol"], d, "protocol", sv)); err != nil {
 		if !fortiAPIPatch(o["protocol"]) {
 			return fmt.Errorf("Error reading protocol: %v", err)
 		}
 	}
 
-	if err = d.Set("port", flattenSystemSessionHelperPort(o["port"], d, "port")); err != nil {
+	if err = d.Set("port", flattenSystemSessionHelperPort(o["port"], d, "port", sv)); err != nil {
 		if !fortiAPIPatch(o["port"]) {
 			return fmt.Errorf("Error reading port: %v", err)
 		}
@@ -191,30 +192,31 @@ func refreshObjectSystemSessionHelper(d *schema.ResourceData, o map[string]inter
 func flattenSystemSessionHelperFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemSessionHelperId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemSessionHelperId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemSessionHelperName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemSessionHelperName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemSessionHelperProtocol(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemSessionHelperProtocol(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemSessionHelperPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemSessionHelperPort(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemSessionHelper(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectSystemSessionHelper(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOkExists("fosid"); ok {
-		t, err := expandSystemSessionHelperId(d, v, "fosid")
+
+		t, err := expandSystemSessionHelperId(d, v, "fosid", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -223,7 +225,8 @@ func getObjectSystemSessionHelper(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandSystemSessionHelperName(d, v, "name")
+
+		t, err := expandSystemSessionHelperName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -232,7 +235,8 @@ func getObjectSystemSessionHelper(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOkExists("protocol"); ok {
-		t, err := expandSystemSessionHelperProtocol(d, v, "protocol")
+
+		t, err := expandSystemSessionHelperProtocol(d, v, "protocol", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -241,7 +245,8 @@ func getObjectSystemSessionHelper(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("port"); ok {
-		t, err := expandSystemSessionHelperPort(d, v, "port")
+
+		t, err := expandSystemSessionHelperPort(d, v, "port", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
