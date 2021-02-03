5fd29297a8f751c16f2501b05fc42a1dff6ad0bc
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:45 -0500

    Add new data source data_source_system_vdomexception

---
+++ fortios/data_source_system_vdomexception.go
@@ -0,0 +1,183 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Global configuration objects that can be configured independently for all VDOMs or for the defined VDOM scope.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemVdomException() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemVdomExceptionRead,
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"object": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"oid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"scope": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"vdom": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceSystemVdomExceptionRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("fosid")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing SystemVdomException: type error")
+	}
+
+	o, err := c.ReadSystemVdomException(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemVdomException: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemVdomException(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemVdomException from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemVdomExceptionId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemVdomExceptionObject(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemVdomExceptionOid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemVdomExceptionScope(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemVdomExceptionVdom(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenSystemVdomExceptionVdomName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenSystemVdomExceptionVdomName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemVdomException(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("fosid", dataSourceFlattenSystemVdomExceptionId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("object", dataSourceFlattenSystemVdomExceptionObject(o["object"], d, "object")); err != nil {
+		if !fortiAPIPatch(o["object"]) {
+			return fmt.Errorf("Error reading object: %v", err)
+		}
+	}
+
+	if err = d.Set("oid", dataSourceFlattenSystemVdomExceptionOid(o["oid"], d, "oid")); err != nil {
+		if !fortiAPIPatch(o["oid"]) {
+			return fmt.Errorf("Error reading oid: %v", err)
+		}
+	}
+
+	if err = d.Set("scope", dataSourceFlattenSystemVdomExceptionScope(o["scope"], d, "scope")); err != nil {
+		if !fortiAPIPatch(o["scope"]) {
+			return fmt.Errorf("Error reading scope: %v", err)
+		}
+	}
+
+	if err = d.Set("vdom", dataSourceFlattenSystemVdomExceptionVdom(o["vdom"], d, "vdom")); err != nil {
+		if !fortiAPIPatch(o["vdom"]) {
+			return fmt.Errorf("Error reading vdom: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemVdomExceptionFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
