2d5fdf0978927f91ea209278b9a0b534fe8f57b8
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:30 -0500

    Update resource resource_firewallipmacbinding_table to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_firewallipmacbinding_table.go
+++ fortios/resource_firewallipmacbinding_table.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -62,7 +63,7 @@ func resourceFirewallIpmacbindingTableCreate(d *schema.ResourceData, m interface
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectFirewallIpmacbindingTable(d)
+	obj, err := getObjectFirewallIpmacbindingTable(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating FirewallIpmacbindingTable resource while getting object: %v", err)
 	}
@@ -87,7 +88,7 @@ func resourceFirewallIpmacbindingTableUpdate(d *schema.ResourceData, m interface
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectFirewallIpmacbindingTable(d)
+	obj, err := getObjectFirewallIpmacbindingTable(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating FirewallIpmacbindingTable resource while getting object: %v", err)
 	}
@@ -140,61 +141,61 @@ func resourceFirewallIpmacbindingTableRead(d *schema.ResourceData, m interface{}
 		return nil
 	}
 
-	err = refreshObjectFirewallIpmacbindingTable(d, o)
+	err = refreshObjectFirewallIpmacbindingTable(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading FirewallIpmacbindingTable resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenFirewallIpmacbindingTableSeqNum(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpmacbindingTableSeqNum(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpmacbindingTableIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpmacbindingTableIp(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpmacbindingTableMac(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpmacbindingTableMac(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpmacbindingTableName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpmacbindingTableName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpmacbindingTableStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpmacbindingTableStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectFirewallIpmacbindingTable(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectFirewallIpmacbindingTable(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("seq_num", flattenFirewallIpmacbindingTableSeqNum(o["seq-num"], d, "seq_num")); err != nil {
+	if err = d.Set("seq_num", flattenFirewallIpmacbindingTableSeqNum(o["seq-num"], d, "seq_num", sv)); err != nil {
 		if !fortiAPIPatch(o["seq-num"]) {
 			return fmt.Errorf("Error reading seq_num: %v", err)
 		}
 	}
 
-	if err = d.Set("ip", flattenFirewallIpmacbindingTableIp(o["ip"], d, "ip")); err != nil {
+	if err = d.Set("ip", flattenFirewallIpmacbindingTableIp(o["ip"], d, "ip", sv)); err != nil {
 		if !fortiAPIPatch(o["ip"]) {
 			return fmt.Errorf("Error reading ip: %v", err)
 		}
 	}
 
-	if err = d.Set("mac", flattenFirewallIpmacbindingTableMac(o["mac"], d, "mac")); err != nil {
+	if err = d.Set("mac", flattenFirewallIpmacbindingTableMac(o["mac"], d, "mac", sv)); err != nil {
 		if !fortiAPIPatch(o["mac"]) {
 			return fmt.Errorf("Error reading mac: %v", err)
 		}
 	}
 
-	if err = d.Set("name", flattenFirewallIpmacbindingTableName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenFirewallIpmacbindingTableName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenFirewallIpmacbindingTableStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenFirewallIpmacbindingTableStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
@@ -206,34 +207,35 @@ func refreshObjectFirewallIpmacbindingTable(d *schema.ResourceData, o map[string
 func flattenFirewallIpmacbindingTableFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandFirewallIpmacbindingTableSeqNum(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpmacbindingTableSeqNum(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpmacbindingTableIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpmacbindingTableIp(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpmacbindingTableMac(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpmacbindingTableMac(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpmacbindingTableName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpmacbindingTableName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpmacbindingTableStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpmacbindingTableStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectFirewallIpmacbindingTable(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectFirewallIpmacbindingTable(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOkExists("seq_num"); ok {
-		t, err := expandFirewallIpmacbindingTableSeqNum(d, v, "seq_num")
+
+		t, err := expandFirewallIpmacbindingTableSeqNum(d, v, "seq_num", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -242,7 +244,8 @@ func getObjectFirewallIpmacbindingTable(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("ip"); ok {
-		t, err := expandFirewallIpmacbindingTableIp(d, v, "ip")
+
+		t, err := expandFirewallIpmacbindingTableIp(d, v, "ip", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -251,7 +254,8 @@ func getObjectFirewallIpmacbindingTable(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("mac"); ok {
-		t, err := expandFirewallIpmacbindingTableMac(d, v, "mac")
+
+		t, err := expandFirewallIpmacbindingTableMac(d, v, "mac", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -260,7 +264,8 @@ func getObjectFirewallIpmacbindingTable(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandFirewallIpmacbindingTableName(d, v, "name")
+
+		t, err := expandFirewallIpmacbindingTableName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -269,7 +274,8 @@ func getObjectFirewallIpmacbindingTable(d *schema.ResourceData) (*map[string]int
 	}
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandFirewallIpmacbindingTableStatus(d, v, "status")
+
+		t, err := expandFirewallIpmacbindingTableStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
