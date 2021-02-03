4e5bf8fa4d8e67afa32c1857e1ce80f2cce7a8c8
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:03 -0500

    Add new data source data_source_firewallwildcardfqdn_custom

---
+++ fortios/data_source_firewallwildcardfqdn_custom.go
@@ -0,0 +1,157 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Config global/VDOM Wildcard FQDN address.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceFirewallWildcardFqdnCustom() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceFirewallWildcardFqdnCustomRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"uuid": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"wildcard_fqdn": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"color": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"visibility": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceFirewallWildcardFqdnCustomRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("name")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing FirewallWildcardFqdnCustom: type error")
+	}
+
+	o, err := c.ReadFirewallWildcardFqdnCustom(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallWildcardFqdnCustom: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectFirewallWildcardFqdnCustom(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallWildcardFqdnCustom from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenFirewallWildcardFqdnCustomName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallWildcardFqdnCustomUuid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallWildcardFqdnCustomWildcardFqdn(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallWildcardFqdnCustomColor(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallWildcardFqdnCustomComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenFirewallWildcardFqdnCustomVisibility(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectFirewallWildcardFqdnCustom(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", dataSourceFlattenFirewallWildcardFqdnCustomName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("uuid", dataSourceFlattenFirewallWildcardFqdnCustomUuid(o["uuid"], d, "uuid")); err != nil {
+		if !fortiAPIPatch(o["uuid"]) {
+			return fmt.Errorf("Error reading uuid: %v", err)
+		}
+	}
+
+	if err = d.Set("wildcard_fqdn", dataSourceFlattenFirewallWildcardFqdnCustomWildcardFqdn(o["wildcard-fqdn"], d, "wildcard_fqdn")); err != nil {
+		if !fortiAPIPatch(o["wildcard-fqdn"]) {
+			return fmt.Errorf("Error reading wildcard_fqdn: %v", err)
+		}
+	}
+
+	if err = d.Set("color", dataSourceFlattenFirewallWildcardFqdnCustomColor(o["color"], d, "color")); err != nil {
+		if !fortiAPIPatch(o["color"]) {
+			return fmt.Errorf("Error reading color: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", dataSourceFlattenFirewallWildcardFqdnCustomComment(o["comment"], d, "comment")); err != nil {
+		if !fortiAPIPatch(o["comment"]) {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("visibility", dataSourceFlattenFirewallWildcardFqdnCustomVisibility(o["visibility"], d, "visibility")); err != nil {
+		if !fortiAPIPatch(o["visibility"]) {
+			return fmt.Errorf("Error reading visibility: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenFirewallWildcardFqdnCustomFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
