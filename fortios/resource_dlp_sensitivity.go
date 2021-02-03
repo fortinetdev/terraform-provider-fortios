1878acbd5cf408d525045ba95e10356a2235cb42
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:58 -0500

    Add new resource resource_dlp_sensitivity

---
+++ fortios/resource_dlp_sensitivity.go
@@ -0,0 +1,172 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Create self-explanatory DLP sensitivity levels to be used when setting sensitivity under config fp-doc-source.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceDlpSensitivity() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceDlpSensitivityCreate,
+		Read:   resourceDlpSensitivityRead,
+		Update: resourceDlpSensitivityUpdate,
+		Delete: resourceDlpSensitivityDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 35),
+				Optional:     true,
+				Computed:     true,
+			},
+		},
+	}
+}
+
+func resourceDlpSensitivityCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectDlpSensitivity(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating DlpSensitivity resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateDlpSensitivity(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating DlpSensitivity resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("DlpSensitivity")
+	}
+
+	return resourceDlpSensitivityRead(d, m)
+}
+
+func resourceDlpSensitivityUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectDlpSensitivity(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating DlpSensitivity resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateDlpSensitivity(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating DlpSensitivity resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("DlpSensitivity")
+	}
+
+	return resourceDlpSensitivityRead(d, m)
+}
+
+func resourceDlpSensitivityDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteDlpSensitivity(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting DlpSensitivity resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceDlpSensitivityRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadDlpSensitivity(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading DlpSensitivity resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectDlpSensitivity(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading DlpSensitivity resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenDlpSensitivityName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectDlpSensitivity(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("name", flattenDlpSensitivityName(o["name"], d, "name", sv)); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenDlpSensitivityFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandDlpSensitivityName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectDlpSensitivity(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+
+		t, err := expandDlpSensitivityName(d, v, "name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	return &obj, nil
+}
