f77b49ee3f92f1ca0d8d27a163fbb9ec5c453592
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:49 -0500

    Add new data source data_source_systemautoupdate_pushupdate

---
+++ fortios/data_source_systemautoupdate_pushupdate.go
@@ -0,0 +1,120 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure push updates.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemAutoupdatePushUpdate() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemAutoupdatePushUpdateRead,
+		Schema: map[string]*schema.Schema{
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"override": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"address": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemAutoupdatePushUpdateRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := "SystemAutoupdatePushUpdate"
+
+	o, err := c.ReadSystemAutoupdatePushUpdate(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemAutoupdatePushUpdate: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemAutoupdatePushUpdate(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemAutoupdatePushUpdate from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemAutoupdatePushUpdateStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemAutoupdatePushUpdateOverride(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemAutoupdatePushUpdateAddress(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemAutoupdatePushUpdatePort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemAutoupdatePushUpdate(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("status", dataSourceFlattenSystemAutoupdatePushUpdateStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("override", dataSourceFlattenSystemAutoupdatePushUpdateOverride(o["override"], d, "override")); err != nil {
+		if !fortiAPIPatch(o["override"]) {
+			return fmt.Errorf("Error reading override: %v", err)
+		}
+	}
+
+	if err = d.Set("address", dataSourceFlattenSystemAutoupdatePushUpdateAddress(o["address"], d, "address")); err != nil {
+		if !fortiAPIPatch(o["address"]) {
+			return fmt.Errorf("Error reading address: %v", err)
+		}
+	}
+
+	if err = d.Set("port", dataSourceFlattenSystemAutoupdatePushUpdatePort(o["port"], d, "port")); err != nil {
+		if !fortiAPIPatch(o["port"]) {
+			return fmt.Errorf("Error reading port: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemAutoupdatePushUpdateFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
