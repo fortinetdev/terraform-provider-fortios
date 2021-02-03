6cbb06724262f56a66c3c74dc9541528d63eb236
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:18 -0500

    Add new resource resource_switchcontroller_snmpuser

---
+++ fortios/resource_switchcontroller_snmpuser.go
@@ -0,0 +1,367 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure FortiSwitch SNMP v3 users globally.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSwitchControllerSnmpUser() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSwitchControllerSnmpUserCreate,
+		Read:   resourceSwitchControllerSnmpUserRead,
+		Update: resourceSwitchControllerSnmpUserUpdate,
+		Delete: resourceSwitchControllerSnmpUserDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 32),
+				ForceNew:     true,
+				Optional:     true,
+				Computed:     true,
+			},
+			"queries": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"query_port": &schema.Schema{
+				Type:         schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 65535),
+				Optional:     true,
+				Computed:     true,
+			},
+			"security_level": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"auth_proto": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"auth_pwd": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 128),
+				Optional:     true,
+				Sensitive:    true,
+			},
+			"priv_proto": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"priv_pwd": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 128),
+				Optional:     true,
+				Sensitive:    true,
+			},
+		},
+	}
+}
+
+func resourceSwitchControllerSnmpUserCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSwitchControllerSnmpUser(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating SwitchControllerSnmpUser resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSwitchControllerSnmpUser(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SwitchControllerSnmpUser resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SwitchControllerSnmpUser")
+	}
+
+	return resourceSwitchControllerSnmpUserRead(d, m)
+}
+
+func resourceSwitchControllerSnmpUserUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSwitchControllerSnmpUser(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating SwitchControllerSnmpUser resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSwitchControllerSnmpUser(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SwitchControllerSnmpUser resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SwitchControllerSnmpUser")
+	}
+
+	return resourceSwitchControllerSnmpUserRead(d, m)
+}
+
+func resourceSwitchControllerSnmpUserDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSwitchControllerSnmpUser(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SwitchControllerSnmpUser resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSwitchControllerSnmpUserRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSwitchControllerSnmpUser(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SwitchControllerSnmpUser resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSwitchControllerSnmpUser(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading SwitchControllerSnmpUser resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSwitchControllerSnmpUserName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerSnmpUserQueries(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerSnmpUserQueryPort(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerSnmpUserSecurityLevel(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerSnmpUserAuthProto(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerSnmpUserAuthPwd(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerSnmpUserPrivProto(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerSnmpUserPrivPwd(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectSwitchControllerSnmpUser(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("name", flattenSwitchControllerSnmpUserName(o["name"], d, "name", sv)); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("queries", flattenSwitchControllerSnmpUserQueries(o["queries"], d, "queries", sv)); err != nil {
+		if !fortiAPIPatch(o["queries"]) {
+			return fmt.Errorf("Error reading queries: %v", err)
+		}
+	}
+
+	if err = d.Set("query_port", flattenSwitchControllerSnmpUserQueryPort(o["query-port"], d, "query_port", sv)); err != nil {
+		if !fortiAPIPatch(o["query-port"]) {
+			return fmt.Errorf("Error reading query_port: %v", err)
+		}
+	}
+
+	if err = d.Set("security_level", flattenSwitchControllerSnmpUserSecurityLevel(o["security-level"], d, "security_level", sv)); err != nil {
+		if !fortiAPIPatch(o["security-level"]) {
+			return fmt.Errorf("Error reading security_level: %v", err)
+		}
+	}
+
+	if err = d.Set("auth_proto", flattenSwitchControllerSnmpUserAuthProto(o["auth-proto"], d, "auth_proto", sv)); err != nil {
+		if !fortiAPIPatch(o["auth-proto"]) {
+			return fmt.Errorf("Error reading auth_proto: %v", err)
+		}
+	}
+
+	if err = d.Set("priv_proto", flattenSwitchControllerSnmpUserPrivProto(o["priv-proto"], d, "priv_proto", sv)); err != nil {
+		if !fortiAPIPatch(o["priv-proto"]) {
+			return fmt.Errorf("Error reading priv_proto: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSwitchControllerSnmpUserFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandSwitchControllerSnmpUserName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerSnmpUserQueries(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerSnmpUserQueryPort(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerSnmpUserSecurityLevel(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerSnmpUserAuthProto(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerSnmpUserAuthPwd(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerSnmpUserPrivProto(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerSnmpUserPrivPwd(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSwitchControllerSnmpUser(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+
+		t, err := expandSwitchControllerSnmpUserName(d, v, "name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("queries"); ok {
+
+		t, err := expandSwitchControllerSnmpUserQueries(d, v, "queries", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["queries"] = t
+		}
+	}
+
+	if v, ok := d.GetOkExists("query_port"); ok {
+
+		t, err := expandSwitchControllerSnmpUserQueryPort(d, v, "query_port", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["query-port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("security_level"); ok {
+
+		t, err := expandSwitchControllerSnmpUserSecurityLevel(d, v, "security_level", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["security-level"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("auth_proto"); ok {
+
+		t, err := expandSwitchControllerSnmpUserAuthProto(d, v, "auth_proto", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["auth-proto"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("auth_pwd"); ok {
+
+		t, err := expandSwitchControllerSnmpUserAuthPwd(d, v, "auth_pwd", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["auth-pwd"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("priv_proto"); ok {
+
+		t, err := expandSwitchControllerSnmpUserPrivProto(d, v, "priv_proto", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["priv-proto"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("priv_pwd"); ok {
+
+		t, err := expandSwitchControllerSnmpUserPrivPwd(d, v, "priv_pwd", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["priv-pwd"] = t
+		}
+	}
+
+	return &obj, nil
+}
