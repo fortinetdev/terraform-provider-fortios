62cceeeed1ae7da4be442edaa7ecbe39a5730c54
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:39 -0500

    Update resource resource_ips_custom to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_ips_custom.go
+++ fortios/resource_ips_custom.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -112,7 +113,7 @@ func resourceIpsCustomCreate(d *schema.ResourceData, m interface{}) error {
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectIpsCustom(d)
+	obj, err := getObjectIpsCustom(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating IpsCustom resource while getting object: %v", err)
 	}
@@ -137,7 +138,7 @@ func resourceIpsCustomUpdate(d *schema.ResourceData, m interface{}) error {
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectIpsCustom(d)
+	obj, err := getObjectIpsCustom(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating IpsCustom resource while getting object: %v", err)
 	}
@@ -190,151 +191,151 @@ func resourceIpsCustomRead(d *schema.ResourceData, m interface{}) error {
 		return nil
 	}
 
-	err = refreshObjectIpsCustom(d, o)
+	err = refreshObjectIpsCustom(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading IpsCustom resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenIpsCustomTag(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomTag(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomSignature(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomSignature(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomSigName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomSigName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomRuleId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomRuleId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomSeverity(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomSeverity(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomLocation(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomLocation(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomOs(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomOs(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomApplication(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomApplication(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomProtocol(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomProtocol(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomLog(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomLog(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomLogPacket(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomLogPacket(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomAction(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIpsCustomComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIpsCustomComment(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectIpsCustom(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectIpsCustom(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("tag", flattenIpsCustomTag(o["tag"], d, "tag")); err != nil {
+	if err = d.Set("tag", flattenIpsCustomTag(o["tag"], d, "tag", sv)); err != nil {
 		if !fortiAPIPatch(o["tag"]) {
 			return fmt.Errorf("Error reading tag: %v", err)
 		}
 	}
 
-	if err = d.Set("signature", flattenIpsCustomSignature(o["signature"], d, "signature")); err != nil {
+	if err = d.Set("signature", flattenIpsCustomSignature(o["signature"], d, "signature", sv)); err != nil {
 		if !fortiAPIPatch(o["signature"]) {
 			return fmt.Errorf("Error reading signature: %v", err)
 		}
 	}
 
-	if err = d.Set("sig_name", flattenIpsCustomSigName(o["sig-name"], d, "sig_name")); err != nil {
+	if err = d.Set("sig_name", flattenIpsCustomSigName(o["sig-name"], d, "sig_name", sv)); err != nil {
 		if !fortiAPIPatch(o["sig-name"]) {
 			return fmt.Errorf("Error reading sig_name: %v", err)
 		}
 	}
 
-	if err = d.Set("rule_id", flattenIpsCustomRuleId(o["rule-id"], d, "rule_id")); err != nil {
+	if err = d.Set("rule_id", flattenIpsCustomRuleId(o["rule-id"], d, "rule_id", sv)); err != nil {
 		if !fortiAPIPatch(o["rule-id"]) {
 			return fmt.Errorf("Error reading rule_id: %v", err)
 		}
 	}
 
-	if err = d.Set("severity", flattenIpsCustomSeverity(o["severity"], d, "severity")); err != nil {
+	if err = d.Set("severity", flattenIpsCustomSeverity(o["severity"], d, "severity", sv)); err != nil {
 		if !fortiAPIPatch(o["severity"]) {
 			return fmt.Errorf("Error reading severity: %v", err)
 		}
 	}
 
-	if err = d.Set("location", flattenIpsCustomLocation(o["location"], d, "location")); err != nil {
+	if err = d.Set("location", flattenIpsCustomLocation(o["location"], d, "location", sv)); err != nil {
 		if !fortiAPIPatch(o["location"]) {
 			return fmt.Errorf("Error reading location: %v", err)
 		}
 	}
 
-	if err = d.Set("os", flattenIpsCustomOs(o["os"], d, "os")); err != nil {
+	if err = d.Set("os", flattenIpsCustomOs(o["os"], d, "os", sv)); err != nil {
 		if !fortiAPIPatch(o["os"]) {
 			return fmt.Errorf("Error reading os: %v", err)
 		}
 	}
 
-	if err = d.Set("application", flattenIpsCustomApplication(o["application"], d, "application")); err != nil {
+	if err = d.Set("application", flattenIpsCustomApplication(o["application"], d, "application", sv)); err != nil {
 		if !fortiAPIPatch(o["application"]) {
 			return fmt.Errorf("Error reading application: %v", err)
 		}
 	}
 
-	if err = d.Set("protocol", flattenIpsCustomProtocol(o["protocol"], d, "protocol")); err != nil {
+	if err = d.Set("protocol", flattenIpsCustomProtocol(o["protocol"], d, "protocol", sv)); err != nil {
 		if !fortiAPIPatch(o["protocol"]) {
 			return fmt.Errorf("Error reading protocol: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenIpsCustomStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenIpsCustomStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("log", flattenIpsCustomLog(o["log"], d, "log")); err != nil {
+	if err = d.Set("log", flattenIpsCustomLog(o["log"], d, "log", sv)); err != nil {
 		if !fortiAPIPatch(o["log"]) {
 			return fmt.Errorf("Error reading log: %v", err)
 		}
 	}
 
-	if err = d.Set("log_packet", flattenIpsCustomLogPacket(o["log-packet"], d, "log_packet")); err != nil {
+	if err = d.Set("log_packet", flattenIpsCustomLogPacket(o["log-packet"], d, "log_packet", sv)); err != nil {
 		if !fortiAPIPatch(o["log-packet"]) {
 			return fmt.Errorf("Error reading log_packet: %v", err)
 		}
 	}
 
-	if err = d.Set("action", flattenIpsCustomAction(o["action"], d, "action")); err != nil {
+	if err = d.Set("action", flattenIpsCustomAction(o["action"], d, "action", sv)); err != nil {
 		if !fortiAPIPatch(o["action"]) {
 			return fmt.Errorf("Error reading action: %v", err)
 		}
 	}
 
-	if err = d.Set("comment", flattenIpsCustomComment(o["comment"], d, "comment")); err != nil {
+	if err = d.Set("comment", flattenIpsCustomComment(o["comment"], d, "comment", sv)); err != nil {
 		if !fortiAPIPatch(o["comment"]) {
 			return fmt.Errorf("Error reading comment: %v", err)
 		}
@@ -346,70 +347,71 @@ func refreshObjectIpsCustom(d *schema.ResourceData, o map[string]interface{}) er
 func flattenIpsCustomFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandIpsCustomTag(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomTag(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomSignature(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomSignature(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomSigName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomSigName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomRuleId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomRuleId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomSeverity(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomSeverity(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomLocation(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomLocation(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomOs(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomOs(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomApplication(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomApplication(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomProtocol(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomProtocol(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomLog(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomLog(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomLogPacket(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomLogPacket(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomAction(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIpsCustomComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIpsCustomComment(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectIpsCustom(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("tag"); ok {
-		t, err := expandIpsCustomTag(d, v, "tag")
+
+		t, err := expandIpsCustomTag(d, v, "tag", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -418,7 +420,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("signature"); ok {
-		t, err := expandIpsCustomSignature(d, v, "signature")
+
+		t, err := expandIpsCustomSignature(d, v, "signature", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -427,7 +430,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("sig_name"); ok {
-		t, err := expandIpsCustomSigName(d, v, "sig_name")
+
+		t, err := expandIpsCustomSigName(d, v, "sig_name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -436,7 +440,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOkExists("rule_id"); ok {
-		t, err := expandIpsCustomRuleId(d, v, "rule_id")
+
+		t, err := expandIpsCustomRuleId(d, v, "rule_id", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -445,7 +450,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("severity"); ok {
-		t, err := expandIpsCustomSeverity(d, v, "severity")
+
+		t, err := expandIpsCustomSeverity(d, v, "severity", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -454,7 +460,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("location"); ok {
-		t, err := expandIpsCustomLocation(d, v, "location")
+
+		t, err := expandIpsCustomLocation(d, v, "location", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -463,7 +470,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("os"); ok {
-		t, err := expandIpsCustomOs(d, v, "os")
+
+		t, err := expandIpsCustomOs(d, v, "os", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -472,7 +480,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("application"); ok {
-		t, err := expandIpsCustomApplication(d, v, "application")
+
+		t, err := expandIpsCustomApplication(d, v, "application", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -481,7 +490,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("protocol"); ok {
-		t, err := expandIpsCustomProtocol(d, v, "protocol")
+
+		t, err := expandIpsCustomProtocol(d, v, "protocol", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -490,7 +500,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandIpsCustomStatus(d, v, "status")
+
+		t, err := expandIpsCustomStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -499,7 +510,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("log"); ok {
-		t, err := expandIpsCustomLog(d, v, "log")
+
+		t, err := expandIpsCustomLog(d, v, "log", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -508,7 +520,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("log_packet"); ok {
-		t, err := expandIpsCustomLogPacket(d, v, "log_packet")
+
+		t, err := expandIpsCustomLogPacket(d, v, "log_packet", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -517,7 +530,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("action"); ok {
-		t, err := expandIpsCustomAction(d, v, "action")
+
+		t, err := expandIpsCustomAction(d, v, "action", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -526,7 +540,8 @@ func getObjectIpsCustom(d *schema.ResourceData) (*map[string]interface{}, error)
 	}
 
 	if v, ok := d.GetOk("comment"); ok {
-		t, err := expandIpsCustomComment(d, v, "comment")
+
+		t, err := expandIpsCustomComment(d, v, "comment", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
