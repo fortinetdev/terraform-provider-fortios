717a8b66e695dc261bd0105300d7e8453d524506
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:22 -0500

    Add new data source data_source_system_fortisandbox

---
+++ fortios/data_source_system_fortisandbox.go
@@ -0,0 +1,190 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure FortiSandbox.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemFortisandbox() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemFortisandboxRead,
+		Schema: map[string]*schema.Schema{
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"forticloud": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"server": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"interface_select_method": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"interface": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"enc_algorithm": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ssl_min_proto_version": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"email": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemFortisandboxRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := "SystemFortisandbox"
+
+	o, err := c.ReadSystemFortisandbox(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemFortisandbox: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemFortisandbox(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemFortisandbox from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemFortisandboxStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxForticloud(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxInterfaceSelectMethod(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxInterface(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxEncAlgorithm(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxSslMinProtoVersion(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemFortisandboxEmail(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemFortisandbox(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("status", dataSourceFlattenSystemFortisandboxStatus(o["status"], d, "status")); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("forticloud", dataSourceFlattenSystemFortisandboxForticloud(o["forticloud"], d, "forticloud")); err != nil {
+		if !fortiAPIPatch(o["forticloud"]) {
+			return fmt.Errorf("Error reading forticloud: %v", err)
+		}
+	}
+
+	if err = d.Set("server", dataSourceFlattenSystemFortisandboxServer(o["server"], d, "server")); err != nil {
+		if !fortiAPIPatch(o["server"]) {
+			return fmt.Errorf("Error reading server: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", dataSourceFlattenSystemFortisandboxSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("interface_select_method", dataSourceFlattenSystemFortisandboxInterfaceSelectMethod(o["interface-select-method"], d, "interface_select_method")); err != nil {
+		if !fortiAPIPatch(o["interface-select-method"]) {
+			return fmt.Errorf("Error reading interface_select_method: %v", err)
+		}
+	}
+
+	if err = d.Set("interface", dataSourceFlattenSystemFortisandboxInterface(o["interface"], d, "interface")); err != nil {
+		if !fortiAPIPatch(o["interface"]) {
+			return fmt.Errorf("Error reading interface: %v", err)
+		}
+	}
+
+	if err = d.Set("enc_algorithm", dataSourceFlattenSystemFortisandboxEncAlgorithm(o["enc-algorithm"], d, "enc_algorithm")); err != nil {
+		if !fortiAPIPatch(o["enc-algorithm"]) {
+			return fmt.Errorf("Error reading enc_algorithm: %v", err)
+		}
+	}
+
+	if err = d.Set("ssl_min_proto_version", dataSourceFlattenSystemFortisandboxSslMinProtoVersion(o["ssl-min-proto-version"], d, "ssl_min_proto_version")); err != nil {
+		if !fortiAPIPatch(o["ssl-min-proto-version"]) {
+			return fmt.Errorf("Error reading ssl_min_proto_version: %v", err)
+		}
+	}
+
+	if err = d.Set("email", dataSourceFlattenSystemFortisandboxEmail(o["email"], d, "email")); err != nil {
+		if !fortiAPIPatch(o["email"]) {
+			return fmt.Errorf("Error reading email: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemFortisandboxFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
