b0fc8a0b3962f14e279d1553008f51bfc20d61bc
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:27 -0500

    Add new data source data_source_system_ipv6neighborcachelist

---
+++ fortios/data_source_system_ipv6neighborcachelist.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+package fortios
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceSystemIpv6NeighborCacheList() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemIpv6NeighborCacheListRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"fosidlist": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeInt},
+			},
+		},
+	}
+}
+
+func dataSourceSystemIpv6NeighborCacheListRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	filter := d.Get("filter").(string)
+	if filter != "" {
+		filter = escapeFilter(filter)
+	}
+
+	o, err := c.GenericGroupRead("/api/v2/cmdb/system/ipv6-neighbor-cache", filter)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemIpv6NeighborCache: %v", err)
+	}
+
+	var tmps []int
+	if o != nil {
+		if len(o) == 0 || o[0] == nil {
+			return nil
+		}
+
+		for _, r := range o {
+			i := r.(map[string]interface{})
+
+			if _, ok := i["id"]; ok {
+				tmps = append(tmps, fortiIntValue(i["id"]))
+			}
+		}
+	}
+	d.Set("fosidlist", tmps)
+
+	d.SetId("DataSourceSystemIpv6NeighborCacheList" + filter)
+
+	return nil
+}
