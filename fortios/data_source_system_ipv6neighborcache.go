95d3dd169d3c159ff87d67ea03843f3960015c78
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:26 -0500

    Add new data source data_source_system_ipv6neighborcache

---
+++ fortios/data_source_system_ipv6neighborcache.go
@@ -0,0 +1,129 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure IPv6 neighbor cache table.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemIpv6NeighborCache() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemIpv6NeighborCacheRead,
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"interface": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ipv6": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"mac": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemIpv6NeighborCacheRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("fosid")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing SystemIpv6NeighborCache: type error")
+	}
+
+	o, err := c.ReadSystemIpv6NeighborCache(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemIpv6NeighborCache: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemIpv6NeighborCache(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemIpv6NeighborCache from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemIpv6NeighborCacheId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemIpv6NeighborCacheInterface(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemIpv6NeighborCacheIpv6(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemIpv6NeighborCacheMac(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemIpv6NeighborCache(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("fosid", dataSourceFlattenSystemIpv6NeighborCacheId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("interface", dataSourceFlattenSystemIpv6NeighborCacheInterface(o["interface"], d, "interface")); err != nil {
+		if !fortiAPIPatch(o["interface"]) {
+			return fmt.Errorf("Error reading interface: %v", err)
+		}
+	}
+
+	if err = d.Set("ipv6", dataSourceFlattenSystemIpv6NeighborCacheIpv6(o["ipv6"], d, "ipv6")); err != nil {
+		if !fortiAPIPatch(o["ipv6"]) {
+			return fmt.Errorf("Error reading ipv6: %v", err)
+		}
+	}
+
+	if err = d.Set("mac", dataSourceFlattenSystemIpv6NeighborCacheMac(o["mac"], d, "mac")); err != nil {
+		if !fortiAPIPatch(o["mac"]) {
+			return fmt.Errorf("Error reading mac: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemIpv6NeighborCacheFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
