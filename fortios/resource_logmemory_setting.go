cfee8f0dc04b6556fa118571889ae68b2dc31e96
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:54 -0500

    Update resource resource_logmemory_setting to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_logmemory_setting.go
+++ fortios/resource_logmemory_setting.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -48,7 +49,7 @@ func resourceLogMemorySettingUpdate(d *schema.ResourceData, m interface{}) error
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectLogMemorySetting(d)
+	obj, err := getObjectLogMemorySetting(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating LogMemorySetting resource while getting object: %v", err)
 	}
@@ -101,31 +102,31 @@ func resourceLogMemorySettingRead(d *schema.ResourceData, m interface{}) error {
 		return nil
 	}
 
-	err = refreshObjectLogMemorySetting(d, o)
+	err = refreshObjectLogMemorySetting(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading LogMemorySetting resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenLogMemorySettingStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogMemorySettingStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogMemorySettingDiskfull(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogMemorySettingDiskfull(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectLogMemorySetting(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectLogMemorySetting(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("status", flattenLogMemorySettingStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenLogMemorySettingStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("diskfull", flattenLogMemorySettingDiskfull(o["diskfull"], d, "diskfull")); err != nil {
+	if err = d.Set("diskfull", flattenLogMemorySettingDiskfull(o["diskfull"], d, "diskfull", sv)); err != nil {
 		if !fortiAPIPatch(o["diskfull"]) {
 			return fmt.Errorf("Error reading diskfull: %v", err)
 		}
@@ -137,22 +138,23 @@ func refreshObjectLogMemorySetting(d *schema.ResourceData, o map[string]interfac
 func flattenLogMemorySettingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandLogMemorySettingStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogMemorySettingStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogMemorySettingDiskfull(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogMemorySettingDiskfull(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectLogMemorySetting(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectLogMemorySetting(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandLogMemorySettingStatus(d, v, "status")
+
+		t, err := expandLogMemorySettingStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -161,7 +163,8 @@ func getObjectLogMemorySetting(d *schema.ResourceData) (*map[string]interface{},
 	}
 
 	if v, ok := d.GetOk("diskfull"); ok {
-		t, err := expandLogMemorySettingDiskfull(d, v, "diskfull")
+
+		t, err := expandLogMemorySettingDiskfull(d, v, "diskfull", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
