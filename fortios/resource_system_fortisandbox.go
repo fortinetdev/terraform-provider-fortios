fa3bbc90fc5a4eaacaf72913d263a2d72146e6b7
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:25:35 -0500

    Update resource resource_system_fortisandbox to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_system_fortisandbox.go
+++ fortios/resource_system_fortisandbox.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -34,6 +35,11 @@ func resourceSystemFortisandbox() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
+			"forticloud": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
 			"server": &schema.Schema{
 				Type:         schema.TypeString,
 				ValidateFunc: validation.StringLenBetween(0, 63),
@@ -43,6 +49,17 @@ func resourceSystemFortisandbox() *schema.Resource {
 			"source_ip": &schema.Schema{
 				Type:         schema.TypeString,
 				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional:     true,
+				Computed:     true,
+			},
+			"interface_select_method": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"interface": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 15),
 				Optional:     true,
 				Computed:     true,
 			},
@@ -71,7 +88,7 @@ func resourceSystemFortisandboxUpdate(d *schema.ResourceData, m interface{}) err
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemFortisandbox(d)
+	obj, err := getObjectSystemFortisandbox(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemFortisandbox resource while getting object: %v", err)
 	}
@@ -124,71 +141,101 @@ func resourceSystemFortisandboxRead(d *schema.ResourceData, m interface{}) error
 		return nil
 	}
 
-	err = refreshObjectSystemFortisandbox(d, o)
+	err = refreshObjectSystemFortisandbox(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemFortisandbox resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemFortisandboxStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemFortisandboxStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemFortisandboxServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemFortisandboxForticloud(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemFortisandboxSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemFortisandboxServer(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemFortisandboxEncAlgorithm(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemFortisandboxSourceIp(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemFortisandboxSslMinProtoVersion(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemFortisandboxInterfaceSelectMethod(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemFortisandboxEmail(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemFortisandboxInterface(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemFortisandbox(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenSystemFortisandboxEncAlgorithm(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemFortisandboxSslMinProtoVersion(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemFortisandboxEmail(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectSystemFortisandbox(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("status", flattenSystemFortisandboxStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenSystemFortisandboxStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("server", flattenSystemFortisandboxServer(o["server"], d, "server")); err != nil {
+	if err = d.Set("forticloud", flattenSystemFortisandboxForticloud(o["forticloud"], d, "forticloud", sv)); err != nil {
+		if !fortiAPIPatch(o["forticloud"]) {
+			return fmt.Errorf("Error reading forticloud: %v", err)
+		}
+	}
+
+	if err = d.Set("server", flattenSystemFortisandboxServer(o["server"], d, "server", sv)); err != nil {
 		if !fortiAPIPatch(o["server"]) {
 			return fmt.Errorf("Error reading server: %v", err)
 		}
 	}
 
-	if err = d.Set("source_ip", flattenSystemFortisandboxSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+	if err = d.Set("source_ip", flattenSystemFortisandboxSourceIp(o["source-ip"], d, "source_ip", sv)); err != nil {
 		if !fortiAPIPatch(o["source-ip"]) {
 			return fmt.Errorf("Error reading source_ip: %v", err)
 		}
 	}
 
-	if err = d.Set("enc_algorithm", flattenSystemFortisandboxEncAlgorithm(o["enc-algorithm"], d, "enc_algorithm")); err != nil {
+	if err = d.Set("interface_select_method", flattenSystemFortisandboxInterfaceSelectMethod(o["interface-select-method"], d, "interface_select_method", sv)); err != nil {
+		if !fortiAPIPatch(o["interface-select-method"]) {
+			return fmt.Errorf("Error reading interface_select_method: %v", err)
+		}
+	}
+
+	if err = d.Set("interface", flattenSystemFortisandboxInterface(o["interface"], d, "interface", sv)); err != nil {
+		if !fortiAPIPatch(o["interface"]) {
+			return fmt.Errorf("Error reading interface: %v", err)
+		}
+	}
+
+	if err = d.Set("enc_algorithm", flattenSystemFortisandboxEncAlgorithm(o["enc-algorithm"], d, "enc_algorithm", sv)); err != nil {
 		if !fortiAPIPatch(o["enc-algorithm"]) {
 			return fmt.Errorf("Error reading enc_algorithm: %v", err)
 		}
 	}
 
-	if err = d.Set("ssl_min_proto_version", flattenSystemFortisandboxSslMinProtoVersion(o["ssl-min-proto-version"], d, "ssl_min_proto_version")); err != nil {
+	if err = d.Set("ssl_min_proto_version", flattenSystemFortisandboxSslMinProtoVersion(o["ssl-min-proto-version"], d, "ssl_min_proto_version", sv)); err != nil {
 		if !fortiAPIPatch(o["ssl-min-proto-version"]) {
 			return fmt.Errorf("Error reading ssl_min_proto_version: %v", err)
 		}
 	}
 
-	if err = d.Set("email", flattenSystemFortisandboxEmail(o["email"], d, "email")); err != nil {
+	if err = d.Set("email", flattenSystemFortisandboxEmail(o["email"], d, "email", sv)); err != nil {
 		if !fortiAPIPatch(o["email"]) {
 			return fmt.Errorf("Error reading email: %v", err)
 		}
@@ -200,38 +247,51 @@ func refreshObjectSystemFortisandbox(d *schema.ResourceData, o map[string]interf
 func flattenSystemFortisandboxFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemFortisandboxStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemFortisandboxStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemFortisandboxServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemFortisandboxForticloud(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemFortisandboxSourceIp(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemFortisandboxServer(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemFortisandboxEncAlgorithm(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemFortisandboxSourceIp(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemFortisandboxSslMinProtoVersion(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemFortisandboxInterfaceSelectMethod(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemFortisandboxEmail(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemFortisandboxInterface(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemFortisandbox(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandSystemFortisandboxEncAlgorithm(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemFortisandboxSslMinProtoVersion(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemFortisandboxEmail(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemFortisandbox(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandSystemFortisandboxStatus(d, v, "status")
+
+		t, err := expandSystemFortisandboxStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -239,8 +299,19 @@ func getObjectSystemFortisandbox(d *schema.ResourceData) (*map[string]interface{
 		}
 	}
 
+	if v, ok := d.GetOk("forticloud"); ok {
+
+		t, err := expandSystemFortisandboxForticloud(d, v, "forticloud", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["forticloud"] = t
+		}
+	}
+
 	if v, ok := d.GetOk("server"); ok {
-		t, err := expandSystemFortisandboxServer(d, v, "server")
+
+		t, err := expandSystemFortisandboxServer(d, v, "server", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -249,7 +320,8 @@ func getObjectSystemFortisandbox(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("source_ip"); ok {
-		t, err := expandSystemFortisandboxSourceIp(d, v, "source_ip")
+
+		t, err := expandSystemFortisandboxSourceIp(d, v, "source_ip", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -257,8 +329,29 @@ func getObjectSystemFortisandbox(d *schema.ResourceData) (*map[string]interface{
 		}
 	}
 
+	if v, ok := d.GetOk("interface_select_method"); ok {
+
+		t, err := expandSystemFortisandboxInterfaceSelectMethod(d, v, "interface_select_method", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["interface-select-method"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("interface"); ok {
+
+		t, err := expandSystemFortisandboxInterface(d, v, "interface", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["interface"] = t
+		}
+	}
+
 	if v, ok := d.GetOk("enc_algorithm"); ok {
-		t, err := expandSystemFortisandboxEncAlgorithm(d, v, "enc_algorithm")
+
+		t, err := expandSystemFortisandboxEncAlgorithm(d, v, "enc_algorithm", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -267,7 +360,8 @@ func getObjectSystemFortisandbox(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("ssl_min_proto_version"); ok {
-		t, err := expandSystemFortisandboxSslMinProtoVersion(d, v, "ssl_min_proto_version")
+
+		t, err := expandSystemFortisandboxSslMinProtoVersion(d, v, "ssl_min_proto_version", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -276,7 +370,8 @@ func getObjectSystemFortisandbox(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOk("email"); ok {
-		t, err := expandSystemFortisandboxEmail(d, v, "email")
+
+		t, err := expandSystemFortisandboxEmail(d, v, "email", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
