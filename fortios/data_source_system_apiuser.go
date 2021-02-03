fa1c493ae1e069b8ff1dc1e1e2ed0d9522282005
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:07 -0500

    Add new data source data_source_system_apiuser

---
+++ fortios/data_source_system_apiuser.go
@@ -0,0 +1,334 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure API users.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemApiUser() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemApiUserRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"api_key": &schema.Schema{
+				Type:      schema.TypeString,
+				Sensitive: true,
+				Computed:  true,
+			},
+			"accprofile": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"vdom": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"schedule": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cors_allow_origin": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"peer_auth": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"peer_group": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"trusthost": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"type": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ipv4_trusthost": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ipv6_trusthost": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceSystemApiUserRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("name")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing SystemApiUser: type error")
+	}
+
+	o, err := c.ReadSystemApiUser(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemApiUser: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemApiUser(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemApiUser from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemApiUserName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserApiKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserAccprofile(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserVdom(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			tmp["name"] = dataSourceFlattenSystemApiUserVdomName(i["name"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenSystemApiUserVdomName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserSchedule(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserCorsAllowOrigin(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserPeerAuth(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserPeerGroup(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserTrusthost(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = dataSourceFlattenSystemApiUserTrusthostId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "type"
+		if _, ok := i["type"]; ok {
+			tmp["type"] = dataSourceFlattenSystemApiUserTrusthostType(i["type"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ipv4_trusthost"
+		if _, ok := i["ipv4-trusthost"]; ok {
+			tmp["ipv4_trusthost"] = dataSourceFlattenSystemApiUserTrusthostIpv4Trusthost(i["ipv4-trusthost"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ipv6_trusthost"
+		if _, ok := i["ipv6-trusthost"]; ok {
+			tmp["ipv6_trusthost"] = dataSourceFlattenSystemApiUserTrusthostIpv6Trusthost(i["ipv6-trusthost"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenSystemApiUserTrusthostId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserTrusthostType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemApiUserTrusthostIpv4Trusthost(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v1, ok := d.GetOkExists(pre); ok && v != nil {
+		if s, ok := v1.(string); ok {
+			v = validateConvIPMask2CIDR(s, v.(string))
+			return v
+		}
+	}
+
+	return v
+}
+
+func dataSourceFlattenSystemApiUserTrusthostIpv6Trusthost(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemApiUser(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", dataSourceFlattenSystemApiUserName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("comments", dataSourceFlattenSystemApiUserComments(o["comments"], d, "comments")); err != nil {
+		if !fortiAPIPatch(o["comments"]) {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+	if err = d.Set("accprofile", dataSourceFlattenSystemApiUserAccprofile(o["accprofile"], d, "accprofile")); err != nil {
+		if !fortiAPIPatch(o["accprofile"]) {
+			return fmt.Errorf("Error reading accprofile: %v", err)
+		}
+	}
+
+	if err = d.Set("vdom", dataSourceFlattenSystemApiUserVdom(o["vdom"], d, "vdom")); err != nil {
+		if !fortiAPIPatch(o["vdom"]) {
+			return fmt.Errorf("Error reading vdom: %v", err)
+		}
+	}
+
+	if err = d.Set("schedule", dataSourceFlattenSystemApiUserSchedule(o["schedule"], d, "schedule")); err != nil {
+		if !fortiAPIPatch(o["schedule"]) {
+			return fmt.Errorf("Error reading schedule: %v", err)
+		}
+	}
+
+	if err = d.Set("cors_allow_origin", dataSourceFlattenSystemApiUserCorsAllowOrigin(o["cors-allow-origin"], d, "cors_allow_origin")); err != nil {
+		if !fortiAPIPatch(o["cors-allow-origin"]) {
+			return fmt.Errorf("Error reading cors_allow_origin: %v", err)
+		}
+	}
+
+	if err = d.Set("peer_auth", dataSourceFlattenSystemApiUserPeerAuth(o["peer-auth"], d, "peer_auth")); err != nil {
+		if !fortiAPIPatch(o["peer-auth"]) {
+			return fmt.Errorf("Error reading peer_auth: %v", err)
+		}
+	}
+
+	if err = d.Set("peer_group", dataSourceFlattenSystemApiUserPeerGroup(o["peer-group"], d, "peer_group")); err != nil {
+		if !fortiAPIPatch(o["peer-group"]) {
+			return fmt.Errorf("Error reading peer_group: %v", err)
+		}
+	}
+
+	if err = d.Set("trusthost", dataSourceFlattenSystemApiUserTrusthost(o["trusthost"], d, "trusthost")); err != nil {
+		if !fortiAPIPatch(o["trusthost"]) {
+			return fmt.Errorf("Error reading trusthost: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemApiUserFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
