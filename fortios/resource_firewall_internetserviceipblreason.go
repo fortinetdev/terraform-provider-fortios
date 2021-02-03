3c222b50f78db240354e770b5a3f3d7927ab0fca
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:07 -0500

    Add new resource resource_firewall_internetserviceipblreason

---
+++ fortios/resource_firewall_internetserviceipblreason.go
@@ -0,0 +1,201 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: IP blacklist reason.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceFirewallInternetServiceIpblReason() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFirewallInternetServiceIpblReasonCreate,
+		Read:   resourceFirewallInternetServiceIpblReasonRead,
+		Update: resourceFirewallInternetServiceIpblReasonUpdate,
+		Delete: resourceFirewallInternetServiceIpblReasonDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional:     true,
+				Computed:     true,
+			},
+		},
+	}
+}
+
+func resourceFirewallInternetServiceIpblReasonCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallInternetServiceIpblReason(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallInternetServiceIpblReason resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateFirewallInternetServiceIpblReason(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallInternetServiceIpblReason resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallInternetServiceIpblReason")
+	}
+
+	return resourceFirewallInternetServiceIpblReasonRead(d, m)
+}
+
+func resourceFirewallInternetServiceIpblReasonUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallInternetServiceIpblReason(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallInternetServiceIpblReason resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateFirewallInternetServiceIpblReason(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallInternetServiceIpblReason resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallInternetServiceIpblReason")
+	}
+
+	return resourceFirewallInternetServiceIpblReasonRead(d, m)
+}
+
+func resourceFirewallInternetServiceIpblReasonDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteFirewallInternetServiceIpblReason(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting FirewallInternetServiceIpblReason resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFirewallInternetServiceIpblReasonRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadFirewallInternetServiceIpblReason(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallInternetServiceIpblReason resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectFirewallInternetServiceIpblReason(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallInternetServiceIpblReason resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenFirewallInternetServiceIpblReasonId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenFirewallInternetServiceIpblReasonName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectFirewallInternetServiceIpblReason(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("fosid", flattenFirewallInternetServiceIpblReasonId(o["id"], d, "fosid", sv)); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenFirewallInternetServiceIpblReasonName(o["name"], d, "name", sv)); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenFirewallInternetServiceIpblReasonFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandFirewallInternetServiceIpblReasonId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallInternetServiceIpblReasonName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectFirewallInternetServiceIpblReason(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOkExists("fosid"); ok {
+
+		t, err := expandFirewallInternetServiceIpblReasonId(d, v, "fosid", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+
+		t, err := expandFirewallInternetServiceIpblReasonName(d, v, "name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	return &obj, nil
+}
