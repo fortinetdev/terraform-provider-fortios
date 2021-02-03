df428135bdd0ba66d90f6b35b224720c29dfcc20
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:41 -0500

    Add new data source data_source_system_sessionttl

---
+++ fortios/data_source_system_sessionttl.go
@@ -0,0 +1,184 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure global session TTL timers for this FortiGate.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemSessionTtl() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemSessionTtlRead,
+		Schema: map[string]*schema.Schema{
+			"default": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"port": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"protocol": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"start_port": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"end_port": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"timeout": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceSystemSessionTtlRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := "SystemSessionTtl"
+
+	o, err := c.ReadSystemSessionTtl(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemSessionTtl: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemSessionTtl(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemSessionTtl from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemSessionTtlDefault(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemSessionTtlPort(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			tmp["id"] = dataSourceFlattenSystemSessionTtlPortId(i["id"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "protocol"
+		if _, ok := i["protocol"]; ok {
+			tmp["protocol"] = dataSourceFlattenSystemSessionTtlPortProtocol(i["protocol"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "start_port"
+		if _, ok := i["start-port"]; ok {
+			tmp["start_port"] = dataSourceFlattenSystemSessionTtlPortStartPort(i["start-port"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "end_port"
+		if _, ok := i["end-port"]; ok {
+			tmp["end_port"] = dataSourceFlattenSystemSessionTtlPortEndPort(i["end-port"], d, pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "timeout"
+		if _, ok := i["timeout"]; ok {
+			tmp["timeout"] = dataSourceFlattenSystemSessionTtlPortTimeout(i["timeout"], d, pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func dataSourceFlattenSystemSessionTtlPortId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemSessionTtlPortProtocol(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemSessionTtlPortStartPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemSessionTtlPortEndPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemSessionTtlPortTimeout(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemSessionTtl(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("default", dataSourceFlattenSystemSessionTtlDefault(o["default"], d, "default")); err != nil {
+		if !fortiAPIPatch(o["default"]) {
+			return fmt.Errorf("Error reading default: %v", err)
+		}
+	}
+
+	if err = d.Set("port", dataSourceFlattenSystemSessionTtlPort(o["port"], d, "port")); err != nil {
+		if !fortiAPIPatch(o["port"]) {
+			return fmt.Errorf("Error reading port: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemSessionTtlFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
