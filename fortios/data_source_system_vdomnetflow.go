81101b32c735f4682023d10c46f5caf2d43c2ac4
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:45 -0500

    Add new data source data_source_system_vdomnetflow

---
+++ fortios/data_source_system_vdomnetflow.go
@@ -0,0 +1,120 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure NetFlow per VDOM.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemVdomNetflow() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemVdomNetflowRead,
+		Schema: map[string]*schema.Schema{
+			"vdom_netflow": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"collector_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"collector_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemVdomNetflowRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := "SystemVdomNetflow"
+
+	o, err := c.ReadSystemVdomNetflow(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemVdomNetflow: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemVdomNetflow(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemVdomNetflow from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemVdomNetflowVdomNetflow(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemVdomNetflowCollectorIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemVdomNetflowCollectorPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemVdomNetflowSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemVdomNetflow(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("vdom_netflow", dataSourceFlattenSystemVdomNetflowVdomNetflow(o["vdom-netflow"], d, "vdom_netflow")); err != nil {
+		if !fortiAPIPatch(o["vdom-netflow"]) {
+			return fmt.Errorf("Error reading vdom_netflow: %v", err)
+		}
+	}
+
+	if err = d.Set("collector_ip", dataSourceFlattenSystemVdomNetflowCollectorIp(o["collector-ip"], d, "collector_ip")); err != nil {
+		if !fortiAPIPatch(o["collector-ip"]) {
+			return fmt.Errorf("Error reading collector_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("collector_port", dataSourceFlattenSystemVdomNetflowCollectorPort(o["collector-port"], d, "collector_port")); err != nil {
+		if !fortiAPIPatch(o["collector-port"]) {
+			return fmt.Errorf("Error reading collector_port: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", dataSourceFlattenSystemVdomNetflowSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemVdomNetflowFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
