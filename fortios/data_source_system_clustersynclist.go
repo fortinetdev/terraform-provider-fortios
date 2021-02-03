380990304266570af953667954b5bd078ec073d4
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:14 -0500

    Add new data source data_source_system_clustersynclist

---
+++ fortios/data_source_system_clustersynclist.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+package fortios
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceSystemClusterSyncList() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemClusterSyncListRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"sync_idlist": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeInt},
+			},
+		},
+	}
+}
+
+func dataSourceSystemClusterSyncListRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	filter := d.Get("filter").(string)
+	if filter != "" {
+		filter = escapeFilter(filter)
+	}
+
+	o, err := c.GenericGroupRead("/api/v2/cmdb/system/cluster-sync", filter)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemClusterSync: %v", err)
+	}
+
+	var tmps []int
+	if o != nil {
+		if len(o) == 0 || o[0] == nil {
+			return nil
+		}
+
+		for _, r := range o {
+			i := r.(map[string]interface{})
+
+			if _, ok := i["sync-id"]; ok {
+				tmps = append(tmps, fortiIntValue(i["sync-id"]))
+			}
+		}
+	}
+	d.Set("sync_idlist", tmps)
+
+	d.SetId("DataSourceSystemClusterSyncList" + filter)
+
+	return nil
+}
