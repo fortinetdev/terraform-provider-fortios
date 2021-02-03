baeace4fb65463f1ffa335b4c705a425e9f94a6d
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:13 -0500

    Add new resource resource_logfortianalyzercloud_overridesetting

---
+++ fortios/resource_logfortianalyzercloud_overridesetting.go
@@ -0,0 +1,147 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Override FortiAnalyzer Cloud settings.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceLogFortianalyzerCloudOverrideSetting() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLogFortianalyzerCloudOverrideSettingUpdate,
+		Read:   resourceLogFortianalyzerCloudOverrideSettingRead,
+		Update: resourceLogFortianalyzerCloudOverrideSettingUpdate,
+		Delete: resourceLogFortianalyzerCloudOverrideSettingDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceLogFortianalyzerCloudOverrideSettingUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectLogFortianalyzerCloudOverrideSetting(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating LogFortianalyzerCloudOverrideSetting resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateLogFortianalyzerCloudOverrideSetting(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating LogFortianalyzerCloudOverrideSetting resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("LogFortianalyzerCloudOverrideSetting")
+	}
+
+	return resourceLogFortianalyzerCloudOverrideSettingRead(d, m)
+}
+
+func resourceLogFortianalyzerCloudOverrideSettingDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteLogFortianalyzerCloudOverrideSetting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting LogFortianalyzerCloudOverrideSetting resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceLogFortianalyzerCloudOverrideSettingRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadLogFortianalyzerCloudOverrideSetting(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading LogFortianalyzerCloudOverrideSetting resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectLogFortianalyzerCloudOverrideSetting(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading LogFortianalyzerCloudOverrideSetting resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenLogFortianalyzerCloudOverrideSettingStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectLogFortianalyzerCloudOverrideSetting(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("status", flattenLogFortianalyzerCloudOverrideSettingStatus(o["status"], d, "status", sv)); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenLogFortianalyzerCloudOverrideSettingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandLogFortianalyzerCloudOverrideSettingStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectLogFortianalyzerCloudOverrideSetting(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("status"); ok {
+
+		t, err := expandLogFortianalyzerCloudOverrideSettingStatus(d, v, "status", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	return &obj, nil
+}
