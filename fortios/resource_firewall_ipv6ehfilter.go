374de99833f2963465248a5954566d00a5e29479
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:14 -0500

    Update resource resource_firewall_ipv6ehfilter to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_firewall_ipv6ehfilter.go
+++ fortios/resource_firewall_ipv6ehfilter.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -80,7 +81,7 @@ func resourceFirewallIpv6EhFilterUpdate(d *schema.ResourceData, m interface{}) e
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectFirewallIpv6EhFilter(d)
+	obj, err := getObjectFirewallIpv6EhFilter(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating FirewallIpv6EhFilter resource while getting object: %v", err)
 	}
@@ -133,91 +134,91 @@ func resourceFirewallIpv6EhFilterRead(d *schema.ResourceData, m interface{}) err
 		return nil
 	}
 
-	err = refreshObjectFirewallIpv6EhFilter(d, o)
+	err = refreshObjectFirewallIpv6EhFilter(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading FirewallIpv6EhFilter resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenFirewallIpv6EhFilterHopOpt(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterHopOpt(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpv6EhFilterDestOpt(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterDestOpt(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpv6EhFilterHdoptType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterHdoptType(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpv6EhFilterRouting(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterRouting(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpv6EhFilterRoutingType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterRoutingType(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpv6EhFilterFragment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterFragment(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpv6EhFilterAuth(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterAuth(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenFirewallIpv6EhFilterNoNext(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenFirewallIpv6EhFilterNoNext(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectFirewallIpv6EhFilter(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectFirewallIpv6EhFilter(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("hop_opt", flattenFirewallIpv6EhFilterHopOpt(o["hop-opt"], d, "hop_opt")); err != nil {
+	if err = d.Set("hop_opt", flattenFirewallIpv6EhFilterHopOpt(o["hop-opt"], d, "hop_opt", sv)); err != nil {
 		if !fortiAPIPatch(o["hop-opt"]) {
 			return fmt.Errorf("Error reading hop_opt: %v", err)
 		}
 	}
 
-	if err = d.Set("dest_opt", flattenFirewallIpv6EhFilterDestOpt(o["dest-opt"], d, "dest_opt")); err != nil {
+	if err = d.Set("dest_opt", flattenFirewallIpv6EhFilterDestOpt(o["dest-opt"], d, "dest_opt", sv)); err != nil {
 		if !fortiAPIPatch(o["dest-opt"]) {
 			return fmt.Errorf("Error reading dest_opt: %v", err)
 		}
 	}
 
-	if err = d.Set("hdopt_type", flattenFirewallIpv6EhFilterHdoptType(o["hdopt-type"], d, "hdopt_type")); err != nil {
+	if err = d.Set("hdopt_type", flattenFirewallIpv6EhFilterHdoptType(o["hdopt-type"], d, "hdopt_type", sv)); err != nil {
 		if !fortiAPIPatch(o["hdopt-type"]) {
 			return fmt.Errorf("Error reading hdopt_type: %v", err)
 		}
 	}
 
-	if err = d.Set("routing", flattenFirewallIpv6EhFilterRouting(o["routing"], d, "routing")); err != nil {
+	if err = d.Set("routing", flattenFirewallIpv6EhFilterRouting(o["routing"], d, "routing", sv)); err != nil {
 		if !fortiAPIPatch(o["routing"]) {
 			return fmt.Errorf("Error reading routing: %v", err)
 		}
 	}
 
-	if err = d.Set("routing_type", flattenFirewallIpv6EhFilterRoutingType(o["routing-type"], d, "routing_type")); err != nil {
+	if err = d.Set("routing_type", flattenFirewallIpv6EhFilterRoutingType(o["routing-type"], d, "routing_type", sv)); err != nil {
 		if !fortiAPIPatch(o["routing-type"]) {
 			return fmt.Errorf("Error reading routing_type: %v", err)
 		}
 	}
 
-	if err = d.Set("fragment", flattenFirewallIpv6EhFilterFragment(o["fragment"], d, "fragment")); err != nil {
+	if err = d.Set("fragment", flattenFirewallIpv6EhFilterFragment(o["fragment"], d, "fragment", sv)); err != nil {
 		if !fortiAPIPatch(o["fragment"]) {
 			return fmt.Errorf("Error reading fragment: %v", err)
 		}
 	}
 
-	if err = d.Set("auth", flattenFirewallIpv6EhFilterAuth(o["auth"], d, "auth")); err != nil {
+	if err = d.Set("auth", flattenFirewallIpv6EhFilterAuth(o["auth"], d, "auth", sv)); err != nil {
 		if !fortiAPIPatch(o["auth"]) {
 			return fmt.Errorf("Error reading auth: %v", err)
 		}
 	}
 
-	if err = d.Set("no_next", flattenFirewallIpv6EhFilterNoNext(o["no-next"], d, "no_next")); err != nil {
+	if err = d.Set("no_next", flattenFirewallIpv6EhFilterNoNext(o["no-next"], d, "no_next", sv)); err != nil {
 		if !fortiAPIPatch(o["no-next"]) {
 			return fmt.Errorf("Error reading no_next: %v", err)
 		}
@@ -229,46 +230,47 @@ func refreshObjectFirewallIpv6EhFilter(d *schema.ResourceData, o map[string]inte
 func flattenFirewallIpv6EhFilterFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandFirewallIpv6EhFilterHopOpt(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterHopOpt(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpv6EhFilterDestOpt(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterDestOpt(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpv6EhFilterHdoptType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterHdoptType(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpv6EhFilterRouting(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterRouting(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpv6EhFilterRoutingType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterRoutingType(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpv6EhFilterFragment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterFragment(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpv6EhFilterAuth(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterAuth(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandFirewallIpv6EhFilterNoNext(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandFirewallIpv6EhFilterNoNext(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectFirewallIpv6EhFilter(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("hop_opt"); ok {
-		t, err := expandFirewallIpv6EhFilterHopOpt(d, v, "hop_opt")
+
+		t, err := expandFirewallIpv6EhFilterHopOpt(d, v, "hop_opt", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -277,7 +279,8 @@ func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interfac
 	}
 
 	if v, ok := d.GetOk("dest_opt"); ok {
-		t, err := expandFirewallIpv6EhFilterDestOpt(d, v, "dest_opt")
+
+		t, err := expandFirewallIpv6EhFilterDestOpt(d, v, "dest_opt", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -286,7 +289,8 @@ func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interfac
 	}
 
 	if v, ok := d.GetOkExists("hdopt_type"); ok {
-		t, err := expandFirewallIpv6EhFilterHdoptType(d, v, "hdopt_type")
+
+		t, err := expandFirewallIpv6EhFilterHdoptType(d, v, "hdopt_type", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -295,7 +299,8 @@ func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interfac
 	}
 
 	if v, ok := d.GetOk("routing"); ok {
-		t, err := expandFirewallIpv6EhFilterRouting(d, v, "routing")
+
+		t, err := expandFirewallIpv6EhFilterRouting(d, v, "routing", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -304,7 +309,8 @@ func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interfac
 	}
 
 	if v, ok := d.GetOkExists("routing_type"); ok {
-		t, err := expandFirewallIpv6EhFilterRoutingType(d, v, "routing_type")
+
+		t, err := expandFirewallIpv6EhFilterRoutingType(d, v, "routing_type", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -313,7 +319,8 @@ func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interfac
 	}
 
 	if v, ok := d.GetOk("fragment"); ok {
-		t, err := expandFirewallIpv6EhFilterFragment(d, v, "fragment")
+
+		t, err := expandFirewallIpv6EhFilterFragment(d, v, "fragment", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -322,7 +329,8 @@ func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interfac
 	}
 
 	if v, ok := d.GetOk("auth"); ok {
-		t, err := expandFirewallIpv6EhFilterAuth(d, v, "auth")
+
+		t, err := expandFirewallIpv6EhFilterAuth(d, v, "auth", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -331,7 +339,8 @@ func getObjectFirewallIpv6EhFilter(d *schema.ResourceData) (*map[string]interfac
 	}
 
 	if v, ok := d.GetOk("no_next"); ok {
-		t, err := expandFirewallIpv6EhFilterNoNext(d, v, "no_next")
+
+		t, err := expandFirewallIpv6EhFilterNoNext(d, v, "no_next", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
