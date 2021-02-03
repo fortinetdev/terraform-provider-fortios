1fae0c400f1f424354ec78aa32d28111a4516b4a
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:26:10 -0500

    Update resource resource_systemreplacemsg_sslvpn to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_systemreplacemsg_sslvpn.go
+++ fortios/resource_systemreplacemsg_sslvpn.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -58,7 +59,7 @@ func resourceSystemReplacemsgSslvpnCreate(d *schema.ResourceData, m interface{})
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemReplacemsgSslvpn(d)
+	obj, err := getObjectSystemReplacemsgSslvpn(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating SystemReplacemsgSslvpn resource while getting object: %v", err)
 	}
@@ -83,7 +84,7 @@ func resourceSystemReplacemsgSslvpnUpdate(d *schema.ResourceData, m interface{})
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemReplacemsgSslvpn(d)
+	obj, err := getObjectSystemReplacemsgSslvpn(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemReplacemsgSslvpn resource while getting object: %v", err)
 	}
@@ -136,51 +137,51 @@ func resourceSystemReplacemsgSslvpnRead(d *schema.ResourceData, m interface{}) e
 		return nil
 	}
 
-	err = refreshObjectSystemReplacemsgSslvpn(d, o)
+	err = refreshObjectSystemReplacemsgSslvpn(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemReplacemsgSslvpn resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemReplacemsgSslvpnMsgType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemReplacemsgSslvpnMsgType(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemReplacemsgSslvpnBuffer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemReplacemsgSslvpnBuffer(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemReplacemsgSslvpnHeader(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemReplacemsgSslvpnHeader(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemReplacemsgSslvpnFormat(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemReplacemsgSslvpnFormat(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemReplacemsgSslvpn(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectSystemReplacemsgSslvpn(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("msg_type", flattenSystemReplacemsgSslvpnMsgType(o["msg-type"], d, "msg_type")); err != nil {
+	if err = d.Set("msg_type", flattenSystemReplacemsgSslvpnMsgType(o["msg-type"], d, "msg_type", sv)); err != nil {
 		if !fortiAPIPatch(o["msg-type"]) {
 			return fmt.Errorf("Error reading msg_type: %v", err)
 		}
 	}
 
-	if err = d.Set("buffer", flattenSystemReplacemsgSslvpnBuffer(o["buffer"], d, "buffer")); err != nil {
+	if err = d.Set("buffer", flattenSystemReplacemsgSslvpnBuffer(o["buffer"], d, "buffer", sv)); err != nil {
 		if !fortiAPIPatch(o["buffer"]) {
 			return fmt.Errorf("Error reading buffer: %v", err)
 		}
 	}
 
-	if err = d.Set("header", flattenSystemReplacemsgSslvpnHeader(o["header"], d, "header")); err != nil {
+	if err = d.Set("header", flattenSystemReplacemsgSslvpnHeader(o["header"], d, "header", sv)); err != nil {
 		if !fortiAPIPatch(o["header"]) {
 			return fmt.Errorf("Error reading header: %v", err)
 		}
 	}
 
-	if err = d.Set("format", flattenSystemReplacemsgSslvpnFormat(o["format"], d, "format")); err != nil {
+	if err = d.Set("format", flattenSystemReplacemsgSslvpnFormat(o["format"], d, "format", sv)); err != nil {
 		if !fortiAPIPatch(o["format"]) {
 			return fmt.Errorf("Error reading format: %v", err)
 		}
@@ -192,30 +193,31 @@ func refreshObjectSystemReplacemsgSslvpn(d *schema.ResourceData, o map[string]in
 func flattenSystemReplacemsgSslvpnFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemReplacemsgSslvpnMsgType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemReplacemsgSslvpnMsgType(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemReplacemsgSslvpnBuffer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemReplacemsgSslvpnBuffer(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemReplacemsgSslvpnHeader(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemReplacemsgSslvpnHeader(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemReplacemsgSslvpnFormat(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemReplacemsgSslvpnFormat(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemReplacemsgSslvpn(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectSystemReplacemsgSslvpn(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("msg_type"); ok {
-		t, err := expandSystemReplacemsgSslvpnMsgType(d, v, "msg_type")
+
+		t, err := expandSystemReplacemsgSslvpnMsgType(d, v, "msg_type", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -224,7 +226,8 @@ func getObjectSystemReplacemsgSslvpn(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("buffer"); ok {
-		t, err := expandSystemReplacemsgSslvpnBuffer(d, v, "buffer")
+
+		t, err := expandSystemReplacemsgSslvpnBuffer(d, v, "buffer", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -233,7 +236,8 @@ func getObjectSystemReplacemsgSslvpn(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("header"); ok {
-		t, err := expandSystemReplacemsgSslvpnHeader(d, v, "header")
+
+		t, err := expandSystemReplacemsgSslvpnHeader(d, v, "header", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -242,7 +246,8 @@ func getObjectSystemReplacemsgSslvpn(d *schema.ResourceData) (*map[string]interf
 	}
 
 	if v, ok := d.GetOk("format"); ok {
-		t, err := expandSystemReplacemsgSslvpnFormat(d, v, "format")
+
+		t, err := expandSystemReplacemsgSslvpnFormat(d, v, "format", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
