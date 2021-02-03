b8fbcb2cad21a0d51386eb5a5fd05c01873de522
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:55 -0500

    Add new data source data_source_user_saml

---
+++ fortios/data_source_user_saml.go
@@ -0,0 +1,227 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: SAML server entry configuration.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceUserSaml() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceUserSamlRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"cert": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"entity_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"single_sign_on_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"single_logout_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"idp_entity_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"idp_single_sign_on_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"idp_single_logout_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"idp_cert": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"user_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"group_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceUserSamlRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("name")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing UserSaml: type error")
+	}
+
+	o, err := c.ReadUserSaml(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing UserSaml: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectUserSaml(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing UserSaml from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenUserSamlName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlCert(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlEntityId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlSingleSignOnUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlSingleLogoutUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlIdpEntityId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlIdpSingleSignOnUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlIdpSingleLogoutUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlIdpCert(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlUserName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenUserSamlGroupName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectUserSaml(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", dataSourceFlattenUserSamlName(o["name"], d, "name")); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("cert", dataSourceFlattenUserSamlCert(o["cert"], d, "cert")); err != nil {
+		if !fortiAPIPatch(o["cert"]) {
+			return fmt.Errorf("Error reading cert: %v", err)
+		}
+	}
+
+	if err = d.Set("entity_id", dataSourceFlattenUserSamlEntityId(o["entity-id"], d, "entity_id")); err != nil {
+		if !fortiAPIPatch(o["entity-id"]) {
+			return fmt.Errorf("Error reading entity_id: %v", err)
+		}
+	}
+
+	if err = d.Set("single_sign_on_url", dataSourceFlattenUserSamlSingleSignOnUrl(o["single-sign-on-url"], d, "single_sign_on_url")); err != nil {
+		if !fortiAPIPatch(o["single-sign-on-url"]) {
+			return fmt.Errorf("Error reading single_sign_on_url: %v", err)
+		}
+	}
+
+	if err = d.Set("single_logout_url", dataSourceFlattenUserSamlSingleLogoutUrl(o["single-logout-url"], d, "single_logout_url")); err != nil {
+		if !fortiAPIPatch(o["single-logout-url"]) {
+			return fmt.Errorf("Error reading single_logout_url: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_entity_id", dataSourceFlattenUserSamlIdpEntityId(o["idp-entity-id"], d, "idp_entity_id")); err != nil {
+		if !fortiAPIPatch(o["idp-entity-id"]) {
+			return fmt.Errorf("Error reading idp_entity_id: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_single_sign_on_url", dataSourceFlattenUserSamlIdpSingleSignOnUrl(o["idp-single-sign-on-url"], d, "idp_single_sign_on_url")); err != nil {
+		if !fortiAPIPatch(o["idp-single-sign-on-url"]) {
+			return fmt.Errorf("Error reading idp_single_sign_on_url: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_single_logout_url", dataSourceFlattenUserSamlIdpSingleLogoutUrl(o["idp-single-logout-url"], d, "idp_single_logout_url")); err != nil {
+		if !fortiAPIPatch(o["idp-single-logout-url"]) {
+			return fmt.Errorf("Error reading idp_single_logout_url: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_cert", dataSourceFlattenUserSamlIdpCert(o["idp-cert"], d, "idp_cert")); err != nil {
+		if !fortiAPIPatch(o["idp-cert"]) {
+			return fmt.Errorf("Error reading idp_cert: %v", err)
+		}
+	}
+
+	if err = d.Set("user_name", dataSourceFlattenUserSamlUserName(o["user-name"], d, "user_name")); err != nil {
+		if !fortiAPIPatch(o["user-name"]) {
+			return fmt.Errorf("Error reading user_name: %v", err)
+		}
+	}
+
+	if err = d.Set("group_name", dataSourceFlattenUserSamlGroupName(o["group-name"], d, "group_name")); err != nil {
+		if !fortiAPIPatch(o["group-name"]) {
+			return fmt.Errorf("Error reading group_name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenUserSamlFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
