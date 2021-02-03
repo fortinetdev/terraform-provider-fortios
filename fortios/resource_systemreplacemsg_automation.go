645dc645108e9a5cb500cb70ea8a73cc0c182e48
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:28 -0500

    Add new resource resource_systemreplacemsg_automation

---
+++ fortios/resource_systemreplacemsg_automation.go
@@ -0,0 +1,260 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Replacement messages.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemReplacemsgAutomation() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemReplacemsgAutomationCreate,
+		Read:   resourceSystemReplacemsgAutomationRead,
+		Update: resourceSystemReplacemsgAutomationUpdate,
+		Delete: resourceSystemReplacemsgAutomationDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"msg_type": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 28),
+				ForceNew:     true,
+				Optional:     true,
+				Computed:     true,
+			},
+			"buffer": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 32768),
+				Optional:     true,
+			},
+			"header": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"format": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemReplacemsgAutomationCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemReplacemsgAutomation(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemReplacemsgAutomation resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSystemReplacemsgAutomation(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemReplacemsgAutomation resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemReplacemsgAutomation")
+	}
+
+	return resourceSystemReplacemsgAutomationRead(d, m)
+}
+
+func resourceSystemReplacemsgAutomationUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSystemReplacemsgAutomation(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemReplacemsgAutomation resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSystemReplacemsgAutomation(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemReplacemsgAutomation resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SystemReplacemsgAutomation")
+	}
+
+	return resourceSystemReplacemsgAutomationRead(d, m)
+}
+
+func resourceSystemReplacemsgAutomationDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSystemReplacemsgAutomation(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemReplacemsgAutomation resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemReplacemsgAutomationRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSystemReplacemsgAutomation(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemReplacemsgAutomation resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemReplacemsgAutomation(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemReplacemsgAutomation resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemReplacemsgAutomationMsgType(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgAutomationBuffer(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgAutomationHeader(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSystemReplacemsgAutomationFormat(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectSystemReplacemsgAutomation(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("msg_type", flattenSystemReplacemsgAutomationMsgType(o["msg-type"], d, "msg_type", sv)); err != nil {
+		if !fortiAPIPatch(o["msg-type"]) {
+			return fmt.Errorf("Error reading msg_type: %v", err)
+		}
+	}
+
+	if err = d.Set("buffer", flattenSystemReplacemsgAutomationBuffer(o["buffer"], d, "buffer", sv)); err != nil {
+		if !fortiAPIPatch(o["buffer"]) {
+			return fmt.Errorf("Error reading buffer: %v", err)
+		}
+	}
+
+	if err = d.Set("header", flattenSystemReplacemsgAutomationHeader(o["header"], d, "header", sv)); err != nil {
+		if !fortiAPIPatch(o["header"]) {
+			return fmt.Errorf("Error reading header: %v", err)
+		}
+	}
+
+	if err = d.Set("format", flattenSystemReplacemsgAutomationFormat(o["format"], d, "format", sv)); err != nil {
+		if !fortiAPIPatch(o["format"]) {
+			return fmt.Errorf("Error reading format: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemReplacemsgAutomationFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandSystemReplacemsgAutomationMsgType(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgAutomationBuffer(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgAutomationHeader(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemReplacemsgAutomationFormat(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemReplacemsgAutomation(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("msg_type"); ok {
+
+		t, err := expandSystemReplacemsgAutomationMsgType(d, v, "msg_type", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["msg-type"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("buffer"); ok {
+
+		t, err := expandSystemReplacemsgAutomationBuffer(d, v, "buffer", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["buffer"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("header"); ok {
+
+		t, err := expandSystemReplacemsgAutomationHeader(d, v, "header", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["header"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("format"); ok {
+
+		t, err := expandSystemReplacemsgAutomationFormat(d, v, "format", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["format"] = t
+		}
+	}
+
+	return &obj, nil
+}
