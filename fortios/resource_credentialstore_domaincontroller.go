2f50173f46c0cb259ed439719dc821098a2eedfd
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:57 -0500

    Add new resource resource_credentialstore_domaincontroller

---
+++ fortios/resource_credentialstore_domaincontroller.go
@@ -0,0 +1,375 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Define known domain controller servers.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceCredentialStoreDomainController() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCredentialStoreDomainControllerCreate,
+		Read:   resourceCredentialStoreDomainControllerRead,
+		Update: resourceCredentialStoreDomainControllerUpdate,
+		Delete: resourceCredentialStoreDomainControllerDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"server_name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				ForceNew:     true,
+				Optional:     true,
+				Computed:     true,
+			},
+			"hostname": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 255),
+				Optional:     true,
+				Computed:     true,
+			},
+			"domain_name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 79),
+				Optional:     true,
+				Computed:     true,
+			},
+			"username": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 64),
+				Optional:     true,
+				Computed:     true,
+			},
+			"password": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 128),
+				Optional:     true,
+				Sensitive:    true,
+			},
+			"port": &schema.Schema{
+				Type:         schema.TypeInt,
+				ValidateFunc: validation.IntBetween(0, 65535),
+				Optional:     true,
+				Computed:     true,
+			},
+			"ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ip6": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceCredentialStoreDomainControllerCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectCredentialStoreDomainController(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating CredentialStoreDomainController resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateCredentialStoreDomainController(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating CredentialStoreDomainController resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("CredentialStoreDomainController")
+	}
+
+	return resourceCredentialStoreDomainControllerRead(d, m)
+}
+
+func resourceCredentialStoreDomainControllerUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectCredentialStoreDomainController(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating CredentialStoreDomainController resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateCredentialStoreDomainController(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating CredentialStoreDomainController resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("CredentialStoreDomainController")
+	}
+
+	return resourceCredentialStoreDomainControllerRead(d, m)
+}
+
+func resourceCredentialStoreDomainControllerDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteCredentialStoreDomainController(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting CredentialStoreDomainController resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceCredentialStoreDomainControllerRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadCredentialStoreDomainController(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading CredentialStoreDomainController resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectCredentialStoreDomainController(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading CredentialStoreDomainController resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenCredentialStoreDomainControllerServerName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenCredentialStoreDomainControllerHostname(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenCredentialStoreDomainControllerDomainName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenCredentialStoreDomainControllerUsername(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenCredentialStoreDomainControllerPassword(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenCredentialStoreDomainControllerPort(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenCredentialStoreDomainControllerIp(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenCredentialStoreDomainControllerIp6(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectCredentialStoreDomainController(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("server_name", flattenCredentialStoreDomainControllerServerName(o["server-name"], d, "server_name", sv)); err != nil {
+		if !fortiAPIPatch(o["server-name"]) {
+			return fmt.Errorf("Error reading server_name: %v", err)
+		}
+	}
+
+	if err = d.Set("hostname", flattenCredentialStoreDomainControllerHostname(o["hostname"], d, "hostname", sv)); err != nil {
+		if !fortiAPIPatch(o["hostname"]) {
+			return fmt.Errorf("Error reading hostname: %v", err)
+		}
+	}
+
+	if err = d.Set("domain_name", flattenCredentialStoreDomainControllerDomainName(o["domain-name"], d, "domain_name", sv)); err != nil {
+		if !fortiAPIPatch(o["domain-name"]) {
+			return fmt.Errorf("Error reading domain_name: %v", err)
+		}
+	}
+
+	if err = d.Set("username", flattenCredentialStoreDomainControllerUsername(o["username"], d, "username", sv)); err != nil {
+		if !fortiAPIPatch(o["username"]) {
+			return fmt.Errorf("Error reading username: %v", err)
+		}
+	}
+
+	if err = d.Set("port", flattenCredentialStoreDomainControllerPort(o["port"], d, "port", sv)); err != nil {
+		if !fortiAPIPatch(o["port"]) {
+			return fmt.Errorf("Error reading port: %v", err)
+		}
+	}
+
+	if err = d.Set("ip", flattenCredentialStoreDomainControllerIp(o["ip"], d, "ip", sv)); err != nil {
+		if !fortiAPIPatch(o["ip"]) {
+			return fmt.Errorf("Error reading ip: %v", err)
+		}
+	}
+
+	if err = d.Set("ip6", flattenCredentialStoreDomainControllerIp6(o["ip6"], d, "ip6", sv)); err != nil {
+		if !fortiAPIPatch(o["ip6"]) {
+			return fmt.Errorf("Error reading ip6: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenCredentialStoreDomainControllerFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandCredentialStoreDomainControllerServerName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandCredentialStoreDomainControllerHostname(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandCredentialStoreDomainControllerDomainName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandCredentialStoreDomainControllerUsername(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandCredentialStoreDomainControllerPassword(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandCredentialStoreDomainControllerPort(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandCredentialStoreDomainControllerIp(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandCredentialStoreDomainControllerIp6(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectCredentialStoreDomainController(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("server_name"); ok {
+
+		t, err := expandCredentialStoreDomainControllerServerName(d, v, "server_name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["server-name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("hostname"); ok {
+
+		t, err := expandCredentialStoreDomainControllerHostname(d, v, "hostname", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["hostname"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("domain_name"); ok {
+
+		t, err := expandCredentialStoreDomainControllerDomainName(d, v, "domain_name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["domain-name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("username"); ok {
+
+		t, err := expandCredentialStoreDomainControllerUsername(d, v, "username", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["username"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("password"); ok {
+
+		t, err := expandCredentialStoreDomainControllerPassword(d, v, "password", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["password"] = t
+		}
+	}
+
+	if v, ok := d.GetOkExists("port"); ok {
+
+		t, err := expandCredentialStoreDomainControllerPort(d, v, "port", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ip"); ok {
+
+		t, err := expandCredentialStoreDomainControllerIp(d, v, "ip", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ip6"); ok {
+
+		t, err := expandCredentialStoreDomainControllerIp6(d, v, "ip6", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ip6"] = t
+		}
+	}
+
+	return &obj, nil
+}
