48af7d6c68c3e58896271616fc25fe3f9bebcf40
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:23:04 -0500

    Update resource resource_logwebtrends_setting to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_logwebtrends_setting.go
+++ fortios/resource_logwebtrends_setting.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -49,7 +50,7 @@ func resourceLogWebtrendsSettingUpdate(d *schema.ResourceData, m interface{}) er
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectLogWebtrendsSetting(d)
+	obj, err := getObjectLogWebtrendsSetting(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating LogWebtrendsSetting resource while getting object: %v", err)
 	}
@@ -102,31 +103,31 @@ func resourceLogWebtrendsSettingRead(d *schema.ResourceData, m interface{}) erro
 		return nil
 	}
 
-	err = refreshObjectLogWebtrendsSetting(d, o)
+	err = refreshObjectLogWebtrendsSetting(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading LogWebtrendsSetting resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenLogWebtrendsSettingStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogWebtrendsSettingStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenLogWebtrendsSettingServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenLogWebtrendsSettingServer(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectLogWebtrendsSetting(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectLogWebtrendsSetting(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("status", flattenLogWebtrendsSettingStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenLogWebtrendsSettingStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
 	}
 
-	if err = d.Set("server", flattenLogWebtrendsSettingServer(o["server"], d, "server")); err != nil {
+	if err = d.Set("server", flattenLogWebtrendsSettingServer(o["server"], d, "server", sv)); err != nil {
 		if !fortiAPIPatch(o["server"]) {
 			return fmt.Errorf("Error reading server: %v", err)
 		}
@@ -138,22 +139,23 @@ func refreshObjectLogWebtrendsSetting(d *schema.ResourceData, o map[string]inter
 func flattenLogWebtrendsSettingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandLogWebtrendsSettingStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogWebtrendsSettingStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandLogWebtrendsSettingServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandLogWebtrendsSettingServer(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectLogWebtrendsSetting(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectLogWebtrendsSetting(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandLogWebtrendsSettingStatus(d, v, "status")
+
+		t, err := expandLogWebtrendsSettingStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -162,7 +164,8 @@ func getObjectLogWebtrendsSetting(d *schema.ResourceData) (*map[string]interface
 	}
 
 	if v, ok := d.GetOk("server"); ok {
-		t, err := expandLogWebtrendsSettingServer(d, v, "server")
+
+		t, err := expandLogWebtrendsSettingServer(d, v, "server", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
