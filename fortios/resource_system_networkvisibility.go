29099c608e1cc5af2629c2a1514e63d01c98340c
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:25:44 -0500

    Update resource resource_system_networkvisibility to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_system_networkvisibility.go
+++ fortios/resource_system_networkvisibility.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -70,7 +71,7 @@ func resourceSystemNetworkVisibilityUpdate(d *schema.ResourceData, m interface{}
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemNetworkVisibility(d)
+	obj, err := getObjectSystemNetworkVisibility(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemNetworkVisibility resource while getting object: %v", err)
 	}
@@ -123,71 +124,71 @@ func resourceSystemNetworkVisibilityRead(d *schema.ResourceData, m interface{}) 
 		return nil
 	}
 
-	err = refreshObjectSystemNetworkVisibility(d, o)
+	err = refreshObjectSystemNetworkVisibility(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemNetworkVisibility resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemNetworkVisibilityDestinationVisibility(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemNetworkVisibilityDestinationVisibility(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemNetworkVisibilitySourceLocation(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemNetworkVisibilitySourceLocation(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemNetworkVisibilityDestinationHostnameVisibility(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemNetworkVisibilityDestinationHostnameVisibility(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemNetworkVisibilityHostnameTtl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemNetworkVisibilityHostnameTtl(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemNetworkVisibilityHostnameLimit(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemNetworkVisibilityHostnameLimit(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemNetworkVisibilityDestinationLocation(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemNetworkVisibilityDestinationLocation(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemNetworkVisibility(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectSystemNetworkVisibility(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("destination_visibility", flattenSystemNetworkVisibilityDestinationVisibility(o["destination-visibility"], d, "destination_visibility")); err != nil {
+	if err = d.Set("destination_visibility", flattenSystemNetworkVisibilityDestinationVisibility(o["destination-visibility"], d, "destination_visibility", sv)); err != nil {
 		if !fortiAPIPatch(o["destination-visibility"]) {
 			return fmt.Errorf("Error reading destination_visibility: %v", err)
 		}
 	}
 
-	if err = d.Set("source_location", flattenSystemNetworkVisibilitySourceLocation(o["source-location"], d, "source_location")); err != nil {
+	if err = d.Set("source_location", flattenSystemNetworkVisibilitySourceLocation(o["source-location"], d, "source_location", sv)); err != nil {
 		if !fortiAPIPatch(o["source-location"]) {
 			return fmt.Errorf("Error reading source_location: %v", err)
 		}
 	}
 
-	if err = d.Set("destination_hostname_visibility", flattenSystemNetworkVisibilityDestinationHostnameVisibility(o["destination-hostname-visibility"], d, "destination_hostname_visibility")); err != nil {
+	if err = d.Set("destination_hostname_visibility", flattenSystemNetworkVisibilityDestinationHostnameVisibility(o["destination-hostname-visibility"], d, "destination_hostname_visibility", sv)); err != nil {
 		if !fortiAPIPatch(o["destination-hostname-visibility"]) {
 			return fmt.Errorf("Error reading destination_hostname_visibility: %v", err)
 		}
 	}
 
-	if err = d.Set("hostname_ttl", flattenSystemNetworkVisibilityHostnameTtl(o["hostname-ttl"], d, "hostname_ttl")); err != nil {
+	if err = d.Set("hostname_ttl", flattenSystemNetworkVisibilityHostnameTtl(o["hostname-ttl"], d, "hostname_ttl", sv)); err != nil {
 		if !fortiAPIPatch(o["hostname-ttl"]) {
 			return fmt.Errorf("Error reading hostname_ttl: %v", err)
 		}
 	}
 
-	if err = d.Set("hostname_limit", flattenSystemNetworkVisibilityHostnameLimit(o["hostname-limit"], d, "hostname_limit")); err != nil {
+	if err = d.Set("hostname_limit", flattenSystemNetworkVisibilityHostnameLimit(o["hostname-limit"], d, "hostname_limit", sv)); err != nil {
 		if !fortiAPIPatch(o["hostname-limit"]) {
 			return fmt.Errorf("Error reading hostname_limit: %v", err)
 		}
 	}
 
-	if err = d.Set("destination_location", flattenSystemNetworkVisibilityDestinationLocation(o["destination-location"], d, "destination_location")); err != nil {
+	if err = d.Set("destination_location", flattenSystemNetworkVisibilityDestinationLocation(o["destination-location"], d, "destination_location", sv)); err != nil {
 		if !fortiAPIPatch(o["destination-location"]) {
 			return fmt.Errorf("Error reading destination_location: %v", err)
 		}
@@ -199,38 +200,39 @@ func refreshObjectSystemNetworkVisibility(d *schema.ResourceData, o map[string]i
 func flattenSystemNetworkVisibilityFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemNetworkVisibilityDestinationVisibility(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemNetworkVisibilityDestinationVisibility(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemNetworkVisibilitySourceLocation(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemNetworkVisibilitySourceLocation(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemNetworkVisibilityDestinationHostnameVisibility(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemNetworkVisibilityDestinationHostnameVisibility(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemNetworkVisibilityHostnameTtl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemNetworkVisibilityHostnameTtl(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemNetworkVisibilityHostnameLimit(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemNetworkVisibilityHostnameLimit(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemNetworkVisibilityDestinationLocation(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemNetworkVisibilityDestinationLocation(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemNetworkVisibility(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectSystemNetworkVisibility(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("destination_visibility"); ok {
-		t, err := expandSystemNetworkVisibilityDestinationVisibility(d, v, "destination_visibility")
+
+		t, err := expandSystemNetworkVisibilityDestinationVisibility(d, v, "destination_visibility", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -239,7 +241,8 @@ func getObjectSystemNetworkVisibility(d *schema.ResourceData) (*map[string]inter
 	}
 
 	if v, ok := d.GetOk("source_location"); ok {
-		t, err := expandSystemNetworkVisibilitySourceLocation(d, v, "source_location")
+
+		t, err := expandSystemNetworkVisibilitySourceLocation(d, v, "source_location", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -248,7 +251,8 @@ func getObjectSystemNetworkVisibility(d *schema.ResourceData) (*map[string]inter
 	}
 
 	if v, ok := d.GetOk("destination_hostname_visibility"); ok {
-		t, err := expandSystemNetworkVisibilityDestinationHostnameVisibility(d, v, "destination_hostname_visibility")
+
+		t, err := expandSystemNetworkVisibilityDestinationHostnameVisibility(d, v, "destination_hostname_visibility", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -257,7 +261,8 @@ func getObjectSystemNetworkVisibility(d *schema.ResourceData) (*map[string]inter
 	}
 
 	if v, ok := d.GetOk("hostname_ttl"); ok {
-		t, err := expandSystemNetworkVisibilityHostnameTtl(d, v, "hostname_ttl")
+
+		t, err := expandSystemNetworkVisibilityHostnameTtl(d, v, "hostname_ttl", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -266,7 +271,8 @@ func getObjectSystemNetworkVisibility(d *schema.ResourceData) (*map[string]inter
 	}
 
 	if v, ok := d.GetOkExists("hostname_limit"); ok {
-		t, err := expandSystemNetworkVisibilityHostnameLimit(d, v, "hostname_limit")
+
+		t, err := expandSystemNetworkVisibilityHostnameLimit(d, v, "hostname_limit", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -275,7 +281,8 @@ func getObjectSystemNetworkVisibility(d *schema.ResourceData) (*map[string]inter
 	}
 
 	if v, ok := d.GetOk("destination_location"); ok {
-		t, err := expandSystemNetworkVisibilityDestinationLocation(d, v, "destination_location")
+
+		t, err := expandSystemNetworkVisibilityDestinationLocation(d, v, "destination_location", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
