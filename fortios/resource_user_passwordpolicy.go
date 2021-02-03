e90866ddfce7c1aac9202963c8bcfed29a5b5a8a
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:26:19 -0500

    Update resource resource_user_passwordpolicy to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_user_passwordpolicy.go
+++ fortios/resource_user_passwordpolicy.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -48,6 +49,11 @@ func resourceUserPasswordPolicy() *schema.Resource {
 				Optional:     true,
 				Computed:     true,
 			},
+			"expired_password_renewal": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -56,7 +62,7 @@ func resourceUserPasswordPolicyCreate(d *schema.ResourceData, m interface{}) err
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectUserPasswordPolicy(d)
+	obj, err := getObjectUserPasswordPolicy(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating UserPasswordPolicy resource while getting object: %v", err)
 	}
@@ -81,7 +87,7 @@ func resourceUserPasswordPolicyUpdate(d *schema.ResourceData, m interface{}) err
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectUserPasswordPolicy(d)
+	obj, err := getObjectUserPasswordPolicy(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating UserPasswordPolicy resource while getting object: %v", err)
 	}
@@ -134,43 +140,53 @@ func resourceUserPasswordPolicyRead(d *schema.ResourceData, m interface{}) error
 		return nil
 	}
 
-	err = refreshObjectUserPasswordPolicy(d, o)
+	err = refreshObjectUserPasswordPolicy(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading UserPasswordPolicy resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenUserPasswordPolicyName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenUserPasswordPolicyName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenUserPasswordPolicyExpireDays(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenUserPasswordPolicyExpireDays(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenUserPasswordPolicyWarnDays(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenUserPasswordPolicyWarnDays(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectUserPasswordPolicy(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenUserPasswordPolicyExpiredPasswordRenewal(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectUserPasswordPolicy(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("name", flattenUserPasswordPolicyName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenUserPasswordPolicyName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("expire_days", flattenUserPasswordPolicyExpireDays(o["expire-days"], d, "expire_days")); err != nil {
+	if err = d.Set("expire_days", flattenUserPasswordPolicyExpireDays(o["expire-days"], d, "expire_days", sv)); err != nil {
 		if !fortiAPIPatch(o["expire-days"]) {
 			return fmt.Errorf("Error reading expire_days: %v", err)
 		}
 	}
 
-	if err = d.Set("warn_days", flattenUserPasswordPolicyWarnDays(o["warn-days"], d, "warn_days")); err != nil {
+	if err = d.Set("warn_days", flattenUserPasswordPolicyWarnDays(o["warn-days"], d, "warn_days", sv)); err != nil {
 		if !fortiAPIPatch(o["warn-days"]) {
 			return fmt.Errorf("Error reading warn_days: %v", err)
+		}
+	}
+
+	if err = d.Set("expired_password_renewal", flattenUserPasswordPolicyExpiredPasswordRenewal(o["expired-password-renewal"], d, "expired_password_renewal", sv)); err != nil {
+		if !fortiAPIPatch(o["expired-password-renewal"]) {
+			return fmt.Errorf("Error reading expired_password_renewal: %v", err)
 		}
 	}
 
@@ -180,26 +196,31 @@ func refreshObjectUserPasswordPolicy(d *schema.ResourceData, o map[string]interf
 func flattenUserPasswordPolicyFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandUserPasswordPolicyName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandUserPasswordPolicyName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandUserPasswordPolicyExpireDays(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandUserPasswordPolicyExpireDays(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandUserPasswordPolicyWarnDays(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandUserPasswordPolicyWarnDays(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectUserPasswordPolicy(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandUserPasswordPolicyExpiredPasswordRenewal(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectUserPasswordPolicy(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandUserPasswordPolicyName(d, v, "name")
+
+		t, err := expandUserPasswordPolicyName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -208,7 +229,8 @@ func getObjectUserPasswordPolicy(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOkExists("expire_days"); ok {
-		t, err := expandUserPasswordPolicyExpireDays(d, v, "expire_days")
+
+		t, err := expandUserPasswordPolicyExpireDays(d, v, "expire_days", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -217,11 +239,22 @@ func getObjectUserPasswordPolicy(d *schema.ResourceData) (*map[string]interface{
 	}
 
 	if v, ok := d.GetOkExists("warn_days"); ok {
-		t, err := expandUserPasswordPolicyWarnDays(d, v, "warn_days")
+
+		t, err := expandUserPasswordPolicyWarnDays(d, v, "warn_days", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
 			obj["warn-days"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("expired_password_renewal"); ok {
+
+		t, err := expandUserPasswordPolicyExpiredPasswordRenewal(d, v, "expired_password_renewal", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["expired-password-renewal"] = t
 		}
 	}
 
