08e75fcd507ea1054a73212d07093a26bd6744cf
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:09 -0500

    Add new resource resource_firewall_internetservicereputation

---
+++ fortios/resource_firewall_internetservicereputation.go
@@ -0,0 +1,201 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Show Internet Service reputation.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceFirewallInternetServiceReputation() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFirewallInternetServiceReputationCreate,
+		Read:   resourceFirewallInternetServiceReputationRead,
+		Update: resourceFirewallInternetServiceReputationUpdate,
+		Delete: resourceFirewallInternetServiceReputationDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"description": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 127),
+				Optional:     true,
+				Computed:     true,
+			},
+		},
+	}
+}
+
+func resourceFirewallInternetServiceReputationCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallInternetServiceReputation(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallInternetServiceReputation resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateFirewallInternetServiceReputation(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating FirewallInternetServiceReputation resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallInternetServiceReputation")
+	}
+
+	return resourceFirewallInternetServiceReputationRead(d, m)
+}
+
+func resourceFirewallInternetServiceReputationUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectFirewallInternetServiceReputation(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallInternetServiceReputation resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateFirewallInternetServiceReputation(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating FirewallInternetServiceReputation resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("FirewallInternetServiceReputation")
+	}
+
+	return resourceFirewallInternetServiceReputationRead(d, m)
+}
+
+func resourceFirewallInternetServiceReputationDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteFirewallInternetServiceReputation(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting FirewallInternetServiceReputation resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFirewallInternetServiceReputationRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadFirewallInternetServiceReputation(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallInternetServiceReputation resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectFirewallInternetServiceReputation(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading FirewallInternetServiceReputation resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenFirewallInternetServiceReputationId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenFirewallInternetServiceReputationDescription(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectFirewallInternetServiceReputation(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("fosid", flattenFirewallInternetServiceReputationId(o["id"], d, "fosid", sv)); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("description", flattenFirewallInternetServiceReputationDescription(o["description"], d, "description", sv)); err != nil {
+		if !fortiAPIPatch(o["description"]) {
+			return fmt.Errorf("Error reading description: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenFirewallInternetServiceReputationFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandFirewallInternetServiceReputationId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandFirewallInternetServiceReputationDescription(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectFirewallInternetServiceReputation(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOkExists("fosid"); ok {
+
+		t, err := expandFirewallInternetServiceReputationId(d, v, "fosid", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+
+		t, err := expandFirewallInternetServiceReputationDescription(d, v, "description", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["description"] = t
+		}
+	}
+
+	return &obj, nil
+}
