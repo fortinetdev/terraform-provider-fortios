648605953663fffabe3a0d43d0812bdb09c8c5bf
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:23:23 -0500

    Update resource resource_switchcontroller_8021Xsettings to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_switchcontroller_8021Xsettings.go
+++ fortios/resource_switchcontroller_8021Xsettings.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -46,6 +47,12 @@ func resourceSwitchController8021XSettings() *schema.Resource {
 				Optional:     true,
 				Computed:     true,
 			},
+			"tx_period": &schema.Schema{
+				Type:         schema.TypeInt,
+				ValidateFunc: validation.IntBetween(4, 60),
+				Optional:     true,
+				Computed:     true,
+			},
 		},
 	}
 }
@@ -55,7 +62,7 @@ func resourceSwitchController8021XSettingsUpdate(d *schema.ResourceData, m inter
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSwitchController8021XSettings(d)
+	obj, err := getObjectSwitchController8021XSettings(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SwitchController8021XSettings resource while getting object: %v", err)
 	}
@@ -108,43 +115,53 @@ func resourceSwitchController8021XSettingsRead(d *schema.ResourceData, m interfa
 		return nil
 	}
 
-	err = refreshObjectSwitchController8021XSettings(d, o)
+	err = refreshObjectSwitchController8021XSettings(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SwitchController8021XSettings resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSwitchController8021XSettingsLinkDownAuth(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSwitchController8021XSettingsLinkDownAuth(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSwitchController8021XSettingsReauthPeriod(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSwitchController8021XSettingsReauthPeriod(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSwitchController8021XSettingsMaxReauthAttempt(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSwitchController8021XSettingsMaxReauthAttempt(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSwitchController8021XSettings(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenSwitchController8021XSettingsTxPeriod(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectSwitchController8021XSettings(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("link_down_auth", flattenSwitchController8021XSettingsLinkDownAuth(o["link-down-auth"], d, "link_down_auth")); err != nil {
+	if err = d.Set("link_down_auth", flattenSwitchController8021XSettingsLinkDownAuth(o["link-down-auth"], d, "link_down_auth", sv)); err != nil {
 		if !fortiAPIPatch(o["link-down-auth"]) {
 			return fmt.Errorf("Error reading link_down_auth: %v", err)
 		}
 	}
 
-	if err = d.Set("reauth_period", flattenSwitchController8021XSettingsReauthPeriod(o["reauth-period"], d, "reauth_period")); err != nil {
+	if err = d.Set("reauth_period", flattenSwitchController8021XSettingsReauthPeriod(o["reauth-period"], d, "reauth_period", sv)); err != nil {
 		if !fortiAPIPatch(o["reauth-period"]) {
 			return fmt.Errorf("Error reading reauth_period: %v", err)
 		}
 	}
 
-	if err = d.Set("max_reauth_attempt", flattenSwitchController8021XSettingsMaxReauthAttempt(o["max-reauth-attempt"], d, "max_reauth_attempt")); err != nil {
+	if err = d.Set("max_reauth_attempt", flattenSwitchController8021XSettingsMaxReauthAttempt(o["max-reauth-attempt"], d, "max_reauth_attempt", sv)); err != nil {
 		if !fortiAPIPatch(o["max-reauth-attempt"]) {
 			return fmt.Errorf("Error reading max_reauth_attempt: %v", err)
+		}
+	}
+
+	if err = d.Set("tx_period", flattenSwitchController8021XSettingsTxPeriod(o["tx-period"], d, "tx_period", sv)); err != nil {
+		if !fortiAPIPatch(o["tx-period"]) {
+			return fmt.Errorf("Error reading tx_period: %v", err)
 		}
 	}
 
@@ -154,26 +171,31 @@ func refreshObjectSwitchController8021XSettings(d *schema.ResourceData, o map[st
 func flattenSwitchController8021XSettingsFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSwitchController8021XSettingsLinkDownAuth(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSwitchController8021XSettingsLinkDownAuth(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSwitchController8021XSettingsReauthPeriod(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSwitchController8021XSettingsReauthPeriod(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSwitchController8021XSettingsMaxReauthAttempt(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSwitchController8021XSettingsMaxReauthAttempt(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSwitchController8021XSettings(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandSwitchController8021XSettingsTxPeriod(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSwitchController8021XSettings(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("link_down_auth"); ok {
-		t, err := expandSwitchController8021XSettingsLinkDownAuth(d, v, "link_down_auth")
+
+		t, err := expandSwitchController8021XSettingsLinkDownAuth(d, v, "link_down_auth", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -182,7 +204,8 @@ func getObjectSwitchController8021XSettings(d *schema.ResourceData) (*map[string
 	}
 
 	if v, ok := d.GetOk("reauth_period"); ok {
-		t, err := expandSwitchController8021XSettingsReauthPeriod(d, v, "reauth_period")
+
+		t, err := expandSwitchController8021XSettingsReauthPeriod(d, v, "reauth_period", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -191,11 +214,22 @@ func getObjectSwitchController8021XSettings(d *schema.ResourceData) (*map[string
 	}
 
 	if v, ok := d.GetOkExists("max_reauth_attempt"); ok {
-		t, err := expandSwitchController8021XSettingsMaxReauthAttempt(d, v, "max_reauth_attempt")
+
+		t, err := expandSwitchController8021XSettingsMaxReauthAttempt(d, v, "max_reauth_attempt", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
 			obj["max-reauth-attempt"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("tx_period"); ok {
+
+		t, err := expandSwitchController8021XSettingsTxPeriod(d, v, "tx_period", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["tx-period"] = t
 		}
 	}
 
