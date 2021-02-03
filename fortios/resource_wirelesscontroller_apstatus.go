0fa2e539f761383c27f2bc6826eca52bbd6ef5f7
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:26:49 -0500

    Update resource resource_wirelesscontroller_apstatus to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_wirelesscontroller_apstatus.go
+++ fortios/resource_wirelesscontroller_apstatus.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -59,7 +60,7 @@ func resourceWirelessControllerApStatusCreate(d *schema.ResourceData, m interfac
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectWirelessControllerApStatus(d)
+	obj, err := getObjectWirelessControllerApStatus(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating WirelessControllerApStatus resource while getting object: %v", err)
 	}
@@ -84,7 +85,7 @@ func resourceWirelessControllerApStatusUpdate(d *schema.ResourceData, m interfac
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectWirelessControllerApStatus(d)
+	obj, err := getObjectWirelessControllerApStatus(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating WirelessControllerApStatus resource while getting object: %v", err)
 	}
@@ -137,51 +138,51 @@ func resourceWirelessControllerApStatusRead(d *schema.ResourceData, m interface{
 		return nil
 	}
 
-	err = refreshObjectWirelessControllerApStatus(d, o)
+	err = refreshObjectWirelessControllerApStatus(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading WirelessControllerApStatus resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenWirelessControllerApStatusId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWirelessControllerApStatusId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWirelessControllerApStatusBssid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWirelessControllerApStatusBssid(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWirelessControllerApStatusSsid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWirelessControllerApStatusSsid(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenWirelessControllerApStatusStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenWirelessControllerApStatusStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectWirelessControllerApStatus(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectWirelessControllerApStatus(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("fosid", flattenWirelessControllerApStatusId(o["id"], d, "fosid")); err != nil {
+	if err = d.Set("fosid", flattenWirelessControllerApStatusId(o["id"], d, "fosid", sv)); err != nil {
 		if !fortiAPIPatch(o["id"]) {
 			return fmt.Errorf("Error reading fosid: %v", err)
 		}
 	}
 
-	if err = d.Set("bssid", flattenWirelessControllerApStatusBssid(o["bssid"], d, "bssid")); err != nil {
+	if err = d.Set("bssid", flattenWirelessControllerApStatusBssid(o["bssid"], d, "bssid", sv)); err != nil {
 		if !fortiAPIPatch(o["bssid"]) {
 			return fmt.Errorf("Error reading bssid: %v", err)
 		}
 	}
 
-	if err = d.Set("ssid", flattenWirelessControllerApStatusSsid(o["ssid"], d, "ssid")); err != nil {
+	if err = d.Set("ssid", flattenWirelessControllerApStatusSsid(o["ssid"], d, "ssid", sv)); err != nil {
 		if !fortiAPIPatch(o["ssid"]) {
 			return fmt.Errorf("Error reading ssid: %v", err)
 		}
 	}
 
-	if err = d.Set("status", flattenWirelessControllerApStatusStatus(o["status"], d, "status")); err != nil {
+	if err = d.Set("status", flattenWirelessControllerApStatusStatus(o["status"], d, "status", sv)); err != nil {
 		if !fortiAPIPatch(o["status"]) {
 			return fmt.Errorf("Error reading status: %v", err)
 		}
@@ -193,30 +194,31 @@ func refreshObjectWirelessControllerApStatus(d *schema.ResourceData, o map[strin
 func flattenWirelessControllerApStatusFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandWirelessControllerApStatusId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWirelessControllerApStatusId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWirelessControllerApStatusBssid(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWirelessControllerApStatusBssid(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWirelessControllerApStatusSsid(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWirelessControllerApStatusSsid(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandWirelessControllerApStatusStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandWirelessControllerApStatusStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectWirelessControllerApStatus(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectWirelessControllerApStatus(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOkExists("fosid"); ok {
-		t, err := expandWirelessControllerApStatusId(d, v, "fosid")
+
+		t, err := expandWirelessControllerApStatusId(d, v, "fosid", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -225,7 +227,8 @@ func getObjectWirelessControllerApStatus(d *schema.ResourceData) (*map[string]in
 	}
 
 	if v, ok := d.GetOk("bssid"); ok {
-		t, err := expandWirelessControllerApStatusBssid(d, v, "bssid")
+
+		t, err := expandWirelessControllerApStatusBssid(d, v, "bssid", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -234,7 +237,8 @@ func getObjectWirelessControllerApStatus(d *schema.ResourceData) (*map[string]in
 	}
 
 	if v, ok := d.GetOk("ssid"); ok {
-		t, err := expandWirelessControllerApStatusSsid(d, v, "ssid")
+
+		t, err := expandWirelessControllerApStatusSsid(d, v, "ssid", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -243,7 +247,8 @@ func getObjectWirelessControllerApStatus(d *schema.ResourceData) (*map[string]in
 	}
 
 	if v, ok := d.GetOk("status"); ok {
-		t, err := expandWirelessControllerApStatusStatus(d, v, "status")
+
+		t, err := expandWirelessControllerApStatusStatus(d, v, "status", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
