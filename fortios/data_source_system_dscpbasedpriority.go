facbaa7b3d737ad173448e53de47f4cefe2053eb
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:18 -0500

    Add new data source data_source_system_dscpbasedpriority

---
+++ fortios/data_source_system_dscpbasedpriority.go
@@ -0,0 +1,115 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure DSCP based priority table.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemDscpBasedPriority() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemDscpBasedPriorityRead,
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"ds": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"priority": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemDscpBasedPriorityRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("fosid")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing SystemDscpBasedPriority: type error")
+	}
+
+	o, err := c.ReadSystemDscpBasedPriority(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemDscpBasedPriority: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemDscpBasedPriority(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemDscpBasedPriority from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemDscpBasedPriorityId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDscpBasedPriorityDs(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemDscpBasedPriorityPriority(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemDscpBasedPriority(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("fosid", dataSourceFlattenSystemDscpBasedPriorityId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("ds", dataSourceFlattenSystemDscpBasedPriorityDs(o["ds"], d, "ds")); err != nil {
+		if !fortiAPIPatch(o["ds"]) {
+			return fmt.Errorf("Error reading ds: %v", err)
+		}
+	}
+
+	if err = d.Set("priority", dataSourceFlattenSystemDscpBasedPriorityPriority(o["priority"], d, "priority")); err != nil {
+		if !fortiAPIPatch(o["priority"]) {
+			return fmt.Errorf("Error reading priority: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemDscpBasedPriorityFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
