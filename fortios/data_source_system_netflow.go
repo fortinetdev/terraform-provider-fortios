7937aae70817d93af13a14544fdf160dc9d9a39e
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:31 -0500

    Add new data source data_source_system_netflow

---
+++ fortios/data_source_system_netflow.go
@@ -0,0 +1,162 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure NetFlow.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemNetflow() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemNetflowRead,
+		Schema: map[string]*schema.Schema{
+			"collector_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"collector_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"source_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"active_flow_timeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"inactive_flow_timeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"template_tx_timeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"template_tx_counter": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemNetflowRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := "SystemNetflow"
+
+	o, err := c.ReadSystemNetflow(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemNetflow: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemNetflow(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemNetflow from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemNetflowCollectorIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemNetflowCollectorPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemNetflowSourceIp(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemNetflowActiveFlowTimeout(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemNetflowInactiveFlowTimeout(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemNetflowTemplateTxTimeout(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemNetflowTemplateTxCounter(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemNetflow(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("collector_ip", dataSourceFlattenSystemNetflowCollectorIp(o["collector-ip"], d, "collector_ip")); err != nil {
+		if !fortiAPIPatch(o["collector-ip"]) {
+			return fmt.Errorf("Error reading collector_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("collector_port", dataSourceFlattenSystemNetflowCollectorPort(o["collector-port"], d, "collector_port")); err != nil {
+		if !fortiAPIPatch(o["collector-port"]) {
+			return fmt.Errorf("Error reading collector_port: %v", err)
+		}
+	}
+
+	if err = d.Set("source_ip", dataSourceFlattenSystemNetflowSourceIp(o["source-ip"], d, "source_ip")); err != nil {
+		if !fortiAPIPatch(o["source-ip"]) {
+			return fmt.Errorf("Error reading source_ip: %v", err)
+		}
+	}
+
+	if err = d.Set("active_flow_timeout", dataSourceFlattenSystemNetflowActiveFlowTimeout(o["active-flow-timeout"], d, "active_flow_timeout")); err != nil {
+		if !fortiAPIPatch(o["active-flow-timeout"]) {
+			return fmt.Errorf("Error reading active_flow_timeout: %v", err)
+		}
+	}
+
+	if err = d.Set("inactive_flow_timeout", dataSourceFlattenSystemNetflowInactiveFlowTimeout(o["inactive-flow-timeout"], d, "inactive_flow_timeout")); err != nil {
+		if !fortiAPIPatch(o["inactive-flow-timeout"]) {
+			return fmt.Errorf("Error reading inactive_flow_timeout: %v", err)
+		}
+	}
+
+	if err = d.Set("template_tx_timeout", dataSourceFlattenSystemNetflowTemplateTxTimeout(o["template-tx-timeout"], d, "template_tx_timeout")); err != nil {
+		if !fortiAPIPatch(o["template-tx-timeout"]) {
+			return fmt.Errorf("Error reading template_tx_timeout: %v", err)
+		}
+	}
+
+	if err = d.Set("template_tx_counter", dataSourceFlattenSystemNetflowTemplateTxCounter(o["template-tx-counter"], d, "template_tx_counter")); err != nil {
+		if !fortiAPIPatch(o["template-tx-counter"]) {
+			return fmt.Errorf("Error reading template_tx_counter: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemNetflowFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
