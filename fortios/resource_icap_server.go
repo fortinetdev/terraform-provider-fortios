33baf499e3620ebaa2f9f1ca0ca4cf9b03ca219d
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:22:39 -0500

    Update resource resource_icap_server to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_icap_server.go
+++ fortios/resource_icap_server.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -63,6 +64,17 @@ func resourceIcapServer() *schema.Resource {
 				Optional:     true,
 				Computed:     true,
 			},
+			"secure": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ssl_cert": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 255),
+				Optional:     true,
+				Computed:     true,
+			},
 		},
 	}
 }
@@ -71,7 +83,7 @@ func resourceIcapServerCreate(d *schema.ResourceData, m interface{}) error {
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectIcapServer(d)
+	obj, err := getObjectIcapServer(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating IcapServer resource while getting object: %v", err)
 	}
@@ -96,7 +108,7 @@ func resourceIcapServerUpdate(d *schema.ResourceData, m interface{}) error {
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectIcapServer(d)
+	obj, err := getObjectIcapServer(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating IcapServer resource while getting object: %v", err)
 	}
@@ -149,73 +161,93 @@ func resourceIcapServerRead(d *schema.ResourceData, m interface{}) error {
 		return nil
 	}
 
-	err = refreshObjectIcapServer(d, o)
+	err = refreshObjectIcapServer(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading IcapServer resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenIcapServerName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIcapServerName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIcapServerIpVersion(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIcapServerIpVersion(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIcapServerIpAddress(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIcapServerIpAddress(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIcapServerIp6Address(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIcapServerIp6Address(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIcapServerPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIcapServerPort(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenIcapServerMaxConnections(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenIcapServerMaxConnections(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectIcapServer(d *schema.ResourceData, o map[string]interface{}) error {
+func flattenIcapServerSecure(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenIcapServerSslCert(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectIcapServer(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("name", flattenIcapServerName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenIcapServerName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("ip_version", flattenIcapServerIpVersion(o["ip-version"], d, "ip_version")); err != nil {
+	if err = d.Set("ip_version", flattenIcapServerIpVersion(o["ip-version"], d, "ip_version", sv)); err != nil {
 		if !fortiAPIPatch(o["ip-version"]) {
 			return fmt.Errorf("Error reading ip_version: %v", err)
 		}
 	}
 
-	if err = d.Set("ip_address", flattenIcapServerIpAddress(o["ip-address"], d, "ip_address")); err != nil {
+	if err = d.Set("ip_address", flattenIcapServerIpAddress(o["ip-address"], d, "ip_address", sv)); err != nil {
 		if !fortiAPIPatch(o["ip-address"]) {
 			return fmt.Errorf("Error reading ip_address: %v", err)
 		}
 	}
 
-	if err = d.Set("ip6_address", flattenIcapServerIp6Address(o["ip6-address"], d, "ip6_address")); err != nil {
+	if err = d.Set("ip6_address", flattenIcapServerIp6Address(o["ip6-address"], d, "ip6_address", sv)); err != nil {
 		if !fortiAPIPatch(o["ip6-address"]) {
 			return fmt.Errorf("Error reading ip6_address: %v", err)
 		}
 	}
 
-	if err = d.Set("port", flattenIcapServerPort(o["port"], d, "port")); err != nil {
+	if err = d.Set("port", flattenIcapServerPort(o["port"], d, "port", sv)); err != nil {
 		if !fortiAPIPatch(o["port"]) {
 			return fmt.Errorf("Error reading port: %v", err)
 		}
 	}
 
-	if err = d.Set("max_connections", flattenIcapServerMaxConnections(o["max-connections"], d, "max_connections")); err != nil {
+	if err = d.Set("max_connections", flattenIcapServerMaxConnections(o["max-connections"], d, "max_connections", sv)); err != nil {
 		if !fortiAPIPatch(o["max-connections"]) {
 			return fmt.Errorf("Error reading max_connections: %v", err)
+		}
+	}
+
+	if err = d.Set("secure", flattenIcapServerSecure(o["secure"], d, "secure", sv)); err != nil {
+		if !fortiAPIPatch(o["secure"]) {
+			return fmt.Errorf("Error reading secure: %v", err)
+		}
+	}
+
+	if err = d.Set("ssl_cert", flattenIcapServerSslCert(o["ssl-cert"], d, "ssl_cert", sv)); err != nil {
+		if !fortiAPIPatch(o["ssl-cert"]) {
+			return fmt.Errorf("Error reading ssl_cert: %v", err)
 		}
 	}
 
@@ -225,38 +257,47 @@ func refreshObjectIcapServer(d *schema.ResourceData, o map[string]interface{}) e
 func flattenIcapServerFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandIcapServerName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIcapServerName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIcapServerIpVersion(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIcapServerIpVersion(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIcapServerIpAddress(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIcapServerIpAddress(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIcapServerIp6Address(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIcapServerIp6Address(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIcapServerPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIcapServerPort(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandIcapServerMaxConnections(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandIcapServerMaxConnections(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectIcapServer(d *schema.ResourceData) (*map[string]interface{}, error) {
+func expandIcapServerSecure(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandIcapServerSslCert(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectIcapServer(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandIcapServerName(d, v, "name")
+
+		t, err := expandIcapServerName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -265,7 +306,8 @@ func getObjectIcapServer(d *schema.ResourceData) (*map[string]interface{}, error
 	}
 
 	if v, ok := d.GetOk("ip_version"); ok {
-		t, err := expandIcapServerIpVersion(d, v, "ip_version")
+
+		t, err := expandIcapServerIpVersion(d, v, "ip_version", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -274,7 +316,8 @@ func getObjectIcapServer(d *schema.ResourceData) (*map[string]interface{}, error
 	}
 
 	if v, ok := d.GetOk("ip_address"); ok {
-		t, err := expandIcapServerIpAddress(d, v, "ip_address")
+
+		t, err := expandIcapServerIpAddress(d, v, "ip_address", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -283,7 +326,8 @@ func getObjectIcapServer(d *schema.ResourceData) (*map[string]interface{}, error
 	}
 
 	if v, ok := d.GetOk("ip6_address"); ok {
-		t, err := expandIcapServerIp6Address(d, v, "ip6_address")
+
+		t, err := expandIcapServerIp6Address(d, v, "ip6_address", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -292,7 +336,8 @@ func getObjectIcapServer(d *schema.ResourceData) (*map[string]interface{}, error
 	}
 
 	if v, ok := d.GetOk("port"); ok {
-		t, err := expandIcapServerPort(d, v, "port")
+
+		t, err := expandIcapServerPort(d, v, "port", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -301,11 +346,32 @@ func getObjectIcapServer(d *schema.ResourceData) (*map[string]interface{}, error
 	}
 
 	if v, ok := d.GetOk("max_connections"); ok {
-		t, err := expandIcapServerMaxConnections(d, v, "max_connections")
+
+		t, err := expandIcapServerMaxConnections(d, v, "max_connections", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
 			obj["max-connections"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("secure"); ok {
+
+		t, err := expandIcapServerSecure(d, v, "secure", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["secure"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ssl_cert"); ok {
+
+		t, err := expandIcapServerSslCert(d, v, "ssl_cert", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ssl-cert"] = t
 		}
 	}
 
