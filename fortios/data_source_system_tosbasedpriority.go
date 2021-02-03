10e5d921a53cb79a699001f7680cf94aa3b3b3ee
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:39:44 -0500

    Add new data source data_source_system_tosbasedpriority

---
+++ fortios/data_source_system_tosbasedpriority.go
@@ -0,0 +1,115 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure Type of Service (ToS) based priority table to set network traffic priorities.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceSystemTosBasedPriority() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceSystemTosBasedPriorityRead,
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"tos": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"priority": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceSystemTosBasedPriorityRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	mkey := ""
+
+	t := d.Get("fosid")
+	if v, ok := t.(string); ok {
+		mkey = v
+	} else if v, ok := t.(int); ok {
+		mkey = strconv.Itoa(v)
+	} else {
+		return fmt.Errorf("Error describing SystemTosBasedPriority: type error")
+	}
+
+	o, err := c.ReadSystemTosBasedPriority(mkey)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemTosBasedPriority: %v", err)
+	}
+
+	if o == nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = dataSourceRefreshObjectSystemTosBasedPriority(d, o)
+	if err != nil {
+		return fmt.Errorf("Error describing SystemTosBasedPriority from API: %v", err)
+	}
+
+	d.SetId(mkey)
+
+	return nil
+}
+
+func dataSourceFlattenSystemTosBasedPriorityId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemTosBasedPriorityTos(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceFlattenSystemTosBasedPriorityPriority(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func dataSourceRefreshObjectSystemTosBasedPriority(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("fosid", dataSourceFlattenSystemTosBasedPriorityId(o["id"], d, "fosid")); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("tos", dataSourceFlattenSystemTosBasedPriorityTos(o["tos"], d, "tos")); err != nil {
+		if !fortiAPIPatch(o["tos"]) {
+			return fmt.Errorf("Error reading tos: %v", err)
+		}
+	}
+
+	if err = d.Set("priority", dataSourceFlattenSystemTosBasedPriorityPriority(o["priority"], d, "priority")); err != nil {
+		if !fortiAPIPatch(o["priority"]) {
+			return fmt.Errorf("Error reading priority: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func dataSourceFlattenSystemTosBasedPriorityFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
