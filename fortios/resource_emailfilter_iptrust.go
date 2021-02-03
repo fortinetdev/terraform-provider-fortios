138774d5c4ecd47032be5084b7364f098cf2fe58
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:01 -0500

    Add new resource resource_emailfilter_iptrust

---
+++ fortios/resource_emailfilter_iptrust.go
@@ -0,0 +1,452 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: Configure AntiSpam IP trust.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceEmailfilterIptrust() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceEmailfilterIptrustCreate,
+		Read:   resourceEmailfilterIptrustRead,
+		Update: resourceEmailfilterIptrustUpdate,
+		Delete: resourceEmailfilterIptrustDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				Optional:     true,
+				Computed:     true,
+			},
+			"comment": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 255),
+				Optional:     true,
+			},
+			"entries": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"status": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"id": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"addr_type": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"ip4_subnet": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"ip6_subnet": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceEmailfilterIptrustCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectEmailfilterIptrust(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating EmailfilterIptrust resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateEmailfilterIptrust(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating EmailfilterIptrust resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("EmailfilterIptrust")
+	}
+
+	return resourceEmailfilterIptrustRead(d, m)
+}
+
+func resourceEmailfilterIptrustUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectEmailfilterIptrust(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating EmailfilterIptrust resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateEmailfilterIptrust(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating EmailfilterIptrust resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(strconv.Itoa(int(o["mkey"].(float64))))
+	} else {
+		d.SetId("EmailfilterIptrust")
+	}
+
+	return resourceEmailfilterIptrustRead(d, m)
+}
+
+func resourceEmailfilterIptrustDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteEmailfilterIptrust(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting EmailfilterIptrust resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceEmailfilterIptrustRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadEmailfilterIptrust(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading EmailfilterIptrust resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectEmailfilterIptrust(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading EmailfilterIptrust resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenEmailfilterIptrustId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenEmailfilterIptrustName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenEmailfilterIptrustComment(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenEmailfilterIptrustEntries(v interface{}, d *schema.ResourceData, pre string, sv string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := i["status"]; ok {
+
+			tmp["status"] = flattenEmailfilterIptrustEntriesStatus(i["status"], d, pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+
+			tmp["id"] = flattenEmailfilterIptrustEntriesId(i["id"], d, pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "addr_type"
+		if _, ok := i["addr-type"]; ok {
+
+			tmp["addr_type"] = flattenEmailfilterIptrustEntriesAddrType(i["addr-type"], d, pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip4_subnet"
+		if _, ok := i["ip4-subnet"]; ok {
+
+			tmp["ip4_subnet"] = flattenEmailfilterIptrustEntriesIp4Subnet(i["ip4-subnet"], d, pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip6_subnet"
+		if _, ok := i["ip6-subnet"]; ok {
+
+			tmp["ip6_subnet"] = flattenEmailfilterIptrustEntriesIp6Subnet(i["ip6-subnet"], d, pre_append, sv)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	dynamic_sort_subtable(result, "id", d)
+	return result
+}
+
+func flattenEmailfilterIptrustEntriesStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenEmailfilterIptrustEntriesId(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenEmailfilterIptrustEntriesAddrType(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenEmailfilterIptrustEntriesIp4Subnet(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	if v1, ok := d.GetOkExists(pre); ok && v != nil {
+		if s, ok := v1.(string); ok {
+			v = validateConvIPMask2CIDR(s, v.(string))
+			return v
+		}
+	}
+
+	return v
+}
+
+func flattenEmailfilterIptrustEntriesIp6Subnet(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectEmailfilterIptrust(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("fosid", flattenEmailfilterIptrustId(o["id"], d, "fosid", sv)); err != nil {
+		if !fortiAPIPatch(o["id"]) {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenEmailfilterIptrustName(o["name"], d, "name", sv)); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", flattenEmailfilterIptrustComment(o["comment"], d, "comment", sv)); err != nil {
+		if !fortiAPIPatch(o["comment"]) {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("entries", flattenEmailfilterIptrustEntries(o["entries"], d, "entries", sv)); err != nil {
+			if !fortiAPIPatch(o["entries"]) {
+				return fmt.Errorf("Error reading entries: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("entries"); ok {
+			if err = d.Set("entries", flattenEmailfilterIptrustEntries(o["entries"], d, "entries", sv)); err != nil {
+				if !fortiAPIPatch(o["entries"]) {
+					return fmt.Errorf("Error reading entries: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenEmailfilterIptrustFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandEmailfilterIptrustId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandEmailfilterIptrustName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandEmailfilterIptrustComment(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandEmailfilterIptrustEntries(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := d.GetOk(pre_append); ok {
+
+			tmp["status"], _ = expandEmailfilterIptrustEntriesStatus(d, i["status"], pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+
+			tmp["id"], _ = expandEmailfilterIptrustEntriesId(d, i["id"], pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "addr_type"
+		if _, ok := d.GetOk(pre_append); ok {
+
+			tmp["addr-type"], _ = expandEmailfilterIptrustEntriesAddrType(d, i["addr_type"], pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip4_subnet"
+		if _, ok := d.GetOk(pre_append); ok {
+
+			tmp["ip4-subnet"], _ = expandEmailfilterIptrustEntriesIp4Subnet(d, i["ip4_subnet"], pre_append, sv)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip6_subnet"
+		if _, ok := d.GetOk(pre_append); ok {
+
+			tmp["ip6-subnet"], _ = expandEmailfilterIptrustEntriesIp6Subnet(d, i["ip6_subnet"], pre_append, sv)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandEmailfilterIptrustEntriesStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandEmailfilterIptrustEntriesId(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandEmailfilterIptrustEntriesAddrType(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandEmailfilterIptrustEntriesIp4Subnet(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandEmailfilterIptrustEntriesIp6Subnet(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectEmailfilterIptrust(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOkExists("fosid"); ok {
+
+		t, err := expandEmailfilterIptrustId(d, v, "fosid", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+
+		t, err := expandEmailfilterIptrustName(d, v, "name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+
+		t, err := expandEmailfilterIptrustComment(d, v, "comment", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("entries"); ok {
+
+		t, err := expandEmailfilterIptrustEntries(d, v, "entries", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["entries"] = t
+		}
+	}
+
+	return &obj, nil
+}
