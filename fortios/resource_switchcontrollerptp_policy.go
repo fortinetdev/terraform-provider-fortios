847645fdcc05dbd3a36c49d3fe7af520bcdcfa7b
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:40:22 -0500

    Add new resource resource_switchcontrollerptp_policy

---
+++ fortios/resource_switchcontrollerptp_policy.go
@@ -0,0 +1,202 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+// Description: PTP policy configuration.
+
+package fortios
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSwitchControllerPtpPolicy() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSwitchControllerPtpPolicyCreate,
+		Read:   resourceSwitchControllerPtpPolicyRead,
+		Update: resourceSwitchControllerPtpPolicyUpdate,
+		Delete: resourceSwitchControllerPtpPolicyDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(0, 63),
+				ForceNew:     true,
+				Optional:     true,
+				Computed:     true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSwitchControllerPtpPolicyCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSwitchControllerPtpPolicy(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error creating SwitchControllerPtpPolicy resource while getting object: %v", err)
+	}
+
+	o, err := c.CreateSwitchControllerPtpPolicy(obj)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SwitchControllerPtpPolicy resource: %v", err)
+	}
+
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SwitchControllerPtpPolicy")
+	}
+
+	return resourceSwitchControllerPtpPolicyRead(d, m)
+}
+
+func resourceSwitchControllerPtpPolicyUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	obj, err := getObjectSwitchControllerPtpPolicy(d, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error updating SwitchControllerPtpPolicy resource while getting object: %v", err)
+	}
+
+	o, err := c.UpdateSwitchControllerPtpPolicy(obj, mkey)
+	if err != nil {
+		return fmt.Errorf("Error updating SwitchControllerPtpPolicy resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+	if o["mkey"] != nil && o["mkey"] != "" {
+		d.SetId(o["mkey"].(string))
+	} else {
+		d.SetId("SwitchControllerPtpPolicy")
+	}
+
+	return resourceSwitchControllerPtpPolicyRead(d, m)
+}
+
+func resourceSwitchControllerPtpPolicyDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	err := c.DeleteSwitchControllerPtpPolicy(mkey)
+	if err != nil {
+		return fmt.Errorf("Error deleting SwitchControllerPtpPolicy resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSwitchControllerPtpPolicyRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	o, err := c.ReadSwitchControllerPtpPolicy(mkey)
+	if err != nil {
+		return fmt.Errorf("Error reading SwitchControllerPtpPolicy resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSwitchControllerPtpPolicy(d, o, c.Fv)
+	if err != nil {
+		return fmt.Errorf("Error reading SwitchControllerPtpPolicy resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSwitchControllerPtpPolicyName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func flattenSwitchControllerPtpPolicyStatus(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
+	return v
+}
+
+func refreshObjectSwitchControllerPtpPolicy(d *schema.ResourceData, o map[string]interface{}, sv string) error {
+	var err error
+
+	if err = d.Set("name", flattenSwitchControllerPtpPolicyName(o["name"], d, "name", sv)); err != nil {
+		if !fortiAPIPatch(o["name"]) {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenSwitchControllerPtpPolicyStatus(o["status"], d, "status", sv)); err != nil {
+		if !fortiAPIPatch(o["status"]) {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSwitchControllerPtpPolicyFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
+}
+
+func expandSwitchControllerPtpPolicyName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSwitchControllerPtpPolicyStatus(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSwitchControllerPtpPolicy(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+
+		t, err := expandSwitchControllerPtpPolicyName(d, v, "name", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+
+		t, err := expandSwitchControllerPtpPolicyStatus(d, v, "status", sv)
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	return &obj, nil
+}
