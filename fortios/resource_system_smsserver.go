6baafd5de5c9ea3bdca3373d527ff30e57b8cb2d
Frank Shen <fshen01@fortinet.com>
2021-02-03 09:25:53 -0500

    Update resource resource_system_smsserver to support FortiOS 6.0 6.2 6.4 6.6

--- fortios/resource_system_smsserver.go
+++ fortios/resource_system_smsserver.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -49,7 +50,7 @@ func resourceSystemSmsServerCreate(d *schema.ResourceData, m interface{}) error 
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemSmsServer(d)
+	obj, err := getObjectSystemSmsServer(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error creating SystemSmsServer resource while getting object: %v", err)
 	}
@@ -74,7 +75,7 @@ func resourceSystemSmsServerUpdate(d *schema.ResourceData, m interface{}) error 
 	c := m.(*FortiClient).Client
 	c.Retries = 1
 
-	obj, err := getObjectSystemSmsServer(d)
+	obj, err := getObjectSystemSmsServer(d, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error updating SystemSmsServer resource while getting object: %v", err)
 	}
@@ -127,31 +128,31 @@ func resourceSystemSmsServerRead(d *schema.ResourceData, m interface{}) error {
 		return nil
 	}
 
-	err = refreshObjectSystemSmsServer(d, o)
+	err = refreshObjectSystemSmsServer(d, o, c.Fv)
 	if err != nil {
 		return fmt.Errorf("Error reading SystemSmsServer resource from API: %v", err)
 	}
 	return nil
 }
 
-func flattenSystemSmsServerName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemSmsServerName(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func flattenSystemSmsServerMailServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+func flattenSystemSmsServerMailServer(v interface{}, d *schema.ResourceData, pre string, sv string) interface{} {
 	return v
 }
 
-func refreshObjectSystemSmsServer(d *schema.ResourceData, o map[string]interface{}) error {
+func refreshObjectSystemSmsServer(d *schema.ResourceData, o map[string]interface{}, sv string) error {
 	var err error
 
-	if err = d.Set("name", flattenSystemSmsServerName(o["name"], d, "name")); err != nil {
+	if err = d.Set("name", flattenSystemSmsServerName(o["name"], d, "name", sv)); err != nil {
 		if !fortiAPIPatch(o["name"]) {
 			return fmt.Errorf("Error reading name: %v", err)
 		}
 	}
 
-	if err = d.Set("mail_server", flattenSystemSmsServerMailServer(o["mail-server"], d, "mail_server")); err != nil {
+	if err = d.Set("mail_server", flattenSystemSmsServerMailServer(o["mail-server"], d, "mail_server", sv)); err != nil {
 		if !fortiAPIPatch(o["mail-server"]) {
 			return fmt.Errorf("Error reading mail_server: %v", err)
 		}
@@ -163,22 +164,23 @@ func refreshObjectSystemSmsServer(d *schema.ResourceData, o map[string]interface
 func flattenSystemSmsServerFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
 	log.Printf(strconv.Itoa(fosdebugsn))
 	e := validation.IntBetween(fosdebugbeg, fosdebugend)
-	log.Printf("ER List: %v", e)
+	log.Printf("ER List: %v, %v", strings.Split("FortiOS Ver", " "), e)
 }
 
-func expandSystemSmsServerName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemSmsServerName(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func expandSystemSmsServerMailServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+func expandSystemSmsServerMailServer(d *schema.ResourceData, v interface{}, pre string, sv string) (interface{}, error) {
 	return v, nil
 }
 
-func getObjectSystemSmsServer(d *schema.ResourceData) (*map[string]interface{}, error) {
+func getObjectSystemSmsServer(d *schema.ResourceData, sv string) (*map[string]interface{}, error) {
 	obj := make(map[string]interface{})
 
 	if v, ok := d.GetOk("name"); ok {
-		t, err := expandSystemSmsServerName(d, v, "name")
+
+		t, err := expandSystemSmsServerName(d, v, "name", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
@@ -187,7 +189,8 @@ func getObjectSystemSmsServer(d *schema.ResourceData) (*map[string]interface{}, 
 	}
 
 	if v, ok := d.GetOk("mail_server"); ok {
-		t, err := expandSystemSmsServerMailServer(d, v, "mail_server")
+
+		t, err := expandSystemSmsServerMailServer(d, v, "mail_server", sv)
 		if err != nil {
 			return &obj, err
 		} else if t != nil {
