bef8be8d29d9f0a54d454363f031d967710a624c
Frank Shen <fshen01@fortinet.com>
2021-06-13 21:21:27 -0400

    Support data source fortios_firewall_centralsnatmaplist

---
+++ fortios/data_source_firewall_centralsnatmaplist.go
@@ -0,0 +1,79 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt), Yuffie Zhu (@yuffiezhu)
+
+package fortios
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceFirewallCentralSnatMapList() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceFirewallCentralSnatMapListRead,
+
+		Schema: map[string]*schema.Schema{
+			"vdomparam": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"filter": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"policyidlist": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeInt},
+			},
+		},
+	}
+}
+
+func dataSourceFirewallCentralSnatMapListRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	vdomparam := ""
+
+	if v, ok := d.GetOk("vdomparam"); ok {
+		if s, ok := v.(string); ok {
+			vdomparam = s
+		}
+	}
+
+	filter := d.Get("filter").(string)
+	if filter != "" {
+		filter = escapeFilter(filter)
+	}
+
+	o, err := c.GenericGroupRead("/api/v2/cmdb/firewall/central-snat-map", filter, vdomparam)
+	if err != nil {
+		return fmt.Errorf("Error describing FirewallCentralSnatMap: %v", err)
+	}
+
+	var tmps []int
+	if o != nil {
+		if len(o) == 0 || o[0] == nil {
+			return nil
+		}
+
+		for _, r := range o {
+			i := r.(map[string]interface{})
+
+			if _, ok := i["policyid"]; ok {
+				tmps = append(tmps, fortiIntValue(i["policyid"]))
+			}
+		}
+	}
+	d.Set("policyidlist", tmps)
+
+	d.SetId("DataSourceFirewallCentralSnatMapList" + filter)
+
+	return nil
+}
